865ba254884f63b8aeff73dfa8429797a1cf34d2
Dries Decramer <dries.decramer@teamleader.be>
2017-02-13 12:21:00 +0100

    Utils index file where the exports happen

--- components/utils/index.js
+++ components/utils/index.js
@@ -2,7 +2,7 @@ import events from './events.js';
 // import prefixer from './prefixer.js';
 // import time from './time.js';
 
-export default {events};
-export {events};
+export default { events };
+export { events };
 // export {prefixer};
 // export {time};
