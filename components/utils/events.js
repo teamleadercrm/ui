1a8ceeb5c84582de0ad4bd91fa8936b9a54f4e43
Dries Decramer <dries.decramer@teamleader.be>
2017-02-13 12:20:09 +0100

    Reads better

--- components/utils/events.js
+++ components/utils/events.js
@@ -2,14 +2,14 @@ export default {
   getMousePosition (event) {
     return {
       x: event.pageX - (window.scrollX || window.pageXOffset),
-      y: event.pageY - (window.scrollY || window.pageYOffset)
+      y: event.pageY - (window.scrollY || window.pageYOffset),
     };
   },
 
   getTouchPosition (event) {
     return {
       x: event.touches[0].pageX - (window.scrollX || window.pageXOffset),
-      y: event.touches[0].pageY - (window.scrollY || window.pageYOffset)
+      y: event.touches[0].pageY - (window.scrollY || window.pageYOffset),
     };
   },
 
@@ -33,7 +33,9 @@ export default {
   targetIsDescendant (event, parent) {
     let node = event.target;
     while (node !== null) {
-      if (node === parent) return true;
+      if (node === parent) {
+        return true;
+      }
       node = node.parentNode;
     }
     return false;
@@ -41,24 +43,28 @@ export default {
 
   addEventListenerOnTransitionEnded (element, fn) {
     const eventName = transitionEventNamesFor(element);
-    if (!eventName) return false;
+    if (!eventName) {
+      return false;
+    }
     element.addEventListener(eventName, fn);
     return true;
   },
 
   removeEventListenerOnTransitionEnded (element, fn) {
     const eventName = transitionEventNamesFor(element);
-    if (!eventName) return false;
+    if (!eventName) {
+      return false;
+    }
     element.removeEventListener(eventName, fn);
     return true;
-  }
+  },
 };
 
 const TRANSITIONS = {
   'transition': 'transitionend',
   'OTransition': 'oTransitionEnd',
   'MozTransition': 'transitionend',
-  'WebkitTransition': 'webkitTransitionEnd'
+  'WebkitTransition': 'webkitTransitionEnd',
 };
 
 function transitionEventNamesFor (element) {
