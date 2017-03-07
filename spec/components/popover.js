966629f9d8c72ecde8e06fb98cf7a39be5ce2ab3
Lowie Benoot <lowie@teamleader.be>
2017-03-07 11:09:50 +0100

    added controls to change the positioning

--- spec/components/popover.js
+++ spec/components/popover.js
@@ -6,6 +6,14 @@ import Button from '../../components/button';
 class PopoverTest extends React.Component {
   state = {
     active: false,
+    anchorOrigin: {
+      vertical: 'top',
+      horizontal: 'left'
+    },
+    targetOrigin: {
+      vertical: 'top',
+      horizontal: 'left'
+    }
   };
 
   componentDidMount () {
@@ -22,21 +30,60 @@ class PopoverTest extends React.Component {
     { label: 'Save', onClick: this.handleToggle, primary: true },
   ];
 
+  setAnchor(positionElement, position) {
+    const { anchorOrigin } = this.state;
+    anchorOrigin[positionElement] = position;
+
+    this.setState({
+      anchorOrigin: anchorOrigin,
+    });
+  }
+
+  setTarget(positionElement, position) {
+    const { targetOrigin } = this.state;
+    targetOrigin[positionElement] = position;
+
+    this.setState({
+      targetOrigin: targetOrigin,
+    });
+  }
+
   render () {
     return (
       <section>
         <h2>Popover</h2>
-        <Button style={{ 'marginLeft': '50%' }} label='Show my popover' onClick={this.handleToggle} ref={(button) => {
-          this.popoverToggleButton = button;
-        }} />
+
+          <Button style={{ 'marginLeft': '50%' }} label='Show my popover' onClick={this.handleToggle} ref={(button) => {
+            this.popoverToggleButton = button;
+          }} />
+
+          <h3>Anchor origin</h3>
+          <h4>Vertical</h4>
+          <label><input type="radio" onChange={this.setAnchor.bind(this, 'vertical', 'top')} checked={this.state.anchorOrigin.vertical === 'top'} /> Top</label>
+          <label><input type="radio" onChange={this.setAnchor.bind(this, 'vertical', 'center')} checked={this.state.anchorOrigin.vertical === 'center'} /> Center</label>
+          <label><input type="radio" onChange={this.setAnchor.bind(this, 'vertical', 'bottom')} checked={this.state.anchorOrigin.vertical === 'bottom'} /> Bottom</label>
+          <h4>Horizontal</h4>
+          <label><input type="radio" onChange={this.setAnchor.bind(this, 'horizontal', 'left')} checked={this.state.anchorOrigin.horizontal === 'left'} /> Left</label>
+          <label><input type="radio" onChange={this.setAnchor.bind(this, 'horizontal', 'middle')} checked={this.state.anchorOrigin.horizontal === 'middle'} /> Middle</label>
+          <label><input type="radio" onChange={this.setAnchor.bind(this, 'horizontal', 'right')} checked={this.state.anchorOrigin.horizontal === 'right'} /> Right</label>
+
+          <h3>Target origin</h3>
+          <h4>Vertical</h4>
+          <label><input type="radio" onChange={this.setTarget.bind(this, 'vertical', 'top')} checked={this.state.targetOrigin.vertical === 'top'} /> Top</label>
+          <label><input type="radio" onChange={this.setTarget.bind(this, 'vertical', 'center')} checked={this.state.targetOrigin.vertical === 'center'} /> Center</label>
+          <label><input type="radio" onChange={this.setTarget.bind(this, 'vertical', 'bottom')} checked={this.state.targetOrigin.vertical === 'bottom'} /> Bottom</label>
+          <h4>Horizontal</h4>
+          <label><input type="radio" onChange={this.setTarget.bind(this, 'horizontal', 'left')} checked={this.state.targetOrigin.horizontal === 'left'} /> Left</label>
+          <label><input type="radio" onChange={this.setTarget.bind(this, 'horizontal', 'middle')} checked={this.state.targetOrigin.horizontal === 'middle'} /> Middle</label>
+          <label><input type="radio" onChange={this.setTarget.bind(this, 'horizontal', 'right')} checked={this.state.targetOrigin.horizontal === 'right'} /> Right</label>
 
         { this.anchorEl &&
           <Popover
             actions={this.actions}
             active={this.state.active}
             anchorEl={this.anchorEl}
-            anchorOrigin={{ horizontal: 'left', vertical: 'center' }}
-            targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={this.state.anchorOrigin}
+            targetOrigin={this.state.targetOrigin}
             onCloseClick={this.handleToggle}
             onEscKeyDown={this.handleToggle}
             onOverlayClick={this.handleToggle}
