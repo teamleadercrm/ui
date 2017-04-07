f44cd5b7377274cdf40b08a8a6278654cc803bfd
Dries Decramer <dries.decramer@teamleader.be>
2017-04-07 16:47:08 +0200

    auto linting fix

--- components/index.js
+++ components/index.js
@@ -2,7 +2,7 @@ import Button, { IconButton } from './button';
 import { FontIcon } from './font_icon';
 import Menu, { IconMenu, MenuItem, MenuDivider } from './menu';
 import LoadingMolecule from './loadingMolecule';
-import { PopoverHorizontal, PopoverVertical} from './popover';
+import { PopoverHorizontal, PopoverVertical } from './popover';
 
 export {
   Button,
