b9a25195c6f8fdd0b8314efee92b76254d353ec2
Dries Decramer <dries.decramer@teamleader.be>
2017-05-15 15:19:30 +0200

    Also export our Overlay component

--- components/index.js
+++ components/index.js
@@ -2,6 +2,7 @@ import Button, { IconButton } from './button';
 import Dialog from './dialog';
 import { FontIcon } from './font_icon';
 import Menu, { IconMenu, MenuItem, MenuDivider } from './menu';
+import Overlay from './overlay';
 import LoadingMolecule from './loadingMolecule';
 import { PopoverHorizontal, PopoverVertical } from './popover';
 
@@ -15,6 +16,7 @@ export {
   MenuItem,
   MenuDivider,
   LoadingMolecule,
+  Overlay,
   PopoverHorizontal,
   PopoverVertical,
 };
