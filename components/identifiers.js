5eb9dd0d4b4e61759467dddb714aa27611e10362
Dries Decramer <dries.decramer@teamleader.be>
2017-08-21 15:33:39 +0200

    Added an identifier constant for our Radio button component

--- components/identifiers.js
+++ components/identifiers.js
@@ -5,4 +5,5 @@ export const MENU = 'TLMenu';
 export const OVERLAY = 'TLOverlay';
 export const POPOVER_VERTICAL = 'TLPopoverVertical';
 export const POPOVER_HORIZONTAL = 'TLPopoverHorizontal';
+export const RADIO = 'TLRadio';
 export const TOAST = 'TLToast';
