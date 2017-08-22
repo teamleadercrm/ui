0c5a515565c614804353c88b102309b4637f7c61
Dries Decramer <dries.decramer@teamleader.be>
2017-08-22 11:28:23 +0200

    Added live preview properties for the Toast component

--- spec/components/toast.js
+++ spec/components/toast.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Button from '../../components/button';
 import Toast from '../../components/toast';
+import { RadioGroup, RadioButton } from '../../components/radio';
 
 class ToastTest extends React.Component {
+  handleActionChange = (event) => {
+    this.setState({ action: event.target.value });
+  };
+
+  handleLabelChange = (event) => {
+    this.setState({ label: event.target.value });
+  };
+
   handleSnackbarClick = () => {
     this.setState({ active: false });
   };
@@ -15,21 +24,48 @@ class ToastTest extends React.Component {
     this.setState({ active: true });
   };
 
+  handleTypeChange = (value) => {
+    this.setState({ type: value });
+  };
+
   state = {
+    action: '',
     active: false,
+    label: 'Toast is ready!',
+    type: 'accept',
   };
 
   render () {
     return (
       <section>
         <h2>Toast</h2>
+
+        <h3>Properties</h3>
+
+        <h4>Type</h4>
+        <RadioGroup name="type" value={this.state.type} onChange={this.handleTypeChange}>
+          <RadioButton label="Accept" value="accept" />
+          <RadioButton label="Cancel" value="cancel" />
+          <RadioButton label="Warning" value="warning" />
+        </RadioGroup>
+
+        <h4>Label</h4>
+        <p><input type="text" value={this.state.label} onChange={event => this.handleLabelChange(event)} /></p>
+
+        <h4>Action</h4>
+        <p><input type="text" value={this.state.action} onChange={event => this.handleActionChange(event)} /></p>
+
+        <h3>Preview</h3>
         <Button label="Make a toast" primary onClick={this.handleClick} />
+
         <Toast
+          action={this.state.action}
           active={this.state.active}
-          label="Toast is ready!"
+          label={this.state.label}
           timeout={3000}
           onClick={this.handleSnackbarClick}
           onTimeout={this.handleSnackbarTimeout}
+          type={this.state.type}
         />
       </section>
     );
