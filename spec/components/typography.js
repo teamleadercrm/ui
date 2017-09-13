5d7264848d6b3a47abdcd28c5db63b3e1475f1e7
Dries Decramer <dries.decramer@teamleader.be>
2017-09-13 12:55:01 +0200

    Added our new typograghy component to the spec

---
+++ spec/components/typography.js
@@ -0,0 +1,59 @@
+import React, { PureComponent } from 'react';
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Monospaced,
+  OldStyle,
+  TextBody,
+  TextSmall,
+  TextTiny,
+} from '../../components/typography';
+
+class TypographyTest extends PureComponent {
+  render () {
+    return (
+      <article>
+        <header>
+          <h1>Typography</h1>
+        </header>
+        <div className="component-spec">
+          <div className="preview">
+            <Heading1>
+              I'm a Header 1 containing a <a href="#">link</a> -
+              0123456789 - <OldStyle>0123456789</OldStyle> - <Monospaced>0123456789</Monospaced>
+            </Heading1>
+            <Heading2>
+              I'm a Header 2 containing a <a href="#">link</a> -
+              0123456789 - <OldStyle>0123456789</OldStyle> - <Monospaced>0123456789</Monospaced>
+            </Heading2>
+            <Heading3>
+              I'm a Header 3 containing a <a href="#">link</a> -
+              0123456789 - <OldStyle>0123456789</OldStyle> - <Monospaced>0123456789</Monospaced>
+            </Heading3>
+            <Heading4>
+              I'm a Header 4 containing a <a href="#">link</a> -
+              0123456789 - <OldStyle>0123456789</OldStyle> - <Monospaced>0123456789</Monospaced>
+            </Heading4>
+
+            <TextBody>
+              I am body text containing a <strong>strong word</strong> and a <a href="#">link</a> -
+              0123456789 - <OldStyle>0123456789</OldStyle> - <Monospaced>0123456789</Monospaced>
+            </TextBody>
+            <TextSmall>
+              I am small text containing a <strong>strong word</strong> and a <a href="#">link</a> -
+              0123456789 - <OldStyle>0123456789</OldStyle> - <Monospaced>0123456789</Monospaced>
+            </TextSmall>
+            <TextTiny>
+              I am tiny text containing a <strong>strong word</strong> and a <a href="#">link</a> -
+              0123456789 - <OldStyle>0123456789</OldStyle> - <Monospaced>0123456789</Monospaced>
+            </TextTiny>
+          </div>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default TypographyTest;
