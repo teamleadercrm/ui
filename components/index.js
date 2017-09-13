b442afe803d341d07c458630bbe34488a1c130e9
Dries Decramer <dries.decramer@teamleader.be>
2017-09-13 12:53:24 +0200

    Export all the typography components

--- components/index.js
+++ components/index.js
@@ -1,4 +1,15 @@
 import Button, { IconButton } from './button';
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Monospaced,
+  OldStyle,
+  TextBody,
+  TextSmall,
+  TextTiny,
+} from './typography';
 import Dialog from './dialog';
 import { FontIcon } from './font_icon';
 import Menu, { IconMenu, MenuItem, MenuDivider } from './menu';
@@ -13,15 +24,24 @@ export {
   IconButton,
   Dialog,
   FontIcon,
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
   Menu,
+  Monospaced,
   IconMenu,
   MenuItem,
   MenuDivider,
   LoadingMolecule,
+  OldStyle,
   Overlay,
   PopoverHorizontal,
   PopoverVertical,
   RadioButton,
   RadioGroup,
+  TextBody,
+  TextSmall,
+  TextTiny,
   Toast,
 };
