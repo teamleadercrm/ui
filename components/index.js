854e73fddb0649bc7c35230f4e14289b88b7dba4
Dries Decramer <dries.decramer@teamleader.be>
2017-09-13 15:06:07 +0200

    Export our basic link component

--- components/index.js
+++ components/index.js
@@ -2,6 +2,7 @@ import Button, { IconButton } from './button';
 import Dialog from './dialog';
 import { FontIcon } from './font_icon';
 import Menu, { IconMenu, MenuItem, MenuDivider } from './menu';
+import Link from './link';
 import Overlay from './overlay';
 import LoadingMolecule from './loadingMolecule';
 import { PopoverHorizontal, PopoverVertical } from './popover';
@@ -18,6 +19,7 @@ export {
   IconMenu,
   MenuItem,
   MenuDivider,
+  Link,
   LoadingMolecule,
   Overlay,
   PopoverHorizontal,
