5f190d77269ea2064a7b0b7747a10d360f5ec535
Dries Decramer <dries.decramer@teamleader.be>
2017-09-28 15:56:59 +0200

    Removed the obsolete hexValues from the background color constants

--- spec/constants/backgroundColors.js
+++ spec/constants/backgroundColors.js
@@ -1,41 +1,33 @@
 const previewBackgroundColors = [
   {
-    hexValue: '#ffffff',
     label: 'Neutral Lightest',
     key: 'neutral',
   },
   {
-    hexValue: '#d3f3f3',
     label: 'Mint Lightest',
     key: 'mint',
   },
   {
-    hexValue: '#e1edfa',
     label: 'Teal Lightest',
     key: 'teal',
   },
   {
-    hexValue: '#f1f0ff',
     label: 'Violet Lightest',
     key: 'violet',
   },
   {
-    hexValue: '#ffe3d9',
     label: 'Ruby Lightest',
     key: 'ruby',
   },
   {
-    hexValue: '#ffeecc',
     label: 'Gold Lightest',
     key: 'gold',
   },
   {
-    hexValue: '#e6f2ff',
     label: 'Aqua Lightest',
     key: 'aqua',
   },
   {
-    hexValue: '#344b63',
     label: 'Teal Dark',
     key: 'tealdark',
   },
