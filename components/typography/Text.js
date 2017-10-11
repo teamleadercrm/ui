a125afeadd3482920c6ca31f784630336afedb2d
Dries Decramer <dries.decramer@teamleader.be>
2017-10-11 17:26:23 +0200

    Added color variations on Text & Heading components

--- components/typography/Text.js
+++ components/typography/Text.js
@@ -8,17 +8,38 @@ const factory = (type, defaultElement) => {
     static propTypes = {
       children: PropTypes.node,
       className: PropTypes.string,
+      color: PropTypes.oneOf(['white', 'neutral', 'mint', 'teal', 'violet', 'ruby', 'gold', 'aqua']),
       element: PropTypes.node,
+      soft: PropTypes.bool,
     };
 
     static defaultProps = {
       element: null,
+      soft: false,
     };
 
-    render() {
-      const { children, className, element } = this.props;
+    isSoft(color) {
+      if (color !== 'white') {
+        return false;
+      }
 
-      const classNames = cx(s['text'], [s[type]], className);
+      return this.props.soft;
+    }
+
+    render() {
+      const { children, className, color, element } = this.props;
+
+      const isSoft = this.isSoft(color);
+
+      const classNames = cx(
+        s['text'],
+        s[type],
+        s[color],
+        {
+          [s['soft']]: isSoft,
+        },
+        className,
+      );
 
       const Element = element || defaultElement;
 
