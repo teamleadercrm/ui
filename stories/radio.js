52fe263a135163df36c05ef9d4f107084a976300
Sambego <sambellen@gmail.com>
2017-10-11 15:02:46 +0200

    Manage radio state inside storybook

--- stories/radio.js
+++ stories/radio.js
@@ -4,32 +4,36 @@ import { action } from '@storybook/addon-actions';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import styles from '@sambego/storybook-styles';
+import { Store, State } from '@sambego/storybook-state';
 import { RadioGroup, RadioButton } from '../components';
 import { baseStyles, centerStyles } from '../.storybook/styles';
 
-let stringValue = 'thewalkingdead';
-const changeValue = value => {
-  stringValue = value;
-  action(`change value: ${value}`)();
-};
+const store = new Store({
+  value: 'thewalkingdead',
+});
+const updateState = value => store.set({ value });
 
 storiesOf('Radio', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
   .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('Enabled', () => (
-    <RadioGroup name="stringValue" value={stringValue} onChange={changeValue}>
-      <RadioButton label="The Walking Dead" value="thewalkingdead" />
-      <RadioButton label="From Hell" value="fromhell" />
-      <RadioButton label="V for a Vendetta" value="vvendetta" onFocus={action('Focus')} />
-      <RadioButton label="Watchmen" value="watchmen" onBlur={action('blur')} />
-    </RadioGroup>
+    <State store={store}>
+      <RadioGroup name="stringValue" onChange={updateState}>
+        <RadioButton label="The Walking Dead" value="thewalkingdead" />
+        <RadioButton label="From Hell" value="fromhell" />
+        <RadioButton label="V for a Vendetta" value="vvendetta" onFocus={action('Focus')} />
+        <RadioButton label="Watchmen" value="watchmen" onBlur={action('blur')} />
+      </RadioGroup>
+    </State>
   ))
   .add('Disabled', () => (
-    <RadioGroup name="stringValue" value={stringValue} onChange={changeValue}>
-      <RadioButton label="The Walking Dead" value="thewalkingdead" disabled />
-      <RadioButton label="From Hell" value="fromhell" disabled />
-      <RadioButton label="V for a Vendetta" value="vvendetta" disabled />
-      <RadioButton label="Watchmen" value="watchmen" disabled />
-    </RadioGroup>
+    <State store={store}>
+      <RadioGroup name="stringValue" onChange={updateState}>
+        <RadioButton label="The Walking Dead" value="thewalkingdead" disabled />
+        <RadioButton label="From Hell" value="fromhell" disabled />
+        <RadioButton label="V for a Vendetta" value="vvendetta" disabled />
+        <RadioButton label="Watchmen" value="watchmen" disabled />
+      </RadioGroup>
+    </State>
   ));
