744686ee4849cb88bf321adccca10a6718d5aa3e
Sambego <sambellen@gmail.com>
2017-10-12 12:05:16 +0200

    Fix loading molecule

--- stories/loadingMolecule.js
+++ stories/loadingMolecule.js
@@ -6,29 +6,27 @@ import { LoadingMolecule } from '../components';
 import styles from '@sambego/storybook-styles';
 import { baseStyles } from '../.storybook/styles';
 
-const basePath = window.location.pathname + window.location.search;
-
 storiesOf('Loading molecules', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
   .addDecorator(styles(baseStyles))
   .add('color', () => (
     <div style={{ height: '300px', position: 'relative' }}>
-      <LoadingMolecule basePath={basePath} startColor="#00ACA9" stopColor="#1F7F79" />
+      <LoadingMolecule basePath="" startColor="#00ACA9" stopColor="#1F7F79" />
     </div>
   ))
   .add('grayscale', () => (
     <div style={{ height: '300px', position: 'relative' }}>
-      <LoadingMolecule basePath={basePath} startColor="#BABABA" stopColor="#DADADA" />
+      <LoadingMolecule basePath="" startColor="#BABABA" stopColor="#DADADA" />
     </div>
   ))
   .add('small', () => (
     <div style={{ height: '300px', position: 'relative' }}>
-      <LoadingMolecule basePath={basePath} startColor="#00ACA9" stopColor="#1F7F79" type="small" />
+      <LoadingMolecule basePath="" startColor="#00ACA9" stopColor="#1F7F79" type="small" />
     </div>
   ))
   .add('large', () => (
     <div style={{ height: '300px', position: 'relative' }}>
-      <LoadingMolecule basePath={basePath} startColor="#00ACA9" stopColor="#1F7F79" type="large" />
+      <LoadingMolecule basePath="" startColor="#00ACA9" stopColor="#1F7F79" type="large" />
     </div>
   ));
