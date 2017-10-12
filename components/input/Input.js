b4372cec04dc2a9c961d6d7a4d04f2920433cb7a
Sambego <sambellen@gmail.com>
2017-10-12 13:55:24 +0200

    Add event handlers

--- components/input/Input.js
+++ components/input/Input.js
@@ -5,11 +5,14 @@ import theme from './theme.css';
 
 export default class Input extends Component {
   static propTypes = {
-    icon: PropTypes.func,
+    icon: PropTypes.oneOfType([PropTypes.func, PropTypes.element]),
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
     placeholder: PropTypes.string,
     type: PropTypes.string,
     size: PropTypes.oneOf(['small', 'medium', 'large']),
-    value: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   };
 
   static defaultProps = {
@@ -19,12 +22,20 @@ export default class Input extends Component {
   };
 
   render() {
-    const { icon, placeholder, type, size, value } = this.props;
+    const { icon, onBlur, onChange, onFocus, placeholder, type, size, value } = this.props;
     const wrapperClasses = cx(theme.wrapper, theme[size]);
     const inputClasses = cx(theme.input, {
       [theme['has-icon']]: icon,
     });
-    const inputProps = { placeholder, type, value, className: inputClasses };
+    const inputProps = {
+      onBlur,
+      onChange,
+      onFocus,
+      placeholder,
+      type,
+      value,
+      className: inputClasses,
+    };
 
     return (
       <div className={wrapperClasses}>
--- stories/input.js
+++ stories/input.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import styles from '@sambego/storybook-styles';
@@ -7,14 +8,21 @@ import { baseStyles, centerStyles } from '../.storybook/styles';
 import Input from '../components/input';
 import { IconCalendarMediumOutline } from '@teamleader/ui-icons';
 
+const props = {
+  onBlur: action('blur'),
+  onChange: action('change'),
+  onFocus: action('focus'),
+  value: 'lorem ipsum',
+};
+
 storiesOf('Inputs', module)
   .addDecorator((story, context) => withInfo('')(story)(context))
   .addDecorator(checkA11y)
   .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('default', () => (
     <div>
-      <Input icon={IconCalendarMediumOutline} size="large" value="lorem ipsum" />
-      <Input icon={IconCalendarMediumOutline} value="lorem ipsum" />
-      <Input icon={IconCalendarMediumOutline} size="small" value="lorem ipsum" />
+      <Input icon={IconCalendarMediumOutline} size="large" {...props} />
+      <Input icon={IconCalendarMediumOutline} {...props} />
+      <Input icon={IconCalendarMediumOutline} size="small" {...props} />
     </div>
   ));
