352bcdc6af2a7c4d304c11beebb96c681ccea564
Sambego <sambellen@gmail.com>
2017-10-12 11:08:20 +0200

    Rewrite webpack config without spread operator

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -29,10 +29,10 @@ module.exports = (storybookBaseConfig, configType) => {
   // The baseConfig already uses a definePlugin instance, webpack ignores
   // every instance but the first, so we need to add our globals to the existing
   // ones. Yep this is dirty!
-  storybookBaseConfig.plugins[0].definitions['process.env'] = {
-    ...storybookBaseConfig.plugins[0].definitions['process.env'],
-    ...globals
-  };
+  Object.assign(
+    storybookBaseConfig.plugins[0].definitions['process.env'],
+    globals
+  );
 
   // Return the altered config
   return storybookBaseConfig;
