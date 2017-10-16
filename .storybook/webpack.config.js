a2011eeccd1575a62b62dfcb67d97268afa6d328
Dries Decramer <dries.decramer@teamleader.be>
2017-10-16 14:07:57 +0200

    Added webpack image loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -22,6 +22,14 @@ module.exports = (storybookBaseConfig, configType) => {
     use: ['style-loader', cssModulesLoader, 'postcss-loader']
   });
 
+  storybookBaseConfig.module.rules.push({
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    loaders: [
+      'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+      'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+    ]
+  });
+
   // The baseConfig already uses a definePlugin instance, webpack ignores
   // every instance but the first, so we need to add our globals to the existing
   // ones. Yep this is dirty!
