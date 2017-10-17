bda19d32156d842f5317422dbb5fd5c31e294603
Lowie Benoot <lowie@teamleader.be>
2017-10-17 15:40:17 +0200

    remove themr

--- components/toggle/index.js
+++ components/toggle/index.js
@@ -1,9 +1,2 @@
-import { themr } from 'react-css-themr';
-import { TOGGLE } from '../identifiers';
 import Toggle from './Toggle';
-import theme from './theme.css';
-
-const ThemedToggle = themr(TOGGLE, theme)(Toggle);
-
-export default ThemedToggle;
-export { ThemedToggle as Toggle };
+export default Toggle;
