161e9d2c806307fa995484552634c926dc3ad542
Dries Decramer <dries.decramer@teamleader.be>
2017-10-19 12:14:35 +0200

    Added marginBottom prop & behavior

--- components/box/Box.js
+++ components/box/Box.js
@@ -3,25 +3,30 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import theme from './theme.css';
 
+const spacings = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
 class Box extends PureComponent {
   static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
     element: PropTypes.node,
-    marginTop: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8]),
+    marginBottom: PropTypes.oneOf(spacings),
+    marginTop: PropTypes.oneOf(spacings),
   };
 
   static defaultProps = {
     element: 'div',
+    marginBottom: 0,
     marginTop: 0,
   };
 
   render() {
-    const { children, className, element, marginTop, ...others } = this.props;
+    const { children, className, element, marginBottom, marginTop, ...others } = this.props;
 
     const classNames = cx(
       theme['box'],
       {
+        [theme[`margin-bottom-${marginBottom}`]]: marginBottom > 0,
         [theme[`margin-top-${marginTop}`]]: marginTop > 0,
       },
       className,
