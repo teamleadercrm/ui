6e8a8f4d7cd67665e3c9dbb516818e81a3a952ab
Sambego <sambellen@gmail.com>
2017-10-23 14:26:10 +0200

    Cleanup

--- stories/input.js
+++ stories/input.js
@@ -12,30 +12,14 @@ const props = {
   onBlur: action('blur'),
   onChange: action('change'),
   onFocus: action('focus'),
-  placeholder: 'Teamleader UI',
+  placeholder: 'Placeholder',
 };
 
 storiesOf('Inputs', module)
   .addDecorator((story, context) => withInfo('')(story)(context))
   .addDecorator(checkA11y)
   .addDecorator(styles({ ...baseStyles, ...centerStyles }))
-  .add('types', () => (
-    <div style={{ width: '400px' }}>
-      <Label for="input1">
-        Text input
-        <Input id="input1" {...props} />
-      </Label>
-      <Label for="input2">
-        Password input
-        <Input id="input2" type="password" {...props} />
-      </Label>
-      <Label for="input3">
-        Number input
-        <Input id="input3" type="number" {...props} meta={{ error: 'This is an error' }} />
-      </Label>
-    </div>
-  ))
-  .add('sizes', () => (
+  .add('text', () => (
     <div style={{ width: '400px' }}>
       <Label for="input1">
         Small input
@@ -51,51 +35,143 @@ storiesOf('Inputs', module)
       </Label>
     </div>
   ))
+  .add('password', () => (
+    <div style={{ width: '400px' }}>
+      <Label for="input1">
+        Small input
+        <Input type="password" id="input1" size="small" {...props} />
+      </Label>
+      <Label for="input2">
+        Medium input
+        <Input type="password" id="input2" {...props} />
+      </Label>
+      <Label for="input3">
+        Large input
+        <Input type="password" id="input3" size="large" {...props} />
+      </Label>
+    </div>
+  ))
+  .add('number', () => (
+    <div style={{ width: '400px' }}>
+      <Label for="input1">
+        Small input
+        <Input type="number" id="input1" size="small" {...props} />
+      </Label>
+      <Label for="input2">
+        Medium input
+        <Input type="number" id="input2" {...props} />
+      </Label>
+      <Label for="input3">
+        Large input
+        <Input type="number" id="input3" size="large" {...props} />
+      </Label>
+    </div>
+  ))
   .add('icons', () => (
     <div style={{ width: '400px' }}>
       <Label for="input1">
-        Icon left
-        <Input id="input1" icon={IconCalendarMediumOutline} {...props} />
+        Small input
+        <Input icon={IconCalendarMediumOutline} id="input1" size="small" {...props} />
       </Label>
       <Label for="input2">
-        Icon right
-        <Input id="input2" icon={IconCalendarMediumOutline} iconPlacement="right" {...props} />
+        Small input
+        <Input icon={IconCalendarMediumOutline} iconPlacement="right" id="input2" size="small" {...props} />
+      </Label>
+      <Label for="input3">
+        Medium input
+        <Input icon={IconCalendarMediumOutline} id="input3" {...props} />
+      </Label>
+      <Label for="input4">
+        Medium input
+        <Input icon={IconCalendarMediumOutline} iconPlacement="right" id="input4" {...props} />
+      </Label>
+      <Label for="input5">
+        Large input
+        <Input icon={IconCalendarMediumOutline} id="input5" size="large" {...props} />
+      </Label>
+      <Label for="input6">
+        Large input
+        <Input icon={IconCalendarMediumOutline} iconPlacement="right" id="input6" size="large" {...props} />
       </Label>
     </div>
   ))
   .add('counter', () => (
     <div style={{ width: '400px' }}>
       <Label for="input1">
-        Counter
-        <Input id="input1" counter={99} {...props} />
+        Small input
+        <Input id="input1" size="small" counter={1} {...props} />
+      </Label>
+      <Label for="input2">
+        Medium input
+        <Input id="input2" counter={9} {...props} />
+      </Label>
+      <Label for="input3">
+        Large input
+        <Input id="input3" size="large" counter={99} {...props} />
       </Label>
     </div>
   ))
   .add('disabled', () => (
     <div style={{ width: '400px' }}>
       <Label for="input1">
-        Disabled
-        <Input id="input1" disabled value="Disabled" {...props} />
+        Small input
+        <Input disabled id="input1" size="small" value="Disabled input" {...props} />
+      </Label>
+      <Label for="input2">
+        Medium input
+        <Input disabled id="input2" value="Disabled input" {...props} />
+      </Label>
+      <Label for="input3">
+        Large input
+        <Input disabled id="input3" size="large" value="Disabled input" {...props} />
       </Label>
     </div>
   ))
   .add('read only', () => (
     <div style={{ width: '400px' }}>
       <Label for="input1">
-        Read only
-        <Input id="input1" readOnly value="Read only" {...props} />
+        Small input
+        <Input readOnly id="input1" size="small" value="Read only input" {...props} />
+      </Label>
+      <Label for="input2">
+        Medium input
+        <Input readOnly id="input2" value="Read only input" {...props} />
+      </Label>
+      <Label for="input3">
+        Large input
+        <Input readOnly id="input3" size="large" value="Read only input" {...props} />
       </Label>
     </div>
   ))
   .add('bold', () => (
     <div style={{ width: '400px' }}>
       <Label for="input1">
-        Normal text
-        <Input id="input1" {...props} />
+        Small input
+        <Input bold id="input1" size="small" {...props} />
       </Label>
       <Label for="input2">
-        Bold text
-        <Input id="input2" bold {...props} />
+        Medium input
+        <Input bold id="input2" {...props} />
+      </Label>
+      <Label for="input3">
+        Large input
+        <Input bold id="input3" size="large" {...props} />
+      </Label>
+    </div>
+  ))
+  .add('errors', () => (
+    <div style={{ width: '400px' }}>
+      <Label for="input1">
+        Small input
+        <Input id="input1" size="small" meta={{ error: 'This is an error message' }} value="wrong value" {...props} />
+      </Label>
+      <Label for="input2">
+        Medium input
+        <Input id="input2" meta={{ error: 'This is an error message' }} value="wrong value" {...props} />
+      </Label>
+      <Label for="input3">
+        Large input
+        <Input id="input3" size="large" meta={{ error: 'This is an error message' }} value="wrong value" {...props} />
       </Label>
     </div>
   ));
