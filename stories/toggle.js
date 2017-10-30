5d78180d7b054ec2392b516bf424dcf51bb7196c
Lowie Benoot <lowie@teamleader.be>
2017-10-30 15:50:21 +0100

    update the toggle colors

--- stories/toggle.js
+++ stories/toggle.js
@@ -47,74 +47,11 @@ storiesOf('Toggles', module)
       </State>
     </div>
   ))
-  .add('colors', () => (
-    <div>
-      <Island color="white">
-        <State store={store}>
-          <Toggle color="neutral" onChange={handleOnChange} label="I'm a white toggle" />
-        </State>
-      </Island>
-      <Island color="neutral" style={{ background: '#354B62'}}> {/*we don't have a teal island, so faking one*/}
-        <State store={store}>
-          <Toggle color="teal" onChange={handleOnChange} label="I'm a teal toggle" />
-        </State>
-      </Island>
-      <Island color="violet">
-        <State store={store}>
-          <Toggle color="violet" onChange={handleOnChange} label="I'm a violet toggle" />
-        </State>
-      </Island>
-      <Island color="ruby">
-        <State store={store}>
-          <Toggle color="ruby" onChange={handleOnChange} label="I'm a ruby toggle" />
-        </State>
-      </Island>
-      <Island color="mint">
-        <State store={store}>
-          <Toggle color="mint" onChange={handleOnChange} label="I'm a mint toggle" />
-        </State>
-      </Island>
-      <Island color="gold">
-        <State store={store}>
-          <Toggle color="gold" onChange={handleOnChange} label="I'm a gold toggle" />
-        </State>
-      </Island>
-      <Island color="aqua">
-        <State store={store}>
-          <Toggle color="aqua" onChange={handleOnChange} label="I'm an aqua toggle" />
-        </State>
-      </Island>
-    </div>
-  ))
   .add('disabled', () => (
     <div>
       <Island color="white">
         <Toggle disabled margin={2} color="neutral" label="I'm a disabled toggle" />
         <Toggle disabled margin={2} checked color="neutral" label="I'm a disabled toggle" />
-      </Island>
-      <Island color="neutral" style={{ background: '#354B62'}}> {/*we don't have a teal island, so faking one*/}
-        <Toggle disabled margin={2} color="teal" label="I'm a disabled toggle" />
-        <Toggle disabled margin={2} checked color="teal" label="I'm a disabled toggle" />
-      </Island>
-      <Island color="violet">
-        <Toggle disabled margin={2} color="violet" label="I'm a disabled toggle" />
-        <Toggle disabled margin={2} checked color="violet" label="I'm a disabled toggle" />
-      </Island>
-      <Island color="ruby">
-        <Toggle disabled margin={2} color="ruby" label="I'm a disabled toggle" />
-        <Toggle disabled margin={2} checked color="ruby" label="I'm a disabled toggle" />
-      </Island>
-      <Island color="mint">
-        <Toggle disabled margin={2} color="mint" label="I'm a disabled toggle" />
-        <Toggle disabled margin={2} checked color="mint" label="I'm a disabled toggle" />
-      </Island>
-      <Island color="gold">
-        <Toggle disabled margin={2} color="gold" label="I'm a disabled toggle" />
-        <Toggle disabled margin={2} checked color="gold" label="I'm a disabled toggle" />
-      </Island>
-      <Island color="aqua">
-        <Toggle disabled margin={2} color="aqua" label="I'm a disabled toggle" />
-        <Toggle disabled margin={2} checked color="aqua" label="I'm a disabled toggle" />
       </Island>
     </div>
   ));
