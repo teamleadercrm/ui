57fdfead3939a7a0ccc465b3c5a49e5a3160f5ae
Dries Decramer <dries.decramer@teamleader.be>
2017-11-06 12:06:17 +0100

    Converted RadioGroup component to use our new Box component

--- stories/radio.js
+++ stories/radio.js
@@ -22,12 +22,20 @@ storiesOf('Radio', module)
   .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('Enabled', () => (
     <State store={store}>
-      <RadioGroup name="stringValue" onChange={updateState}>
-        <RadioButton size="small" marginBottom={2} label="The Walking Dead" value="thewalkingdead" />
-        <RadioButton size="medium" marginBottom={2} label="From Hell" value="fromhell" />
-        <RadioButton size="medium" marginBottom={2} label="V for a Vendetta" value="vvendetta" onFocus={action('Focus')} />
-        <RadioButton size="large" marginBottom={2} label="Watchmen" value="watchmen" onBlur={action('blur')} />
-      </RadioGroup>
+      <div>
+        <RadioGroup name="stringValue" onChange={updateState}>
+          <RadioButton size="small" marginBottom={2} label="The Walking Dead" value="thewalkingdead" />
+          <RadioButton size="medium" marginBottom={2} label="From Hell" value="fromhell" />
+          <RadioButton size="medium" marginBottom={2} label="V for a Vendetta" value="vvendetta" onFocus={action('Focus')} />
+          <RadioButton size="large" marginBottom={2} label="Watchmen" value="watchmen" onBlur={action('blur')} />
+        </RadioGroup>
+        <RadioGroup name="stringValue" onChange={updateState} marginTop={6}>
+          <RadioButton size="small" marginBottom={2} label="The Walking Dead" value="thewalkingdead" />
+          <RadioButton size="medium" marginBottom={2} label="From Hell" value="fromhell" />
+          <RadioButton size="medium" marginBottom={2} label="V for a Vendetta" value="vvendetta" onFocus={action('Focus')} />
+          <RadioButton size="large" marginBottom={2} label="Watchmen" value="watchmen" onBlur={action('blur')} />
+        </RadioGroup>
+      </div>
     </State>
   ))
   .add('Disabled', () => (
