2bf043f15f59ebb937cba1c570450c9512c7d310
Dries Decramer <dries.decramer@teamleader.be>
2017-11-08 12:46:45 +0100

    Finished all functionality and theming

--- components/tag/Tag.js
+++ components/tag/Tag.js
@@ -1,34 +1,19 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Box from '../box';
-import { IconButton } from '../button';
+import Button, { IconButton } from '../button';
 import cx from 'classnames';
 import theme from './theme.css';
-import { IconCloseSmallOutline } from '@teamleader/ui-icons';
-
-const SIZES = {
-  small: {
-    paddingHorizontal: 2,
-    paddingVertical: 1,
-  },
-  medium: {
-    paddingHorizontal: 3,
-    paddingVertical: 2,
-  },
-  large: {
-    paddingHorizontal: 3,
-    paddingVertical: 2,
-  },
-};
+import { IconCloseMediumOutline, IconCloseSmallOutline } from '@teamleader/ui-icons';
 
 class Tag extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
     inverse: PropTypes.bool,
     label: PropTypes.string.isRequired,
-    onClick: PropTypes.func,
-    onClose: PropTypes.func,
-    size: PropTypes.oneOf(Object.keys(SIZES)),
+    onLabelClick: PropTypes.func,
+    onRemoveClick: PropTypes.func,
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
   };
 
   static defaultProps = {
@@ -36,47 +21,46 @@ class Tag extends PureComponent {
     size: 'medium',
   };
 
-  constructor() {
-    super(...arguments);
-    this.handleClick = ::this.handleClick;
-    this.handleClose = ::this.handleClose;
+  getCloseButtonColor() {
+    return this.props.inverse ? 'white' : 'neutral';
   }
 
-  handleClick() {
-    this.props.onClick();
-  }
-
-  handleClose() {
-    this.props.onClose();
+  getCloseIcon() {
+    return this.props.size === 'large' ? <IconCloseMediumOutline /> : <IconCloseSmallOutline />;
   }
 
   render() {
-    const { className, inverse, label, onClose, size, ...others } = this.props;
+    const { className, inverse, label, onLabelClick, onRemoveClick, size, ...others } = this.props;
 
-    const classNames = cx(theme.tag, className, {
-      [theme['inverse']]: inverse,
-    });
+    const classNames = cx(
+      theme.tag,
+      {
+        [theme['removable']]: onRemoveClick,
+        [theme['inverse']]: inverse,
+        [theme[size]]: theme[size],
+      },
+      className,
+    );
 
     return (
-      <Box
-        className={classNames}
-        data-teamleader-ui="tag"
-        element="span"
-        onClick={this.handleClick}
-        {...SIZES[size]}
-        {...others}
-      >
-        <span className={theme['inner']}>
+      <Box className={classNames} data-teamleader-ui="tag" {...others}>
+        {onLabelClick ? (
+          <Button className={theme['label-button']} onClick={onLabelClick} level="outline" inverse={inverse}>
+            {label}
+          </Button>
+        ) : (
           <span className={theme['label']}>{label}</span>
-          {onClose && (
-            <IconButton
-              className={theme['close-button']}
-              icon={<IconCloseSmallOutline />}
-              onClick={this.handleClose}
-              size="small"
-            />
-          )}
-        </span>
+        )}
+
+        {onRemoveClick && (
+          <IconButton
+            className={theme['remove-button']}
+            color={this.getCloseButtonColor()}
+            icon={this.getCloseIcon()}
+            onClick={onRemoveClick}
+            size="small"
+          />
+        )}
       </Box>
     );
   }
