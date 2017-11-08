a2cfc51aa7606d1fed49b0d7191afa46fe6db585
Dries Decramer <dries.decramer@teamleader.be>
2017-11-08 12:47:16 +0100

    Added stories for our Tag component

--- stories/tag.js
+++ stories/tag.js
@@ -26,7 +26,57 @@ storiesOf('Tags', module)
   .add('size', () => (
     <Box>
       {sizes.map((size, index) => (
-        <Tag key={index} label={`${size} tag`} marginHorizontal={3} onClose={action('')} size={size}/>
+        <Tag key={index} label={`${size} tag`} marginHorizontal={3} size={size}/>
       ))}
+    </Box>
+  ))
+  .add('clickable', () => (
+    <Box>
+      {sizes.map((size, index) => (
+        <Tag
+          key={index}
+          label={`${size} tag`}
+          marginHorizontal={3}
+          onLabelClick={action('Tag label clicked')}
+          size={size}
+        />
+      ))}
+    </Box>
+  ))
+  .add('closable', () => (
+    <Box>
+      {sizes.map((size, index) => (
+        <Tag
+          key={index}
+          label={`${size} tag`}
+          marginHorizontal={3}
+          onRemoveClick={action('Tag removed')}
+          size={size}
+        />
+      ))}
+    </Box>
+  ))
+  .add('clickable & closable', () => (
+    <Box>
+      {sizes.map((size, index) => (
+        <Tag
+          key={index}
+          label={`${size} tag`}
+          marginHorizontal={3}
+          onLabelClick={action('Tag label clicked')}
+          onRemoveClick={action('Tag removed')}
+          size={size}
+        />
+      ))}
+    </Box>
+  ))
+  .add('inverse', () => (
+    <Box>
+      <Tag
+        inverse={true}
+        label="Inverse tag"
+        onLabelClick={action('Tag label clicked')}
+        onRemoveClick={action('Tag removed')}
+      />
     </Box>
   ));
