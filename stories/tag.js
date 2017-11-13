bc23d0ec9d30a6eb1db89f54f086048af368df71
Dries Decramer <dries.decramer@teamleader.be>
2017-11-13 15:58:21 +0100

    Removed commented code

--- stories/tag.js
+++ stories/tag.js
@@ -3,21 +3,11 @@ import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
-// import { Store, State } from '@sambego/storybook-state';
 import styles from '@sambego/storybook-styles';
-import { Box, Tag, Island } from '../components';
+import { Box, Tag } from '../components';
 import { baseStyles, centerStyles } from '../.storybook/styles';
 
 const sizes = ['small', 'medium', 'large'];
-
-// const store = new Store({
-//   checked: false,
-// });
-
-// const handleOnChange = checked => {
-//   store.set({ checked });
-//   action(`onChange - checked: ${checked}`)();
-// };
 
 storiesOf('Tags', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
