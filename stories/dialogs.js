92afe0dcbeeac303838b3791189140e5ac29e4a8
Dries Decramer <dries.decramer@teamleader.be>
2017-11-14 10:09:51 +0100

    Storybook: added button to open the Dialog

--- stories/dialogs.js
+++ stories/dialogs.js
@@ -1,30 +1,50 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { Store, State } from '@sambego/storybook-state';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import styles from '@sambego/storybook-styles';
-import { Dialog, TextBody } from '../components/';
-import { baseStyles } from '../.storybook/styles';
+import { Box, Button, Dialog, TextBody } from '../components/';
+import { baseStyles, centerStyles } from '../.storybook/styles';
 
-const openDialog = () => {
-  return action('action button clicked');
+const actions = [
+  { label: 'Cancel', onClick: action('Cancel clicked') },
+  { label: 'Save', onClick: action('Save clicked'), level: "primary" },
+];
+
+const store = new Store({
+  active: false,
+});
+
+const handleButtonClick = () => {
+  store.set({ active: true });
+  return action('onClick - active: true');
 };
-const actions = [{ label: 'Cancel', onClick: openDialog }, { label: 'Save', onClick: openDialog, level: 'primary' }];
+
+const handleCloseClick = () => {
+  store.set({ active: false });
+  return action('onClick - active: false');
+};
 
 storiesOf('Dialogs', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .addDecorator(styles(baseStyles))
+  .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('colors', () => (
-    <Dialog
-      actions={actions}
-      active
-      onCloseClick={openDialog}
-      onEscKeyDown={openDialog}
-      onOverlayClick={openDialog}
-      title="Dialog title"
-    >
-      <TextBody>Here you can add arbitrary content.</TextBody>
-    </Dialog>
+    <Box>
+      <Button onClick={handleButtonClick} label="Open a dialog" />
+      <State store={store}>
+        <Dialog
+          actions={actions}
+          active={false}
+          onCloseClick={handleCloseClick}
+          onEscKeyDown={handleCloseClick}
+          onOverlayClick={handleCloseClick}
+          title="Dialog title"
+        >
+          <TextBody>Here you can add arbitrary content.</TextBody>
+        </Dialog>
+      </State>
+    </Box>
   ));
