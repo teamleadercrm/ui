8f9dba90a48117a2ffe4b6cbeebb58606f1faea7
Dries Decramer <dries.decramer@teamleader.be>
2017-11-16 11:02:38 +0100

    Storybook: changed background color teal darkest value

--- .storybook/config.js
+++ .storybook/config.js
@@ -16,7 +16,7 @@ setOptions({
   url: 'https://teamleader.design'
 });
 
-addDecorator(backgroundColor(['#ffffff', '#e6f2ff', '#ffeecc', '#d3f3f3', '#ffe3d9', '#e1edfa', '#f1f0ff', '#344b63']));
+addDecorator(backgroundColor(['#ffffff', '#e6f2ff', '#ffeecc', '#d3f3f3', '#ffe3d9', '#e1edfa', '#f1f0ff', '#2a3b4d']));
 
 const req = require.context('../stories', true, /\.js$/);
 
