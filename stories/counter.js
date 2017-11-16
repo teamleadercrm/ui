bcfb4a70828839651ec6cf42368f66518c041817
Dries Decramer <dries.decramer@teamleader.be>
2017-11-16 14:26:45 +0100

    Storybook: updated the Counter stories

--- stories/counter.js
+++ stories/counter.js
@@ -6,7 +6,7 @@ import styles from '@sambego/storybook-styles';
 import { Box, Counter } from '../components';
 import { baseStyles, centerStyles } from '../.storybook/styles';
 
-const colors = ['neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua'];
+const colors = ['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby'];
 const sizes = ['small', 'medium'];
 
 storiesOf('Counters', module)
@@ -15,22 +15,16 @@ storiesOf('Counters', module)
   .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('colors', () => (
     <Box>
-      {colors.map((color, index) => <Counter count={99} color={color} key={index} marginHorizontal={1} />)}
-      <Counter count={99} inactive marginHorizontal={1} />
+      {colors.map((color, index) => <Counter count={99} color={color} key={index} marginHorizontal={3} />)}
     </Box>
   ))
-  .add('size', () => (
+  .add('sizes', () => (
     <Box>
-      {sizes.map((size, index) => <Counter count={99} color="neutral" key={index} size={size} marginHorizontal={1} />)}
-    </Box>
-  ))
-  .add('no count', () => (
-    <Box>
-      {sizes.map((size, index) => <Counter color="neutral" key={index} size={size} marginHorizontal={2} />)}
+      {sizes.map((size, index) => <Counter count={99} color="ruby" key={index} size={size} marginHorizontal={3} />)}
     </Box>
   ))
   .add('maximum count', () => (
     <Box>
-      <Counter count={116} color="neutral" maxCount={99} />
+      <Counter count={116} color="ruby" maxCount={99} />
     </Box>
   ));
