cbf025cbd17bffa46a1b679838b7f2096406d9e1
Dries Decramer <dries.decramer@teamleader.be>
2017-11-20 12:16:07 +0100

    Removed the borderColor prop because it won't cover everything

--- components/counter/Counter.js
+++ components/counter/Counter.js
@@ -6,7 +6,6 @@ import theme from './theme.css';
 
 class Counter extends PureComponent {
   static propTypes = {
-    borderColor: PropTypes.oneOf(['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby', 'tealdark']),
     className: PropTypes.string,
     color: PropTypes.oneOf(['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby']),
     count: PropTypes.number.isRequired,
@@ -15,15 +14,14 @@ class Counter extends PureComponent {
   };
 
   static defaultProps = {
-    borderColor: 'neutral',
     color: 'neutral',
     size: 'medium',
   };
 
   render() {
-    const { borderColor, className, color, count, maxCount, size, ...others } = this.props;
+    const { className, color, count, maxCount, size, ...others } = this.props;
 
-    const classNames = cx(theme['counter'], theme[color], theme[size], theme[`border-${borderColor}`], className);
+    const classNames = cx(theme['counter'], theme[color], theme[size], className);
 
     return (
       <Box className={classNames} element="span" {...others} data-teamleader-ui="counter">
