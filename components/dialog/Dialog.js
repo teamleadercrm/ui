63981a51a05e579375c6473c89eaa9bcf730d3be
Dries Decramer <dries.decramer@teamleader.be>
2017-11-21 13:32:20 +0100

    Removed all the clutter inside the dialog, because we'll build it with our existing components

--- components/dialog/Dialog.js
+++ components/dialog/Dialog.js
@@ -2,45 +2,29 @@ import React, { PureComponent } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { Heading3 } from '../typography';
-import InjectButton, { ButtonGroup } from '../button';
-import InjectIconButton from '../button/IconButton';
 import InjectOverlay from '../overlay/Overlay';
 import Transition from 'react-transition-group/Transition';
-import { IconCloseMediumOutline, IconWarningMediumOutline } from '@teamleader/ui-icons';
 import theme from './theme.css';
 
-const factory = (Overlay, Button, IconButton) => {
+const factory = Overlay => {
   class Dialog extends PureComponent {
     static propTypes = {
-      actions: PropTypes.arrayOf(
-        PropTypes.shape({
-          className: PropTypes.string,
-          label: PropTypes.string,
-          children: PropTypes.node,
-        }),
-      ),
       active: PropTypes.bool,
       backdrop: PropTypes.string,
       children: PropTypes.node,
       className: PropTypes.string,
-      onCloseClick: PropTypes.func,
       onEscKeyDown: PropTypes.func,
       onOverlayClick: PropTypes.func,
       onOverlayMouseDown: PropTypes.func,
       onOverlayMouseMove: PropTypes.func,
       onOverlayMouseUp: PropTypes.func,
       size: PropTypes.oneOf(['small', 'medium', 'large', 'fullscreen']),
-      title: PropTypes.string,
-      type: PropTypes.oneOf(['regular', 'warning']),
     };
 
     static defaultProps = {
-      actions: [],
       active: false,
       backdrop: 'dark',
       size: 'medium',
-      type: 'regular',
     };
 
     constructor() {
@@ -60,29 +44,23 @@ const factory = (Overlay, Button, IconButton) => {
 
     render() {
       const {
-        actions,
         active,
         backdrop,
         children,
         className,
-        onCloseClick,
         onEscKeyDown,
         onOverlayClick,
         onOverlayMouseDown,
         onOverlayMouseMove,
         onOverlayMouseUp,
         size,
-        title,
-        type,
       } = this.props;
 
       if (!active) {
         return null;
       }
 
-      const actionButtons = actions.map((action, index) => <Button key={index} {...action} />);
-
-      const dialogClassNames = cx(theme['dialog'], theme[`is-${size}`], theme[`is-${type}`], className);
+      const dialogClassNames = cx(theme['dialog'], theme[`is-${size}`], className);
 
       const dialog = (
         <Transition timeout={0} in={active} appear>
@@ -105,19 +83,7 @@ const factory = (Overlay, Button, IconButton) => {
                   onMouseUp={onOverlayMouseUp}
                 />
                 <div data-teamleader-ui="dialog" className={dialogClassNames}>
-                  <header className={theme['header']}>
-                    {type === 'warning' && <IconWarningMediumOutline className={theme['icon']} />}
-                    {title && <Heading3 className={theme['title']}>{title}</Heading3>}
-                    <IconButton icon={<IconCloseMediumOutline />} className={theme['close']} onClick={onCloseClick} />
-                  </header>
-                  <section role="body" className={theme['body']}>
-                    {children}
-                  </section>
-                  {actionButtons.length ? (
-                    <ButtonGroup role="navigation" className={theme['navigation']}>
-                      {actionButtons}
-                    </ButtonGroup>
-                  ) : null}
+                  <div className={theme['inner']}>{children}</div>
                 </div>
               </div>
             );
@@ -132,7 +98,7 @@ const factory = (Overlay, Button, IconButton) => {
   return Dialog;
 };
 
-const Dialog = factory(InjectOverlay, InjectButton, InjectIconButton);
+const Dialog = factory(InjectOverlay);
 
 export default Dialog;
 export { Dialog, factory as dialogFactory };
