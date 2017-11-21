d68f8d75cbdc001827dd30aaba27ec79fe487123
Dries Decramer <dries.decramer@teamleader.be>
2017-11-21 13:43:33 +0100

    Get rid of the unneeded Dialog factory, works very well without it

--- components/dialog/Dialog.js
+++ components/dialog/Dialog.js
@@ -2,103 +2,97 @@ import React, { PureComponent } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import InjectOverlay from '../overlay/Overlay';
+import Overlay from '../overlay/Overlay';
 import Transition from 'react-transition-group/Transition';
 import theme from './theme.css';
 
-const factory = Overlay => {
-  class Dialog extends PureComponent {
-    static propTypes = {
-      active: PropTypes.bool,
-      backdrop: PropTypes.string,
-      children: PropTypes.node,
-      className: PropTypes.string,
-      onEscKeyDown: PropTypes.func,
-      onOverlayClick: PropTypes.func,
-      onOverlayMouseDown: PropTypes.func,
-      onOverlayMouseMove: PropTypes.func,
-      onOverlayMouseUp: PropTypes.func,
-      size: PropTypes.oneOf(['small', 'medium', 'large', 'fullscreen']),
-    };
+class Dialog extends PureComponent {
+  static propTypes = {
+    active: PropTypes.bool,
+    backdrop: PropTypes.string,
+    children: PropTypes.node,
+    className: PropTypes.string,
+    onEscKeyDown: PropTypes.func,
+    onOverlayClick: PropTypes.func,
+    onOverlayMouseDown: PropTypes.func,
+    onOverlayMouseMove: PropTypes.func,
+    onOverlayMouseUp: PropTypes.func,
+    size: PropTypes.oneOf(['small', 'medium', 'large', 'fullscreen']),
+  };
 
-    static defaultProps = {
-      active: false,
-      backdrop: 'dark',
-      size: 'medium',
-    };
+  static defaultProps = {
+    active: false,
+    backdrop: 'dark',
+    size: 'medium',
+  };
 
-    constructor() {
-      super(...arguments);
+  constructor() {
+    super(...arguments);
 
-      this.dialogRoot = document.createElement('div');
-      this.dialogRoot.id = 'dialog-root';
-    }
-
-    componentDidMount() {
-      document.body.appendChild(this.dialogRoot);
-    }
-
-    componentWillUnmount() {
-      document.body.removeChild(this.dialogRoot);
-    }
-
-    render() {
-      const {
-        active,
-        backdrop,
-        children,
-        className,
-        onEscKeyDown,
-        onOverlayClick,
-        onOverlayMouseDown,
-        onOverlayMouseMove,
-        onOverlayMouseUp,
-        size,
-      } = this.props;
-
-      if (!active) {
-        return null;
-      }
-
-      const dialogClassNames = cx(theme['dialog'], theme[`is-${size}`], className);
-
-      const dialog = (
-        <Transition timeout={0} in={active} appear>
-          {state => {
-            return (
-              <div
-                className={cx(theme['wrapper'], {
-                  [theme['is-entering']]: state === 'entering',
-                  [theme['is-entered']]: state === 'entered',
-                })}
-              >
-                <Overlay
-                  active={active}
-                  backdrop={backdrop}
-                  className={theme['overlay']}
-                  onClick={onOverlayClick}
-                  onEscKeyDown={onEscKeyDown}
-                  onMouseDown={onOverlayMouseDown}
-                  onMouseMove={onOverlayMouseMove}
-                  onMouseUp={onOverlayMouseUp}
-                />
-                <div data-teamleader-ui="dialog" className={dialogClassNames}>
-                  <div className={theme['inner']}>{children}</div>
-                </div>
-              </div>
-            );
-          }}
-        </Transition>
-      );
-
-      return createPortal(dialog, this.dialogRoot);
-    }
+    this.dialogRoot = document.createElement('div');
+    this.dialogRoot.id = 'dialog-root';
   }
 
-  return Dialog;
-};
+  componentDidMount() {
+    document.body.appendChild(this.dialogRoot);
+  }
 
-const Dialog = factory(InjectOverlay);
+  componentWillUnmount() {
+    document.body.removeChild(this.dialogRoot);
+  }
+
+  render() {
+    const {
+      active,
+      backdrop,
+      children,
+      className,
+      onEscKeyDown,
+      onOverlayClick,
+      onOverlayMouseDown,
+      onOverlayMouseMove,
+      onOverlayMouseUp,
+      size,
+    } = this.props;
+
+    if (!active) {
+      return null;
+    }
+
+    const dialogClassNames = cx(theme['dialog'], theme[`is-${size}`], className);
+
+    const dialog = (
+      <Transition timeout={0} in={active} appear>
+        {state => {
+          return (
+            <div
+              className={cx(theme['wrapper'], {
+                [theme['is-entering']]: state === 'entering',
+                [theme['is-entered']]: state === 'entered',
+              })}
+            >
+              <Overlay
+                active={active}
+                backdrop={backdrop}
+                className={theme['overlay']}
+                onClick={onOverlayClick}
+                onEscKeyDown={onEscKeyDown}
+                onMouseDown={onOverlayMouseDown}
+                onMouseMove={onOverlayMouseMove}
+                onMouseUp={onOverlayMouseUp}
+              />
+              <div data-teamleader-ui="dialog" className={dialogClassNames}>
+                <div className={theme['inner']}>{children}</div>
+              </div>
+            </div>
+          );
+        }}
+      </Transition>
+    );
+
+    return createPortal(dialog, this.dialogRoot);
+  }
+}
 
 export default Dialog;
-export { Dialog, factory as dialogFactory };
+export { Dialog };
