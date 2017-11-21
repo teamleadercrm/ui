1e956d73ef36d415da3aeb08a4658884730879dc
Dries Decramer <dries.decramer@teamleader.be>
2017-11-21 09:50:34 +0100

    Removed all the clutter inside the popover, because we'll build it with our existing components

--- components/popover/Popover.js
+++ components/popover/Popover.js
@@ -3,26 +3,16 @@ import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import throttle from 'lodash.throttle';
-import InjectButton, { IconButton, ButtonGroup } from '../button';
 import InjectOverlay from '../overlay';
 import Transition from 'react-transition-group/Transition';
 import ReactResizeDetector from 'react-resize-detector';
-import { Heading3, TextSmall } from '../typography';
 import { events } from '../utils';
 import { calculateHorizontalPositions, calculateVerticalPositions } from './positionCalculation';
-import { IconCloseMediumOutline } from '@teamleader/ui-icons';
 import theme from './theme.css';
 
-const factory = (axis, calculatePositions, Overlay, Button) => {
+const factory = (axis, calculatePositions, Overlay) => {
   class Popover extends PureComponent {
     static propTypes = {
-      actions: PropTypes.arrayOf(
-        PropTypes.shape({
-          className: PropTypes.string,
-          label: PropTypes.string,
-          children: PropTypes.node,
-        }),
-      ),
       active: PropTypes.bool,
       anchorEl: PropTypes.object,
       backdrop: PropTypes.string,
@@ -30,19 +20,15 @@ const factory = (axis, calculatePositions, Overlay, Button) => {
       className: PropTypes.string,
       direction: PropTypes.string.isRequired,
       offsetCorrection: PropTypes.number,
-      onCloseClick: PropTypes.func,
       onEscKeyDown: PropTypes.func,
       onOverlayClick: PropTypes.func,
       onOverlayMouseDown: PropTypes.func,
       onOverlayMouseMove: PropTypes.func,
       onOverlayMouseUp: PropTypes.func,
       position: PropTypes.string.isRequired,
-      title: PropTypes.string,
-      subtitle: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     };
 
     static defaultProps = {
-      actions: [],
       active: true,
       backdrop: 'dark',
       offsetCorrection: 0,
@@ -107,30 +93,20 @@ const factory = (axis, calculatePositions, Overlay, Button) => {
       const { left, top, arrowLeft, arrowTop } = this.state.positioning;
 
       const {
-        actions,
         active,
         backdrop,
         children,
         className,
-        onCloseClick,
         onOverlayClick,
         onEscKeyDown,
         onOverlayMouseDown,
         onOverlayMouseMove,
         onOverlayMouseUp,
-        subtitle,
-        title,
       } = this.props;
 
       if (!active) {
         return null;
       }
-
-      const actionButtons = actions.map((action, idx) => <Button key={idx} {...action} />);
-
-      const arrowClassNames = cx(theme['arrow'], {
-        [theme['at-header']]: (title || subtitle || onCloseClick) && arrowTop < 0,
-      });
 
       const popover = (
         <Transition timeout={0} in={active} appear>
@@ -160,31 +136,11 @@ const factory = (axis, calculatePositions, Overlay, Button) => {
                     this.popoverNode = node;
                   }}
                 >
-                  <div className={arrowClassNames} style={{ left: `${arrowLeft}px`, top: `${arrowTop}px` }} />
-                  {(title || subtitle || onCloseClick) && (
-                    <header className={theme['header']}>
-                      {title && <Heading3 className={theme['title']}>{title}</Heading3>}
-                      {subtitle && (
-                        <TextSmall className={theme['subtitle']} marginTop={1}>
-                          {subtitle}
-                        </TextSmall>
-                      )}
-                      {onCloseClick && (
-                        <IconButton
-                          icon={<IconCloseMediumOutline />}
-                          className={theme['close']}
-                          onMouseUp={onCloseClick}
-                        />
-                      )}
-                    </header>
-                  )}
-                  <section role="body" className={theme['body']}>
+                  <div className={theme['arrow']} style={{ left: `${arrowLeft}px`, top: `${arrowTop}px` }} />
+                  <div className={theme['inner']}>
                     {children}
-                  </section>
-                  {actionButtons.length ? (
-                    <ButtonGroup className={theme['navigation']}>{actionButtons}</ButtonGroup>
-                  ) : null}
-                  <ReactResizeDetector handleHeight onResize={this.setPlacementThrottled} />
+                    <ReactResizeDetector handleHeight onResize={this.setPlacementThrottled} />
+                  </div>
                 </div>
               </div>
             );
@@ -199,6 +155,6 @@ const factory = (axis, calculatePositions, Overlay, Button) => {
   return Popover;
 };
 
-export const PopoverHorizontal = factory('horizontal', calculateHorizontalPositions, InjectOverlay, InjectButton);
+export const PopoverHorizontal = factory('horizontal', calculateHorizontalPositions, InjectOverlay);
 
-export const PopoverVertical = factory('vertical', calculateVerticalPositions, InjectOverlay, InjectButton);
+export const PopoverVertical = factory('vertical', calculateVerticalPositions, InjectOverlay);
