4d1c5547b634da9328fe409d2cf841b61ab6e2da
Kevin Waelkens <kevin.waelkens@teamleader.eu>
2017-11-27 12:16:56 +0100

    Adding state + logic to change state

--- stories/titleTab.js
+++ stories/titleTab.js
@@ -4,15 +4,54 @@ import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { Island, TitleTab } from '../components';
+import { Store, State } from '@sambego/storybook-state';
+import { action } from '@storybook/addon-actions';
 import { baseStyles, centerStyles } from '../.storybook/styles';
+
+const store = new Store({
+  items: [
+    {
+      title: 'Invoices',
+      active: false,
+    },
+    {
+      title: 'CRM',
+      active: true,
+    },
+    {
+      title: 'Planning',
+      active: false,
+    },
+  ],
+});
+
+// Real-life tabs should not have a clickHandler but rather listen to state updates
+const handleClick = title => {
+  store.set({
+    items: store.state.items.map(item => ({
+      ...item,
+      active: item.title === title,
+    })),
+  });
+  action(`New active is ${title}`)(store.state.items);
+};
 
 storiesOf('TitleTab', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
   .addDecorator(styles({ ...baseStyles, ...centerStyles }))
-  .add('general', () => (
-    <Island style={{display: 'flex'}}>
-      <TitleTab>Default</TitleTab>
-      <TitleTab active={true}>Active</TitleTab>
-    </Island>
-  ));
+  .add('general', () => {
+    return (
+      <Island style={{ display: 'flex' }}>
+        {store.state.items.map((item, key) => {
+          return (
+            <State store={store}>
+              <TitleTab onClick={() => handleClick(item.title)} active={item.active} key={key}>
+                {item.title}
+              </TitleTab>
+            </State>
+          );
+        })}
+      </Island>
+    );
+  });
