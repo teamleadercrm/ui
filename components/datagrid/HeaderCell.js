813e193df109fa41051abcd6606e2c9822a76c73
Dries Decramer <dries.decramer@teamleader.be>
2017-11-28 13:05:48 +0100

    Don't use our Heading4 component, instead use custom styling on the header-cell

--- components/datagrid/HeaderCell.js
+++ components/datagrid/HeaderCell.js
@@ -2,16 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import theme from './theme.css';
 import Cell from './Cell';
-import { Heading4 } from '../typography';
 import { IconSortSmallOutline } from '@teamleader/ui-icons';
 
 const HeaderCell = ({ children, onClick }) => {
   return (
     <Cell className={theme['header-cell']} onClick={onClick}>
-      <Heading4 color="neutral">
-        {children}
-        {onClick && <IconSortSmallOutline />}
-      </Heading4>
+      {children}
+      {onClick && <IconSortSmallOutline />}
     </Cell>
   );
 };
