a7efe403fc1d297aa5459fb9332268129d30095d
Dries Decramer <dries.decramer@teamleader.be>
2017-11-29 10:07:13 +0100

    Converted to PureComponent

--- components/datagrid/Cell.js
+++ components/datagrid/Cell.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import theme from './theme.css';
 
-const Cell = props => {
-  const { align, children, className, flex, soft, strong, ...others } = props;
+class Cell extends PureComponent {
+  static propTypes = {
+    align: PropTypes.oneOf(['left', 'center', 'right']),
+    children: PropTypes.any,
+    className: PropTypes.string,
+    flex: PropTypes.oneOf(['fit-content', 'min-width', '1', '2', '3', '4']),
+    soft: PropTypes.bool,
+    strong: PropTypes.bool,
+  };
 
-  const classNames = cx(
-    theme['cell'],
-    theme[`align-${align}`],
-    theme[`flex-${flex}`],
-    {
-      [theme['is-soft']]: soft,
-      [theme['is-strong']]: strong,
-    },
-    className,
-  );
+  render() {
+    const { align, children, className, flex, soft, strong, ...others } = this.props;
 
-  return (
-    <div className={classNames} data-teamleader-ui="datagrid-cell" {...others}>
-      {children}
-    </div>
-  );
-};
+    const classNames = cx(
+      theme['cell'],
+      theme[`align-${align}`],
+      theme[`flex-${flex}`],
+      {
+        [theme['is-soft']]: soft,
+        [theme['is-strong']]: strong,
+      },
+      className,
+    );
 
-Cell.propTypes = {
-  align: PropTypes.oneOf(['left', 'center', 'right']),
-  children: PropTypes.any,
-  className: PropTypes.string,
-  flex: PropTypes.oneOf(['fit-content', 'min-width', '1', '2', '3', '4']),
-  soft: PropTypes.bool,
-  strong: PropTypes.bool,
-};
+    return (
+      <div className={classNames} data-teamleader-ui="datagrid-cell" {...others}>
+        {children}
+      </div>
+    );
+  }
+}
 
 export default Cell;
