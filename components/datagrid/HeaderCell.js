e6e75ba055d98f5f621ccab0f510bb50216f3dee
Dries Decramer <dries.decramer@teamleader.be>
2017-11-30 11:04:38 +0100

    Converted functional component to PureComponent

--- components/datagrid/HeaderCell.js
+++ components/datagrid/HeaderCell.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import theme from './theme.css';
 import Cell from './Cell';
 import cx from 'classnames';
 import { IconSortSmallOutline } from '@teamleader/ui-icons';
 
-const HeaderCell = ({ children, className, onClick, ...others }) => {
-  return (
-    <Cell className={cx(theme['header-cell'], className)} onClick={onClick} {...others}>
-      {children}
-      {onClick && <IconSortSmallOutline />}
-    </Cell>
-  );
-};
+class HeaderCell extends PureComponent {
+  static propTypes = {
+    children: PropTypes.any,
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+  };
 
-HeaderCell.propTypes = {
-  children: PropTypes.any,
-  className: PropTypes.string,
-  onClick: PropTypes.func,
-};
+  render() {
+    const { children, className, onClick, ...others } = this.props;
+
+    const classNames = cx(theme['header-cell'], className);
+
+    return (
+      <Cell className={classNames} onClick={onClick} {...others}>
+        {children}
+        {onClick && <IconSortSmallOutline />}
+      </Cell>
+    );
+  }
+}
 
 export default HeaderCell;
