295f18980f882ab74c0300d5624089d3d1a7ac9e
Dries Decramer <dries.decramer@teamleader.be>
2017-12-19 08:36:23 +0100

    Added LinkButton component

---
+++ components/button/LinkButton.js
@@ -0,0 +1,100 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import theme from './theme.css';
+
+class LinkButton extends PureComponent {
+  static propTypes = {
+    active: PropTypes.bool,
+    children: PropTypes.node,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    element: PropTypes.element,
+    icon: PropTypes.element,
+    iconPlacement: PropTypes.oneOf(['left', 'right']),
+    inverse: PropTypes.bool,
+    label: PropTypes.string,
+    onMouseLeave: PropTypes.func,
+    onMouseUp: PropTypes.func,
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+  };
+
+  static defaultProps = {
+    active: false,
+    className: '',
+    iconPlacement: 'left',
+    inverse: false,
+    size: 'medium',
+  };
+
+  handleMouseUp = event => {
+    this.buttonNode.blur();
+    if (this.props.onMouseUp) {
+      this.props.onMouseUp(event);
+    }
+  };
+
+  handleMouseLeave = event => {
+    this.buttonNode.blur();
+    if (this.props.onMouseLeave) {
+      this.props.onMouseLeave(event);
+    }
+  };
+
+  render() {
+    const {
+      active,
+      children,
+      className,
+      disabled,
+      element,
+      icon,
+      iconPlacement,
+      inverse,
+      label,
+      size,
+      ...others
+    } = this.props;
+
+    const Element = element || 'button';
+
+    const classNames = cx(
+      theme['link-button'],
+      {
+        [theme['active']]: active,
+        [theme['disabled']]: disabled,
+        [theme['icon-only']]: !label && !children,
+        [theme['inverse']]: inverse,
+        [theme[size]]: theme[size],
+      },
+      className,
+    );
+
+    const props = {
+      ...others,
+      ref: node => {
+        this.buttonNode = node;
+      },
+      className: classNames,
+      onMouseUp: this.handleMouseUp,
+      onMouseLeave: this.handleMouseLeave,
+      'data-teamleader-ui': 'link-button',
+    };
+
+    return React.createElement(
+      Element,
+      props,
+      icon && iconPlacement === 'left' ? icon : null,
+      label || children ? (
+        <span className={theme['children']}>
+          {label}
+          {children}
+        </span>
+      ) : null,
+      icon && iconPlacement === 'right' ? icon : null,
+    );
+  }
+}
+
+export default LinkButton;
+export { LinkButton };
