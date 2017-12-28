5485fd45d39cf2e442f3f68888d5d4c0162a21d8
Dries Decramer <dries.decramer@teamleader.be>
2017-12-28 13:18:40 +0100

    Splitted up button and button group stories

---
+++ stories/buttonGroup.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { checkA11y } from 'storybook-addon-a11y';
+import { withInfo } from '@storybook/addon-info';
+import styles from '@sambego/storybook-styles';
+import { IconAddMediumOutline, IconAddSmallOutline,IconMagicMediumOutline } from '@teamleader/ui-icons';
+import { Button, ButtonGroup, IconButton, Island, Box } from '../components';
+import { baseStyles, centerStyles } from '../.storybook/styles';
+
+storiesOf('Button groups', module)
+  .addDecorator((story, context) => withInfo('')(story)(context))
+  .addDecorator(checkA11y)
+  .addDecorator(styles({ ...baseStyles, ...centerStyles }))
+  .add('normal', () => (
+    <ButtonGroup>
+      <Button label="Button 1"/>
+      <Button label="Button 2"/>
+    </ButtonGroup>
+  ))
+  .add('segmented', () => (
+    <ButtonGroup segmented>
+      <Button icon={<IconAddMediumOutline />} />
+      <Button label="Button 1"/>
+      <Button label="Button 2"/>
+    </ButtonGroup>
+  ));
