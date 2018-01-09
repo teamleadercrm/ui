3b04929079f8ed6a7107175c0157fbee7f7904de
Dries Decramer <dries.decramer@teamleader.be>
2018-01-09 15:58:16 +0100

    Modified our recycled Badge component according to the new guidelines

--- components/badge/Badge.js
+++ components/badge/Badge.js
@@ -1,39 +1,80 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Box from '../box';
+import { TextSmall } from '../typography';
 import cx from 'classnames';
 import theme from './theme.css';
 
 class Badge extends PureComponent {
   static propTypes = {
-    children: PropTypes.node,
+    children: PropTypes.any,
     className: PropTypes.string,
+    disabled: PropTypes.bool,
+    element: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
     icon: PropTypes.element,
+    iconPlacement: PropTypes.oneOf(['left', 'right']),
+    inherit: PropTypes.bool,
     inverse: PropTypes.bool,
+    onMouseLeave: PropTypes.func,
+    onMouseUp: PropTypes.func,
   };
 
   static defaultProps = {
+    disabled: false,
+    element: 'button',
+    iconPlacement: 'left',
+    inherit: true,
     inverse: false,
   };
 
-  render() {
-    const { children, className, icon, inverse, ...others } = this.props;
+  handleMouseUp = event => {
+    this.badgeNode.node.blur();
+    if (this.props.onMouseUp) {
+      this.props.onMouseUp(event);
+    }
+  };
 
-    const classNames = cx(theme.badge, className, {
-      [theme['inverse']]: inverse,
-    });
+  handleMouseLeave = event => {
+    this.badgeNode.node.blur();
+    if (this.props.onMouseLeave) {
+      this.props.onMouseLeave(event);
+    }
+  };
+
+  render() {
+    const { children, className, disabled, element, icon, iconPlacement, inherit, inverse, ...others } = this.props;
+
+    const classNames = cx(
+      theme['badge'],
+      {
+        [theme['is-disabled']]: disabled,
+        [theme['is-inherit']]: inherit,
+        [theme['is-inverse']]: inverse,
+      },
+      className,
+    );
 
     return (
       <Box
         className={classNames}
         data-teamleader-ui="badge"
-        element="span"
+        element={element}
+        ref={node => (this.badgeNode = node)}
+        onMouseUp={this.handleMouseUp}
+        onMouseLeave={this.handleMouseLeave}
         paddingHorizontal={2}
         paddingVertical={1}
         {...others}
       >
-        {icon}
-        {children}
+        {icon && iconPlacement === 'left' && <span className={theme['icon']}>{icon}</span>}
+        {inherit ? (
+          <span className={theme['label']}>{children}</span>
+        ) : (
+          <TextSmall className={theme['label']} element="span">
+            {children}
+          </TextSmall>
+        )}
+        {icon && iconPlacement === 'right' && <span className={theme['icon']}>{icon}</span>}
       </Box>
     );
   }
