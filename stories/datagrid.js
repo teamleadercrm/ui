e05e3c556a4862c6d5331cf201bc3cf7c9cfc5de
Sambego <sambellen@gmail.com>
2018-01-18 15:00:06 +0100

    Update simple datagrid

--- stories/datagrid.js
+++ stories/datagrid.js
@@ -10,14 +10,26 @@ import { rows1, rows2 } from '../static/data/datagrid';
 
 const TooltippedStatusBullet = Tooltip(StatusBullet);
 
-let rows = rows1;
+// let rows = rows1;
 
 const store = new Store({
   comparableId: 1,
+  rows: rows1,
+  activeRow: 1,
 });
 
 const handleButtonClick = () => {
-  rows = rows === rows1 ? rows2 : rows1;
+  if (store.get('activeRow') === 1) {
+    store.set({
+      rows: rows2,
+      activeRow: 2,
+    });
+  } else {
+    store.set({
+      rows: rows1,
+      activeRow: 1,
+    });
+  }
   store.set({ comparableId: store.get('comparableId') + 1 });
   action('onClick - refresh data - set comparableId to other value')();
 };
@@ -25,6 +37,49 @@ const handleButtonClick = () => {
 const handleRowSelectionChange = (selectedRows) => {
   action(`onSelectionChange - selected row indexes: ${selectedRows}`)();
 };
+
+const BasicDataGrid = ({rows, comparableId}) => {
+  return <DataGrid
+    selectable={boolean('Selectable', true)}
+    comparableId={comparableId}
+    onSelectionChange={handleRowSelectionChange}
+  >
+    <DataGrid.HeaderRow>
+      <DataGrid.HeaderCell flex="min-width" />
+      <DataGrid.HeaderCell onClick={action('onClick: column sort')} sorted="asc">Invoice</DataGrid.HeaderCell>
+      <DataGrid.HeaderCell onClick={action('onClick: column sort')} align="right">Amount</DataGrid.HeaderCell>
+      <DataGrid.HeaderCell flex="2" onClick={action('onClick: column sort')}>Customer</DataGrid.HeaderCell>
+      <DataGrid.HeaderCell onClick={action('onClick: column sort')}>Due date</DataGrid.HeaderCell>
+      <DataGrid.HeaderCell flex="min-width"/>
+    </DataGrid.HeaderRow>
+    {
+      rows.map((row, index) => {
+        return (
+          <DataGrid.BodyRow key={index}>
+            <DataGrid.Cell align="center" flex="min-width">
+              <TooltippedStatusBullet
+                color={row.column1}
+                tooltip={<TextTiny>Overdue</TextTiny>}
+                tooltipColor={row.column1}
+                tooltipSize="small"
+                size="large"
+              />
+            </DataGrid.Cell>
+            <DataGrid.Cell><Link href="#" inherit={false}>{row.column5}</Link> </DataGrid.Cell>
+            <DataGrid.Cell align="right" strong> {`€ ${row.column3}`}</DataGrid.Cell>
+            <DataGrid.Cell flex="2">{row.column2}</DataGrid.Cell>
+            <DataGrid.Cell soft>{row.column4}</DataGrid.Cell>
+            <DataGrid.Cell align="right" flex="min-width" preventOverflow={false}>
+              <IconMenu position="top-right">
+                <MenuItem onClick={action('onClick: delete row')}>Remove row</MenuItem>
+              </IconMenu>
+            </DataGrid.Cell>
+          </DataGrid.BodyRow>
+        );
+      })
+    }
+  </DataGrid>;
+}
 
 storiesOf('DataGrids', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
@@ -36,46 +91,7 @@ storiesOf('DataGrids', module)
         <Button onClick={handleButtonClick} label="Refresh data" level="primary" />
       </Section>
       <State store={store}>
-        <DataGrid
-          selectable={boolean('Selectable', true)}
-          comparableId={store.get('comparableId')}
-          onSelectionChange={handleRowSelectionChange}
-        >
-          <DataGrid.HeaderRow>
-            <DataGrid.HeaderCell flex="min-width" />
-            <DataGrid.HeaderCell onClick={action('onClick: column sort')} sorted="asc">Invoice</DataGrid.HeaderCell>
-            <DataGrid.HeaderCell onClick={action('onClick: column sort')} align="right">Amount</DataGrid.HeaderCell>
-            <DataGrid.HeaderCell flex="2" onClick={action('onClick: column sort')}>Customer</DataGrid.HeaderCell>
-            <DataGrid.HeaderCell onClick={action('onClick: column sort')}>Due date</DataGrid.HeaderCell>
-            <DataGrid.HeaderCell flex="min-width"/>
-          </DataGrid.HeaderRow>
-          {
-            rows.map((row, index) => {
-              return (
-                <DataGrid.BodyRow key={index}>
-                  <DataGrid.Cell align="center" flex="min-width">
-                    <TooltippedStatusBullet
-                      color={row.column1}
-                      tooltip={<TextTiny>Overdue</TextTiny>}
-                      tooltipColor={row.column1}
-                      tooltipSize="small"
-                      size="large"
-                    />
-                  </DataGrid.Cell>
-                  <DataGrid.Cell><Link href="#" inherit={false}>{row.column5}</Link> </DataGrid.Cell>
-                  <DataGrid.Cell align="right" strong> {`€ ${row.column3}`}</DataGrid.Cell>
-                  <DataGrid.Cell flex="2">{row.column2}</DataGrid.Cell>
-                  <DataGrid.Cell soft>{row.column4}</DataGrid.Cell>
-                  <DataGrid.Cell align="right" flex="min-width" preventOverflow={false}>
-                    <IconMenu position="top-right">
-                      <MenuItem onClick={action('onClick: delete row')}>Remove row</MenuItem>
-                    </IconMenu>
-                  </DataGrid.Cell>
-                </DataGrid.BodyRow>
-              );
-            })
-          }
-        </DataGrid>
+        <BasicDataGrid />
       </State>
     </Box>
   ))
