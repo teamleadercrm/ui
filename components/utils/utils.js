f5539dfccd9e51ae083e2ebe239df2dfb721f528
Dries Decramer <dries.decramer@teamleader.be>
2018-01-24 16:55:45 +0100

    Add util function to detect if an element is overflowing

--- components/utils/utils.js
+++ components/utils/utils.js
@@ -2,3 +2,5 @@ export const getViewport = () => ({
   height: window.innerHeight || document.documentElement.offsetHeight,
   width: window.innerWidth || document.documentElement.offsetWidth,
 });
+
+export const isElementOverflowing = (element) => element.clientWidth < element.scrollWidth || element.clientHeight < element.scrollHeight;
