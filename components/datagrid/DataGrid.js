b02666ba69f01c58bd60582b56a873188f9094a2
Dries Decramer <dries.decramer@teamleader.be>
2018-01-25 10:10:42 +0100

    Prettier Linting™

--- components/datagrid/DataGrid.js
+++ components/datagrid/DataGrid.js
@@ -104,17 +104,15 @@ class DataGrid extends PureComponent {
       const rowDOMNodes = [];
       let maxRowWidth = 0;
 
-      [...this.rowNodes.values()]
-        .filter(rowNode => rowNode != null)
-        .forEach(rowNode => {
-          const rowDOMNode = ReactDOM.findDOMNode(rowNode);
-          const totalRowChildrenWidth = [...rowDOMNode.children]
-            .map(child => child.offsetWidth)
-            .reduce((accumulatedChildWidth, currentChildWidth) => accumulatedChildWidth + currentChildWidth);
+      [...this.rowNodes.values()].filter(rowNode => rowNode != null).forEach(rowNode => {
+        const rowDOMNode = ReactDOM.findDOMNode(rowNode);
+        const totalRowChildrenWidth = [...rowDOMNode.children]
+          .map(child => child.offsetWidth)
+          .reduce((accumulatedChildWidth, currentChildWidth) => accumulatedChildWidth + currentChildWidth);
 
-          maxRowWidth = maxRowWidth < totalRowChildrenWidth ? totalRowChildrenWidth : maxRowWidth;
-          rowDOMNodes.push(rowDOMNode);
-        });
+        maxRowWidth = maxRowWidth < totalRowChildrenWidth ? totalRowChildrenWidth : maxRowWidth;
+        rowDOMNodes.push(rowDOMNode);
+      });
 
       rowDOMNodes.forEach(rowDOMNode => (rowDOMNode.style.minWidth = `${maxRowWidth}px`));
     }
