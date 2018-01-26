beca491e38858917bd5adb554419abcabe539a16
Dries Decramer <dries.decramer@teamleader.be>
2018-01-26 11:11:12 +0100

    Storybook: add stories with Text/Heading components

--- stories/statusBullet.js
+++ stories/statusBullet.js
@@ -3,7 +3,17 @@ import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import styles from '@sambego/storybook-styles';
-import { Box, StatusBullet, TextBody } from '../components';
+import {
+  Box,
+  StatusBullet,
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  TextBody,
+  TextDisplay,
+  TextSmall,
+} from '../components';
 import { baseStyles, centerStyles } from '../.storybook/styles';
 
 const colors = ['mint', 'violet', 'ruby', 'gold', 'aqua', 'neutral'];
@@ -29,5 +39,30 @@ storiesOf('Status Bullets', module)
           <TextBody element="span">{size}</TextBody>
         </StatusBullet>
       ))}
+    </Box>
+  ))
+  .add('with custom labels', () => (
+    <Box>
+      <StatusBullet margin={4}>
+        <Heading1>Heading 1</Heading1>
+      </StatusBullet>
+      <StatusBullet margin={4}>
+        <Heading2>Heading 2</Heading2>
+      </StatusBullet>
+      <StatusBullet size="small" margin={4}>
+        <Heading3>Heading 3</Heading3>
+      </StatusBullet>
+      <StatusBullet size="small" margin={4}>
+        <Heading4>Heading 4</Heading4>
+      </StatusBullet>
+      <StatusBullet size="small" margin={4}>
+        <TextDisplay>Text Display</TextDisplay>
+      </StatusBullet>
+      <StatusBullet size="small" margin={4}>
+        <TextBody>TextBody</TextBody>
+      </StatusBullet>
+      <StatusBullet size="small" margin={4}>
+        <TextSmall>TextSmall</TextSmall>
+      </StatusBullet>
     </Box>
   ));
