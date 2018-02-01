b5ae012bb09ccb0969db2cf7ae209b99a96f6904
Dries Decramer <dries.decramer@teamleader.be>
2018-02-01 12:09:16 +0100

    Update Checkbox stories to use the new centerStyles and not to use Box as wrapper

--- stories/checkbox.js
+++ stories/checkbox.js
@@ -5,9 +5,8 @@ import { withInfo } from '@storybook/addon-info';
 import { withKnobs, boolean } from '@storybook/addon-knobs/react';
 import { action } from '@storybook/addon-actions';
 import { Store, State } from '@sambego/storybook-state';
-import styles from '@sambego/storybook-styles';
-import { Box, Checkbox, Link, TextBody } from '../components';
-import { baseStyles, centerStyles } from '../.storybook/styles';
+import { Checkbox, Link, TextBody } from '../components';
+import { centerStyles } from '../.storybook/styles';
 
 const sizes = ['small', 'medium', 'large'];
 
@@ -24,29 +23,30 @@ storiesOf('Checkboxes', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
   .addDecorator(withKnobs)
-  .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('sizes', () => (
-    <Box>
+    <div style={centerStyles}>
       {sizes.map((size, key) => (
         <State store={store} key={key}>
           <Checkbox size={size} marginVertical={3} onChange={handleOnChange} disabled={boolean('Disabled', false)}/>
         </State>
       ))}
-    </Box>
+    </div>
   ))
   .add('with labels', () => (
-    <Box>
+    <div style={centerStyles}>
       {sizes.map((size, key) => (
         <State store={store} key={key}>
           <Checkbox size={size} marginVertical={3} onChange={handleOnChange} disabled={boolean('Disabled', false)} label={`I'm a ${size} Checkbox`}/>
         </State>
       ))}
-    </Box>
+    </div>
   ))
   .add('with link in label', () => (
-    <State store={store}>
-      <Checkbox marginVertical={3} onChange={handleOnChange} disabled={boolean('Disabled', false)}>
-        <TextBody>I'm a medium label with a <Link href="#" inherit={false}>link</Link> inside</TextBody>
-      </Checkbox>
-    </State>
+    <div style={centerStyles}>
+      <State store={store}>
+        <Checkbox marginVertical={3} onChange={handleOnChange} disabled={boolean('Disabled', false)}>
+          <TextBody>I'm a medium label with a <Link href="#" inherit={false}>link</Link> inside</TextBody>
+        </Checkbox>
+      </State>
+    </div>
   ));
