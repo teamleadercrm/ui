17f93ca6b34081d80d0249d4f0be820fee3d2994
Dries Decramer <dries.decramer@teamleader.be>
2018-02-01 11:51:22 +0100

    Info addon: add basic stylesheet overrides to the config file

--- .storybook/config.js
+++ .storybook/config.js
@@ -10,6 +10,61 @@ setDefaults({
   inline: true,
   source: true,
   propTablesExclude: [State],
+  styles: stylesheet => {
+    stylesheet.infoBody = {
+      color: '#2a3b4d',
+      fontFamily: 'Inter-UI-Regular',
+      fontSize: '14px',
+      margin: '48px auto',
+      maxWidth: '90%',
+      padding: '0',
+    };
+
+    stylesheet.header = {
+      h1: {
+        fontFamily: 'Inter-UI-Medium',
+        fontWeight: 500,
+        margin: 0,
+        fontSize: '24px',
+        lineHeight: '30px',
+      },
+      h2: {
+        color: '#82828c',
+        fontFamily: 'Inter-UI-Medium',
+        margin: '24px 0 10px 0',
+        fontWeight: 500,
+        fontSize: '18px',
+        lineHeight: '24px',
+      },
+      body: {
+        borderBottom: '1px solid #eee',
+        paddingTop: 10,
+        marginBottom: 10,
+      },
+    };
+
+    stylesheet.source = {
+      h1: {
+        color: '#82828c',
+        fontFamily: 'Inter-UI-Medium',
+        margin: '24px 0 10px 0',
+        fontWeight: 500,
+        fontSize: '18px',
+        lineHeight: '24px',
+        borderBottom: '1px solid #eee',
+      },
+    };
+
+    stylesheet.propTableHead = {
+      fontFamily: 'Inter-UI-Medium',
+      fontWeight: 500,
+      fontSize: '16px',
+      lineHeight: '21px',
+      margin: '20px 0 0 0',
+    };
+
+    return stylesheet;
+  }
 });
 
 setOptions({
