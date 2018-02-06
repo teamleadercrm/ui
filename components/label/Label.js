e67da801354a12d87576d6fa550c6627e964370f
Dries Decramer <dries.decramer@teamleader.be>
2018-02-06 13:45:58 +0100

    Implement 'reverse' prop in Label component

--- components/label/Label.js
+++ components/label/Label.js
@@ -2,25 +2,36 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { TextBody, TextDisplay } from '../typography';
 import theme from './theme.css';
+import cx from 'classnames';
 
 export default class Label extends PureComponent {
   static propTypes = {
     children: PropTypes.oneOfType([PropTypes.element, PropTypes.string, PropTypes.array]),
     for: PropTypes.string,
+    inverse: PropTypes.bool,
     size: PropTypes.oneOf(['small', 'medium']),
   };
 
   static defaultProps = {
+    inverse: false,
     size: 'medium',
   };
 
   render() {
-    const { children, size } = this.props;
-
+    const { children, inverse, size } = this.props;
+    const classNames = cx(theme['label'], {
+      [theme['is-inverse']]: inverse,
+    });
     const Element = size === 'medium' ? TextDisplay : TextBody;
 
     return (
-      <Element element="label" htmlFor={this.props.for} marginBottom={3} className={theme['label']}>
+      <Element
+        color={inverse ? 'white' : 'teal'}
+        element="label"
+        htmlFor={this.props.for}
+        marginBottom={3}
+        className={classNames}
+      >
         {children}
       </Element>
     );
