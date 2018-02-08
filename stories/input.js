3e42acba33b5404b8981ad63f82b1f1d2d5fa260
Dries Decramer <dries.decramer@teamleader.be>
2018-02-08 11:34:13 +0100

    Storybook: update stories

--- stories/input.js
+++ stories/input.js
@@ -7,12 +7,13 @@ import { withKnobs, boolean, select } from "@storybook/addon-knobs/react";
 import styles from '@sambego/storybook-styles';
 import { baseStyles, centerStyles } from '../.storybook/styles';
 import { Input, Label } from '../components';
-import { IconCalendarSmallOutline } from '@teamleader/ui-icons';
+import { IconCalendarMediumOutline, IconCalendarSmallOutline } from '@teamleader/ui-icons';
 
 const iconPlacement = ['left', 'right'];
 const sizes = ['small', 'medium', 'large'];
 
 const props = {
+  helpText: 'This is the fields help text',
   onBlur: action('blur'),
   onChange: action('change'),
   onFocus: action('focus'),
@@ -24,22 +25,30 @@ storiesOf('Inputs', module)
   .addDecorator(checkA11y)
   .addDecorator(withKnobs)
   .addDecorator(styles({ ...baseStyles, ...centerStyles }))
-  .add('text', () => (
-    <Label
-      for="input1"
-      inverse={boolean('Inverse', false)}
-      size={select('Size', sizes, 'medium')}
-    >
-      Input label
-      <Input
-        id="input1"
-        bold={boolean('Bold', false)}
-        disabled={boolean('Disabled', false)}
-        readOnly={boolean('Read only', false)}
-        {...props}
-      />
-    </Label>
-  ))
+  .add('input only', () => (
+    <Input
+      id="input1"
+      bold={boolean('Bold', false)}
+      disabled={boolean('Disabled', false)}
+      readOnly={boolean('Read only', false)}
+      {...props}
+    />
+  )).add('text', () => (
+  <Label
+    for="input1"
+    inverse={boolean('Inverse', false)}
+    size={select('Size', sizes, 'medium')}
+  >
+    Input label
+    <Input
+      id="input1"
+      bold={boolean('Bold', false)}
+      disabled={boolean('Disabled', false)}
+      readOnly={boolean('Read only', false)}
+      {...props}
+    />
+  </Label>
+))
   .add('password', () => (
     <Label
       for="input1"
@@ -70,6 +79,7 @@ storiesOf('Inputs', module)
         bold={boolean('Bold', false)}
         disabled={boolean('Disabled', false)}
         readOnly={boolean('Read only', false)}
+        meta={{ error: 'This is an error message' }}
         {...props}
       />
     </Label>
@@ -82,7 +92,7 @@ storiesOf('Inputs', module)
     >
       Input label
       <Input
-        icon={IconCalendarSmallOutline}
+        icon={select('Size', sizes, 'medium') === 'large' ? IconCalendarMediumOutline : IconCalendarSmallOutline}
         iconPlacement={select('Icon placement', iconPlacement, 'left')}
         id="input1"
         bold={boolean('Bold', false)}
