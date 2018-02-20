6b8f8deb28e4767cdd9f3c3726d5e9183f2d49cf
Dries Decramer <dries.decramer@teamleader.be>
2018-02-20 16:59:29 +0100

    Remove label prop and make children required

--- components/tag/Tag.js
+++ components/tag/Tag.js
@@ -8,10 +8,9 @@ import { IconCloseMediumOutline, IconCloseSmallOutline } from '@teamleader/ui-ic
 
 class Tag extends PureComponent {
   static propTypes = {
-    children: PropTypes.any,
+    children: PropTypes.any.isRequired,
     className: PropTypes.string,
     inverse: PropTypes.bool,
-    label: PropTypes.string,
     onLabelClick: PropTypes.func,
     onRemoveClick: PropTypes.func,
     size: PropTypes.oneOf(['small', 'medium', 'large']),
@@ -23,7 +22,7 @@ class Tag extends PureComponent {
   };
 
   render() {
-    const { children, className, inverse, label, onLabelClick, onRemoveClick, size, ...others } = this.props;
+    const { children, className, inverse, onLabelClick, onRemoveClick, size, ...others } = this.props;
 
     const classNames = cx(
       theme['tag'],
@@ -42,12 +41,10 @@ class Tag extends PureComponent {
       <Box className={classNames} data-teamleader-ui="tag" {...others}>
         {onLabelClick ? (
           <Button className={theme['label-button']} onClick={onLabelClick} level="outline" inverse={inverse}>
-            {label}
             {children}
           </Button>
         ) : (
           <span className={theme['label']}>
-            {label}
             {children}
           </span>
         )}
