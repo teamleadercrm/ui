040cf37d9a6fef94f6992b24fdc028d8392849d9
Dries Decramer <dries.decramer@teamleader.be>
2018-02-21 09:28:40 +0100

    Storybook: update stories and integrate with Knobs

--- stories/bullet.js
+++ stories/bullet.js
@@ -2,24 +2,35 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
+import { withKnobs, select } from "@storybook/addon-knobs/react";
 import styles from '@sambego/storybook-styles';
-import { Box, Bullet } from '../components';
+import { Box, Bullet, TextBody, Tooltip } from '../components';
 import { baseStyles, centerStyles } from '../.storybook/styles';
 
 const colors = ['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby'];
 const sizes = ['small', 'medium'];
 
+const TooltippedBullet = Tooltip(Bullet);
+
 storiesOf('Bullets', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
+  .addDecorator(withKnobs)
   .addDecorator(styles({ ...baseStyles, ...centerStyles }))
-  .add('colors', () => (
+  .add('basic', () => (
     <Box>
-      {colors.map((color, index) => <Bullet color={color} key={index} marginHorizontal={3} />)}
+      <Bullet
+        color={select('Color', colors, 'neutral')}
+        size={select('Size', sizes, 'medium')}
+      />
     </Box>
   ))
-  .add('sizes', () => (
+  .add('with tooltip', () => (
     <Box>
-      {sizes.map((size, index) => <Bullet key={index} color="ruby" size={size} marginHorizontal={3} />)}
+      <TooltippedBullet
+        color={select('Color', colors, 'ruby')}
+        size={select('Size', sizes, 'medium')}
+        tooltip={<TextBody>I am the tooltip</TextBody>}
+      />
     </Box>
   ));
