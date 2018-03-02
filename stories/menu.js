9ad33f77482f533b1eb827786cc3edea01c3c0a9
Dries Decramer <dries.decramer@teamleader.be>
2018-03-02 15:42:34 +0100

    Menu: remove centerStyles because we moved this to a higher level

--- stories/menu.js
+++ stories/menu.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import styles from '@sambego/storybook-styles';
 import { IconAddSmallOutline, IconUserSmallFilled, IconClockSmallOutline } from '@teamleader/ui-icons';
 import { IconMenu, Menu, MenuItem, MenuDivider } from '../components';
-import { baseStyles, centerStyles } from '../.storybook/styles';
 
 storiesOf('Menus', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('Menu', () => (
     <Menu selectable={false}>
       <MenuItem value="foo" caption="Caption" />
