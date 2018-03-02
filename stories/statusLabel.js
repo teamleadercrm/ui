1d7a53fbfb6049f7ca21d92af5974c60db008283
Dries Decramer <dries.decramer@teamleader.be>
2018-03-02 15:52:34 +0100

    StatusLabel: remove centerStyles because we moved this to a higher level

--- stories/statusLabel.js
+++ stories/statusLabel.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import styles from '@sambego/storybook-styles';
 import { Box, StatusLabel } from '../components';
-import { baseStyles, centerStyles } from '../.storybook/styles';
 
 const colors = ['mint', 'violet', 'ruby', 'gold', 'aqua', 'neutral'];
 const sizes = ['small', 'medium'];
@@ -12,7 +10,6 @@ const sizes = ['small', 'medium'];
 storiesOf('Status Labels', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('colors', () => (
     <Box>
       {colors.map((color, key) => (
