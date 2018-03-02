563fc2bd0fd76ac4b6ddc5deef0c2a520b704256
Dries Decramer <dries.decramer@teamleader.be>
2018-03-02 15:04:08 +0100

    Counter: remove centerStyles because we moved this to a higher level

--- stories/counter.js
+++ stories/counter.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import styles from '@sambego/storybook-styles';
 import { Box, Counter, Tooltip, TextBody } from '../components';
-import { baseStyles, centerStyles } from '../.storybook/styles';
 
 const colors = ['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby'];
 const sizes = ['small', 'medium'];
@@ -14,7 +12,6 @@ const TooltippedCounter = Tooltip(Counter);
 storiesOf('Counters', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('colors', () => (
     <Box>
       {colors.map((color, index) => <Counter count={99} color={color} key={index} marginHorizontal={3} />)}
