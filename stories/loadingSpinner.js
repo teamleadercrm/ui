babac5859772707380ca5ffc1558080071c6d887
Dries Decramer <dries.decramer@teamleader.be>
2018-03-02 15:37:41 +0100

    LoadingSpinner: remove centerStyles because we moved this to a higher level

--- stories/loadingSpinner.js
+++ stories/loadingSpinner.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import styles from '@sambego/storybook-styles';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { Box, LoadingSpinner } from '../components';
-import { baseStyles, centerStyles } from '../.storybook/styles';
 
 const colors = ['teal', 'white'];
 const sizes = ['small', 'medium'];
@@ -12,7 +10,6 @@ const sizes = ['small', 'medium'];
 storiesOf('Loading spinners', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('sizes', () => (
     <Box padding={5}>
       {sizes.map((size, index) => <LoadingSpinner key={index} size={size} marginHorizontal={3} />)}
