4408bb1d5c41749e06332e0fd434d2c6cc0e549f
Dries Decramer <dries.decramer@teamleader.be>
2018-03-02 15:46:04 +0100

    Popover: remove centerStyles because we moved this to a higher level

--- stories/popover.js
+++ stories/popover.js
@@ -4,8 +4,6 @@ import { Store, State } from '@sambego/storybook-state';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { Banner, Box, Button, ButtonGroup, Heading3, Link, PopoverHorizontal, PopoverVertical, Section, TextBody, TextSmall } from '../components';
-import { baseStyles, centerStyles } from '../.storybook/styles';
-import styles from '@sambego/storybook-styles';
 
 const store = new Store({
   active: false,
@@ -28,7 +26,6 @@ const contentBoxWithSingleTextLine = (
 storiesOf('Popover', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('horizontal', () => (
     <Box>
       <Button onClick={handleButtonClick} label="Open a horizontalPopover" />
