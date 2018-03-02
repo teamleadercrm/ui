2be459b25f429662016c3563d17f2ecac3df4e5f
Dries Decramer <dries.decramer@teamleader.be>
2018-03-02 14:55:35 +0100

    Bullet: remove centerStyles because we moved this to a higher level

--- stories/bullet.js
+++ stories/bullet.js
@@ -3,9 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, select } from "@storybook/addon-knobs/react";
-import styles from '@sambego/storybook-styles';
 import { Box, Bullet, TextBody, Tooltip } from '../components';
-import { baseStyles, centerStyles } from '../.storybook/styles';
 
 const colors = ['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby'];
 const sizes = ['small', 'medium'];
@@ -16,7 +14,6 @@ storiesOf('Bullets', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
   .addDecorator(withKnobs)
-  .addDecorator(styles({ ...baseStyles, ...centerStyles }))
   .add('basic', () => (
     <Box>
       <Bullet
