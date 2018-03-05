6a5c06e77b896397cb63bd7054364feca77a4064
Dries Decramer <dries.decramer@teamleader.be>
2018-03-05 16:25:22 +0100

    LoadingMolecule: implement Knobs and simplify stories

--- stories/loadingMolecule.js
+++ stories/loadingMolecule.js
@@ -2,28 +2,22 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import { Box, LoadingMolecule } from '../components';
+import { withKnobs, select } from "@storybook/addon-knobs/react";
+import { LoadingMolecule } from '../components';
+
+const sizes = ['small', 'large'];
 
 storiesOf('Loading molecules', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .add('color', () => (
-    <Box style={{ height: '300px', position: 'relative' }}>
-      <LoadingMolecule basePath="" startColor="#00ACA9" stopColor="#1F7F79" />
-    </Box>
+  .addDecorator(withKnobs)
+  .add('Color', () => (
+    <div style={{ height: '300px', position: 'relative' }}>
+      <LoadingMolecule basePath="" startColor="#00ACA9" stopColor="#1F7F79" type={select('Type', sizes, 'large')} />
+    </div>
   ))
-  .add('grayscale', () => (
-    <Box style={{ height: '300px', position: 'relative' }}>
-      <LoadingMolecule basePath="" startColor="#BABABA" stopColor="#DADADA" />
-    </Box>
-  ))
-  .add('small', () => (
-    <Box style={{ height: '300px', position: 'relative' }}>
-      <LoadingMolecule basePath="" startColor="#00ACA9" stopColor="#1F7F79" type="small" />
-    </Box>
-  ))
-  .add('large', () => (
-    <Box style={{ height: '300px', position: 'relative' }}>
-      <LoadingMolecule basePath="" startColor="#00ACA9" stopColor="#1F7F79" type="large" />
-    </Box>
+  .add('Grayscale', () => (
+    <div style={{ height: '300px', position: 'relative' }}>
+      <LoadingMolecule basePath="" startColor="#BABABA" stopColor="#DADADA" type={select('Type', sizes, 'large')} />
+    </div>
   ));
