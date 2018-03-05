2437c1a5b68d61e55f393307eb65954082ac0a9d
Dries Decramer <dries.decramer@teamleader.be>
2018-03-05 18:03:46 +0100

    StatusLabel: implement Knobs and simplify stories

--- stories/statusLabel.js
+++ stories/statusLabel.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import { Box, StatusLabel } from '../components';
+import { withKnobs, select } from '@storybook/addon-knobs/react';
+import { StatusLabel } from '../components';
 
 const colors = ['mint', 'violet', 'ruby', 'gold', 'aqua', 'neutral'];
 const sizes = ['small', 'medium'];
@@ -10,21 +11,12 @@ const sizes = ['small', 'medium'];
 storiesOf('Status Labels', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .add('colors', () => (
-    <Box>
-      {colors.map((color, key) => (
-        <StatusLabel color={color} key={key} marginHorizontal={1}>
-          {color}
-        </StatusLabel>
-      ))}
-    </Box>
-  ))
-  .add('sizes', () => (
-    <Box>
-      {sizes.map((size, key) => (
-        <StatusLabel size={size} key={key} marginHorizontal={1}>
-          {size}
-        </StatusLabel>
-      ))}
-    </Box>
+  .addDecorator(withKnobs)
+  .add('Basic', () => (
+    <StatusLabel
+      color={select('Color', colors, 'neutral')}
+      size={select('Size', sizes, 'medium')}
+    >
+      label
+    </StatusLabel>
   ));
