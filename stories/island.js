6afdebeeb0b72bcbf30027c51be08c782ebac7c1
Dries Decramer <dries.decramer@teamleader.be>
2018-03-05 15:51:22 +0100

    Island: implement Knobs and simplify stories

--- stories/island.js
+++ stories/island.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import { Box, Island, TextBody } from '../components';
+import { withKnobs, boolean, select } from '@storybook/addon-knobs/react';
+import { Island, TextBody } from '../components';
 
 const colors = ['neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua', 'white'];
 const sizes = ['small', 'medium', 'large'];
@@ -10,24 +11,13 @@ const sizes = ['small', 'medium', 'large'];
 storiesOf('Island', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .add('colors', () => (
-    <Box padding={5}>
-      {colors.map((color, index) => (
-        <Island key={index} color={color} marginTop={4}>
-          <TextBody>I am a {color} island.</TextBody>
-        </Island>
-      ))}
-      <Island marginTop={4} dark>
-        <TextBody>I am a dark island.</TextBody>
-      </Island>
-    </Box>
-  ))
-  .add('sizes', () => (
-    <Box padding={5}>
-      {sizes.map((size, index) => (
-        <Island key={index} size={size} marginTop={4}>
-          <TextBody>I am a {size} island.</TextBody>
-        </Island>
-      ))}
-    </Box>
+  .addDecorator(withKnobs)
+  .add('Basic', () => (
+    <Island
+      color={select('Color', colors, 'white')}
+      dark={boolean('Dark', false)}
+      size={select('Size', sizes, 'medium')}
+    >
+      <TextBody>I am an island.</TextBody>
+    </Island>
   ));
