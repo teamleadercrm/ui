c00097dc1ea9491366b1cbd0b70cd5648ea88743
Dries Decramer <dries.decramer@teamleader.be>
2018-03-05 18:17:18 +0100

    Toggle: implement Knobs and simplify stories

--- stories/toggle.js
+++ stories/toggle.js
@@ -1,51 +1,28 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
-import { withKnobs, boolean } from '@storybook/addon-knobs/react';
+import { withKnobs, boolean, select } from '@storybook/addon-knobs/react';
 import { withInfo } from '@storybook/addon-info';
-import { Store, State } from '@sambego/storybook-state';
-import { Box, Toggle } from '../components';
+import { Toggle } from '../components';
 
 const sizes = ['small', 'medium', 'large'];
-
-const store = new Store({
-  checked: false,
-});
-
-const handleOnChange = checked => {
-  store.set({ checked });
-};
 
 storiesOf('Toggles', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
   .addDecorator(withKnobs)
-  .add('sizes', () => (
-    <Box>
-      {sizes.map((size, key) => (
-        <State store={store} key={key}>
-          <Toggle
-            size={size}
-            marginVertical={3}
-            onChange={handleOnChange}
-            disabled={boolean('Disabled', false)}
-          />
-        </State>
-      ))}
-    </Box>
+  .add('Basic', () => (
+    <Toggle
+      checked={boolean('Checked', false)}
+      disabled={boolean('Disabled', false)}
+      size={select('Size', sizes, 'medium')}
+    />
   ))
-  .add('labels', () => (
-    <Box>
-      {sizes.map((size, key) => (
-        <State store={store} key={key}>
-          <Toggle
-            size={size}
-            marginVertical={3}
-            onChange={handleOnChange}
-            label={`I'm a ${size} toggle`}
-            disabled={boolean('Disabled', false)}
-          />
-        </State>
-      ))}
-    </Box>
+  .add('With labels', () => (
+    <Toggle
+      checked={boolean('Checked', false)}
+      disabled={boolean('Disabled', false)}
+      label={`I'm a toggle`}
+      size={select('Size', sizes, 'medium')}
+    />
   ));
