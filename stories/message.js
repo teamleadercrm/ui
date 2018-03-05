0b76c7cb5aaab7583aaed4fd11c8f0516115780a
Dries Decramer <dries.decramer@teamleader.be>
2018-03-05 16:57:19 +0100

    Message: implement Knobs and simplify stories

--- stories/message.js
+++ stories/message.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import { Banner, Box, Button, Message, CompactMessage, Island, Heading2, Link, TextBody, TextSmall } from '../components';
+import { withKnobs, select } from '@storybook/addon-knobs/react';
+import { Button, Message, CompactMessage, Island, Heading2, Link, TextBody, TextSmall } from '../components';
 import { IllustrationInvoices120X120Static, IllustrationSharpie48X48Static } from '@teamleader/ui-illustrations';
+
+const imagePositions = ['left', 'center', 'right'];
 
 const button = <Button level="primary">Button text</Button>;
 const link = <TextSmall><Link href="https://teamleader.eu" inherit={false}>I'm a link</Link></TextSmall>;
@@ -19,41 +22,36 @@ const content = (
 storiesOf('Message', module)
   .addDecorator((story, context) => withInfo('common info')(story)(context))
   .addDecorator(checkA11y)
-  .add('image positioning', () => (
-    <Box padding={5}>
-      <div>
-        <Island marginBottom={2}>
-          <Message button={button} link={link}>
-            {content}
-          </Message>
-        </Island>
-        <Island marginBottom={2}>
-          <Message button={button} link={link} image={image} imagePositioning="left">
-            {content}
-          </Message>
-        </Island>
-        <Island marginBottom={2}>
-          <Message  button={button} link={link} image={image} imagePositioning="right">
-            {content}
-          </Message>
-        </Island>
-        <Island marginBottom={2}>
-          <Message button={button} link={link} image={image} imagePositioning="center">
-            {content}
-          </Message>
-        </Island>
-      </div>
-    </Box>
+  .addDecorator(withKnobs)
+  .add('Basic', () => (
+    <Island>
+      <Message
+        button={button}
+        link={link}
+      >
+        {content}
+      </Message>
+    </Island>
   ))
-  .add('compact', () => (
-    <Box padding={6}>
-      <div>
-        <Island marginBottom={2}>
-          <CompactMessage button={button} image={imageSmall}>
-            <Heading2 color="teal">Compact version</Heading2>
-            <TextBody color="teal" marginVertical={2}>I don't have a lot to say so I'll just leave it there.</TextBody>
-          </CompactMessage>
-        </Island>
-      </div>
-    </Box>
+  .add('Compact', () => (
+    <Island>
+      <CompactMessage
+        button={button}
+        image={imageSmall}
+      >
+        {content}
+      </CompactMessage>
+    </Island>
+  ))
+  .add('With image', () => (
+    <Island>
+      <Message
+        button={button}
+        link={link}
+        image={image}
+        imagePositioning={select('Image positioning', imagePositions, 'left')}
+      >
+        {content}
+      </Message>
+    </Island>
   ));
