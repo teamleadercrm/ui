439331069c99797845217c4da0cb2e5b40f2a419
Dries Decramer <dries.decramer@teamleader.be>
2018-03-07 16:52:33 +0100

    MenuItem: eliminate factory

--- components/menu/MenuItem.js
+++ components/menu/MenuItem.js
@@ -3,61 +3,54 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import theme from './theme.css';
 
-const factory = () => {
-  class MenuItem extends PureComponent {
-    static propTypes = {
-      caption: PropTypes.string,
-      children: PropTypes.any,
-      className: PropTypes.string,
-      disabled: PropTypes.bool,
-      icon: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-      onClick: PropTypes.func,
-      selected: PropTypes.bool,
-      shortcut: PropTypes.string,
-    };
+class MenuItem extends PureComponent {
+  static propTypes = {
+    caption: PropTypes.string,
+    children: PropTypes.any,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    icon: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+    onClick: PropTypes.func,
+    selected: PropTypes.bool,
+    shortcut: PropTypes.string,
+  };
 
-    static defaultProps = {
-      className: '',
-      disabled: false,
-      selected: false,
-    };
+  static defaultProps = {
+    className: '',
+    disabled: false,
+    selected: false,
+  };
 
-    handleClick = event => {
-      const { disabled, onClick } = this.props;
+  handleClick = event => {
+    const { disabled, onClick } = this.props;
 
-      if (onClick && !disabled) {
-        onClick(event, this);
-      }
-    };
-
-    render() {
-      const { icon, caption, children, className, shortcut, selected, disabled, ...others } = this.props;
-
-      const classNames = cx(
-        theme['menu-item'],
-        theme['text-small'],
-        {
-          [theme['selected']]: selected,
-          [theme['disabled']]: disabled,
-        },
-        className,
-      );
-
-      return (
-        <li {...others} data-teamleader-ui="menu-item" className={classNames} onClick={this.handleClick}>
-          {icon && <span className={theme['icon']}>{icon}</span>}
-          <span className={theme['caption']}>{caption}</span>
-          {shortcut && <small className={theme['shortcut']}>{shortcut}</small>}
-          {children}
-        </li>
-      );
+    if (onClick && !disabled) {
+      onClick(event, this);
     }
+  };
+
+  render() {
+    const { icon, caption, children, className, shortcut, selected, disabled, ...others } = this.props;
+
+    const classNames = cx(
+      theme['menu-item'],
+      theme['text-small'],
+      {
+        [theme['selected']]: selected,
+        [theme['disabled']]: disabled,
+      },
+      className,
+    );
+
+    return (
+      <li {...others} data-teamleader-ui="menu-item" className={classNames} onClick={this.handleClick}>
+        {icon && <span className={theme['icon']}>{icon}</span>}
+        <span className={theme['caption']}>{caption}</span>
+        {shortcut && <small className={theme['shortcut']}>{shortcut}</small>}
+        {children}
+      </li>
+    );
   }
-
-  return MenuItem;
-};
-
-const MenuItem = factory();
+}
 
 export default MenuItem;
-export { factory as menuItemFactory, MenuItem };
--- components/menu/index.js
+++ components/menu/index.js
@@ -1,10 +1,9 @@
 import { IconButton } from '../button';
 import { MenuDivider } from './MenuDivider';
-import { menuItemFactory } from './MenuItem';
+import MenuItem from './MenuItem';
 import Menu from './Menu';
 import { iconMenuFactory } from './IconMenu';
 
-const MenuItem = menuItemFactory();
 const IconMenu = iconMenuFactory(IconButton, Menu);
 
 export default Menu;
