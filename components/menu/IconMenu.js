39374862557394c801595d8606b5c09ba5ebb5a0
Dries Decramer <dries.decramer@teamleader.be>
2018-03-08 09:53:17 +0100

    IconMenu: eliminate factory

--- components/menu/IconMenu.js
+++ components/menu/IconMenu.js
@@ -2,88 +2,81 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { IconMoreMediumOutline } from '@teamleader/ui-icons';
-import InjectIconButton from '../button/IconButton.js';
-import InjectMenu from './Menu.js';
+import IconButton from '../button/IconButton.js';
+import Menu from './Menu.js';
 import theme from './theme.css';
 
-const factory = (IconButton, Menu) => {
-  class IconMenu extends PureComponent {
-    static propTypes = {
-      children: PropTypes.node,
-      className: PropTypes.string,
-      icon: PropTypes.element,
-      onClick: PropTypes.func,
-      onHide: PropTypes.func,
-      onSelect: PropTypes.func,
-      onShow: PropTypes.func,
-      position: PropTypes.string,
-      selectable: PropTypes.bool,
-      selected: PropTypes.any,
-    };
+class IconMenu extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    icon: PropTypes.element,
+    onClick: PropTypes.func,
+    onHide: PropTypes.func,
+    onSelect: PropTypes.func,
+    onShow: PropTypes.func,
+    position: PropTypes.string,
+    selectable: PropTypes.bool,
+    selected: PropTypes.any,
+  };
 
-    static defaultProps = {
-      className: '',
-      position: 'auto',
-      selectable: false,
-    };
+  static defaultProps = {
+    className: '',
+    position: 'auto',
+    selectable: false,
+  };
 
-    state = {
-      active: false,
-    };
+  state = {
+    active: false,
+  };
 
-    handleButtonClick = event => {
-      this.setState({ active: !this.state.active });
-      if (this.props.onClick) {
-        this.props.onClick(event);
-      }
-    };
-
-    handleMenuHide = () => {
-      this.setState({ active: false });
-      if (this.props.onHide) {
-        this.props.onHide();
-      }
-    };
-
-    render() {
-      const {
-        children,
-        className,
-        icon,
-        onHide, // eslint-disable-line
-        onSelect,
-        onShow,
-        position,
-        selectable,
-        selected,
-        ...other
-      } = this.props;
-
-      const buttonIcon = icon || <IconMoreMediumOutline />;
-
-      return (
-        <div data-teamleader-ui="icon-menu" {...other} className={cx(theme['icon-menu'], className)}>
-          <IconButton className={theme['icon']} icon={buttonIcon} onClick={this.handleButtonClick} />
-          <Menu
-            active={this.state.active}
-            onHide={this.handleMenuHide}
-            onSelect={onSelect}
-            onShow={onShow}
-            position={position}
-            selectable={selectable}
-            selected={selected}
-          >
-            {children}
-          </Menu>
-        </div>
-      );
+  handleButtonClick = event => {
+    this.setState({ active: !this.state.active });
+    if (this.props.onClick) {
+      this.props.onClick(event);
     }
+  };
+
+  handleMenuHide = () => {
+    this.setState({ active: false });
+    if (this.props.onHide) {
+      this.props.onHide();
+    }
+  };
+
+  render() {
+    const {
+      children,
+      className,
+      icon,
+      onHide, // eslint-disable-line
+      onSelect,
+      onShow,
+      position,
+      selectable,
+      selected,
+      ...other
+    } = this.props;
+
+    const buttonIcon = icon || <IconMoreMediumOutline />;
+
+    return (
+      <div data-teamleader-ui="icon-menu" {...other} className={cx(theme['icon-menu'], className)}>
+        <IconButton className={theme['icon']} icon={buttonIcon} onClick={this.handleButtonClick} />
+        <Menu
+          active={this.state.active}
+          onHide={this.handleMenuHide}
+          onSelect={onSelect}
+          onShow={onShow}
+          position={position}
+          selectable={selectable}
+          selected={selected}
+        >
+          {children}
+        </Menu>
+      </div>
+    );
   }
-
-  return IconMenu;
-};
-
-const IconMenu = factory(InjectIconButton, InjectMenu);
+}
 
 export default IconMenu;
-export { factory as iconMenuFactory, IconMenu };
--- components/menu/index.js
+++ components/menu/index.js
@@ -1,10 +1,7 @@
-import { IconButton } from '../button';
 import { MenuDivider } from './MenuDivider';
 import MenuItem from './MenuItem';
 import Menu from './Menu';
-import { iconMenuFactory } from './IconMenu';
-
-const IconMenu = iconMenuFactory(IconButton, Menu);
+import IconMenu from './IconMenu';
 
 export default Menu;
 export { MenuDivider, MenuItem, Menu, IconMenu };
