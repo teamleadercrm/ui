9f17ba33cd0198b70787258afe0012f977704e72
Dries Decramer <dries.decramer@teamleader.be>
2018-03-14 16:22:06 +0100

    Storybook: optimize DatePicker stories since we've updated @sambego/storybook-state

--- stories/datePicker.js
+++ stories/datePicker.js
@@ -14,58 +14,121 @@ const basicStore = new Store({
   selectedDays: undefined,
 });
 
-const rangeStore = new Store({
-  modifiers: {
-    from: undefined,
-    to: undefined,
-  },
-  selectedDays: [
-    undefined,
-    {
-      from: undefined,
-      to: undefined,
-    }
-  ],
+const rangeClickStore = new Store({
+  from: undefined,
+  to: undefined,
 });
 
-const handleBasicDayClick = (day) => {
-  basicStore.set({ selectedDays: day });
-};
-
-const handleRangeDayClick = (day) => {
-  const [, stateRange ] = rangeStore.get('selectedDays');
-  const range = DateUtils.addDayToRange(day, stateRange);
-
-  rangeStore.set({
-    modifiers: {...range},
-    selectedDays: [day, range]
-  });
-};
+const rangeMouseEnterStore = new Store({
+  from: null,
+  enteredTo: null,
+  to: null,
+});
 
 storiesOf('DatePicker', module)
   .addDecorator((story, context) => withInfo({TableComponent: PropTable})(story)(context))
   .addDecorator(checkA11y)
   .addDecorator(withKnobs)
-  .add('Basic', () => (
-    <State store={basicStore}>
-      <DatePicker
-        numberOfMonths={number('Number of months', 1)}
-        onDayClick={handleBasicDayClick}
-        showOutsideDays={boolean('Show outside days', true)}
-        showWeekNumbers={boolean('Show week numbers', true)}
-        size={select('Size', sizes, 'medium')}
-      />
-    </State>
-  ))
-  .add('Date range', () => (
-    <State store={rangeStore}>
-      <DatePicker
-        range={boolean('Soft highlight range', true)}
-        numberOfMonths={number('Number of months', 2)}
-        onDayClick={handleRangeDayClick}
-        showOutsideDays={boolean('Show outside days', true)}
-        showWeekNumbers={boolean('Show week numbers', true)}
-        size={select('Size', sizes, 'medium')}
-      />
-    </State>
-  ));
+  .add('Basic', () => {
+    const handleDayClick = (day) => {
+      basicStore.set({ selectedDays: day });
+    };
+
+    return (
+      <State store={basicStore}>
+        <DatePicker
+          numberOfMonths={number('Number of months', 1)}
+          onDayClick={handleDayClick}
+          showOutsideDays={boolean('Show outside days', true)}
+          showWeekNumbers={boolean('Show week numbers', true)}
+          size={select('Size', sizes, 'medium')}
+        />
+      </State>
+    )
+  })
+  .add('Range onClick', () => {
+    const handleDayClick = (day) => {
+      const from = rangeClickStore.get('from');
+      const to = rangeClickStore.get('to');
+      const range = DateUtils.addDayToRange(day, {from, to});
+
+      rangeClickStore.set({...range});
+    };
+
+    const parseState = (state) => {
+      return {
+        ...state,
+        modifiers: {from: state.from, to: state.to},
+        selectedDays: [state.from, {from: state.from, to: state.to}],
+      };
+    };
+
+    return (
+      <State store={rangeClickStore} parseState={parseState}>
+        <DatePicker
+          numberOfMonths={number('Number of months', 2)}
+          onDayClick={handleDayClick}
+          showOutsideDays={boolean('Show outside days', true)}
+          showWeekNumbers={boolean('Show week numbers', true)}
+          size={select('Size', sizes, 'medium')}
+        />
+      </State>
+    )
+  })
+  .add('Range onMouseEnter', () => {
+    const handleDayClick = (day) => {
+      const from = rangeMouseEnterStore.get('from');
+      const to = rangeMouseEnterStore.get('to');
+
+      if (from && to && day >= from && day <= to) {
+        resetState();
+        return;
+      }
+
+      if (isSelectingFirstDay(from, to, day)) {
+        rangeMouseEnterStore.set({from: day, to: null, enteredTo: null });
+      } else {
+        rangeMouseEnterStore.set({from, to: day, enteredTo: day });
+      }
+    };
+
+    const handleDayMouseEnter = (day) => {
+      const from = rangeMouseEnterStore.get('from');
+      const to = rangeMouseEnterStore.get('to');
+
+      if (!isSelectingFirstDay(from, to, day)) {
+        rangeMouseEnterStore.set({ from, to, enteredTo: day });
+      }
+    };
+
+    const isSelectingFirstDay = (from, to, day) => {
+      const isBeforeFirstDay = from && DateUtils.isDayBefore(day, from);
+      const isRangeSelected = from && to;
+      return !from || isBeforeFirstDay || isRangeSelected;
+    };
+
+    const parseState = (state) => {
+      return {
+        ...state,
+        modifiers: {from: state.from, to: state.enteredTo},
+        selectedDays: [state.from, {from: state.from, to: state.enteredTo}],
+      };
+    };
+
+    const resetState = () => {
+      rangeMouseEnterStore.set({ from: null, enteredTo: null, to: null });
+    };
+
+    return (
+      <State store={rangeMouseEnterStore} parseState={parseState}>
+        <DatePicker
+          numberOfMonths={number('Number of months', 2)}
+          onDayClick={handleDayClick}
+          onDayMouseEnter={handleDayMouseEnter}
+          showOutsideDays={boolean('Show outside days', true)}
+          showWeekNumbers={boolean('Show week numbers', true)}
+          size={select('Size', sizes, 'medium')}
+        />
+      </State>
+    )
+  });
