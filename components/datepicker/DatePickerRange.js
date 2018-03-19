8d41ba014fca099587661376c852c6e54581d695
Dries Decramer <dries.decramer@teamleader.be>
2018-03-19 13:55:39 +0100

    Add first version of the DatePickerRange component

---
+++ components/datepicker/DatePickerRange.js
@@ -0,0 +1,71 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import DayPicker from 'react-day-picker';
+import NavigationBar from './NavigationBar';
+import WeekDay from './WeekDay';
+import { convertModifiersToClassnames, hasRange } from './utils';
+import { DateUtils } from 'react-day-picker/lib/src';
+import cx from 'classnames';
+import theme from './theme.css';
+
+class DatePickerRange extends PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    modifiers: PropTypes.object,
+    selectedDays: PropTypes.oneOfType([PropTypes.object, PropTypes.func, PropTypes.array]),
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+  };
+
+  static defaultProps = {
+    size: 'medium',
+  };
+
+  constructor() {
+    super(...arguments);
+
+    this.handleDayClick = ::this.handleDayClick;
+
+    this.state = {
+      from: null,
+      to: null,
+    };
+  }
+
+  handleDayClick(day) {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range, () => {
+      this.props.onChange(this.state)
+    });
+  }
+
+  render() {
+    const { className, size, ...others } = this.props;
+    const { from, to } = this.state;
+    const modifiers = { from, to };
+    const selectedDays = [from, { from, to }];
+
+    const classNames = cx(
+      theme['date-picker'],
+      theme[`is-${size}`],
+      {
+        [theme['has-range']]: hasRange(selectedDays),
+      },
+      className,
+    );
+
+    return (
+      <DayPicker
+        className={classNames}
+        classNames={theme}
+        modifiers={convertModifiersToClassnames(modifiers, theme)}
+        navbarElement={<NavigationBar size={size} />}
+        onDayClick={this.handleDayClick}
+        selectedDays={selectedDays}
+        weekdayElement={<WeekDay size={size} />}
+        {...others}
+      />
+    );
+  }
+}
+
+export default DatePickerRange;
