f0014a0f0fda0cd0ffc770e8e84961daf570a24c
Dries Decramer <dries.decramer@teamleader.be>
2018-05-30 17:08:56 +0200

    Add hasRange method to the utils

--- components/datepicker/utils.js
+++ components/datepicker/utils.js
@@ -10,3 +10,29 @@ export const convertModifiersToClassnames = (modifiers, theme) => {
     };
   }, {});
 };
+
+export const hasRange = selectedDays => {
+  let containsFrom = false;
+  let containsTo = false;
+
+  if (!selectedDays || typeof selectedDays !== 'object') {
+    return false;
+  }
+
+  Object.keys(selectedDays).map(key => {
+    if (!selectedDays[key] || typeof selectedDays[key] !== 'object') {
+      return;
+    }
+
+    Object.keys(selectedDays[key]).map(key => {
+      if (key === 'from') {
+        containsFrom = true;
+      }
+      if (key === 'to') {
+        containsTo = true;
+      }
+    });
+  });
+
+  return containsFrom && containsTo;
+};
