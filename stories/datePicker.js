bb00835eede8504ee9cbfa55b4c957c21bdf03a6
Dries Decramer <dries.decramer@teamleader.be>
2018-05-31 12:22:37 +0200

    Add a story to demo the DatePickerRange

--- stories/datePicker.js
+++ stories/datePicker.js
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, boolean, number, select } from "@storybook/addon-knobs/react";
-import { DatePicker } from '../components';
+import { DatePicker, DatePickerRange } from '../components';
 import { DateTime } from 'luxon';
 import MomentLocaleUtils from 'react-day-picker/moment';
 
@@ -12,6 +12,10 @@ const languages = ['da', 'de', 'fr', 'en', 'es', 'fi', 'it', 'nl', 'pt', 'pl', '
 const sizes = ['small', 'medium', 'large'];
 
 const preSelectedDate = DateTime.local().plus({days: 3}).toJSDate();
+const preSelectedRange = {
+  from: DateTime.local().plus({days: 3}).toJSDate(),
+  to: DateTime.local().plus({days: 8}).toJSDate(),
+};
 
 storiesOf('DatePicker', module)
   .addDecorator((story, context) => withInfo({TableComponent: PropTable})(story)(context))
@@ -30,6 +34,24 @@ storiesOf('DatePicker', module)
         onChange={handleOnChange}
         selectedDate={preSelectedDate}
         showOutsideDays={boolean('Show outside days', true)}
+        showWeekNumbers={boolean('Show week numbers', true)}
+        size={select('Size', sizes, 'medium')}
+      />
+    )
+  })
+  .add('Range', () => {
+    const handleOnChange = (selectedRange) => {
+      console.log("Selected range", selectedRange);
+    };
+
+    return (
+      <DatePickerRange
+        locale={select('Locale', languages, 'nl')}
+        localeUtils={MomentLocaleUtils}
+        numberOfMonths={number('Number of months', 2)}
+        onChange={handleOnChange}
+        selectedRange={preSelectedRange}
+        showOutsideDays={boolean('Show outside days', false)}
         showWeekNumbers={boolean('Show week numbers', true)}
         size={select('Size', sizes, 'medium')}
       />
