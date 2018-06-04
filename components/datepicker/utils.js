d096550d1cf5d6a1405177ddf0f4f98dce9e943e
Dries Decramer <dries.decramer@teamleader.be>
2018-06-04 12:17:23 +0200

    Delete this util function since we're not using it anymore

--- components/datepicker/utils.js
+++ components/datepicker/utils.js
@@ -13,32 +13,6 @@ export const convertModifiersToClassnames = (modifiers, theme) => {
   }, {});
 };
 
-export const hasRange = selectedDays => {
-  let containsFrom = false;
-  let containsTo = false;
-
-  if (!selectedDays || typeof selectedDays !== 'object') {
-    return false;
-  }
-
-  Object.keys(selectedDays).map(key => {
-    if (!selectedDays[key] || typeof selectedDays[key] !== 'object') {
-      return;
-    }
-
-    Object.keys(selectedDays[key]).map(key => {
-      if (key === 'from') {
-        containsFrom = true;
-      }
-      if (key === 'to') {
-        containsTo = true;
-      }
-    });
-  });
-
-  return containsFrom && containsTo;
-};
-
 export const isSelectingFirstDay = (from, to, day) => {
   const isBeforeFirstDay = from && DateUtils.isDayBefore(day, from);
   const isRangeSelected = from && to;
