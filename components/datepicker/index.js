5534e9a2960ff81be89663ad86c4ffe853a18a8f
Dries Decramer <dries.decramer@teamleader.be>
2018-06-06 09:16:26 +0200

    Export DatePickerInputRange

--- components/datepicker/index.js
+++ components/datepicker/index.js
@@ -1,5 +1,6 @@
 import DatePicker from './DatePicker';
 import DatePickerRange from './DatePickerRange';
+import DatePickerInputRange from './DatePickerInputRange';
 
 export default DatePicker;
-export { DatePicker, DatePickerRange };
+export { DatePicker, DatePickerRange, DatePickerInputRange };
