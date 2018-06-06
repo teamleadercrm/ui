525da20670f0e9f60b0229aff50633ecbf8ef728
Bert Coppens <bertcoppens@Berts-MacBook-Pro.local>
2018-06-06 12:11:16 +0200

    remove state from tab component and simplify inverted state for components

---
+++ static/data/tab.js
@@ -0,0 +1,45 @@
+const tabItems = [
+  {
+    active: false,
+    href: '#',
+    icon: 'Invoices',
+    title: 'Invoices',
+  },
+  {
+    active: true,
+    href: '#',
+    icon: 'CRM',
+    title: 'CRM',
+  },
+  {
+    active: false,
+    count: 30,
+    href: '#',
+    icon: 'Planning',
+    title: 'Planning',
+  },
+];
+
+const invertedTabItems = [
+  {
+    active: true,
+    count: 8,
+    href: '#',
+    icon: 'Products',
+    title: 'Products',
+  },
+  {
+    active: false,
+    href: '#',
+    icon: 'Deals',
+    title: 'Deals',
+  },
+  {
+    active: false,
+    href: '#',
+    icon: 'Stats',
+    title: 'Stats',
+  },
+];
+
+export { tabItems, invertedTabItems };
