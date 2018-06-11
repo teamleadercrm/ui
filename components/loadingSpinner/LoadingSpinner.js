95f9d5d493bd95bbd5772f69747978eaffcf0894
Dries Decramer <dries.decramer@teamleader.be>
2018-06-11 15:17:53 +0200

    LoadingSpinner: move static propTypes & defaultProps outside of the component class

--- components/loadingSpinner/LoadingSpinner.js
+++ components/loadingSpinner/LoadingSpinner.js
@@ -5,17 +5,6 @@ import cx from 'classnames';
 import theme from './theme.css';
 
 class LoadingSpinner extends PureComponent {
-  static propTypes = {
-    className: PropTypes.string,
-    color: PropTypes.oneOf(['white', 'teal']),
-    size: PropTypes.oneOf(['small', 'medium']),
-  };
-
-  static defaultProps = {
-    color: 'teal',
-    size: 'medium',
-  };
-
   render() {
     const { className, color, size, ...others } = this.props;
     const classNames = cx(theme['loading-spinner'], theme[`is-${color}`], theme[`is-${size}`], className);
@@ -23,5 +12,16 @@ class LoadingSpinner extends PureComponent {
     return <Box data-teamleader-ui="loading-spinner" className={classNames} {...others} />;
   }
 }
+
+LoadingSpinner.propTypes = {
+  className: PropTypes.string,
+  color: PropTypes.oneOf(['white', 'teal']),
+  size: PropTypes.oneOf(['small', 'medium']),
+};
+
+LoadingSpinner.defaultProps = {
+  color: 'teal',
+  size: 'medium',
+};
 
 export default LoadingSpinner;
