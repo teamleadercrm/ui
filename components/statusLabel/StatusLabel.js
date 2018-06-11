33fb13522a533722a3e2bf2608b176e4a4883167
Dries Decramer <dries.decramer@teamleader.be>
2018-06-11 15:27:34 +0200

    StatusLabel: move static propTypes & defaultProps outside of the component class

--- components/statusLabel/StatusLabel.js
+++ components/statusLabel/StatusLabel.js
@@ -15,18 +15,6 @@ const SIZES = {
 };
 
 class StatusLabel extends PureComponent {
-  static propTypes = {
-    children: PropTypes.any.isRequired,
-    className: PropTypes.string,
-    color: PropTypes.oneOf(['neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua']),
-    size: PropTypes.oneOf(Object.keys(SIZES)),
-  };
-
-  static defaultProps = {
-    color: 'neutral',
-    size: 'medium',
-  };
-
   render() {
     const { children, className, color, size, ...others } = this.props;
 
@@ -39,5 +27,17 @@ class StatusLabel extends PureComponent {
     );
   }
 }
+
+StatusLabel.propTypes = {
+  children: PropTypes.any.isRequired,
+  className: PropTypes.string,
+  color: PropTypes.oneOf(['neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua']),
+  size: PropTypes.oneOf(Object.keys(SIZES)),
+};
+
+StatusLabel.defaultProps = {
+  color: 'neutral',
+  size: 'medium',
+};
 
 export default StatusLabel;
