1b93373f51aa5109a994893952d2d83f7dc9c04a
Dries Decramer <dries.decramer@teamleader.be>
2018-06-13 10:44:13 +0200

    Add static data for demo purposes

---
+++ static/data/select.js
@@ -0,0 +1,13 @@
+const selectOptions = [
+  { label: 'Chocolate', value: 'chocolate', color: 'yellow' },
+  { label: 'Vanilla', value: 'vanilla', color: 'red' },
+  { label: 'Strawberry', value: 'strawberry', color: 'blue' },
+  { label: 'Caramel', value: 'caramel', color: 'yellow', disabled: true },
+  { label: 'Cookies and Cream', value: 'cookiescream', color: 'red' },
+  { label: 'Peppermint', value: 'peppermint', color: 'blue' },
+];
+
+export default selectOptions;
+export {
+  selectOptions,
+}
