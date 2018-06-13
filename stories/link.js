341599487a7ee240fc2e7ca2f3d264d56ca5e8e5
Kemosabert <bert.coppens14@gmail.com>
2018-06-13 16:15:05 +0200

    fix lint errors in stories/link.js

--- stories/link.js
+++ stories/link.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTable from './components/propTable';
 import { storiesOf } from '@storybook/react';
-import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, boolean, select } from '@storybook/addon-knobs/react';
 import { Link, TextDisplay } from '../components';
