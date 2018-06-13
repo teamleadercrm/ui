83fcfaf5514d0a3576c1b47ef36ee39788d3c042
Dries Decramer <dries.decramer@teamleader.be>
2018-06-13 14:10:58 +0200

    Add custom styles

--- components/select/Select.js
+++ components/select/Select.js
@@ -4,6 +4,31 @@ import { IconChevronDownSmallOutline } from '@teamleader/ui-icons';
 import { Button } from '../button';
 import theme from './theme.css';
 
+const customStyles = {
+  control: (base, state) => ({
+    ...base,
+    backgroundColor: state.isDisabled ? '#e4e4e6' : '#fff',
+    '&:hover': {
+      borderColor: '#82828c'
+    },
+    borderColor: state.isFocused ? '#82828c' : '#c0c0c4',
+    boxShadow: state.isFocused ? '0 0 0 1px #82828c' : 'none',
+  }),
+  indicatorSeparator: (base) => ({
+    ...base,
+    marginTop: 0,
+    marginBottom: 0,
+  }),
+  option: (base, state) => ({
+    ...base,
+    color: '#344b63',
+    backgroundColor: state.isFocused ? '#e4e4e6' : state.isSelected ? '#f7f7fa' : '#fff',
+    '&:active': {
+      backgroundColor: '#e4e4e6'
+    },
+  }),
+};
+
 const DropdownIndicator = () => <Button className={theme['dropdown-indicator']} icon={<IconChevronDownSmallOutline />}/>;
 
 class Select extends PureComponent {
@@ -23,6 +48,7 @@ class Select extends PureComponent {
         className={theme['select']}
         components={{ DropdownIndicator }}
         onChange={this.handleChange}
+        styles={customStyles}
         value={selectedOptions}
         {...this.props}
       />
