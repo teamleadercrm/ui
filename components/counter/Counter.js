9ff489459053565ede8671d06bfd1a08ed06edfa
Kemosabert <bert.coppens14@gmail.com>
2018-06-15 10:56:02 +0200

    add descriptions to the components prop types

--- components/counter/Counter.js
+++ components/counter/Counter.js
@@ -30,13 +30,21 @@ class Counter extends PureComponent {
 }
 
 Counter.propTypes = {
+  /** A border color to give to the counter */
   borderColor: PropTypes.oneOf(['mint', 'aqua', 'violet', 'teal', 'gold', 'ruby', 'white', 'teal']),
+  /** A border tint to give to the counter */
   borderTint: PropTypes.oneOf(['darkest', 'dark', 'none', 'light', 'lightest']),
+  /** The content to display inside the Counter */
   children: PropTypes.any,
+  /** A class name for the counter to give custom styles */
   className: PropTypes.string,
+  /** The color theme you want to style the counter in */
   color: PropTypes.oneOf(['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby']),
+  /** The value of the counter. */
   count: PropTypes.number.isRequired,
+  /** The max value of the counter. */
   maxCount: PropTypes.number,
+  /** The size of the counter */
   size: PropTypes.oneOf(['small', 'medium']),
 };
 
