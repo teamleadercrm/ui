0439fb65f925dcf427673e698eab20c4f68622c5
Kemosabert <bert.coppens14@gmail.com>
2018-06-15 14:37:28 +0200

    add color prop to component

--- components/progressTracker/ProgressTracker.js
+++ components/progressTracker/ProgressTracker.js
@@ -1,12 +1,19 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
+import Island from '../island';
+
 class ProgressTracker extends PureComponent {
   render() {
-    return null;
+    const { color } = this.props;
+
+    return <Island color={color} />;
   }
 }
 
-ProgressTracker.propTypes = {};
+ProgressTracker.propTypes = {
+  /** Color theme of the progress tracker. */
+  color: PropTypes.oneOf(['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby']),
+};
 
 export default ProgressTracker;
--- stories/progressTracker.js
+++ stories/progressTracker.js
@@ -3,9 +3,13 @@ import PropTable from './components/propTable';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
+import { withKnobs, select } from '@storybook/addon-knobs/react';
 import { ProgressTracker } from '../components';
+
+const colors = ['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby'];
 
 storiesOf('ProgressTracker', module)
   .addDecorator((story, context) => withInfo({ TableComponent: PropTable })(story)(context))
+  .addDecorator(withKnobs)
   .addDecorator(checkA11y)
-  .add('Basic', () => <ProgressTracker />);
+  .add('Basic', () => <ProgressTracker color={select('Color', colors, 'neutral')} />);
