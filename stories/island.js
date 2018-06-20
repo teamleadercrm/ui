9b0d965ef6f86942b4d087713520c50969d93a69
Kemosabert <bert.coppens14@gmail.com>
2018-06-20 10:17:50 +0200

    :sparkles: Add an example story of segmented children

--- stories/island.js
+++ stories/island.js
@@ -4,7 +4,12 @@ import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, boolean, select } from '@storybook/addon-knobs/react';
-import { Island, TextBody } from '../components';
+import { Island, TextBody, Heading3 } from '../components';
+import {
+  IllustrationInvoices120X120Static,
+  IllustrationMeetings120X120Static,
+  IllustrationDeals120X120Static,
+} from '@teamleader/ui-illustrations';
 
 const colors = ['neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua', 'white'];
 const sizes = ['small', 'medium', 'large'];
@@ -13,7 +18,13 @@ storiesOf('Island', module)
   .addDecorator((story, context) =>
     withInfo({
       TableComponent: PropTable,
-      propTablesExclude: [TextBody],
+      propTablesExclude: [
+        TextBody,
+        Heading3,
+        IllustrationInvoices120X120Static,
+        IllustrationMeetings120X120Static,
+        IllustrationDeals120X120Static,
+      ],
     })(story)(context),
   )
   .addDecorator(checkA11y)
@@ -25,5 +36,31 @@ storiesOf('Island', module)
       size={select('Size', sizes, 'medium')}
     >
       <TextBody>I am an island.</TextBody>
+    </Island>
+  ))
+  .add('Segmented', () => (
+    <Island
+      color={select('Color', colors, 'white')}
+      dark={boolean('Dark', false)}
+      size={select('Size', sizes, 'medium')}
+      display="flex"
+      flexDirection="row"
+      padding={0}
+    >
+      <Island segmented>
+        <IllustrationInvoices120X120Static />
+        <Heading3 marginBottom={3}>Invoices</Heading3>
+        <TextBody align="center">Send invoices to your clients straight from Teamleader.</TextBody>
+      </Island>
+      <Island segmented>
+        <IllustrationMeetings120X120Static />
+        <Heading3 marginBottom={3}>Meetings</Heading3>
+        <TextBody>Plan meetings and see them straight away in your favourite calendar.</TextBody>
+      </Island>
+      <Island segmented>
+        <IllustrationDeals120X120Static />
+        <Heading3 marginBottom={3}>Deals</Heading3>
+        <TextBody>Keep track of all your deals with our fully integrated module.</TextBody>
+      </Island>
     </Island>
   ));
