703d8671edf5616c3bc65bbb1ec9efc330201cc4
Dries Decramer <dries.decramer@teamleader.be>
2018-07-30 17:14:27 +0200

    Extend stories to demo different use cases

--- stories/select.js
+++ stories/select.js
@@ -4,10 +4,30 @@ import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, boolean, select } from '@storybook/addon-knobs/react';
-import { Label, Select } from '../components';
-import { groupedOptions } from "../static/data/select";
+import { Avatar, Box, Label, Select, TextBody } from '../components';
+import { customOptions, groupedOptions, options } from "../static/data/select";
 
 const sizes = ['small', 'medium', 'large'];
+
+const CustomOption = ({ children, data, innerRef, innerProps, isFocused, isSelected, isDisabled }) => {
+  const boxStyles = {
+    backgroundColor: isFocused ? '#e4e4e6' : isSelected ? '#82828c' : '#fff',
+    '&:active': {
+      backgroundColor: isDisabled ? '#fff' : '#e4e4e6',
+    },
+  };
+
+  const textStyles = {
+    color: isDisabled ? '#c0c0c4' : isSelected && !isFocused ? '#fff' : '#344b63',
+  };
+
+  return (
+    <Box paddingVertical={2} paddingHorizontal={2} display="flex" alignItems="center" ref={innerRef} {...innerProps} style={boxStyles}>
+      <Avatar image={data.avatar} size="tiny" marginRight={2} />
+      <TextBody style={textStyles}>{children}</TextBody>
+    </Box>
+  );
+};
 
 storiesOf('Select', module)
   .addDecorator((story, context) =>
@@ -19,6 +39,51 @@ storiesOf('Select', module)
   .addDecorator(checkA11y)
   .addDecorator(withKnobs)
   .add('Basic', () => (
+    <Select
+      closeMenuOnSelect={boolean('Close menu on select', true)}
+      inverse={boolean('Inverse', false)}
+      isClearable={boolean('Clearable', false)}
+      isDisabled={boolean('Disabled', false)}
+      isMulti={boolean('Multi select', false)}
+      isSearchable={boolean('Searchable', false)}
+      options={options}
+      placeholder="Select your favourite(s)"
+      size={select('Size', sizes, 'medium')}
+      hideSelectedOptions={boolean('Hide selected options', true)}
+    />
+  ))
+  .add('Grouped', () => (
+    <Select
+      closeMenuOnSelect={boolean('Close menu on select', true)}
+      inverse={boolean('Inverse', false)}
+      isClearable={boolean('Clearable', false)}
+      isDisabled={boolean('Disabled', false)}
+      isMulti={boolean('Multi select', false)}
+      isSearchable={boolean('Searchable', false)}
+      options={groupedOptions}
+      placeholder="Select your favourite(s)"
+      size={select('Size', sizes, 'medium')}
+      hideSelectedOptions={boolean('Hide selected options', true)}
+    />
+  ))
+  .add('Custom Option', () => (
+    <Select
+      closeMenuOnSelect={boolean('Close menu on select', true)}
+      components={{
+        Option: CustomOption,
+      }}
+      inverse={boolean('Inverse', false)}
+      isClearable={boolean('Clearable', false)}
+      isDisabled={boolean('Disabled', false)}
+      isMulti={boolean('Multi select', false)}
+      isSearchable={boolean('Searchable', false)}
+      options={customOptions}
+      placeholder="Select your favourite(s)"
+      size={select('Size', sizes, 'medium')}
+      hideSelectedOptions={boolean('Hide selected options', true)}
+    />
+  ))
+  .add('With label', () => (
     <Label inverse={boolean('Inverse', false)}>
       Select something
       <Select
@@ -28,7 +93,7 @@ storiesOf('Select', module)
         isDisabled={boolean('Disabled', false)}
         isMulti={boolean('Multi select', false)}
         isSearchable={boolean('Searchable', false)}
-        options={groupedOptions}
+        options={options}
         placeholder="Select your favourite(s)"
         size={select('Size', sizes, 'medium')}
         hideSelectedOptions={boolean('Hide selected options', true)}
