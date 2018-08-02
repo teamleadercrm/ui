23a60863ebcf130efb8cb30f9449be805a4d4bc1
Dries Decramer <dries.decramer@teamleader.be>
2018-08-02 16:34:34 +0200

    Update stories so we get the right component markup & prop type tables

--- stories/typography.js
+++ stories/typography.js
@@ -23,55 +23,23 @@ storiesOf('Typography', module)
     })(story)(context),
   )
   .addDecorator(checkA11y)
-  .add('Headings', () => (
-    <div>
-      <Heading1>Heading 1 / font-size: 24px / line-height: 30px / weight: bold (700) / tracking: 0</Heading1>
-      <Heading2 marginTop={4}>
-        Heading 2 / font-size: 18px / line-height: 24px / weight: medium (500) / tracking: 0
-      </Heading2>
-      <Heading3 marginTop={4}>
-        Heading 3 / font-size: 16px / line-height: 21px / weight: medium (500) / tracking: 0
-      </Heading3>
-      <Heading4 marginTop={4}>
-        Heading 4 / font-size: 12px / line-height: 18px / weight: bold (700) / tracking: 0.6px
-      </Heading4>
-    </div>
+  .add('Heading 1', () => <Heading1>Font-size: 24px / line-height: 30px / weight: bold (700) / tracking: 0</Heading1>)
+  .add('Heading 2', () => <Heading2>Font-size: 18px / line-height: 24px / weight: medium (500) / tracking: 0</Heading2>)
+  .add('Heading 3', () => <Heading3>Font-size: 16px / line-height: 21px / weight: medium (500) / tracking: 0</Heading3>)
+  .add('Heading 4', () => (
+    <Heading4>Font-size: 12px / line-height: 18px / weight: bold (700) / tracking: 0.6px</Heading4>
   ))
-  .add('Text', () => (
-    <div>
-      <TextDisplay>
-        Text display / font-size: 16px / line-height: 24px / weight: regular (400) / tracking: 0
-      </TextDisplay>
-      <TextBody marginTop={2}>
-        Text body / font-size: 14px / line-height: 21px / weight: regular (400) / tracking: 0
-      </TextBody>
-      <TextSmall marginTop={2}>
-        Text small / font-size: 12px / line-height: 18px / weight: regular (400) / tracking: 0
-      </TextSmall>
-    </div>
+  .add('Display Text', () => (
+    <TextDisplay>Font-size: 16px / line-height: 24px / weight: regular (400) / tracking: 0</TextDisplay>
+  ))
+  .add('Body Text', () => (
+    <TextBody>Font-size: 14px / line-height: 21px / weight: regular (400) / tracking: 0</TextBody>
+  ))
+  .add('Small Text', () => (
+    <TextSmall>Font-size: 12px / line-height: 18px / weight: regular (400) / tracking: 0</TextSmall>
   ))
   .add('Monospaced', () => (
-    <Box padding={5}>
-      <Heading1>
-        <Monospaced>1234567890</Monospaced>
-      </Heading1>
-      <Heading2 marginTop={2}>
-        <Monospaced>1234567890</Monospaced>
-      </Heading2>
-      <Heading3 marginTop={2}>
-        <Monospaced>1234567890</Monospaced>
-      </Heading3>
-      <Heading4 marginTop={2}>
-        <Monospaced>1234567890</Monospaced>
-      </Heading4>
-      <TextDisplay marginTop={6}>
-        <Monospaced>1234567890</Monospaced>
-      </TextDisplay>
-      <TextBody marginTop={2}>
-        <Monospaced>1234567890</Monospaced>
-      </TextBody>
-      <TextSmall marginTop={2}>
-        <Monospaced>1234567890</Monospaced>
-      </TextSmall>
-    </Box>
+    <TextBody>
+      <Monospaced>1234567890</Monospaced>
+    </TextBody>
   ));
