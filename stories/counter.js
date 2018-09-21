498c91c693566fd8ddc971a9698ec55b0ab9a132
Dries Decramer <dries.decramer@teamleader.be>
2018-09-21 15:40:20 +0200

    Counter: update story

--- stories/counter.js
+++ stories/counter.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import PropTable from './components/propTable';
 import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { withKnobs, number, select } from '@storybook/addon-knobs/react';
+import { number, select } from '@storybook/addon-knobs/react';
 import { Counter, Tooltip, TextBody } from '../components';
 
 const colors = ['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby'];
@@ -11,8 +9,6 @@ const sizes = ['small', 'medium'];
 const TooltippedCounter = Tooltip(Counter);
 
 storiesOf('Counters', module)
-  .addDecorator((story, context) => withInfo({ TableComponent: PropTable })(story)(context))
-  .addDecorator(withKnobs)
   .add('Basic', () => (
     <Counter
       count={number('Count', 99)}
