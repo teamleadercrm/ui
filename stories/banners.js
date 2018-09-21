ff9c1ecc4fcd49dcb43fe0c327697a4208dabeff
Dries Decramer <dries.decramer@teamleader.be>
2018-09-21 15:09:57 +0200

    Banner: update story

--- stories/banners.js
+++ stories/banners.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import PropTable from './components/propTable';
 import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { withKnobs, boolean, select, text } from '@storybook/addon-knobs/react';
+import { boolean, select, text } from '@storybook/addon-knobs/react';
 import { Banner, Link, TextDisplay } from '../components';
 import { IconIdeaMediumOutline } from '@teamleader/ui-icons';
 
 const colors = ['white', 'neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua'];
 
 storiesOf('Banner', module)
-  .addDecorator((story, context) =>
-    withInfo({
-      TableComponent: PropTable,
+  .addParameters({
+    info: {
       propTablesExclude: [TextDisplay, Link],
-    })(story)(context),
-  )
-  .addDecorator(withKnobs)
+    },
+  })
   .add('Default', () => (
     <Banner
       color={select('Color', colors, 'white')}
