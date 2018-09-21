dd605741de549ed85e6996b6317d8e27071f4d65
Dries Decramer <dries.decramer@teamleader.be>
2018-09-21 16:14:38 +0200

    StatusBullet: update story

--- stories/statusBullet.js
+++ stories/statusBullet.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import PropTable from './components/propTable';
 import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { withKnobs, select } from '@storybook/addon-knobs/react';
+import { select } from '@storybook/addon-knobs/react';
 import { StatusBullet, TextBody } from '../components';
 
 const colors = ['mint', 'violet', 'ruby', 'gold', 'aqua', 'neutral'];
 const sizes = ['small', 'medium'];
 
 storiesOf('Status Bullets', module)
-  .addDecorator((story, context) =>
-    withInfo({
-      TableComponent: PropTable,
+  .addParameters({
+    info: {
       propTablesExclude: [TextBody],
-    })(story)(context),
-  )
-  .addDecorator(withKnobs)
+    },
+  })
   .add('Basic', () => (
     <StatusBullet color={select('Color', colors, 'neutral')} size={select('Size', sizes, 'medium')}>
       <TextBody>label</TextBody>
