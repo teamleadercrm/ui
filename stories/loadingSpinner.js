4652e39824702c89188dd548c0c47bbad7c266be
LennertBlommaert <lennert@teamleader.eu>
2018-09-27 11:45:12 +0200

    Update story so it reflects the prop changes

--- stories/loadingSpinner.js
+++ stories/loadingSpinner.js
@@ -3,9 +3,14 @@ import { storiesOf } from '@storybook/react';
 import { select } from '@storybook/addon-knobs/react';
 import { LoadingSpinner } from '../components';
 
-const colors = ['teal', 'white'];
+const colors = ['neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua', 'teal'];
 const sizes = ['small', 'medium'];
+const tints = ['lightest', 'light', 'normal', 'dark', 'darkest'];
 
 storiesOf('Loading spinners', module).add('Basic', () => (
-  <LoadingSpinner color={select('Color', colors, 'teal')} size={select('Size', sizes, 'medium')} />
+  <LoadingSpinner
+    color={select('Color', colors, 'neutral')}
+    size={select('Size', sizes, 'medium')}
+    tint={select('Tint', tints, 'lightest')}
+  />
 ));
