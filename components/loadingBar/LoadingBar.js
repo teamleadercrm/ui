d031695f365bb5e378ca44340288773897ba7514
LennertBlommaert <lennert@teamleader.eu>
2018-10-02 17:15:05 +0200

    Style the static version styling SVGs via CSS is poorly supported at the moment, therefore some styling is done by adding attributes to the tags

--- components/loadingBar/LoadingBar.js
+++ components/loadingBar/LoadingBar.js
@@ -1,10 +1,14 @@
 import React, { PureComponent } from 'react';
+import cx from 'classnames';
+import theme from './theme.css';
 
 class LoadingBar extends PureComponent {
   render() {
+    const classNames = cx(theme['loading-bar']);
+
     return (
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <rect />
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" className={classNames}>
+        <rect width="20%" height="100%" rx="2px" ry="2px" className={theme['loading-bar-indicator']} />
       </svg>
     );
   }
