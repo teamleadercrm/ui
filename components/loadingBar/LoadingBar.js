5bc4f2c660f5e5b8d1a0745617df72b7c489fc77
LennertBlommaert <lennert@teamleader.eu>
2018-10-02 16:37:31 +0200

    Setup new LoadingBar component

---
+++ components/loadingBar/LoadingBar.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+class LoadingBar extends PureComponent {
+  render() {
+    return null;
+  }
+}
+
+export default LoadingBar;
---
+++ stories/loadingBar.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { LoadingBar } from '../components';
+
+storiesOf('LoadingBar', module).add('Basic', () => <LoadingBar />);
