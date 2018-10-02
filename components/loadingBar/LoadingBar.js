3b81a97838b7c176ad0ae1c3c0e4323780446a39
LennertBlommaert <lennert@teamleader.eu>
2018-10-02 16:53:53 +0200

    Create SVG containing a rectangle as SVG is performance wise the best option for animation

--- components/loadingBar/LoadingBar.js
+++ components/loadingBar/LoadingBar.js
@@ -2,7 +2,11 @@ import React, { PureComponent } from 'react';
 
 class LoadingBar extends PureComponent {
   render() {
-    return null;
+    return (
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <rect />
+      </svg>
+    );
   }
 }
 
