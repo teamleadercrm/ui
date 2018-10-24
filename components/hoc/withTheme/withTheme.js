ec4fb7f9a9dfa380dd48a5c12f9d2230fa60207b
LennertBlommaert <lennert@teamleader.eu>
2018-10-24 17:29:05 +0200

    Create InjectTheme HoC, that is responsible of adding the correct CSS classes regarding colors, tints and sizes

---
+++ components/hoc/withTheme/withTheme.js
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+const withTheme = theme => WrappedComponent => {
+  class WithTheme extends PureComponent {
+    render() {
+      const { color, size, tint, className, ...others } = this.props;
+
+      const classNames = cx(theme[`is-${color}`], theme[`is-${size}`], theme[`is-${tint}`], className);
+
+      return <WrappedComponent className={classNames} {...others} />;
+    }
+  }
+
+  WithTheme.propTypes = {
+    /** The color of the component */
+    color: PropTypes.PropTypes.oneOf(['aqua', 'gold', 'mint', 'neutral', 'ruby', 'teal', 'violet']),
+    /** The size of the component */
+    size: PropTypes.oneOf(['tiny', 'small', 'medium', 'large', 'fullscreen']),
+    /** The tint of the component */
+    tint: PropTypes.oneOf(['lightest', 'light', 'normal', 'dark', 'darkest']),
+  };
+
+  WithTheme.defaultProps = {
+    color: 'neutral',
+    size: 'medium',
+    tint: 'normal',
+  };
+
+  return WithTheme;
+};
+
+export default withTheme;
