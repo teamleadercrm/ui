ed5a44c52a299edbb19be8ac74cc0144b7d75684
LennertBlommaert <lennert@teamleader.eu>
2018-10-29 11:32:07 +0100

    Add story for SingleLineInputBase

--- stories/input.js
+++ stories/input.js
@@ -14,6 +14,7 @@ import {
   TextBody,
   TextSmall,
   Tooltip,
+  SingleLineInputBase,
 } from '../components';
 import { IconCalendarSmallOutline, IconInfoBadgedSmallFilled } from '@teamleader/ui-icons';
 
@@ -40,6 +41,7 @@ const TooltippedIcon = Tooltip(Icon);
 
 storiesOf('Inputs', module)
   .add('Input base', () => <InputBase />)
+  .add('Single line input base', () => <SingleLineInputBase />)
   .add('Input only', () => (
     <Input
       id="input1"
