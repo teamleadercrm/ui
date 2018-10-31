4a12437d3c3d50b69f15baf035a0623c08c4e0f6
Dries Decramer <dries.decramer@teamleader.be>
2018-10-31 15:24:21 +0100

    Remove the implementation of the 'soft' prop

--- components/typography/Text.js
+++ components/typography/Text.js
@@ -2,40 +2,19 @@ import React, { PureComponent } from 'react';
 import Box from '../box';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import omit from 'lodash.omit';
 import theme from './theme.css';
 
 const factory = (baseType, type, defaultElement) => {
   class Text extends PureComponent {
-    isSoft(color) {
-      if (color !== 'white' && color !== 'teal') {
-        return false;
-      }
-
-      return this.props.soft;
-    }
-
     render() {
       const { children, className, color, element, ...others } = this.props;
 
-      const isSoft = this.isSoft(color);
-
-      const classNames = cx(
-        theme[baseType],
-        theme[type],
-        theme[color],
-        {
-          [theme['soft']]: isSoft,
-        },
-        className,
-      );
+      const classNames = cx(theme[baseType], theme[type], theme[color], className);
 
       const Element = element || defaultElement;
 
-      const rest = omit(others, ['soft']);
-
       return (
-        <Box className={classNames} data-teamleader-ui={baseType} element={Element} {...rest}>
+        <Box className={classNames} data-teamleader-ui={baseType} element={Element} {...others}>
           {children}
         </Box>
       );
@@ -47,13 +26,11 @@ const factory = (baseType, type, defaultElement) => {
     className: PropTypes.string,
     color: PropTypes.oneOf(['white', 'neutral', 'mint', 'teal', 'violet', 'ruby', 'gold', 'aqua']),
     element: PropTypes.node,
-    soft: PropTypes.bool,
   };
 
   Text.defaultProps = {
     color: 'teal',
     element: null,
-    soft: false,
   };
 
   return Text;
