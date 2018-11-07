bbb2618c6dce0ce889a32cfa06ff77b2b3e50792
LennertBlommaert <lennert@teamleader.eu>
2018-11-07 17:15:40 +0100

    Add dialog header

--- src/components/dialog/Dialog.js
+++ src/components/dialog/Dialog.js
@@ -1,14 +1,44 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import omit from 'lodash.omit';
+
 import { DialogBase } from './index';
+import { Banner, COLORS, Heading3 } from '../../index';
 
 class Dialog extends PureComponent {
+  getHeader = () => {
+    const { headerColor, headerIcon, onCloseClick, title } = this.props;
+
+    return (
+      <Banner color={headerColor} fullWidth icon={headerIcon} onClose={onCloseClick}>
+        <Heading3>{title}</Heading3>
+      </Banner>
+    );
+  };
+
   render() {
-    return <DialogBase {...this.props} />;
+    const { children, title, ...otherProps } = this.props;
+
+    const restProps = omit(otherProps, ['headerColor', 'onCloseClick']);
+
+    return (
+      <DialogBase {...restProps}>
+        {title && this.getHeader()}
+        {children}
+      </DialogBase>
+    );
   }
 }
 
-Dialog.propTypes = {};
+Dialog.propTypes = {
+  headerColor: PropTypes.oneOf(COLORS),
+  headerIcon: PropTypes.element,
+  onCloseClick: PropTypes.func,
+  title: PropTypes.string,
+};
 
-Dialog.defaultProps = {};
+Dialog.defaultProps = {
+  headerColor: 'neutral',
+};
 
 export default Dialog;
