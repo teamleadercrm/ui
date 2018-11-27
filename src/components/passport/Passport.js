53d246bfee02b7530fc6522690bb8cc53d1ba6ff
Dries Decramer <dries.decramer@teamleader.be>
2018-11-27 13:44:43 +0100

    Add Passport component

---
+++ src/components/passport/Passport.js
@@ -0,0 +1,97 @@
+import React, { Fragment, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import theme from './theme.css';
+import Avatar from '../avatar';
+import Box from '../box';
+import Icon from '../icon';
+import Link from '../link';
+import Popover from '../popover';
+import { Heading3, TextBody } from '../typography';
+
+class Passport extends PureComponent {
+  renderDescription = () => {
+    const { description } = this.props;
+
+    if (!description) {
+      return null;
+    }
+
+    if (Array.isArray(description)) {
+      return (
+        <Fragment>
+          {description.map((child, index) => (
+            <TextBody color="teal" key={index}>
+              {child}
+            </TextBody>
+          ))}
+        </Fragment>
+      );
+    } else {
+      return <TextBody color="teal">{description}</TextBody>;
+    }
+  };
+
+  renderTitle = () => {
+    const { title } = this.props;
+
+    return (
+      <Heading3 className={cx(theme['prevent-overflow'], theme['prevent-wrap'], theme['show-ellipsis'])} color="teal">
+        {typeof title === 'string' ? title : <Link {...title} inherit={false} />}
+      </Heading3>
+    );
+  };
+
+  render() {
+    const { avatar, className, links, ...others } = this.props;
+
+    return (
+      <Popover {...others} backdrop="transparent" className={cx(theme['passport'], className)}>
+        <Box padding={3}>
+          <Box display="flex">
+            <Box flex="48px 0 0" paddingRight={3}>
+              <Avatar {...avatar} size="small" />
+            </Box>
+            <Box className={theme['prevent-overflow']}>
+              {this.renderTitle()}
+              {this.renderDescription()}
+            </Box>
+          </Box>
+          {links && (
+            <Box marginTop={3}>
+              {links.map(({ icon, ...link }, index) => (
+                <Box alignItems="flex-start" display="flex" key={index}>
+                  <Box display="flex" flex="48px 0 0" justifyContent="center" paddingRight={3}>
+                    {icon && (
+                      <Icon color="aqua" tint="dark">
+                        {icon}
+                      </Icon>
+                    )}
+                  </Box>
+                  <TextBody className={cx(theme['prevent-overflow'], theme['prevent-wrap'], theme['show-ellipsis'])}>
+                    <Link {...link} inherit={false} />
+                  </TextBody>
+                </Box>
+              ))}
+            </Box>
+          )}
+        </Box>
+      </Popover>
+    );
+  }
+}
+
+Passport.propTypes = {
+  /** Object containing the props of an Avatar. */
+  avatar: PropTypes.object.isRequired,
+  /** The class names for the wrapper to apply custom styling. */
+  className: PropTypes.string,
+  /** The description of the Passport. */
+  description: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+  /** Array of objects containing the props of a Link. */
+  links: PropTypes.array,
+  /** The title of the Passport (can be a string of an object containing the props of a Link). */
+  title: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+};
+
+export default Passport;
