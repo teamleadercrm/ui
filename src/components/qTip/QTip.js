edbb4eac7f48f3a54cb3de2db49224cca3405beb
Dries Decramer <dries.decramer@teamleader.be>
2019-01-15 09:08:50 +0100

    Implement onEscKeyDown, onOverlayClick, onOverlayMouseDown, onOverlayMouseMove & onOverlayMouseUp

--- src/components/qTip/QTip.js
+++ src/components/qTip/QTip.js
@@ -9,7 +9,19 @@ import Box from '../box';
 
 class QTip extends PureComponent {
   render() {
-    const { active, children, highlighted, icon, onChange, ...others } = this.props;
+    const {
+      active,
+      children,
+      highlighted,
+      icon,
+      onChange,
+      onEscKeyDown,
+      onOverlayClick,
+      onOverlayMouseDown,
+      onOverlayMouseMove,
+      onOverlayMouseUp,
+      ...others
+    } = this.props;
 
     const classNames = cx(theme['container'], {
       [theme['is-active']]: active,
@@ -20,7 +32,14 @@ class QTip extends PureComponent {
 
     return (
       <Box className={classNames} {...others}>
-        <Overlay active={active} onEscKeyDown={onChange} />
+        <Overlay
+          active={active}
+          onClick={onOverlayClick}
+          onEscKeyDown={onEscKeyDown}
+          onMouseDown={onOverlayMouseDown}
+          onMouseMove={onOverlayMouseMove}
+          onMouseUp={onOverlayMouseUp}
+        />
         <div className={theme['qtip']}>
           <Button className={theme['icon']} level={level} onClick={onChange} icon={icon} />
           <div className={theme['inner-container']}>
@@ -47,8 +66,18 @@ QTip.propTypes = {
   highlighted: PropTypes.bool,
   /** The icon displayed inside the toggle button. */
   icon: PropTypes.element.isRequired,
-  /** Callback function that is fired when the active prop has changed. */
+  /** The function executed, when the active prop has changed. */
   onChange: PropTypes.func.isRequired,
+  /** The function executed, when the "ESC" key is down. */
+  onEscKeyDown: PropTypes.func,
+  /** The function executed, when the Overlay is clicked. */
+  onOverlayClick: PropTypes.func,
+  /** The function executed, when the mouse is down on the Overlay. */
+  onOverlayMouseDown: PropTypes.func,
+  /** The function executed, when the mouse is being moved over the Overlay. */
+  onOverlayMouseMove: PropTypes.func,
+  /** The function executed, when the mouse is up on the Overlay. */
+  onOverlayMouseUp: PropTypes.func,
 };
 
 QTip.defaultProps = {
