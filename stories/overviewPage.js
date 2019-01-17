bdcf55c489666dfa0315b3ace8349d58a107220a
Dries Decramer <dries.decramer@teamleader.be>
2019-01-17 17:08:34 +0100

    Add basic story

---
+++ stories/overviewPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { OverviewPage } from '../src';
+
+storiesOf('OverviewPage', module).add('default', () => <OverviewPage>Hello world!</OverviewPage>);
