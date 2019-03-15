21ade7f6ecb2346d1a6cc2290289b2512bdcaa64
TaraldRotsaert <tarald.rotsaert@teamleader.eu>
2019-03-15 11:11:39 +0100

    Pass props to SingleInputBase

--- src/components/input/DurationInput.js
+++ src/components/input/DurationInput.js
@@ -5,10 +5,21 @@ import SingleLineInputBase from './SingleLineInputBase';
 
 class DurationInput extends PureComponent {
   state = {
-    value: 'time',
+    value: '00:00',
   };
   render() {
-    return <SingleLineInputBase value={this.state.value} type={'time'} />;
+    const { ...others } = this.props;
+    return (
+      <SingleLineInputBase
+        value={this.state.value}
+        onChange={event => {
+          console.log(this.state.value);
+          this.setState({ value: event.currentTarget.value });
+        }}
+        type={'time'}
+        {...others}
+      />
+    );
   }
 }
 
