4240ff230ae5fc870a4f335ac6659df39cb6335b
TaraldRotsaert <tarald.rotsaert@teamleader.eu>
2019-03-15 10:50:38 +0100

    Import of new DurationInput component

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,8 +1,9 @@
 import Input from './Input';
 import InputBase from './InputBase';
+import DurationInput from './DurationInput';
 import SingleLineInputBase from './SingleLineInputBase';
 import NumericInput from './NumericInput';
 import Textarea from './Textarea';
 
-export { InputBase, NumericInput, SingleLineInputBase, Textarea };
+export { InputBase, DurationInput, NumericInput, SingleLineInputBase, Textarea };
 export default Input;
