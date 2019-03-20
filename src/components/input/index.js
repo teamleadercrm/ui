77774b98d20cbc77e8c420a4e6e8f89d56b77eef
Dries Decramer <dries.decramer@teamleader.be>
2019-03-20 15:34:14 +0100

    Add TimeInput component

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -3,6 +3,7 @@ import InputBase from './InputBase';
 import SingleLineInputBase from './SingleLineInputBase';
 import NumericInput from './NumericInput';
 import Textarea from './Textarea';
+import TimeInput from './TimeInput';
 
-export { InputBase, NumericInput, SingleLineInputBase, Textarea };
+export { InputBase, NumericInput, SingleLineInputBase, Textarea, TimeInput };
 export default Input;
