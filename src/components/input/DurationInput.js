1a7ca835e5b05643c285746a55f37df829a97391
TaraldRotsaert <tarald.rotsaert@teamleader.eu>
2019-03-22 10:52:46 +0100

    Remove splitSecondsIntoHoursMinutesSeconds function form DurationInput

--- src/components/input/DurationInput.js
+++ src/components/input/DurationInput.js
@@ -197,20 +197,6 @@ class DurationInput extends PureComponent {
     );
   };
 
-  splitSecondsIntoHoursMinutesSeconds = seconds => {
-    const secondsInMinute = 60;
-    const secondsInHour = secondsInMinute * 60;
-
-    const hours = Math.floor(seconds / secondsInHour);
-    const minutes = Math.floor((seconds % secondsInHour) / secondsInMinute);
-    const remainingSeconds = seconds % secondsInMinute;
-    return {
-      hours,
-      minutes,
-      seconds: remainingSeconds,
-    };
-  };
-
   render() {
     const {
       className,
