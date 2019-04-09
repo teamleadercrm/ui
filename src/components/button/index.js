e89d1198308207be24155dab11fae1c741f880aa
TaraldRotsaert <tarald.rotsaert@teamleader.eu>
2019-04-09 17:17:11 +0200

    Import new component

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,7 @@
 import Button from './Button';
 import ButtonGroup from './ButtonGroup';
 import IconButton from './IconButton';
+import SplitButtonMenu from './SplitButtonMenu';
 
 export default Button;
-export { Button, ButtonGroup, IconButton };
+export { Button, ButtonGroup, IconButton, SplitButtonMenu };
