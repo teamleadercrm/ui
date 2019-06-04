ee3ba9f26db4133b9eb2cf4bc1a15564bdc61d7b
Dries Decramer <dries.decramer@teamleader.be>
2019-06-04 14:44:52 +0200

    Add & implement the optional 'onClick' prop, to provide visual interaction if set

--- src/components/progressTracker/ProgressStep.js
+++ src/components/progressTracker/ProgressStep.js
@@ -8,13 +8,14 @@ import { TextSmall } from '../typography';
 
 class ProgressStep extends PureComponent {
   render() {
-    const { label, active, completed } = this.props;
+    const { label, active, completed, onClick } = this.props;
     const classNames = cx(theme['step'], {
       [theme['is-active']]: active,
       [theme['is-completed']]: completed,
+      [theme['is-clickable']]: onClick,
     });
     return (
-      <Box className={classNames}>
+      <Box className={classNames} onClick={onClick}>
         <TextSmall className={theme['step-label']}>{label}</TextSmall>
         <span className={theme['status-bullet']} />
       </Box>
@@ -29,6 +30,8 @@ ProgressStep.propTypes = {
   active: PropTypes.bool.isRequired,
   /** Whether or not the step has been completed */
   completed: PropTypes.bool.isRequired,
+  /** Callback function that is fired when the progress step is clicked */
+  onClick: PropTypes.func,
 };
 
 ProgressStep.defaultProps = {
