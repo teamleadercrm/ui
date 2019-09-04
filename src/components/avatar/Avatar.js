c1ff79b80361377a9716ee89d1bfde6bf84d3c81
Dries Decramer <dries.decramer@teamleader.be>
2019-09-04 16:35:28 +0200

    Pick box props and pass them to our Avatar Box wrapper

--- src/components/avatar/Avatar.js
+++ src/components/avatar/Avatar.js
@@ -1,11 +1,12 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { AvatarImage, AvatarInitials } from '../avatar';
-import Box from '../box';
+import AvatarImage from './AvatarImage';
+import AvatarInitials from './AvatarInitials';
+import AvatarOverlay from './AvatarOverlay';
+import Box, { pickBoxProps } from '../box';
 import cx from 'classnames';
 import theme from './theme.css';
 import { COLORS } from '../../constants';
-import AvatarOverlay from './AvatarOverlay';
 
 const hashCode = str => {
   let hash = 0;
@@ -26,9 +27,10 @@ class Avatar extends PureComponent {
   render() {
     const { className, editable, imageUrl, fullName, id, onImageChange, size, shape, ...others } = this.props;
     const avatarClassNames = cx(theme['avatar'], theme[`is-${size}`], theme[`is-${shape}`], className);
+    const boxProps = pickBoxProps(others);
 
     return (
-      <Box className={avatarClassNames}>
+      <Box className={avatarClassNames} {...boxProps}>
         {imageUrl ? (
           <AvatarImage image={imageUrl} imageAlt={fullName} {...others} />
         ) : (
