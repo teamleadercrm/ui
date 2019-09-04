9ce6853e27373ea7524b92cb5e45a398e29a1dfd
Dries Decramer <dries.decramer@teamleader.be>
2019-09-04 16:23:22 +0200

    Update stories

--- stories/avatars.js
+++ stories/avatars.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, number, select, text } from '@storybook/addon-knobs';
-import { Avatar, AvatarImage, AvatarInitials, AvatarStack, Bullet, Counter, TextBody, Tooltip } from '../src';
+import { Avatar, AvatarStack, Bullet, Counter, TextBody, Tooltip } from '../src';
 import avatars from './static/data/avatar';
 
 const directions = ['horizontal', 'vertical'];
 const sizes = ['tiny', 'small', 'medium', 'large', 'hero'];
-const shapes = [null, 'circle', 'rounded'];
-const colors = ['teal', 'neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua'];
+const shapes = ['circle', 'rounded'];
 
-const TooltippedAvatarImage = Tooltip(AvatarImage);
+const TooltippedAvatar = Tooltip(Avatar);
 
 storiesOf('Avatars', module)
   .addParameters({
@@ -19,26 +18,13 @@ storiesOf('Avatars', module)
   })
   .add('Avatar', () => (
     <Avatar
+      editable={boolean('Editable', false)}
       fullName={text('Full name', 'John Doe')}
       id="63227a3c-c80b-11e9-a32f-2a2ae2dbcce4"
       imageUrl={boolean('Image available', false) ? avatars[0].image : null}
-      size={select('Size', sizes, 'medium')}
-      shape={select('Shape', shapes) || undefined}
-    />
-  ))
-  .add('AvatarImage', () => (
-    <AvatarImage
-      image={avatars[0].image}
-      size={select('Size', sizes, 'medium')}
-      shape={select('Shape', shapes) || undefined}
-    />
-  ))
-  .add('AvatarInitials', () => (
-    <AvatarInitials
-      color={select('Color', colors, 'neutral')}
-      size={select('Size', sizes, 'medium')}
-      shape={select('Shape', shapes) || undefined}
-      name={text('Name', undefined)}
+      onImageChange={() => console.log('Change image')}
+      size={select('Size', sizes, 'large')}
+      shape={select('Shape', shapes, 'circle')}
     />
   ))
   .add('AvatarStack', () => (
@@ -46,27 +32,48 @@ storiesOf('Avatars', module)
       direction={select('Direction', directions, 'horizontal')}
       displayMax={number('Display max', 5)}
       inverse={boolean('Inverse', false)}
-      size={select('Size', sizes, 'medium')}
+      size={select('Size', sizes, 'large')}
     >
       {avatars.map(({ image }, index) => (
-        <AvatarImage key={index} image={image} />
+        <Avatar key={index} imageUrl={image} />
       ))}
     </AvatarStack>
   ))
   .add('With bullet', () => (
-    <AvatarImage image={avatars[0].image} size={select('Size', sizes, 'medium')}>
+    <Avatar
+      editable={boolean('Editable', false)}
+      fullName={text('Full name', 'John Doe')}
+      id="63227a3c-c80b-11e9-a32f-2a2ae2dbcce4"
+      imageUrl={boolean('Image available', false) ? avatars[0].image : null}
+      onImageChange={() => console.log('Change image')}
+      size={select('Size', sizes, 'large')}
+      shape={select('Shape', shapes, 'circle')}
+    >
       <Bullet borderColor="neutral" borderTint="lightest" color="ruby" />
-    </AvatarImage>
+    </Avatar>
   ))
   .add('With counter', () => (
-    <AvatarImage image={avatars[0].image} size={select('Size', sizes, 'medium')}>
+    <Avatar
+      editable={boolean('Editable', false)}
+      fullName={text('Full name', 'John Doe')}
+      id="63227a3c-c80b-11e9-a32f-2a2ae2dbcce4"
+      imageUrl={boolean('Image available', false) ? avatars[0].image : null}
+      onImageChange={() => console.log('Change image')}
+      size={select('Size', sizes, 'large')}
+      shape={select('Shape', shapes, 'circle')}
+    >
       <Counter color="ruby" count={15} maxCount={6} borderColor="neutral" borderTint="lightest" />
-    </AvatarImage>
+    </Avatar>
   ))
   .add('With tooltip', () => (
-    <TooltippedAvatarImage
-      image={avatars[0].image}
-      size={select('Size', sizes, 'medium')}
+    <TooltippedAvatar
+      editable={boolean('Editable', false)}
+      fullName={text('Full name', 'John Doe')}
+      id="63227a3c-c80b-11e9-a32f-2a2ae2dbcce4"
+      imageUrl={boolean('Image available', false) ? avatars[0].image : null}
+      onImageChange={() => console.log('Change image')}
+      size={select('Size', sizes, 'large')}
+      shape={select('Shape', shapes, 'circle')}
       tooltip={<TextBody>I am the tooltip</TextBody>}
     />
   ));
