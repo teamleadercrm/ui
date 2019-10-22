df007107f4abaac0f16ec76351da2a71c9c8e025
Arnaud Weyts <arnaud.weyts@gmail.com>
2019-10-22 14:49:09 +0200

    :recycle: don't pass boxProps to child components

--- src/components/avatar/Avatar.js
+++ src/components/avatar/Avatar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import AvatarImage from './AvatarImage';
 import AvatarInitials from './AvatarInitials';
 import AvatarOverlay from './AvatarOverlay';
-import Box, { pickBoxProps } from '../box';
+import Box, { pickBoxProps, omitBoxProps } from '../box';
 import cx from 'classnames';
 import theme from './theme.css';
 import { colorsWithout } from '../../constants';
@@ -19,13 +19,14 @@ class Avatar extends PureComponent {
     const { className, editable, imageUrl, fullName, id, onImageChange, size, shape, ...others } = this.props;
     const avatarClassNames = cx(theme['avatar'], theme[`is-${size}`], theme[`is-${shape}`], className);
     const boxProps = pickBoxProps(others);
+    const propsWithoutBoxProps = omitBoxProps(others);
 
     return (
       <Box className={avatarClassNames} {...boxProps}>
         {imageUrl ? (
-          <AvatarImage image={imageUrl} imageAlt={fullName} {...others} />
+          <AvatarImage image={imageUrl} imageAlt={fullName} {...propsWithoutBoxProps} />
         ) : (
-          <AvatarInitials color={getColor(id)} name={fullName} {...others} />
+          <AvatarInitials color={getColor(id)} name={fullName} {...propsWithoutBoxProps} />
         )}
         {editable && (size === 'large' || size === 'hero') && <AvatarOverlay onClick={onImageChange} />}
       </Box>
