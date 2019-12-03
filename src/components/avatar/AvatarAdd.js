cae95540b896d30e42ac1e17d5b8f8b94f64ff53
Dries Decramer <dries.decramer@teamleader.be>
2019-12-03 14:09:44 +0100

    AvatarAdd component

---
+++ src/components/avatar/AvatarAdd.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import theme from './theme.css';
+import Box from '../box';
+import Icon from '../icon';
+import { IconUserAddMediumOutline, IconUserAddSmallOutline } from '@teamleader/ui-icons';
+
+class AvatarAdd extends PureComponent {
+  render() {
+    const { size } = this.props;
+
+    return (
+      <Box
+        backgroundColor="neutral"
+        backgroundTint="normal"
+        className={theme['avatar-add']}
+        data-teamleader-ui="avatar-add"
+      >
+        <Icon color="neutral" tint="darkest">
+          {size === 'tiny' || size === 'small' ? <IconUserAddSmallOutline /> : <IconUserAddMediumOutline />}
+        </Icon>
+      </Box>
+    );
+  }
+}
+
+AvatarAdd.propTypes = {
+  /** The size of the avatar. */
+  size: PropTypes.oneOf(['tiny', 'small', 'medium', 'large', 'hero']),
+};
+
+export default AvatarAdd;
