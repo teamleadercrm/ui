b791352cc395cd36aaeec8a699c05fc89162c8f7
Dries Decramer <dries.decramer@teamleader.be>
2020-01-09 10:12:39 +0100

    Check if document exists while setting menuPortalTarget's default value

--- src/components/select/Select.js
+++ src/components/select/Select.js
@@ -371,7 +371,7 @@ Select.propTypes = {
 Select.defaultProps = {
   creatable: false,
   inverse: false,
-  menuPortalTarget: document.body,
+  menuPortalTarget: document && document.body,
   size: 'medium',
   width: '100%',
 };
