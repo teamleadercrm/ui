5c273076ff647cf3c9556f4abefb29e37aaed15b
Dries Decramer <dries.decramer@teamleader.be>
2020-02-07 16:18:07 +0100

    Also parse input value when setting state based on props

--- src/components/input/NumericInput.js
+++ src/components/input/NumericInput.js
@@ -43,7 +43,7 @@ class NumericInput extends PureComponent {
       const newValue = nextProps.value || '';
       if (newValue !== prevState.value) {
         return {
-          value: newValue,
+          value: parseValue(newValue, nextProps.min, nextProps.max),
         };
       }
     }
