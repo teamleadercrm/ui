3972fdc25a99d5cafa4192548cdac37e98b49996
Dries Decramer <dries.decramer@teamleader.be>
2020-02-20 11:47:26 +0100

    SuccessText: remove the Box wrapper

--- src/components/validationText/SuccessText.js
+++ src/components/validationText/SuccessText.js
@@ -1,25 +1,22 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { TextSmall } from '../typography';
-import Box from '../box';
 
 export default class SuccessText extends PureComponent {
   render() {
     const { children, className, inverse, ...others } = this.props;
 
     return (
-      <Box
+      <TextSmall
         className={className}
-        alignItems="center"
+        color="mint"
         data-teamleader-ui="success-text"
-        display="flex"
-        marginTop={2}
+        marginTop={1}
+        tint={inverse ? 'light' : 'dark'}
         {...others}
       >
-        <TextSmall color="mint" element="span" marginLeft={1} tint={inverse ? 'light' : 'dark'}>
-          {children}
-        </TextSmall>
-      </Box>
+        {children}
+      </TextSmall>
     );
   }
 }
