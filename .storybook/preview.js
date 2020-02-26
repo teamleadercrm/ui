a011270949b63ec086b09b5ad7a286c919ecc0af
Dries Decramer <dries.decramer@teamleader.be>
2020-02-26 11:21:21 +0100

    Replace addon-info by addon-docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,5 @@
-import { addDecorator, addParameters } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
+import { addParameters } from '@storybook/react';
 import { COLOR } from '../src/constants';
-import styles from '@sambego/storybook-styles';
-import PropTable from './components/propTable';
 
 addParameters({
   backgrounds: [
@@ -16,76 +13,3 @@ addParameters({
     { name: 'Teal darkest', value: COLOR.TEAL.DARKEST },
   ],
 });
-
-// addon-info
-addDecorator(
-  withInfo({
-    inline: true,
-    source: true,
-    styles: stylesheet => {
-      stylesheet.infoBody = {
-        color: COLOR.TEAL.DARKEST,
-        fontFamily: 'Inter-Regular',
-        fontSize: '14px',
-        margin: '48px 0',
-        padding: '0',
-      };
-
-      stylesheet.header = {
-        h1: {
-          fontFamily: 'Inter-Medium',
-          fontSize: '24px',
-          fontWeight: 500,
-          lineHeight: '30px',
-          margin: 0,
-        },
-        h2: {
-          color: COLOR.NEUTRAL.DARKEST,
-          fontFamily: 'Inter-Medium',
-          fontSize: '18px',
-          fontWeight: 500,
-          lineHeight: '24px',
-          margin: '24px 0 10px 0',
-        },
-        body: {
-          borderBottom: `1px solid ${COLOR.NEUTRAL.NORMAL}`,
-          paddingTop: 10,
-          marginBottom: 10,
-        },
-      };
-
-      stylesheet.source = {
-        h1: {
-          borderBottom: `1px solid ${COLOR.NEUTRAL.NORMAL}`,
-          color: COLOR.NEUTRAL.DARKEST,
-          fontFamily: 'Inter-Medium',
-          margin: '24px 0 10px 0',
-          fontWeight: 500,
-          fontSize: '18px',
-          lineHeight: '24px',
-          padding: '12px 0',
-        },
-      };
-
-      stylesheet.propTableHead = {
-        color: COLOR.TEAL.DARKEST,
-        fontFamily: 'Inter-Medium',
-        fontWeight: 500,
-        fontSize: '16px',
-        lineHeight: '21px',
-        margin: '20px 0 0 0',
-      };
-
-      return stylesheet;
-    },
-    TableComponent: PropTable,
-  }),
-);
-
-// addon-styles
-addDecorator(
-  styles({
-    margin: '24px auto',
-    maxWidth: '90%',
-  }),
-);
