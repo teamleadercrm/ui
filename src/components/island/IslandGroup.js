b34fa1cb30f6967f179a0ee6707bad5550226369
Dries Decramer <dries.decramer@teamleader.be>
2020-03-16 14:08:53 +0100

    IslandGroup: use flex Box props for border radius instead of CSS styles

--- src/components/island/IslandGroup.js
+++ src/components/island/IslandGroup.js
@@ -12,10 +12,43 @@ class IslandGroup extends PureComponent {
 
     const classNames = cx(theme[`direction-${direction}`], theme['island-group'], className);
 
+    const hasMoreThanOneChild = children.length > 1;
+
     return (
       <Box {...boxProps} className={classNames}>
-        {React.Children.map(children, child => {
+        {React.Children.map(children, (child, index) => {
+          const isFirstChild = index === 0;
+          const isLastChild = index === children.length - 1;
+
           return React.cloneElement(child, {
+            ...(!isFirstChild &&
+              !isLastChild && {
+                borderRadius: 'none',
+              }),
+            ...(direction === 'horizontal' &&
+              hasMoreThanOneChild &&
+              isFirstChild && {
+                borderBottomRightRadius: 'square',
+                borderTopRightRadius: 'square',
+              }),
+            ...(direction === 'horizontal' &&
+              hasMoreThanOneChild &&
+              isLastChild && {
+                borderBottomLeftRadius: 'square',
+                borderTopLeftRadius: 'square',
+              }),
+            ...(direction === 'vertical' &&
+              hasMoreThanOneChild &&
+              isFirstChild && {
+                borderBottomLeftRadius: 'square',
+                borderBottomRightRadius: 'square',
+              }),
+            ...(direction === 'vertical' &&
+              hasMoreThanOneChild &&
+              isLastChild && {
+                borderTopLeftRadius: 'square',
+                borderTopRightRadius: 'square',
+              }),
             ...child.props,
             color: color || child.props.color,
             dark: dark || child.props.dark,
