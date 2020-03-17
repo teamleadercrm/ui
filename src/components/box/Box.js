d6c28e203a648ebe689cb6c192c3c99eb576d3aa
Dries Decramer <dries.decramer@teamleader.be>
2020-03-17 17:04:56 +0100

    Add negative margin values to the array

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -5,7 +5,7 @@ import { COLOR, COLORS, TINTS } from '../../constants';
 import theme from './theme.css';
 
 const overflows = ['auto', 'hidden', 'scroll', 'visible'];
-const margins = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const margins = [-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8];
 const paddings = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 const borderRadiuses = {
