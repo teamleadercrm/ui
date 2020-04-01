e6d3656c0937fc62ccce3220500892d2d76a6fdc
Dries Decramer <dries.decramer@teamleader.be>
2020-04-01 18:21:40 +0200

    Add OverviewPageHeader component

---
+++ src/components/overviewPage/OverviewPageHeader.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Box from '../box';
+import Container from '../container';
+import { Heading1 } from '../typography';
+
+class OverviewPageHeader extends PureComponent {
+  render() {
+    const { children, title, ...others } = this.props;
+
+    return (
+      <Container {...others} display="flex" marginBottom={5} paddingTop={7} justifyContent="space-between">
+        <Heading1 color="teal">{title}</Heading1>
+        {children && <Box>{children}</Box>}
+      </Container>
+    );
+  }
+}
+
+OverviewPageHeader.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string.isRequired,
+};
+
+export default OverviewPageHeader;
