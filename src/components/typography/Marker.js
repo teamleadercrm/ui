7107ebda58c6ac8dad97897b355f24fb5665eb7b
Dries Decramer <dries.decramer@teamleader.be>
2020-04-06 14:25:25 +0200

    Add component

---
+++ src/components/typography/Marker.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Box from '../box';
+import cx from 'classnames';
+import theme from './theme.css';
+
+class Marker extends Component {
+  render() {
+    const { children, className, ...others } = this.props;
+
+    const classNames = cx(theme['marker'], className);
+
+    return (
+      <Box {...others} className={classNames} element="mark" paddingHorizontal={1} marginHorizontal={-1}>
+        {children}
+      </Box>
+    );
+  }
+}
+
+Marker.propTypes = {
+  children: PropTypes.oneOf([PropTypes.node, PropTypes.string]),
+};
+
+export default Marker;
