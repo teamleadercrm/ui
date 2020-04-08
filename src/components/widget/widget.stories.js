a708c23767fd582e14081d0584841fa78f58a6b8
Dries Decramer <dries.decramer@teamleader.be>
2020-04-08 13:03:22 +0200

    Widget: update stories - reuse existing DataGrid story

--- src/components/widget/widget.stories.js
+++ src/components/widget/widget.stories.js
@@ -1,32 +1,25 @@
 import React from 'react';
 import { addStoryInGroup, COMPOSITIONS } from '../../../.storybook/utils';
-import { boolean, select } from '@storybook/addon-knobs/react';
+import { select } from '@storybook/addon-knobs/react';
 import {
   Box,
-  Button,
   ButtonGroup,
-  DataGrid,
   DatePicker,
   Heading3,
   Heading5,
   IconButton,
   IconMenu,
-  Link,
   MenuItem,
-  StatusBullet,
   TextBody,
-  TextSmall,
-  Tooltip,
   Widget,
   MenuDivider,
 } from '../../index';
-import { rows1 } from '../../static/data/datagrid';
 import { IconAddMediumOutline, IconEditMediumOutline } from '@teamleader/ui-icons';
+import { basic as DataGrid } from '../datagrid/datagrid.stories';
 import { basic as EmptyState, withTitle as EmptyStateWithTitle } from '../emptyState/emptyState.stories';
 
 const colors = ['mint', 'violet', 'ruby', 'gold', 'aqua', 'white', 'neutral'];
 const sizes = ['small', 'medium', 'large'];
-const TooltippedStatusBullet = Tooltip(StatusBullet);
 
 export default {
   title: addStoryInGroup(COMPOSITIONS, 'Widget'),
@@ -192,74 +185,7 @@ withDatePicker.story = {
 export const withDataGridOnly = () => (
   <Widget size={select('Size', sizes, 'medium')}>
     <Widget.Body padding={0}>
-      <DataGrid
-        bordered={boolean('bordered', false)}
-        selectable={boolean('Selectable', true)}
-        comparableId={1}
-        onSelectionChange={() => console.log('Datagrid selection changed')}
-        checkboxSize={select('Checkbox size', ['small', 'medium', 'large'], 'small')}
-        padding={0}
-        processing={boolean('Processing', false)}
-      >
-        <DataGrid.HeaderRowOverlay
-          numSelectedRowsLabel={(numSelectedRows) => (numSelectedRows === 1 ? 'sélectionné' : 'sélectionnés')}
-        >
-          <Button size="small" level="primary" label="Marks as paid" />
-          <ButtonGroup segmented marginHorizontal={3}>
-            <Button size="small" label="Book" />
-            <Button size="small" label="Merge" />
-          </ButtonGroup>
-          <Button size="small" level="destructive" label="Delete" />
-        </DataGrid.HeaderRowOverlay>
-
-        <DataGrid.HeaderRow>
-          <DataGrid.HeaderCell flex="min-width" />
-          <DataGrid.HeaderCell onClick={() => console.log('onClick: column sort')} sorted="asc">
-            Invoice
-          </DataGrid.HeaderCell>
-          <DataGrid.HeaderCell onClick={() => console.log('onClick: column sort')} align="right">
-            Amount
-          </DataGrid.HeaderCell>
-          <DataGrid.HeaderCell flex="2" onClick={() => console.log('onClick: column sort')}>
-            Customer
-          </DataGrid.HeaderCell>
-          <DataGrid.HeaderCell onClick={() => console.log('onClick: column sort')}>Due date</DataGrid.HeaderCell>
-          <DataGrid.HeaderCell flex="min-width" />
-        </DataGrid.HeaderRow>
-        {rows1.map((row, index) => {
-          return (
-            <DataGrid.BodyRow key={index} onClick={(event) => console.log('onClick:', row.column5, event)}>
-              <DataGrid.Cell align="center" flex="min-width">
-                <TooltippedStatusBullet
-                  color={row.column1}
-                  tooltip={<TextSmall>Overdue</TextSmall>}
-                  tooltipColor={row.column1}
-                  tooltipSize="small"
-                  size="medium"
-                />
-              </DataGrid.Cell>
-              <DataGrid.Cell>
-                <Link href="#" inherit={false}>
-                  {row.column5}
-                </Link>{' '}
-              </DataGrid.Cell>
-              <DataGrid.Cell align="right" strong>
-                {' '}
-                {`€ ${row.column3}`}
-              </DataGrid.Cell>
-              <DataGrid.Cell flex="2">{row.column2}</DataGrid.Cell>
-              <DataGrid.Cell soft>{row.column4}</DataGrid.Cell>
-              <DataGrid.Cell align="right" flex="min-width" preventOverflow={false}>
-                <IconMenu position="top-right">
-                  <MenuItem onClick={() => console.log('onClick: duplicate row')}>Duplicate row</MenuItem>
-                  <MenuItem onClick={() => console.log('onClick: inactivate row')}>Inactive row</MenuItem>
-                  <MenuItem onClick={() => console.log('onClick: remove row')}>Remove row</MenuItem>
-                </IconMenu>
-              </DataGrid.Cell>
-            </DataGrid.BodyRow>
-          );
-        })}
-      </DataGrid>
+      <DataGrid />
     </Widget.Body>
   </Widget>
 );
@@ -274,73 +200,7 @@ export const withDataGrid = () => (
       <Heading3>I am the widget header title</Heading3>
     </Widget.Header>
     <Widget.Body padding={0}>
-      <DataGrid
-        bordered={boolean('Bordered', false)}
-        selectable={boolean('Selectable', true)}
-        comparableId={1}
-        onSelectionChange={() => console.log('Datagrid selection changed')}
-        checkboxSize={select('Checkbox size', ['small', 'medium', 'large'], 'small')}
-        processing={boolean('Processing', false)}
-      >
-        <DataGrid.HeaderRowOverlay
-          numSelectedRowsLabel={(numSelectedRows) => (numSelectedRows === 1 ? 'sélectionné' : 'sélectionnés')}
-        >
-          <Button size="small" level="primary" label="Marks as paid" />
-          <ButtonGroup segmented marginHorizontal={3}>
-            <Button size="small" label="Book" />
-            <Button size="small" label="Merge" />
-          </ButtonGroup>
-          <Button size="small" level="destructive" label="Delete" />
-        </DataGrid.HeaderRowOverlay>
-
-        <DataGrid.HeaderRow>
-          <DataGrid.HeaderCell flex="min-width" />
-          <DataGrid.HeaderCell onClick={() => console.log('onClick: column sort')} sorted="asc">
-            Invoice
-          </DataGrid.HeaderCell>
-          <DataGrid.HeaderCell onClick={() => console.log('onClick: column sort')} align="right">
-            Amount
-          </DataGrid.HeaderCell>
-          <DataGrid.HeaderCell flex="2" onClick={() => console.log('onClick: column sort')}>
-            Customer
-          </DataGrid.HeaderCell>
-          <DataGrid.HeaderCell onClick={() => console.log('onClick: column sort')}>Due date</DataGrid.HeaderCell>
-          <DataGrid.HeaderCell flex="min-width" />
-        </DataGrid.HeaderRow>
-        {rows1.map((row, index) => {
-          return (
-            <DataGrid.BodyRow key={index} onClick={(event) => console.log('onClick:', row.column5, event)}>
-              <DataGrid.Cell align="center" flex="min-width">
-                <TooltippedStatusBullet
-                  color={row.column1}
-                  tooltip={<TextSmall>Overdue</TextSmall>}
-                  tooltipColor={row.column1}
-                  tooltipSize="small"
-                  size="medium"
-                />
-              </DataGrid.Cell>
-              <DataGrid.Cell>
-                <Link href="#" inherit={false}>
-                  {row.column5}
-                </Link>{' '}
-              </DataGrid.Cell>
-              <DataGrid.Cell align="right" strong>
-                {' '}
-                {`€ ${row.column3}`}
-              </DataGrid.Cell>
-              <DataGrid.Cell flex="2">{row.column2}</DataGrid.Cell>
-              <DataGrid.Cell soft>{row.column4}</DataGrid.Cell>
-              <DataGrid.Cell align="right" flex="min-width" preventOverflow={false}>
-                <IconMenu position="top-right">
-                  <MenuItem onClick={() => console.log('onClick: duplicate row')}>Duplicate row</MenuItem>
-                  <MenuItem onClick={() => console.log('onClick: inactivate row')}>Inactive row</MenuItem>
-                  <MenuItem onClick={() => console.log('onClick: remove row')}>Remove row</MenuItem>
-                </IconMenu>
-              </DataGrid.Cell>
-            </DataGrid.BodyRow>
-          );
-        })}
-      </DataGrid>
+      <DataGrid />
     </Widget.Body>
     <Widget.Footer>
       <Box display="flex" alignItems="center" justifyContent="space-between">
