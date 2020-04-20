16c9c61e618f351b711b73d201a40d477cf83355
mikeverf <verfaillie.mike@gmail.com>
2020-04-20 15:51:02 +0200

    :sparkles: Show tooltip with shortcut for inline styling

--- src/components/wysiwygEditor/InlineStylingOptions.js
+++ src/components/wysiwygEditor/InlineStylingOptions.js
@@ -2,6 +2,36 @@ import React from 'react';
 
 import Box from '../box';
 import IconButton from '../iconButton';
+import Tooltip from '../tooltip';
+import { TextSmall } from '../typography';
+import getOS, { OS_TYPES } from '../../utils/getOS';
+
+const TooltippedIconButton = Tooltip(IconButton);
+
+const OPERATING_SYSTEM = getOS();
+
+const TooltipMessage = ({ optionType }) => {
+  const isMacOS = OPERATING_SYSTEM === OS_TYPES.MAC_OS;
+  const shortcutsByOptionType = {
+    bold: {
+      name: 'Bold',
+      shortcut: isMacOS ? '⌘ Cmd + B' : 'Ctrl + B',
+    },
+    italic: {
+      name: 'Italic',
+      shortcut: isMacOS ? '⌘ Cmd + I' : 'Ctrl + I',
+    },
+  };
+
+  return (
+    <>
+      <TextSmall>
+        <strong>{shortcutsByOptionType[optionType].name}</strong>
+      </TextSmall>
+      <TextSmall color="neutral">{shortcutsByOptionType[optionType].shortcut}</TextSmall>
+    </>
+  );
+};
 
 const InlineStylingOptions = ({
   config: {
@@ -20,7 +50,9 @@ const InlineStylingOptions = ({
   return (
     <Box display="flex" borderRightWidth={1} marginRight={2}>
       {options.map((optionType) => (
-        <IconButton
+        <TooltippedIconButton
+          tooltip={<TooltipMessage optionType={optionType} />}
+          tooltipSize="small"
           icon={iconsByOptionType[optionType]}
           color="black"
           key={optionType}
