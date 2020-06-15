e8169de7593d2768e204cfd54f2ff02f2818fe6f
Dries Decramer <dries.decramer@teamleader.be>
2020-06-15 18:28:37 +0200

    Remove IconTab component

--- src/components/tab/index.js
+++ src/components/tab/index.js
@@ -1,5 +1,4 @@
-import IconTab from './IconTab';
 import TabGroup from './TabGroup';
 import TitleTab from './TitleTab';
 
-export { IconTab, TabGroup, TitleTab };
+export { TabGroup, TitleTab };
--- src/components/tab/tab.stories.js
+++ src/components/tab/tab.stories.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import { select } from '@storybook/addon-knobs';
 import { addStoryInGroup, MID_LEVEL_BLOCKS } from '../../../.storybook/utils';
-import { IconTab, TabGroup, TitleTab, Box, Counter as UICounter } from '../../index';
+import { TabGroup, TitleTab, Box, Counter as UICounter } from '../../index';
 
 import { tabItems } from '../../static/data/tab';
-import {
-  IconCalendarMediumOutline,
-  IconCheckmarkMediumOutline,
-  IconContactsMediumOutline,
-  IconInvoiceMediumOutline,
-  IconProductsMediumOutline,
-  IconStatsMediumOutline,
-  IconTimerMediumOutline,
-} from '@teamleader/ui-icons';
-
-const iconMap = {
-  crm: IconContactsMediumOutline,
-  invoices: IconInvoiceMediumOutline,
-  products: IconProductsMediumOutline,
-  stats: IconStatsMediumOutline,
-  time: IconTimerMediumOutline,
-  deals: IconCheckmarkMediumOutline,
-  planning: IconCalendarMediumOutline,
-};
 
 // Real-life tabs should not have a clickHandler but rather listen to state updates
 const handleClick = (event, title) => {
@@ -30,7 +11,6 @@ const handleClick = (event, title) => {
 };
 
 const TitleCounter = (props) => <UICounter color="ruby" marginLeft={3} {...props} />;
-const IconCounter = (props) => <UICounter color="ruby" {...props} />;
 
 export default {
   title: addStoryInGroup(MID_LEVEL_BLOCKS, 'Tab'),
@@ -77,39 +57,4 @@ export const titleTab = () => (
 
 titleTab.story = {
   name: 'Title tab',
-};
-
-export const iconTab = () => (
-  <TabGroup size={select('Size', ['small', 'medium'], 'medium')}>
-    {tabItems.map((item, key) => {
-      const optionalProps = item.count
-        ? {
-            counter: React.createElement(IconCounter, {
-              count: item.count,
-              color: item.active ? 'mint' : 'neutral',
-              size: select('Size', ['small', 'medium'], 'medium'),
-            }),
-          }
-        : {};
-      const IconToRender = iconMap[item.icon.toLowerCase()];
-      return (
-        <IconTab
-          active={item.active}
-          href={item.href}
-          onClick={(event) => {
-            handleClick(event, item.title);
-          }}
-          icon={<IconToRender />}
-          {...optionalProps}
-          key={key}
-        >
-          {item.title}
-        </IconTab>
-      );
-    })}
-  </TabGroup>
-);
-
-iconTab.story = {
-  name: 'Icon tab',
 };
