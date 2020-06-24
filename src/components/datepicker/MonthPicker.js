0695bbb0c44f81f02bbba2f1c6a9ad1928225009
Dries Decramer <dries.decramer@teamleader.be>
2020-06-24 16:02:57 +0200

    French language exception: use 4 characters for short month labels

--- src/components/datepicker/MonthPicker.js
+++ src/components/datepicker/MonthPicker.js
@@ -29,12 +29,19 @@ const getMonthOptions = (localeUtils, locale) => {
 };
 
 // e.g. "February 2020" => "Feb"
-const formatSelectedMonth = ({ label, value }) => ({ value, label: label.substring(0, 3) });
+// Exception for French language: we use 4 characters
+const formatSelectedMonth = ({ label, value }, locale) => ({
+  value,
+  label: label.substring(0, locale.startsWith('fr-') ? 4 : 3),
+});
 
 // e.g. "February 2020" => "Feb 2020"
-const formatSelectMonthAndYear = ({ label, value }) => ({
+// Exception for French language: we use 4 characters
+const formatSelectMonthAndYear = ({ label, value }, locale) => ({
   value,
-  label: label.replace(/(\w{3})\w+\s(\d{4})/, '$1 $2'),
+  label: locale.startsWith('fr-')
+    ? label.replace(/(\w{4})\w+\s(\d{4})/, '$1 $2')
+    : label.replace(/(\w{3})\w+\s(\d{4})/, '$1 $2'),
 });
 
 const MonthPickerUnary = ({ date, locale, localeUtils, onChange }) => {
@@ -50,7 +57,7 @@ const MonthPickerUnary = ({ date, locale, localeUtils, onChange }) => {
     <Box className={theme['caption']}>
       <Box display="flex" justifyContent="center">
         <Select
-          value={formatSelectMonthAndYear(selectedMonth)}
+          value={formatSelectMonthAndYear(selectedMonth, locale)}
           className={theme['month-picker-field']}
           options={getMonthOptions(localeUtils, locale)}
           onChange={handleChangeMonth}
@@ -96,7 +103,7 @@ const MonthPickerSplit = ({ date, locale, localeUtils, onChange, size }) => {
     <Box className={theme['caption']}>
       <Box display="flex" justifyContent="center">
         <Select
-          value={formatSelectedMonth(selectedMonth)}
+          value={formatSelectedMonth(selectedMonth, locale)}
           className={theme['month-picker-field']}
           options={months}
           onChange={handleChangeMonth}
