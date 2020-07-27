56dc8f90a09d2445f83ab9b97c61957eff082f94
Dries Decramer <dries.decramer@teamleader.be>
2020-07-27 11:41:48 +0200

    Fix config for the backgrounds addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,16 +6,18 @@ import styles from '@sambego/storybook-styles';
 import PropTable from './components/propTable';
 
 addParameters({
-  backgrounds: [
-    { name: 'Aqua lightest', value: COLOR.AQUA.LIGHTEST },
-    { name: 'Neutral light', value: COLOR.NEUTRAL.LIGHT },
-    { name: 'Gold lightest', value: COLOR.GOLD.LIGHTEST },
-    { name: 'Mint lightest', value: COLOR.MINT.LIGHTEST },
-    { name: 'Ruby lightest', value: COLOR.RUBY.LIGHTEST },
-    { name: 'Teal lightest', value: COLOR.TEAL.LIGHTEST },
-    { name: 'Violet lightest', value: COLOR.VIOLET.LIGHTEST },
-    { name: 'Teal darkest', value: COLOR.TEAL.DARKEST },
-  ],
+  backgrounds: {
+    values: [
+      { name: 'Aqua lightest', value: COLOR.AQUA.LIGHTEST },
+      { name: 'Neutral light', value: COLOR.NEUTRAL.LIGHT },
+      { name: 'Gold lightest', value: COLOR.GOLD.LIGHTEST },
+      { name: 'Mint lightest', value: COLOR.MINT.LIGHTEST },
+      { name: 'Ruby lightest', value: COLOR.RUBY.LIGHTEST },
+      { name: 'Teal lightest', value: COLOR.TEAL.LIGHTEST },
+      { name: 'Violet lightest', value: COLOR.VIOLET.LIGHTEST },
+      { name: 'Teal darkest', value: COLOR.TEAL.DARKEST },
+    ],
+  },
 });
 
 addParameters({
