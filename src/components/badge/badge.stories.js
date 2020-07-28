749a22457c2788d27b66a89c035072e5b5f72408
Dries Decramer <dries.decramer@teamleader.be>
2020-07-28 12:45:59 +0200

    Badge: update story

--- src/components/badge/badge.stories.js
+++ src/components/badge/badge.stories.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
-import { boolean, select } from '@storybook/addon-knobs';
 import { IconBuildingSmallOutline } from '@teamleader/ui-icons';
-import { Badge, TextDisplay } from '../../index';
-
-const iconPositions = ['left', 'right'];
-const sizes = ['small', 'medium', 'large'];
+import Badge from './Badge';
 
 export default {
   component: Badge,
@@ -16,45 +12,27 @@ export default {
       type: 'figma',
       url: 'https://www.figma.com/file/LHH25GN90ljQaBEUNMsdJn/Desktop-components?node-id=3943%3A715',
     },
-    info: {
-      propTablesExclude: [TextDisplay],
-    },
   },
 };
 
-export const basic = () => (
-  <Badge disabled={boolean('Disabled', false)} size={select('Size', sizes, 'medium')}>
-    I'm a badge
-  </Badge>
-);
+export const basic = (args) => <Badge {...args}>I'm a badge</Badge>;
 
-export const withIcon = () => (
-  <Badge
-    disabled={boolean('Disabled', false)}
-    icon={<IconBuildingSmallOutline />}
-    iconPlacement={select('Icon placement', iconPositions, 'left')}
-    selected={boolean('Selected', false)}
-    size={select('Size', sizes, 'medium')}
-  >
-    I'm a badge
-  </Badge>
-);
+export const withIcon = (args) => <Badge {...args}>I'm a badge</Badge>;
+
+withIcon.args = {
+  icon: <IconBuildingSmallOutline />,
+};
 
 withIcon.story = {
   name: 'With icon',
 };
 
-export const withCustomElement = () => (
-  <Badge
-    disabled={boolean('Disabled', false)}
-    element="a"
-    href="https://teamleader.eu"
-    selected={boolean('Selected', false)}
-    size={select('Size', sizes, 'medium')}
-  >
-    I'm a badge
-  </Badge>
-);
+export const withCustomElement = (args) => <Badge {...args}>I'm a badge</Badge>;
+
+withCustomElement.args = {
+  element: 'a',
+  href: 'https://teamleader.eu',
+};
 
 withCustomElement.story = {
   name: 'With custom element',
