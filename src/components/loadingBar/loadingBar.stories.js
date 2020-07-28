083a7d00de66bc035733d362889a84976db76b3d
Dries Decramer <dries.decramer@teamleader.be>
2020-07-28 13:51:08 +0200

    LoadingBar: update story

--- src/components/loadingBar/loadingBar.stories.js
+++ src/components/loadingBar/loadingBar.stories.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
-import { select } from '@storybook/addon-knobs';
-import { LoadingBar } from '../../index';
-
-const colors = ['neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua', 'teal'];
-const sizes = ['small', 'medium', 'large'];
-const tints = ['lightest', 'light', 'normal', 'dark', 'darkest'];
+import LoadingBar from './LoadingBar';
 
 export default {
   component: LoadingBar,
   title: addStoryInGroup(LOW_LEVEL_BLOCKS, 'LoadingBar'),
 };
 
-export const basic = () => (
-  <LoadingBar
-    color={select('Color', colors, 'mint')}
-    size={select('Size', sizes, 'small')}
-    tint={select('Tint', tints, 'normal')}
-  />
-);
+export const basic = (args) => <LoadingBar {...args} />;
