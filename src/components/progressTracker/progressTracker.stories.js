4f003043e5f3f3cb84b61d2272524957316dab15
Dries Decramer <dries.decramer@teamleader.be>
2020-07-30 12:00:47 +0200

    ProgressTracker: updated stories

--- src/components/progressTracker/progressTracker.stories.js
+++ src/components/progressTracker/progressTracker.stories.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { addStoryInGroup, MID_LEVEL_BLOCKS } from '../../../.storybook/utils';
-import { number, boolean } from '@storybook/addon-knobs';
-import { ProgressTracker, Island } from '../../index';
+import ProgressTracker from './ProgressTracker';
 
 const steps = ['Draft', 'Book', 'Send invoices', 'Get paid'];
-const options = {
-  range: true,
-  min: 0,
-  max: steps.length - 1,
-  step: 1,
-};
 
 export default {
   component: ProgressTracker,
@@ -20,28 +13,17 @@ export default {
       type: 'figma',
       url: 'https://www.figma.com/file/LHH25GN90ljQaBEUNMsdJn/Desktop-components?node-id=865%3A0',
     },
-    info: {
-      propTablesExclude: [Island],
-    },
   },
 };
 
-export const basic = () => (
-  <Island size="small">
-    <ProgressTracker currentStep={number('Current step', 1, options)} done={boolean('Completed', false)}>
-      {steps.map((step, index) => (
-        <ProgressTracker.ProgressStep label={step} key={index} />
-      ))}
-    </ProgressTracker>
-  </Island>
+export const DefaultStory = (args) => (
+  <ProgressTracker {...args}>
+    {steps.map((step, index) => (
+      <ProgressTracker.ProgressStep label={step} key={index} onClick={() => console.log('step clicked')} />
+    ))}
+  </ProgressTracker>
 );
 
-export const interactional = () => (
-  <Island size="small">
-    <ProgressTracker currentStep={number('Current step', 1, options)} done={boolean('Completed', false)}>
-      {steps.map((step, index) => (
-        <ProgressTracker.ProgressStep label={step} key={index} onClick={() => console.log('step clicked')} />
-      ))}
-    </ProgressTracker>
-  </Island>
-);
+DefaultStory.args = {
+  currentStep: 1,
+};
