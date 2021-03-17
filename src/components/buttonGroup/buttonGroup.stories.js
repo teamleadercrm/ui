355e892013aefe1f8cf44043278e98c8b7fecac1
Tim De Groote <timdg@protonmail.com>
2021-03-17 11:06:40 +0100

    Replace storybook-state usage from buttonGroup story with useState

--- src/components/buttonGroup/buttonGroup.stories.js
+++ src/components/buttonGroup/buttonGroup.stories.js
@@ -1,17 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
-import { Store, State } from '@sambego/storybook-state';
 import { IconAddMediumOutline } from '@teamleader/ui-icons';
 import Button from '../button';
 import ButtonGroup from './ButtonGroup';
-
-const store = new Store({
-  value: 'option2',
-});
-
-const handleChangeValue = (value, event) => {
-  store.set({ value });
-};
 
 export default {
   component: ButtonGroup,
@@ -26,15 +17,21 @@ export const Normal = (args) => (
   </ButtonGroup>
 );
 
-export const withActive = () => (
-  <State store={store}>
-    <ButtonGroup segmented value="option2" onChange={handleChangeValue} level="secondary">
+export const withActive = () => {
+  const [value, setValue] = useState('option2');
+
+  const handleChange = (value) => {
+    setValue(value);
+  };
+
+  return (
+    <ButtonGroup segmented value={value} onChange={handleChange} level="secondary">
       <Button label="Option 1" value="option1" />
       <Button label="Option 2" value="option2" />
       <Button label="Option 3" value="option3" />
     </ButtonGroup>
-  </State>
-);
+  );
+};
 
 withActive.story = {
   name: 'With active',
