e68cfa7a1c08d337bc6203301eb1db6c5619bef8
Dries Decramer <dries.decramer@teamleader.be>
2021-03-29 18:19:45 +0200

    Convert stories to use addon Docs instead of Knobs

--- src/components/select/select.stories.js
+++ src/components/select/select.stories.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
-import { boolean, select, number, text } from '@storybook/addon-knobs';
 import { Avatar, Box, Label, Select, AsyncSelect, TextBody } from '../../index';
 import { customOptions, groupedOptions, options } from '../../static/data/select';
-
-const sizes = ['small', 'medium', 'large'];
 
 /* eslint react/prop-types: 0 */
 const CustomOption = ({ children, data, innerProps, isFocused, isSelected, isDisabled, selectProps }) => {
@@ -29,6 +26,22 @@ const CustomOption = ({ children, data, innerProps, isFocused, isSelected, isDis
   );
 };
 
+const loadOptions = (searchTerm, pageSize = 10, pageNumber = 1) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const options = [];
+      for (let i = 0; i < pageSize; i++) {
+        const optionNr = (pageNumber - 1) * pageSize + i + 1;
+        options.push({
+          value: `${searchTerm}${optionNr}`,
+          label: `${searchTerm} ${optionNr}`,
+        });
+      }
+      resolve(options);
+    }, 500);
+  });
+};
+
 export default {
   component: Select,
   title: addStoryInGroup(LOW_LEVEL_BLOCKS, 'Form elements/Select'),
@@ -40,115 +53,22 @@ export default {
   },
 };
 
-export const basic = () => (
-  <Select
-    closeMenuOnSelect={boolean('Close menu on select', true)}
-    creatable={boolean('creatable', false)}
-    inverse={boolean('Inverse', false)}
-    isClearable={boolean('Clearable', false)}
-    isDisabled={boolean('Disabled', false)}
-    isMulti={boolean('Multi select', false)}
-    isSearchable={boolean('Searchable', false)}
-    options={options}
-    placeholder="Select your favourite(s)"
-    size={select('Size', sizes, 'medium')}
-    truncateOptionText={boolean('Truncate option text', false)}
-    hideSelectedOptions={boolean('Hide selected options', false)}
-    menuWidth={text('Menu width', undefined)}
-    error={text('error', '')}
-    helpText={text('helpText', '')}
-    success={text('success', '')}
-    warning={text('warning', '')}
-    width={text('width', undefined)}
-  />
-);
+export const basic = (args) => <Select {...args} />;
 
-export const grouped = () => (
-  <Select
-    closeMenuOnSelect={boolean('Close menu on select', true)}
-    creatable={boolean('creatable', false)}
-    inverse={boolean('Inverse', false)}
-    isClearable={boolean('Clearable', false)}
-    isDisabled={boolean('Disabled', false)}
-    isMulti={boolean('Multi select', false)}
-    isSearchable={boolean('Searchable', false)}
-    options={groupedOptions}
-    placeholder="Select your favourite(s)"
-    size={select('Size', sizes, 'medium')}
-    truncateOptionText={boolean('Truncate option text', false)}
-    hideSelectedOptions={boolean('Hide selected options', false)}
-    error={text('error', '')}
-    helpText={text('helpText', '')}
-    success={text('success', '')}
-    warning={text('warning', '')}
-    width={text('width', undefined)}
-  />
-);
-
-export const customOption = () => (
-  <Select
-    closeMenuOnSelect={boolean('Close menu on select', true)}
-    components={{
-      Option: CustomOption,
-    }}
-    creatable={boolean('creatable', false)}
-    inverse={boolean('Inverse', false)}
-    isClearable={boolean('Clearable', false)}
-    isDisabled={boolean('Disabled', false)}
-    isMulti={boolean('Multi select', false)}
-    isSearchable={boolean('Searchable', false)}
-    options={customOptions}
-    placeholder="Select your favourite(s)"
-    size={select('Size', sizes, 'medium')}
-    truncateOptionText={boolean('Truncate option text', false)}
-    hideSelectedOptions={boolean('Hide selected options', false)}
-    error={text('error', '')}
-    helpText={text('helpText', '')}
-    success={text('success', '')}
-    warning={text('warning', '')}
-    width={text('width', undefined)}
-  />
-);
-
-export const async = () => {
-  const loadOptions = (searchTerm, pageSize = 10, pageNumber = 1) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const options = [];
-        for (let i = 0; i < pageSize; i += 1) {
-          const optionNr = (pageNumber - 1) * pageSize + i + 1;
-          options.push({
-            value: `${searchTerm}${optionNr}`,
-            label: `${searchTerm} ${optionNr}`,
-          });
-        }
-        resolve(options);
-      }, 500);
-    });
-  };
-
-  return (
-    <AsyncSelect
-      loadOptions={loadOptions}
-      cacheOptions={boolean('cacheOptions', true)}
-      creatable={boolean('creatable', false)}
-      paginate={boolean('paginate', true)}
-      pageSize={number('pageSize', 10)}
-      closeMenuOnSelect={boolean('closeMenuOnSelectj', true)}
-      inverse={boolean('inverse', false)}
-      isClearable={boolean('isClearable', false)}
-      isDisabled={boolean('isDisabled', false)}
-      isMulti={boolean('isMulti', false)}
-      options={options}
-      placeholder="Select your favourite(s)"
-      size={select('size', sizes, 'medium')}
-      truncateOptionText={boolean('Truncate option text', false)}
-      hideSelectedOptions={boolean('hideSelectedOptions', false)}
-      error={text('error', '')}
-      helpText={text('helpText', '')}
-      success={text('success', '')}
-      warning={text('warning', '')}
-      width={text('width', undefined)}
-    />
-  );
+basic.args = {
+  isClearable: false,
+  isDisabled: false,
+  isMulti: false,
+  isSearchable: false,
+  hideSelectedOptions: false,
+  options,
+  placeholder: 'Select your favourite(s)',
 };
+
+export const grouped = () => <Select options={groupedOptions} />;
+
+export const customOption = () => <Select components={{ Option: CustomOption }} options={customOptions} />;
+
+export const async = () => (
+  <AsyncSelect loadOptions={loadOptions} cacheOptions paginate pageSize={10} options={options} />
+);
