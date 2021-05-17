7b156c307c590be0b1c7c31e606514cf9c4ebda5
Arnaud Weyts <arnaud.weyts@gmail.com>
2021-05-17 20:42:59 +0200

    :sparkles: set up the handlers

--- src/components/input/DurationInput.js
+++ src/components/input/DurationInput.js
@@ -2,13 +2,45 @@ import React from 'react';
 import { Box, NumericInput } from '../../';
 
 const DurationInput = ({ value, onChange, onKeyDown }) => {
+  const handleHoursChanged = (_, hours) => {
+    onChange &&
+      onChange({
+        ...value,
+        hours,
+      });
+  };
+
+  const handleMinutesChange = (_, minutes) => {
+    onChange &&
+      onChange({
+        ...value,
+        minutes,
+      });
+  };
+
   return (
     <Box display="flex" alignItems="center">
-      <NumericInput placeholder="00" min={0} onKeyDown={onKeyDown} size="small" flexGrow={1} />
+      <NumericInput
+        placeholder="00"
+        min={0}
+        value={value?.hours || ''}
+        onChange={handleHoursChanged}
+        onKeyDown={onKeyDown}
+        size="small"
+        flexGrow={1}
+      />
       <Box element="span" marginHorizontal={2}>
         :
       </Box>
-      <NumericInput placeholder="00" min={0} onKeyDown={onKeyDown} size="small" flexGrow={1} />
+      <NumericInput
+        placeholder="00"
+        min={0}
+        value={value?.minutes || ''}
+        onChange={handleMinutesChange}
+        onKeyDown={onKeyDown}
+        size="small"
+        flexGrow={1}
+      />
     </Box>
   );
 };
