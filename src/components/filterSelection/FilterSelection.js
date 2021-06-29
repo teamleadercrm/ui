62757f62b439f15997d18a975dfbf21625c8e6f9
Joren Saey <joren.saey@teamleader.eu>
2021-06-29 15:13:58 +0200

    Implement active, focused and default state

--- src/components/filterSelection/FilterSelection.js
+++ src/components/filterSelection/FilterSelection.js
@@ -1,7 +1,12 @@
-import React, { memo } from 'react';
+import { IconCloseSmallOutline, IconChevronDownSmallOutline } from '@teamleader/ui-icons';
+import React, { forwardRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
+import { Heading4, Monospaced, TextBodyCompact } from '../typography';
+import Icon from '../icon';
 import Box from '../box';
+import theme from './theme.css';
 
 export const Status = {
   ACTIVE: 'active',
@@ -13,7 +18,70 @@ export const Status = {
 };
 
 const FilterSelection = ({ label, status, amount, onClick, onClearClick }, ref) => {
-  return <Box ref={ref}>Hello FilterSelection!</Box>;
+  const handleClearClick = useCallback(
+    (event) => {
+      event.stopPropagation();
+      onClearClick();
+    },
+    [onClearClick],
+  );
+
+  return (
+    <Box
+      ref={ref}
+      className={cx(
+        theme['select-control'],
+        status === Status.ACTIVE && theme['select-control--active'],
+        status === Status.FOCUSED && theme['select-control--focused'],
+      )}
+      onClick={onClick}
+    >
+      <Box className={theme['select-value-container']}>
+        {status === Status.ACTIVE && (
+          <Box display="flex">
+            {amount !== null && (
+              <Heading4
+                paddingHorizontal={2}
+                backgroundColor="aqua"
+                backgroundTint="dark"
+                color="neutral"
+                tint="lightest"
+                className={theme['select-active-options-counter']}
+              >
+                <Monospaced>{amount}</Monospaced>
+              </Heading4>
+            )}
+            <Box
+              alignItems="center"
+              backgroundTint="dark"
+              backgroundColor="aqua"
+              className={cx(
+                theme['select-clear-options-button'],
+                amount === null && theme['select-clear-options-button--amount-hidden'],
+              )}
+              display="flex"
+              onMouseDown={(event) => event.stopPropagation()}
+              onClick={handleClearClick}
+              paddingHorizontal={1}
+            >
+              <Icon color="aqua" tint="lightest" opacity={1}>
+                <IconCloseSmallOutline />
+              </Icon>
+            </Box>
+          </Box>
+        )}
+        <TextBodyCompact marginLeft={status === Status.ACTIVE && 2}>{label}</TextBodyCompact>
+      </Box>
+      <Icon
+        className={cx(
+          theme['select-dropdown-indicator'],
+          status === Status.FOCUSED && theme['select-dropdown-indicator--focused'],
+        )}
+      >
+        <IconChevronDownSmallOutline />
+      </Icon>
+    </Box>
+  );
 };
 
 FilterSelection.propTypes = {
@@ -32,4 +100,4 @@ FilterSelection.defaultProps = {
   onClearClick: null,
 };
 
-export default memo(FilterSelection);
+export default forwardRef(FilterSelection);
