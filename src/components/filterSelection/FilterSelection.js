0cc3ee10ba39fb9d0b5f3371418623edfa20c41d
Joren Saey <joren.saey@teamleader.eu>
2021-06-29 14:28:11 +0200

    Add prop types

--- src/components/filterSelection/FilterSelection.js
+++ src/components/filterSelection/FilterSelection.js
@@ -1,13 +1,35 @@
 import React, { memo } from 'react';
+import PropTypes from 'prop-types';
 
 import Box from '../box';
 
-const FilterSelection = () => {
-  return <Box>Hello FilterSelection!</Box>;
+export const Status = {
+  ACTIVE: 'active',
+  DEFAULT: 'default',
+  DISABLED: 'disabled',
+  FOCUSED: 'focused',
+  INVALID: 'invalid',
+  BROKEN: 'broken',
 };
 
-FilterSelection.propTypes = {};
+const FilterSelection = ({ label, status, amount, onClick, onClearClick }, ref) => {
+  return <Box ref={ref}>Hello FilterSelection!</Box>;
+};
 
-FilterSelection.defaultProps = {};
+FilterSelection.propTypes = {
+  label: PropTypes.string,
+  status: PropTypes.oneOf(Object.values(Status)),
+  amount: PropTypes.number,
+  onClick: PropTypes.func,
+  onClearClick: PropTypes.func,
+};
+
+FilterSelection.defaultProps = {
+  label: '',
+  status: Status.DEFAULT,
+  amount: null,
+  onClick: null,
+  onClearClick: null,
+};
 
 export default memo(FilterSelection);
