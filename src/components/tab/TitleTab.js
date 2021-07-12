09c73a2d539f6f51856342d433803674e9e7e1ce
Arnaud Weyts <arnaud.weyts@gmail.com>
2021-07-12 17:40:57 +0200

    :recycle: convert TitleTab to function component

--- src/components/tab/TitleTab.js
+++ src/components/tab/TitleTab.js
@@ -1,36 +1,31 @@
-import React, { createRef, forwardRef, PureComponent } from 'react';
+import React, { forwardRef, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Box from '../box';
 import cx from 'classnames';
 import theme from './theme.css';
-import omit from 'lodash.omit';
 
 import { Heading4, Heading5 } from '../typography';
 
-class TitleTab extends PureComponent {
-  tabNode = createRef();
+const TitleTab = ({ active, children, className, counter = null, forwardedRef, size, onClick, ...others }) => {
+  const tabRef = useRef();
 
-  handleClick = (event) => {
-    if (this.props.onClick) {
-      this.props.onClick(event);
+  const handleClick = (event) => {
+    if (onClick) {
+      onClick(event);
     }
     if (event.pageX !== 0 && event.pageY !== 0) {
-      this.blur();
+      blur();
     }
   };
 
-  blur = () => {
-    const { forwardedRef } = this.props;
-    if (this.tabNode.current) {
-      this.tabNode.current.blur();
-    }
-    if (forwardedRef && forwardedRef.current) {
-      forwardedRef.current.blur();
+  const blur = () => {
+    const currentTabRef = tabRef.current;
+    if (currentTabRef.blur) {
+      currentTabRef.blur();
     }
   };
 
-  getPaddingHorizontal = () => {
-    const { size } = this.props;
+  const getPaddingHorizontal = () => {
     switch (size) {
       case 'small':
         return 2;
@@ -40,30 +35,25 @@ class TitleTab extends PureComponent {
     }
   };
 
-  render() {
-    const { active, children, className, counter = null, forwardedRef, size, ...others } = this.props;
-    const classNames = cx(theme['title-tab'], { [theme['is-active']]: active }, className);
+  const classNames = cx(theme['title-tab'], { [theme['is-active']]: active }, className);
 
-    const rest = omit(others, ['onClick']);
+  const TextElement = size === 'small' ? Heading5 : Heading4;
 
-    const TextElement = size === 'small' ? Heading5 : Heading4;
-
-    return (
-      <Box
-        data-teamleader-ui="title-tab"
-        className={classNames}
-        marginHorizontal={size === 'small' ? 1 : 2}
-        paddingHorizontal={this.getPaddingHorizontal()}
-        ref={forwardedRef || this.tabNode}
-        onClick={this.handleClick}
-        {...rest}
-      >
-        <TextElement element="span">{children}</TextElement>
-        {counter}
-      </Box>
-    );
-  }
-}
+  return (
+    <Box
+      data-teamleader-ui="title-tab"
+      className={classNames}
+      marginHorizontal={size === 'small' ? 1 : 2}
+      paddingHorizontal={getPaddingHorizontal()}
+      ref={tabRef}
+      onClick={handleClick}
+      {...others}
+    >
+      <TextElement element="span">{children}</TextElement>
+      {counter}
+    </Box>
+  );
+};
 
 TitleTab.propTypes = {
   active: PropTypes.bool,
