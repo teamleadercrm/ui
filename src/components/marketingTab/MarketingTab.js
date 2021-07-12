b6efa3ddf707d897d76bc7f7fe94e475e377d47f
Arnaud Weyts <arnaud.weyts@gmail.com>
2021-07-12 17:36:16 +0200

    :recycle: convert MarketingTab to function component

--- src/components/marketingTab/MarketingTab.js
+++ src/components/marketingTab/MarketingTab.js
@@ -1,58 +1,49 @@
-import React, { createRef, forwardRef, PureComponent } from 'react';
+import React, { forwardRef, useRef } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import theme from './theme.css';
-import omit from 'lodash.omit';
 import Box from '../box';
 import MarketingLockBadge from '../marketingLockBadge';
 import { Heading4, Heading5 } from '../typography';
 
-class MarketingTab extends PureComponent {
-  tabNode = createRef();
+const MarketingTab = ({ active, children, className, forwardedRef, size, onClick, ...others }) => {
+  const tabRef = useRef();
 
-  handleClick = (event) => {
-    if (this.props.onClick) {
-      this.props.onClick(event);
+  const handleClick = (event) => {
+    if (onClick) {
+      onClick(event);
     }
     if (event.pageX !== 0 && event.pageY !== 0) {
-      this.blur();
+      blur();
     }
   };
 
-  blur = () => {
-    const { forwardedRef } = this.props;
-    if (this.tabNode.current) {
-      this.tabNode.current.blur();
-    }
-    if (forwardedRef && forwardedRef.current) {
-      forwardedRef.current.blur();
+  const blur = () => {
+    const currentTabRef = tabRef.current;
+    if (currentTabRef.blur) {
+      currentTabRef.blur();
     }
   };
 
-  render() {
-    const { active, children, className, forwardedRef, size, ...others } = this.props;
-    const classNames = cx(theme['wrapper'], { [theme['is-active']]: active }, className);
+  const classNames = cx(theme['wrapper'], { [theme['is-active']]: active }, className);
 
-    const rest = omit(others, ['onClick']);
+  const TextElement = size === 'small' ? Heading5 : Heading4;
 
-    const TextElement = size === 'small' ? Heading5 : Heading4;
-
-    return (
-      <Box
-        data-teamleader-ui="marketing-tab"
-        className={classNames}
-        marginHorizontal={size === 'small' ? 1 : 2}
-        paddingHorizontal={size === 'small' ? 2 : 3}
-        ref={forwardedRef || this.tabNode}
-        onClick={this.handleClick}
-        {...rest}
-      >
-        <TextElement element="span">{children}</TextElement>
-        <MarketingLockBadge marginLeft={3} size={size} />
-      </Box>
-    );
-  }
-}
+  return (
+    <Box
+      data-teamleader-ui="marketing-tab"
+      className={classNames}
+      marginHorizontal={size === 'small' ? 1 : 2}
+      paddingHorizontal={size === 'small' ? 2 : 3}
+      ref={tabRef}
+      onClick={handleClick}
+      {...others}
+    >
+      <TextElement element="span">{children}</TextElement>
+      <MarketingLockBadge marginLeft={3} size={size} />
+    </Box>
+  );
+};
 
 MarketingTab.propTypes = {
   active: PropTypes.bool,
