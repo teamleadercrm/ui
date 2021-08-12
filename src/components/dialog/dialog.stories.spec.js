fa265c92555c98e38abd40ec0b68e4a17421ce2d
Lowie Benoot <lowie@teamleader.eu>
2021-08-12 18:30:34 +0800

    Add a test story for the Dialog component

---
+++ src/components/dialog/dialog.stories.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { IconWarningBadgedMediumOutline } from '@teamleader/ui-icons';
+
+import { Dialog, Box, TextBody } from '../..';
+
+export default {
+  component: Dialog,
+  title: 'Dialog',
+};
+
+export const Main = () => (
+  <Dialog
+    headerIcon={<IconWarningBadgedMediumOutline />}
+    active
+    onCloseClick={() => {}}
+    onEscKeyDown={() => {}}
+    onOverlayClick={() => {}}
+    primaryAction={{
+      label: 'Confirm',
+    }}
+    secondaryAction={{
+      label: 'Cancel',
+    }}
+    tertiaryAction={{
+      children: 'Read more',
+    }}
+    title="Dialog title"
+  >
+    <Box padding={4}>
+      <TextBody>Here you can add arbitrary content.</TextBody>
+    </Box>
+  </Dialog>
+);
