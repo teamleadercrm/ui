adfa17a3ad1aafce2ccf0355a5efa2f43fe652de
Arnaud Weyts <arnaud.weyts@gmail.com>
2021-12-15 21:05:05 +0100

    :sparkles: add ref forwarding to widget body

--- src/components/widget/Body.js
+++ src/components/widget/Body.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import Island from '../island';
 
-const Body = ({ children, ...others }) => {
-  return <Island {...others}>{children}</Island>;
-};
+const Body = forwardRef(({ children, ...others }, ref) => {
+  return (
+    <Island ref={ref} {...others}>
+      {children}
+    </Island>
+  );
+});
+
+Body.displayName = 'Body';
 
 Body.propTypes = {
   /** The content to display inside the widget body. */
