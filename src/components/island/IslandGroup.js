c863c02ccf810f6356930e79542670c3963801f9
Arnaud Weyts <arnaud.weyts@gmail.com>
2021-12-17 12:25:24 +0100

    :sparkles: add ref forwaring to IslandGroup component

--- src/components/island/IslandGroup.js
+++ src/components/island/IslandGroup.js
@@ -1,76 +1,81 @@
-import React, { isValidElement } from 'react';
+import React, { forwardRef, isValidElement } from 'react';
 import PropTypes from 'prop-types';
 import Box, { pickBoxProps } from '../box';
 
-const IslandGroup = ({ children: originalChildren, className, color, dark, direction, size, ...otherProps }) => {
-  const boxProps = pickBoxProps(otherProps);
-  const children = [];
+const IslandGroup = forwardRef(
+  ({ children: originalChildren, className, color, dark, direction, size, ...otherProps }, ref) => {
+    const boxProps = pickBoxProps(otherProps);
+    const children = [];
 
-  React.Children.forEach(originalChildren, (child) => {
-    if (isValidElement(child)) {
-      children.push(child);
-    }
-  });
+    React.Children.forEach(originalChildren, (child) => {
+      if (isValidElement(child)) {
+        children.push(child);
+      }
+    });
 
-  const hasMoreThanOneChild = children.length > 1;
+    const hasMoreThanOneChild = children.length > 1;
 
-  return (
-    <Box
-      {...boxProps}
-      className={className}
-      display="flex"
-      flexDirection={direction === 'horizontal' ? 'row' : 'column'}
-    >
-      {React.Children.map(children, (child, index) => {
-        const isFirstChild = index === 0;
-        const isLastChild = index === children.length - 1;
+    return (
+      <Box
+        ref={ref}
+        {...boxProps}
+        className={className}
+        display="flex"
+        flexDirection={direction === 'horizontal' ? 'row' : 'column'}
+      >
+        {React.Children.map(children, (child, index) => {
+          const isFirstChild = index === 0;
+          const isLastChild = index === children.length - 1;
 
-        return React.cloneElement(child, {
-          ...(!isFirstChild &&
-            !isLastChild && {
-              borderRadius: 'square',
-            }),
-          ...(direction === 'horizontal' &&
-            !isFirstChild && {
-              borderLeftWidth: 0,
-            }),
-          ...(direction === 'horizontal' &&
-            hasMoreThanOneChild &&
-            isFirstChild && {
-              borderBottomRightRadius: 'square',
-              borderTopRightRadius: 'square',
-            }),
-          ...(direction === 'horizontal' &&
-            hasMoreThanOneChild &&
-            isLastChild && {
-              borderBottomLeftRadius: 'square',
-              borderTopLeftRadius: 'square',
-            }),
-          ...(direction === 'vertical' &&
-            !isFirstChild && {
-              borderTopWidth: 0,
-            }),
-          ...(direction === 'vertical' &&
-            hasMoreThanOneChild &&
-            isFirstChild && {
-              borderBottomLeftRadius: 'square',
-              borderBottomRightRadius: 'square',
-            }),
-          ...(direction === 'vertical' &&
-            hasMoreThanOneChild &&
-            isLastChild && {
-              borderTopLeftRadius: 'square',
-              borderTopRightRadius: 'square',
-            }),
-          ...child.props,
-          color: color || child.props.color,
-          dark: dark || child.props.dark,
-          size: size || child.props.size,
-        });
-      })}
-    </Box>
-  );
-};
+          return React.cloneElement(child, {
+            ...(!isFirstChild &&
+              !isLastChild && {
+                borderRadius: 'square',
+              }),
+            ...(direction === 'horizontal' &&
+              !isFirstChild && {
+                borderLeftWidth: 0,
+              }),
+            ...(direction === 'horizontal' &&
+              hasMoreThanOneChild &&
+              isFirstChild && {
+                borderBottomRightRadius: 'square',
+                borderTopRightRadius: 'square',
+              }),
+            ...(direction === 'horizontal' &&
+              hasMoreThanOneChild &&
+              isLastChild && {
+                borderBottomLeftRadius: 'square',
+                borderTopLeftRadius: 'square',
+              }),
+            ...(direction === 'vertical' &&
+              !isFirstChild && {
+                borderTopWidth: 0,
+              }),
+            ...(direction === 'vertical' &&
+              hasMoreThanOneChild &&
+              isFirstChild && {
+                borderBottomLeftRadius: 'square',
+                borderBottomRightRadius: 'square',
+              }),
+            ...(direction === 'vertical' &&
+              hasMoreThanOneChild &&
+              isLastChild && {
+                borderTopLeftRadius: 'square',
+                borderTopRightRadius: 'square',
+              }),
+            ...child.props,
+            color: color || child.props.color,
+            dark: dark || child.props.dark,
+            size: size || child.props.size,
+          });
+        })}
+      </Box>
+    );
+  },
+);
+
+IslandGroup.displayName = 'IslandGroup';
 
 IslandGroup.propTypes = {
   children: PropTypes.any,
