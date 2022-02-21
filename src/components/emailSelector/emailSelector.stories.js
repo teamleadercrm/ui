a501c71acb08c44c5331178000f8c1a8e31bdf94
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-02-21 12:15:03 +0100

    Add first version of the story

---
+++ src/components/emailSelector/emailSelector.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
+import { text } from '@storybook/addon-knobs';
+import { EmailSelector } from '../../index';
+
+export default {
+  component: EmailSelector,
+  title: addStoryInGroup(LOW_LEVEL_BLOCKS, 'Form elements/EmailSelector'),
+
+  parameters: {},
+};
+
+export const basic = () => <EmailSelector warning={text('Warning', '')} error={text('Error', '')} />;
