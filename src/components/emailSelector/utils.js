ac73f6a8705dec330b2e0a41db1bb477df295c10
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-02-22 11:34:32 +0100

    Add utility to move the cursor in an array or object of suggestions

--- src/components/emailSelector/utils.js
+++ src/components/emailSelector/utils.js
@@ -68,3 +68,41 @@ export function validateSuggestion(filter, suggestion) {
       (suggestion.label?.toLowerCase().includes(lowerFilter) ?? false))
   );
 }
+
+export function moveSuggestion(suggestions, selectedSuggestion, moveBy) {
+  if (Array.isArray(suggestions)) {
+    const index = suggestions.indexOf(selectedSuggestion);
+
+    if (suggestions[index + moveBy]) {
+      return suggestions[index + moveBy];
+    }
+
+    return suggestions[moveBy > 0 ? 0 : suggestions.length - 1] ?? null;
+  }
+
+  const keys = Object.keys(suggestions);
+  if (keys.length === 0) {
+    return null;
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+    const group = suggestions[keys[i]];
+    const index = group.indexOf(selectedSuggestion);
+    if (index === -1) {
+      continue;
+    }
+
+    if (group[index + moveBy]) {
+      return group[index + moveBy];
+    }
+
+    const nextGroup = suggestions[keys[i + moveBy]];
+    if (nextGroup) {
+      return nextGroup[moveBy > 0 ? 0 : nextGroup.length - 1];
+    }
+    break;
+  }
+
+  const group = suggestions[keys[moveBy > 0 ? 0 : keys.length - 1]];
+  return group[moveBy > 0 ? 0 : group.length - 1] ?? null;
+}
