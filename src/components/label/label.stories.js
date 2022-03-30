182000d2440162ba0225fe2dc99a46549f5e0e43
qubis741 <jakub.kovacikk@gmail.com>
2022-03-30 13:07:53 +0200

    cleanup stories

--- src/components/label/label.stories.js
+++ src/components/label/label.stories.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { IconInfoBadgedSmallFilled, IconMarkerSmallOutline } from '@teamleader/ui-icons';
-import { Icon, Input, Label, TextSmall, Tooltip } from '../../index';
 import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
-
-const TooltippedIcon = Tooltip(Icon);
+import { Input, Label } from '../../index';
 
 export default {
   component: Label,
@@ -14,16 +11,7 @@ export const DefaultStory = (args) => (
   <Label
     {...args}
     htmlFor="input1"
-    connectedLeft={
-      <Icon>
-        <IconMarkerSmallOutline />
-      </Icon>
-    }
-    connectedRight={
-      <TooltippedIcon tooltip={<TextSmall>This is the label tooltip text</TextSmall>} tooltipSize="small">
-        <IconInfoBadgedSmallFilled />
-      </TooltippedIcon>
-    }
+    tooltip="This is the label tooltip text"
   >
     Input label
     <Input id="input1" placeholder="I am the placeholder" />
