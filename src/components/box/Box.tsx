c9ff0ece338441e7e28e1fcfdf03d4bbcf659608
Arnaud Weyts <arnaud.weyts@gmail.com>
2022-04-15 13:50:13 +0200

    :recycle: convert Box to function component

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, PureComponent } from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { COLOR, COLORS, TINTS } from '../../constants';
@@ -14,129 +14,127 @@ const borderRadiuses = {
   rounded: '4px',
 };
 
-class Box extends PureComponent {
-  getBorder = (value) => {
-    const { borderColor, borderTint } = this.props;
+const Box = (props) => {
+  const getBorder = (value) => {
+    const { borderColor, borderTint } = props;
     return `${value}px solid ${COLOR[borderColor.toUpperCase()][borderTint.toUpperCase()]}`;
   };
 
-  render() {
-    const {
-      alignContent,
-      alignItems,
-      alignSelf,
-      backgroundColor,
-      backgroundTint,
-      borderBottomWidth,
-      borderColor,
-      borderLeftWidth,
-      borderRightWidth,
-      borderTint,
-      borderTopWidth,
-      borderWidth,
-      borderRadius,
-      borderTopLeftRadius = borderRadius,
-      borderTopRightRadius = borderRadius,
-      borderBottomLeftRadius = borderRadius,
-      borderBottomRightRadius = borderRadius,
-      boxSizing,
-      children,
-      className,
-      display,
-      element,
-      flex,
-      flexBasis,
-      flexDirection,
-      flexGrow,
-      flexShrink,
-      flexWrap,
-      forwardedRef,
-      justifyContent,
-      margin,
-      marginHorizontal = margin,
-      marginVertical = margin,
-      marginBottom = marginVertical,
-      marginLeft = marginHorizontal,
-      marginRight = marginHorizontal,
-      marginTop = marginVertical,
-      order,
-      overflow,
-      overflowX,
-      overflowY,
-      padding,
-      paddingHorizontal = padding,
-      paddingVertical = padding,
-      paddingBottom = paddingVertical,
-      paddingLeft = paddingHorizontal,
-      paddingRight = paddingHorizontal,
-      paddingTop = paddingVertical,
-      style,
-      textAlign,
-      ...others
-    } = this.props;
+  const {
+    alignContent,
+    alignItems,
+    alignSelf,
+    backgroundColor,
+    backgroundTint,
+    borderBottomWidth,
+    borderColor,
+    borderLeftWidth,
+    borderRightWidth,
+    borderTint,
+    borderTopWidth,
+    borderWidth,
+    borderRadius,
+    borderTopLeftRadius = borderRadius,
+    borderTopRightRadius = borderRadius,
+    borderBottomLeftRadius = borderRadius,
+    borderBottomRightRadius = borderRadius,
+    boxSizing,
+    children,
+    className,
+    display,
+    element,
+    flex,
+    flexBasis,
+    flexDirection,
+    flexGrow,
+    flexShrink,
+    flexWrap,
+    forwardedRef,
+    justifyContent,
+    margin,
+    marginHorizontal = margin,
+    marginVertical = margin,
+    marginBottom = marginVertical,
+    marginLeft = marginHorizontal,
+    marginRight = marginHorizontal,
+    marginTop = marginVertical,
+    order,
+    overflow,
+    overflowX,
+    overflowY,
+    padding,
+    paddingHorizontal = padding,
+    paddingVertical = padding,
+    paddingBottom = paddingVertical,
+    paddingLeft = paddingHorizontal,
+    paddingRight = paddingHorizontal,
+    paddingTop = paddingVertical,
+    style,
+    textAlign,
+    ...others
+  } = props;
 
-    const classNames = cx(
-      theme['box'],
-      {
-        [theme[`align-content-${alignContent}`]]: alignContent,
-        [theme[`align-items-${alignItems}`]]: alignItems,
-        [theme[`align-self-${alignSelf}`]]: alignSelf,
-        [theme[`background-color-${backgroundColor}-${backgroundTint}`]]: backgroundColor && backgroundTint,
-        [theme[`background-color-${backgroundColor}`]]:
-          backgroundColor && (!backgroundTint || backgroundTint === 'normal'),
-        [theme[`display-${display}`]]: display,
-        [theme[`flex-direction-${flexDirection}`]]: flexDirection,
-        [theme[`flex-wrap-${flexWrap}`]]: flexWrap,
-        [theme[`justify-content-${justifyContent}`]]: justifyContent,
-        [theme[`margin-bottom-${marginBottom}`]]: marginBottom > 0,
-        [theme[`margin-left-${marginLeft}`]]: marginLeft > 0,
-        [theme[`margin-right-${marginRight}`]]: marginRight > 0,
-        [theme[`margin-top-${marginTop}`]]: marginTop > 0,
-        [theme[`margin-bottom-negative-${Math.abs(marginBottom)}`]]: marginBottom < 0,
-        [theme[`margin-left-negative-${Math.abs(marginLeft)}`]]: marginLeft < 0,
-        [theme[`margin-right-negative-${Math.abs(marginRight)}`]]: marginRight < 0,
-        [theme[`margin-top-negative-${Math.abs(marginTop)}`]]: marginTop < 0,
-        [theme[`padding-bottom-${paddingBottom}`]]: paddingBottom > 0,
-        [theme[`padding-left-${paddingLeft}`]]: paddingLeft > 0,
-        [theme[`padding-right-${paddingRight}`]]: paddingRight > 0,
-        [theme[`padding-top-${paddingTop}`]]: paddingTop > 0,
-        [theme[`text-align-${textAlign}`]]: textAlign,
-      },
-      className,
-    );
+  const classNames = cx(
+    theme['box'],
+    {
+      [theme[`align-content-${alignContent}`]]: alignContent,
+      [theme[`align-items-${alignItems}`]]: alignItems,
+      [theme[`align-self-${alignSelf}`]]: alignSelf,
+      [theme[`background-color-${backgroundColor}-${backgroundTint}`]]: backgroundColor && backgroundTint,
+      [theme[`background-color-${backgroundColor}`]]:
+        backgroundColor && (!backgroundTint || backgroundTint === 'normal'),
+      [theme[`display-${display}`]]: display,
+      [theme[`flex-direction-${flexDirection}`]]: flexDirection,
+      [theme[`flex-wrap-${flexWrap}`]]: flexWrap,
+      [theme[`justify-content-${justifyContent}`]]: justifyContent,
+      [theme[`margin-bottom-${marginBottom}`]]: marginBottom > 0,
+      [theme[`margin-left-${marginLeft}`]]: marginLeft > 0,
+      [theme[`margin-right-${marginRight}`]]: marginRight > 0,
+      [theme[`margin-top-${marginTop}`]]: marginTop > 0,
+      [theme[`margin-bottom-negative-${Math.abs(marginBottom)}`]]: marginBottom < 0,
+      [theme[`margin-left-negative-${Math.abs(marginLeft)}`]]: marginLeft < 0,
+      [theme[`margin-right-negative-${Math.abs(marginRight)}`]]: marginRight < 0,
+      [theme[`margin-top-negative-${Math.abs(marginTop)}`]]: marginTop < 0,
+      [theme[`padding-bottom-${paddingBottom}`]]: paddingBottom > 0,
+      [theme[`padding-left-${paddingLeft}`]]: paddingLeft > 0,
+      [theme[`padding-right-${paddingRight}`]]: paddingRight > 0,
+      [theme[`padding-top-${paddingTop}`]]: paddingTop > 0,
+      [theme[`text-align-${textAlign}`]]: textAlign,
+    },
+    className,
+  );
 
-    const elementStyles = {
-      ...(borderWidth && { border: this.getBorder(borderWidth) }),
-      ...(borderBottomWidth && { borderBottom: this.getBorder(borderBottomWidth) }),
-      ...(borderLeftWidth && { borderLeft: this.getBorder(borderLeftWidth) }),
-      ...(borderRightWidth && { borderRight: this.getBorder(borderRightWidth) }),
-      ...(borderTopWidth && { borderTop: this.getBorder(borderTopWidth) }),
-      ...(borderRadius && { borderRadius: borderRadiuses[borderRadius] }),
-      ...(borderTopLeftRadius && { borderTopLeftRadius: borderRadiuses[borderTopLeftRadius] }),
-      ...(borderTopRightRadius && { borderTopRightRadius: borderRadiuses[borderTopRightRadius] }),
-      ...(borderBottomLeftRadius && { borderBottomLeftRadius: borderRadiuses[borderBottomLeftRadius] }),
-      ...(borderBottomRightRadius && { borderBottomRightRadius: borderRadiuses[borderBottomRightRadius] }),
-      ...(boxSizing && { boxSizing }),
-      ...(flex && { flex }),
-      ...(flexBasis && { flexBasis }),
-      ...(!isNaN(flexGrow) ? { flexGrow } : {}),
-      ...(!isNaN(flexShrink) ? { flexShrink } : {}),
-      ...(!isNaN(order) ? { order } : {}),
-      ...(overflow && { overflow }),
-      ...(overflowX && { overflowX }),
-      ...(overflowY && { overflowY }),
-      ...style,
-    };
+  const elementStyles = {
+    ...(borderWidth && { border: getBorder(borderWidth) }),
+    ...(borderBottomWidth && { borderBottom: getBorder(borderBottomWidth) }),
+    ...(borderLeftWidth && { borderLeft: getBorder(borderLeftWidth) }),
+    ...(borderRightWidth && { borderRight: getBorder(borderRightWidth) }),
+    ...(borderTopWidth && { borderTop: getBorder(borderTopWidth) }),
+    ...(borderRadius && { borderRadius: borderRadiuses[borderRadius] }),
+    ...(borderTopLeftRadius && { borderTopLeftRadius: borderRadiuses[borderTopLeftRadius] }),
+    ...(borderTopRightRadius && { borderTopRightRadius: borderRadiuses[borderTopRightRadius] }),
+    ...(borderBottomLeftRadius && { borderBottomLeftRadius: borderRadiuses[borderBottomLeftRadius] }),
+    ...(borderBottomRightRadius && { borderBottomRightRadius: borderRadiuses[borderBottomRightRadius] }),
+    ...(boxSizing && { boxSizing }),
+    ...(flex && { flex }),
+    ...(flexBasis && { flexBasis }),
+    ...(!isNaN(flexGrow) ? { flexGrow } : {}),
+    ...(!isNaN(flexShrink) ? { flexShrink } : {}),
+    ...(!isNaN(order) ? { order } : {}),
+    ...(overflow && { overflow }),
+    ...(overflowX && { overflowX }),
+    ...(overflowY && { overflowY }),
+    ...style,
+  };
 
-    const Element = element;
+  const Element = element;
 
-    return (
-      <Element className={classNames} ref={forwardedRef} style={elementStyles} {...others}>
-        {children}
-      </Element>
-    );
-  }
-}
+  return (
+    <Element className={classNames} ref={forwardedRef} style={elementStyles} {...others}>
+      {children}
+    </Element>
+  );
+};
 
 Box.propTypes = {
   alignContent: PropTypes.oneOf(['center', 'flex-start', 'flex-end', 'space-around', 'space-between', 'space-evenly']),
