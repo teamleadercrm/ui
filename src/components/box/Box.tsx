0a24775b44fae69868002cf0582d0428343a29bc
Arnaud Weyts <arnaud.weyts@gmail.com>
2022-04-15 14:22:47 +0200

    :recycle: wrap component with forwardRef instead of creating a separate component
    the prevous way was used as a work around because the component was a class

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -64,131 +64,133 @@ type Props = Partial<{
   textAlign: 'center' | 'left' | 'right';
 }>;
 
-const Box = ({
-  alignContent,
-  alignItems,
-  alignSelf,
-  backgroundColor,
-  backgroundTint,
-  borderBottomWidth,
-  borderColor = 'neutral',
-  borderLeftWidth,
-  borderRightWidth,
-  borderTint = 'dark',
-  borderTopWidth,
-  borderWidth,
-  borderRadius = 'square',
-  borderTopLeftRadius = borderRadius,
-  borderTopRightRadius = borderRadius,
-  borderBottomLeftRadius = borderRadius,
-  borderBottomRightRadius = borderRadius,
-  boxSizing,
-  children,
-  className,
-  display,
-  element = 'div',
-  flex,
-  flexBasis,
-  flexDirection,
-  flexGrow,
-  flexShrink,
-  flexWrap,
-  forwardedRef,
-  justifyContent,
-  margin = 0,
-  marginHorizontal = margin,
-  marginVertical = margin,
-  marginBottom = marginVertical,
-  marginLeft = marginHorizontal,
-  marginRight = marginHorizontal,
-  marginTop = marginVertical,
-  order,
-  overflow,
-  overflowX,
-  overflowY,
-  padding = 0,
-  paddingHorizontal = padding,
-  paddingVertical = padding,
-  paddingBottom = paddingVertical,
-  paddingLeft = paddingHorizontal,
-  paddingRight = paddingHorizontal,
-  paddingTop = paddingVertical,
-  style,
-  textAlign,
-  ...others
-}: Props) => {
-  const getBorder = (value: number) => {
-    return `${value}px solid ${
-      COLOR[borderColor.toUpperCase() as Uppercase<keyof typeof COLOR>][
-        borderTint.toUpperCase() as Uppercase<keyof typeof COLOR['AQUA']>
-      ]
-    }`;
-  };
-
-  const classNames = cx(
-    theme['box'],
+const Box = forwardRef(
+  (
     {
-      [theme[`align-content-${alignContent}`]]: alignContent,
-      [theme[`align-items-${alignItems}`]]: alignItems,
-      [theme[`align-self-${alignSelf}`]]: alignSelf,
-      [theme[`background-color-${backgroundColor}-${backgroundTint}`]]: backgroundColor && backgroundTint,
-      [theme[`background-color-${backgroundColor}`]]:
-        backgroundColor && (!backgroundTint || backgroundTint === 'normal'),
-      [theme[`display-${display}`]]: display,
-      [theme[`flex-direction-${flexDirection}`]]: flexDirection,
-      [theme[`flex-wrap-${flexWrap}`]]: flexWrap,
-      [theme[`justify-content-${justifyContent}`]]: justifyContent,
-      [theme[`margin-bottom-${marginBottom}`]]: marginBottom > 0,
-      [theme[`margin-left-${marginLeft}`]]: marginLeft > 0,
-      [theme[`margin-right-${marginRight}`]]: marginRight > 0,
-      [theme[`margin-top-${marginTop}`]]: marginTop > 0,
-      [theme[`margin-bottom-negative-${Math.abs(marginBottom)}`]]: marginBottom < 0,
-      [theme[`margin-left-negative-${Math.abs(marginLeft)}`]]: marginLeft < 0,
-      [theme[`margin-right-negative-${Math.abs(marginRight)}`]]: marginRight < 0,
-      [theme[`margin-top-negative-${Math.abs(marginTop)}`]]: marginTop < 0,
-      [theme[`padding-bottom-${paddingBottom}`]]: paddingBottom > 0,
-      [theme[`padding-left-${paddingLeft}`]]: paddingLeft > 0,
-      [theme[`padding-right-${paddingRight}`]]: paddingRight > 0,
-      [theme[`padding-top-${paddingTop}`]]: paddingTop > 0,
-      [theme[`text-align-${textAlign}`]]: textAlign,
-    },
-    className,
-  );
+      alignContent,
+      alignItems,
+      alignSelf,
+      backgroundColor,
+      backgroundTint,
+      borderBottomWidth,
+      borderColor = 'neutral',
+      borderLeftWidth,
+      borderRightWidth,
+      borderTint = 'dark',
+      borderTopWidth,
+      borderWidth,
+      borderRadius = 'square',
+      borderTopLeftRadius = borderRadius,
+      borderTopRightRadius = borderRadius,
+      borderBottomLeftRadius = borderRadius,
+      borderBottomRightRadius = borderRadius,
+      boxSizing,
+      children,
+      className,
+      display,
+      element = 'div',
+      flex,
+      flexBasis,
+      flexDirection,
+      flexGrow,
+      flexShrink,
+      flexWrap,
+      justifyContent,
+      margin = 0,
+      marginHorizontal = margin,
+      marginVertical = margin,
+      marginBottom = marginVertical,
+      marginLeft = marginHorizontal,
+      marginRight = marginHorizontal,
+      marginTop = marginVertical,
+      order,
+      overflow,
+      overflowX,
+      overflowY,
+      padding = 0,
+      paddingHorizontal = padding,
+      paddingVertical = padding,
+      paddingBottom = paddingVertical,
+      paddingLeft = paddingHorizontal,
+      paddingRight = paddingHorizontal,
+      paddingTop = paddingVertical,
+      style,
+      textAlign,
+      ...others
+    }: Props,
+    ref,
+  ) => {
+    const getBorder = (value: number) => {
+      return `${value}px solid ${
+        COLOR[borderColor.toUpperCase() as Uppercase<keyof typeof COLOR>][
+          borderTint.toUpperCase() as Uppercase<keyof typeof COLOR['AQUA']>
+        ]
+      }`;
+    };
 
-  const elementStyles = {
-    ...(borderWidth && { border: getBorder(borderWidth) }),
-    ...(borderBottomWidth && { borderBottom: getBorder(borderBottomWidth) }),
-    ...(borderLeftWidth && { borderLeft: getBorder(borderLeftWidth) }),
-    ...(borderRightWidth && { borderRight: getBorder(borderRightWidth) }),
-    ...(borderTopWidth && { borderTop: getBorder(borderTopWidth) }),
-    ...(borderRadius && { borderRadius: borderRadiuses[borderRadius] }),
-    ...(borderTopLeftRadius && { borderTopLeftRadius: borderRadiuses[borderTopLeftRadius] }),
-    ...(borderTopRightRadius && { borderTopRightRadius: borderRadiuses[borderTopRightRadius] }),
-    ...(borderBottomLeftRadius && { borderBottomLeftRadius: borderRadiuses[borderBottomLeftRadius] }),
-    ...(borderBottomRightRadius && { borderBottomRightRadius: borderRadiuses[borderBottomRightRadius] }),
-    ...(boxSizing && { boxSizing }),
-    ...(flex && { flex }),
-    ...(flexBasis && { flexBasis }),
-    ...(typeof flexGrow === 'number' && !isNaN(flexGrow) ? { flexGrow } : {}),
-    ...(typeof flexShrink === 'number' && !isNaN(flexShrink) ? { flexShrink } : {}),
-    ...(typeof order === 'number' && !isNaN(order) ? { order } : {}),
-    ...(overflow && { overflow }),
-    ...(overflowX && { overflowX }),
-    ...(overflowY && { overflowY }),
-    ...style,
-  };
+    const classNames = cx(
+      theme['box'],
+      {
+        [theme[`align-content-${alignContent}`]]: alignContent,
+        [theme[`align-items-${alignItems}`]]: alignItems,
+        [theme[`align-self-${alignSelf}`]]: alignSelf,
+        [theme[`background-color-${backgroundColor}-${backgroundTint}`]]: backgroundColor && backgroundTint,
+        [theme[`background-color-${backgroundColor}`]]:
+          backgroundColor && (!backgroundTint || backgroundTint === 'normal'),
+        [theme[`display-${display}`]]: display,
+        [theme[`flex-direction-${flexDirection}`]]: flexDirection,
+        [theme[`flex-wrap-${flexWrap}`]]: flexWrap,
+        [theme[`justify-content-${justifyContent}`]]: justifyContent,
+        [theme[`margin-bottom-${marginBottom}`]]: marginBottom > 0,
+        [theme[`margin-left-${marginLeft}`]]: marginLeft > 0,
+        [theme[`margin-right-${marginRight}`]]: marginRight > 0,
+        [theme[`margin-top-${marginTop}`]]: marginTop > 0,
+        [theme[`margin-bottom-negative-${Math.abs(marginBottom)}`]]: marginBottom < 0,
+        [theme[`margin-left-negative-${Math.abs(marginLeft)}`]]: marginLeft < 0,
+        [theme[`margin-right-negative-${Math.abs(marginRight)}`]]: marginRight < 0,
+        [theme[`margin-top-negative-${Math.abs(marginTop)}`]]: marginTop < 0,
+        [theme[`padding-bottom-${paddingBottom}`]]: paddingBottom > 0,
+        [theme[`padding-left-${paddingLeft}`]]: paddingLeft > 0,
+        [theme[`padding-right-${paddingRight}`]]: paddingRight > 0,
+        [theme[`padding-top-${paddingTop}`]]: paddingTop > 0,
+        [theme[`text-align-${textAlign}`]]: textAlign,
+      },
+      className,
+    );
 
-  const Element = element;
+    const elementStyles = {
+      ...(borderWidth && { border: getBorder(borderWidth) }),
+      ...(borderBottomWidth && { borderBottom: getBorder(borderBottomWidth) }),
+      ...(borderLeftWidth && { borderLeft: getBorder(borderLeftWidth) }),
+      ...(borderRightWidth && { borderRight: getBorder(borderRightWidth) }),
+      ...(borderTopWidth && { borderTop: getBorder(borderTopWidth) }),
+      ...(borderRadius && { borderRadius: borderRadiuses[borderRadius] }),
+      ...(borderTopLeftRadius && { borderTopLeftRadius: borderRadiuses[borderTopLeftRadius] }),
+      ...(borderTopRightRadius && { borderTopRightRadius: borderRadiuses[borderTopRightRadius] }),
+      ...(borderBottomLeftRadius && { borderBottomLeftRadius: borderRadiuses[borderBottomLeftRadius] }),
+      ...(borderBottomRightRadius && { borderBottomRightRadius: borderRadiuses[borderBottomRightRadius] }),
+      ...(boxSizing && { boxSizing }),
+      ...(flex && { flex }),
+      ...(flexBasis && { flexBasis }),
+      ...(typeof flexGrow === 'number' && !isNaN(flexGrow) ? { flexGrow } : {}),
+      ...(typeof flexShrink === 'number' && !isNaN(flexShrink) ? { flexShrink } : {}),
+      ...(typeof order === 'number' && !isNaN(order) ? { order } : {}),
+      ...(overflow && { overflow }),
+      ...(overflowX && { overflowX }),
+      ...(overflowY && { overflowY }),
+      ...style,
+    };
 
-  return (
-    <Element className={classNames} ref={forwardedRef} style={elementStyles} {...others}>
-      {children}
-    </Element>
-  );
-};
+    const Element = element;
 
-const ForwardedBox = forwardRef((props: Props, ref) => <Box {...props} forwardedRef={ref} />);
+    return (
+      <Element className={classNames} ref={ref} style={elementStyles} {...others}>
+        {children}
+      </Element>
+    );
+  },
+);
 
-ForwardedBox.displayName = 'Box';
+Box.displayName = 'Box';
 
-export default ForwardedBox;
+export default Box;
