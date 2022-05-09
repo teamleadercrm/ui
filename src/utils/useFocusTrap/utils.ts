6538e5aae01e937ef26a0f19325e2b76a5e21627
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-09 19:54:14 +0200

    🏷️ Convert `utils/useFocusTrap` to typescript

--- src/utils/useFocusTrap/utils.ts
+++ src/utils/useFocusTrap/utils.ts
@@ -1,4 +1,4 @@
-const attemptFocus = (node) => {
+function attemptFocus(node: HTMLElement) {
   try {
     node.focus();
   } catch (exception) {
@@ -6,21 +6,33 @@ const attemptFocus = (node) => {
   }
 
   return document.activeElement === node;
-};
+}
 
-export const focusOnFirstDescendent = (node) => {
-  if (attemptFocus(node)) {
+function isElement(node: Element | Node | null): node is Element {
+  return (node as Element | null)?.children !== undefined;
+}
+
+export function isHTMLElement(node: HTMLElement | Node | null): node is HTMLElement {
+  return (node as HTMLElement | null)?.focus !== undefined;
+}
+
+export const focusOnFirstDescendent = (node: Node | null): boolean => {
+  if (isHTMLElement(node) && attemptFocus(node)) {
     return true;
   }
-  return Array.from(node.children).some((childNode) => focusOnFirstDescendent(childNode));
+
+  return isElement(node) && Array.from(node.children).some((childNode) => focusOnFirstDescendent(childNode));
 };
 
-export const focusOnLastDescendent = (node) => {
-  if (attemptFocus(node)) {
+export const focusOnLastDescendent = (node: Node): boolean => {
+  if (isHTMLElement(node) && attemptFocus(node)) {
     return true;
   }
 
-  return Array.from(node.children)
-    .reverse()
-    .some((childNode) => focusOnLastDescendent(childNode));
+  return (
+    isElement(node) &&
+    Array.from(node.children)
+      .reverse()
+      .some((childNode) => focusOnLastDescendent(childNode))
+  );
 };
