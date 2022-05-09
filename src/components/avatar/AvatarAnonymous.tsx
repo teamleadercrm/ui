77f243e9747f773dcec69794bf722b57c56db10b
Lowie Benoot <lowie@teamleader.eu>
2022-05-09 16:20:09 +0200

    Convert AvatarAnonymous to a function compoonent in TypeScript

--- src/components/avatar/AvatarAnonymous.tsx
+++ src/components/avatar/AvatarAnonymous.tsx
@@ -1,39 +1,36 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import theme from './theme.css';
 import Box from '../box';
 import Icon from '../icon';
+import { Size } from './types';
 import { IconUserMediumOutline, IconUserSmallOutline } from '@teamleader/ui-icons';
 
-class AvatarAnonymous extends PureComponent {
-  render() {
-    const { children, size } = this.props;
-
-    return (
-      <Box
-        alignItems="center"
-        backgroundColor="neutral"
-        backgroundTint="normal"
-        className={cx(theme['avatar'], theme['avatar-anonymous'])}
-        data-teamleader-ui="avatar-anonymous"
-        display="flex"
-        justifyContent="center"
-      >
-        <Icon color="neutral" tint="darkest">
-          {size === 'tiny' || size === 'small' ? <IconUserSmallOutline /> : <IconUserMediumOutline />}
-        </Icon>
-        {children && <div className={theme['children']}>{children}</div>}
-      </Box>
-    );
-  }
+interface Props {
+  /** Component that will be placed top right of the avatar image. */
+  children?: React.ReactNode;
+  /** The size of the avatar. */
+  size: Size;
 }
 
-AvatarAnonymous.propTypes = {
-  /** Component that will be placed top right of the avatar image. */
-  children: PropTypes.any,
-  /** The size of the avatar. */
-  size: PropTypes.oneOf(['tiny', 'small', 'medium', 'large', 'hero']),
+const AvatarAnonymous = ({ children, size }: Props) => {
+  return (
+    <Box
+      alignItems="center"
+      backgroundColor="neutral"
+      backgroundTint="normal"
+      className={cx(theme['avatar'], theme['avatar-anonymous'])}
+      data-teamleader-ui="avatar-anonymous"
+      display="flex"
+      justifyContent="center"
+    >
+      <Icon color="neutral" tint="darkest">
+        {size === Size.tiny || size === Size.small ? <IconUserSmallOutline /> : <IconUserMediumOutline />}
+      </Icon>
+      {children && <div className={theme['children']}>{children}</div>}
+    </Box>
+  );
 };
 
 export default AvatarAnonymous;
