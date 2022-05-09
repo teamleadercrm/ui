d60a3c1a35717ac46059d91fc0297b2f2dc4f21c
Dries Decramer <dries.decramer@teamleader.be>
2022-05-09 13:17:05 +0200

    Convert to functional component

--- src/components/advancedCollapsible/AdvancedCollapsible.js
+++ src/components/advancedCollapsible/AdvancedCollapsible.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { TextBody, Heading3 } from '../typography';
 import Icon from '../icon';
@@ -6,48 +6,34 @@ import Box, { pickBoxProps } from '../box';
 import { IconChevronDownSmallOutline, IconChevronRightSmallOutline } from '@teamleader/ui-icons';
 import theme from './theme.css';
 
-/** @type {React.ComponentType<any>} */
-class AdvancedCollapsible extends PureComponent {
-  state = {
-    collapsed: true,
+const AdvancedCollapsible = ({ children, color, size, title, ...others }) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const boxProps = pickBoxProps(others);
+  const TitleElement = size === 'large' ? Heading3 : TextBody;
+
+  const handleTitleClick = () => {
+    setCollapsed(!collapsed);
   };
 
-  handleTitleClick = () => {
-    this.setState(({ collapsed }) => ({ collapsed: !collapsed }));
-  };
-
-  render() {
-    const { children, color, size, title, ...others } = this.props;
-    const { collapsed } = this.state;
-
-    const boxProps = pickBoxProps(others);
-    const TitleElement = size === 'large' ? Heading3 : TextBody;
-
-    return (
-      <Box>
-        <Box
-          className={theme['title']}
-          display="flex"
-          alignItems="center"
-          onClick={this.handleTitleClick}
-          {...boxProps}
-        >
-          <Icon color={color} tint="darkest">
-            {collapsed ? <IconChevronRightSmallOutline /> : <IconChevronDownSmallOutline />}
-          </Icon>
-          <TitleElement color={color} marginLeft={2} tint="darkest">
-            {size === 'medium' ? <strong>{title}</strong> : title}
-          </TitleElement>
-        </Box>
-        {!collapsed && (
-          <Box className={theme['children']} marginTop={2}>
-            {children}
-          </Box>
-        )}
+  return (
+    <Box>
+      <Box className={theme['title']} display="flex" alignItems="center" onClick={handleTitleClick} {...boxProps}>
+        <Icon color={color} tint="darkest">
+          {collapsed ? <IconChevronRightSmallOutline /> : <IconChevronDownSmallOutline />}
+        </Icon>
+        <TitleElement color={color} marginLeft={2} tint="darkest">
+          {size === 'medium' ? <strong>{title}</strong> : title}
+        </TitleElement>
       </Box>
-    );
-  }
-}
+      {!collapsed && (
+        <Box className={theme['children']} marginTop={2}>
+          {children}
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 AdvancedCollapsible.propTypes = {
   color: PropTypes.oneOf(['neutral', 'teal']),
