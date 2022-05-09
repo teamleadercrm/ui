34d0fee4e76a7f31a0ea9d9cd258a7b2071a6541
Lowie Benoot <lowie@teamleader.eu>
2022-05-09 16:39:07 +0200

    Convert AvatarImage to a function compoonent in TypeScript

--- src/components/avatar/AvatarImage.tsx
+++ src/components/avatar/AvatarImage.tsx
@@ -1,56 +1,52 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import cx from 'classnames';
 import theme from './theme.css';
 import AvatarOverlay from './AvatarOverlay';
+import { Size } from './types';
 
-class AvatarImage extends PureComponent {
-  state = {
-    displayAvatarOverlay: false,
-  };
-
-  handleOnMouseEnter = () => {
-    this.setState({ displayAvatarOverlay: true });
-  };
-
-  handleOnMouseLeave = () => {
-    this.setState({ displayAvatarOverlay: false });
-  };
-
-  render() {
-    const { children, editable, image, imageAlt, onImageChange, size, onImageLoadFailure } = this.props;
-    const { displayAvatarOverlay } = this.state;
-    const shouldShowAvatarOverlay =
-      editable && (size === 'large' || size === 'hero' || (size === 'medium' && displayAvatarOverlay));
-
-    return (
-      <div
-        className={cx(theme['avatar'], theme['avatar-image'])}
-        data-teamleader-ui="avatar-image"
-        onMouseEnter={this.handleOnMouseEnter}
-        onMouseLeave={this.handleOnMouseLeave}
-      >
-        <img alt={imageAlt} onError={onImageLoadFailure} src={image} className={theme['image']} />
-        {shouldShowAvatarOverlay && <AvatarOverlay onClick={onImageChange} size={size} />}
-        {children && <div className={theme['children']}>{children}</div>}
-      </div>
-    );
-  }
+interface Props {
+  /** Component that will be placed top right of the avatar image. */
+  children?: React.ReactNode;
+  /** If true, an overlay will be shown with edit icon. */
+  editable?: boolean;
+  /** An image source or an image element. */
+  image: string;
+  /** An alternative text for the image element. */
+  imageAlt?: string;
+  /** Callback function that is fired when user clicks the edit icon. */
+  onImageChange?: (event: React.MouseEvent<HTMLDivElement>) => void;
+  /** Callback function that is fired the image fails to load. */
+  onImageLoadFailure?: React.ReactEventHandler<HTMLImageElement>;
+  /** The size of the avatar. */
+  size: Size;
 }
 
-AvatarImage.propTypes = {
-  /** Component that will be placed top right of the avatar image. */
-  children: PropTypes.any,
-  /** If true, an overlay will be shown with edit icon. */
-  editable: PropTypes.bool,
-  /** An image source or an image element. */
-  image: PropTypes.string,
-  /** An alternative text for the image element. */
-  imageAlt: PropTypes.string,
-  /** Callback function that is fired when user clicks the edit icon. */
-  onImageChange: PropTypes.func,
-  /** The size of the avatar. */
-  size: PropTypes.oneOf(['tiny', 'small', 'medium', 'large', 'hero']),
+const AvatarImage = ({ children, editable, image, imageAlt, onImageChange, size, onImageLoadFailure }: Props) => {
+  const [displayAvatarOverlay, setDisplayAvatarOverlay] = useState(false);
+
+  const handleOnMouseEnter = () => {
+    setDisplayAvatarOverlay(true);
+  };
+
+  const handleOnMouseLeave = () => {
+    setDisplayAvatarOverlay(false);
+  };
+
+  const shouldShowAvatarOverlay =
+    editable && (size === Size.large || size === Size.hero || (size === Size.medium && displayAvatarOverlay));
+
+  return (
+    <div
+      className={cx(theme['avatar'], theme['avatar-image'])}
+      data-teamleader-ui="avatar-image"
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}
+    >
+      <img alt={imageAlt} onError={onImageLoadFailure} src={image} className={theme['image']} />
+      {shouldShowAvatarOverlay && <AvatarOverlay onClick={onImageChange} size={size} />}
+      {children && <div className={theme['children']}>{children}</div>}
+    </div>
+  );
 };
 
 export default AvatarImage;
