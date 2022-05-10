1f00a5ecf3de7a8d836a8a30ec6f428abc549115
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2022-05-10 16:40:16 +0200

    convert stories to typescript

--- src/components/badgedLink/BadgedLink.tsx
+++ src/components/badgedLink/BadgedLink.tsx
@@ -1,11 +1,11 @@
 import uiTypography from '@teamleader/ui-typography';
 import uiUtilities from '@teamleader/ui-utilities';
 import cx from 'classnames';
-import React, { ElementType, forwardRef, ReactElement, ReactNode, useImperativeHandle, useRef } from 'react';
-import Box from '../box';
+import React, { ElementType, forwardRef, HTMLProps, ReactElement, ReactNode } from 'react';
+import Box, { pickBoxProps } from '../box';
 import theme from './theme.css';
 
-interface BadgedLinkProps {
+interface BadgedLinkProps extends HTMLProps<HTMLButtonElement> {
   /** The content to display inside the badged link. */
   children: ReactNode;
   /** A class name for the badged link to give custom styles. */
@@ -18,10 +18,6 @@ interface BadgedLinkProps {
   inherit?: boolean;
   /** A custom element to be rendered */
   element?: ElementType | undefined;
-  /** Callback function that is fired when mouse leaves the component. */
-  onMouseLeave?: () => void;
-  /** Callback function that is fired when the mouse button is released. */
-  onMouseUp?: () => void;
 }
 
 const BadgedLink = forwardRef(
@@ -33,32 +29,10 @@ const BadgedLink = forwardRef(
       iconPlacement = 'left',
       element = 'a',
       inherit = true,
-      onMouseUp,
-      onMouseLeave,
       ...others
     }: BadgedLinkProps,
     ref,
   ) => {
-    const badgedLinkRef = useRef<HTMLButtonElement>();
-    useImperativeHandle(ref, () => badgedLinkRef.current);
-
-    const blur = () => {
-      const currentBadgedLinkRef = badgedLinkRef.current;
-      if (currentBadgedLinkRef) {
-        currentBadgedLinkRef.blur();
-      }
-    };
-
-    const handleMouseUp = () => {
-      blur();
-      onMouseUp && onMouseUp();
-    };
-
-    const handleMouseLeave = () => {
-      blur();
-      onMouseLeave && onMouseLeave();
-    };
-
     const classNames = cx(
       uiUtilities['reset-font-smoothing'],
       uiTypography['ui-text-body'],
@@ -72,15 +46,7 @@ const BadgedLink = forwardRef(
     );
 
     return (
-      <Box
-        element={element}
-        ref={badgedLinkRef}
-        className={classNames}
-        data-teamleader-ui="badgedLink"
-        onMouseUp={handleMouseUp}
-        onMouseLeave={handleMouseLeave}
-        {...others}
-      >
+      <Box element={element} ref={pickBoxProps(ref)} className={classNames} data-teamleader-ui="badgedLink" {...others}>
         {iconPlacement === 'left' && icon}
         {children}
         {iconPlacement === 'right' && icon}
