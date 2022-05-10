ed92c3489fe0dfed977a1aba26d84381069500fd
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-10 16:39:38 +0200

    🏷️ Convert `Container` stories to typescript

--- src/components/container/container.stories.tsx
+++ src/components/container/container.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
 import Container from '.';
 import Box from '../box';
@@ -6,9 +7,9 @@ import Box from '../box';
 export default {
   component: Container,
   title: addStoryInGroup(LOW_LEVEL_BLOCKS, 'Container'),
-};
+} as ComponentMeta<typeof Container>;
 
-export const Basic = (args) => (
+export const Basic: ComponentStory<typeof Container> = (args) => (
   <Box backgroundColor="gold" backgroundTint="lightest">
     <Container {...args} backgroundColor="violet" backgroundTint="light">
       <Box backgroundColor="aqua" backgroundTint="light" style={{ height: 400 }} />
