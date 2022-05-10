22d906c22fe0fce11a00879895431d7ed3108f83
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-10 11:06:14 +0200

    🏷️ Convert `Bullet` stories to typescript

--- src/components/bullet/bullet.stories.spec.tsx
+++ src/components/bullet/bullet.stories.spec.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import Bullet from './Bullet';
 
 export default {
   component: Bullet,
   title: 'Bullet',
-};
+} as ComponentMeta<typeof Bullet>;
 
-const Wrapper = ({ children }) => <div style={{ padding: '6px', display: 'flex' }}>{children}</div>;
+const Wrapper = ({ children }: { children: ReactNode }) => (
+  <div style={{ padding: '6px', display: 'flex' }}>{children}</div>
+);
 
-export const Main = () => (
+export const Main: ComponentStory<typeof Bullet> = () => (
   <div>
-    {['small', 'medium', 'large'].map((size) => (
+    {(['small', 'medium', 'large'] as const).map((size) => (
       <Wrapper key={size}>
-        {['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby'].map((color) => (
+        {(['neutral', 'mint', 'aqua', 'violet', 'teal', 'gold', 'ruby'] as const).map((color) => (
           <React.Fragment key={color}>
             <Bullet color={color} size={size} marginRight={1} />
           </React.Fragment>
