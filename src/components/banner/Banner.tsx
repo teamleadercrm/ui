6ebdf5f9396cf4c618658d2cddb4e6c78e5b2704
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-10 11:51:58 +0200

    📝 Reintroduced prop type descriptions

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -8,12 +8,19 @@ import theme from './theme.css';
 import { IconCloseMediumOutline } from '@teamleader/ui-icons';
 
 interface BannerProps {
+  /** The content to display inside the banner. */
   children?: React.ReactNode;
+  /** A class name for the wrapper to give custom styles. */
   className?: string;
+  /** The color of the banner. */
   color?: 'white' | 'neutral' | 'mint' | 'violet' | 'ruby' | 'gold' | 'aqua';
+  /** The size of the banner. */
   size?: 'small' | 'medium' | 'large';
+  /** The icon displayed on the left inside the banner. */
   icon?: React.ReactElement;
+  /** Callback function that is fired when the close button of the banner is clicked. */
   onClose?(...args: unknown[]): unknown;
+  /** If true, component will take the full width of it's container. */
   fullWidth?: boolean;
 }
 
