1307e8e3ec0da5cf5c576678940edabec917e249
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-10 15:17:18 +0200

    🏷️ Convert `ButtonGroup` to typescript

--- src/components/buttonGroup/ButtonGroup.tsx
+++ src/components/buttonGroup/ButtonGroup.tsx
@@ -1,22 +1,44 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent, ReactElement } from 'react';
 import omit from 'lodash.omit';
-import Box, { omitBoxProps } from '../box';
+import Box, { omitBoxProps, pickBoxProps } from '../box';
 import Button from '../button';
 import cx from 'classnames';
 import isComponentOfType from '../utils/is-component-of-type';
 import theme from './theme.css';
+import { BoxProps } from '../box/Box';
+import { BUTTON_LEVELS } from '../button/Button';
 
-/** @type {React.ComponentType<any>} */
-class ButtonGroup extends PureComponent {
-  handleChange = (value, event) => {
+interface ButtonGroupProps extends Omit<BoxProps, 'size' | 'onChange' | 'ref'> {
+  /** The content to display inside the button group. */
+  children?: React.ReactNode;
+  /** A class name for the wrapper to give custom styles. */
+  className?: string;
+  /** If true, child components will be displayed segmented. */
+  segmented?: boolean;
+  /** The level of the buttons */
+  size?: 'tiny' | 'small' | 'medium' | 'large';
+  /** The level of the buttons */
+  level?: keyof typeof BUTTON_LEVELS;
+  /** The value of the currently active button. */
+  value?: string;
+  /** Callback function that is fired when the active button changes. */
+  onChange?: (value: string, event: Event) => void;
+}
+
+function isReactElement(node: unknown): node is ReactElement {
+  const element = node as ReactElement;
+  return element.type !== undefined && element.props !== undefined;
+}
+
+class ButtonGroup extends PureComponent<ButtonGroupProps> {
+  handleChange = (value: string, event: Event) => {
     if (this.props.onChange) {
       this.props.onChange(value, event);
     }
   };
 
   render() {
-    const { children, className, segmented, value, ...others } = this.props;
+    const { children, className, segmented = false, value, ...others } = this.props;
 
     const classNames = cx(
       theme['group'],
@@ -27,9 +49,9 @@ class ButtonGroup extends PureComponent {
     );
 
     return (
-      <Box data-teamleader-ui="button-group" className={classNames} {...others}>
+      <Box data-teamleader-ui="button-group" className={classNames} {...pickBoxProps(others)}>
         {React.Children.map(children, (child) => {
-          if (!isComponentOfType(Button, child)) {
+          if (!isComponentOfType(Button, child) || !isReactElement(child)) {
             return child;
           }
 
@@ -37,7 +59,7 @@ class ButtonGroup extends PureComponent {
           if (value) {
             optionalChildProps = {
               active: child.props.value === value,
-              onClick: (event) => this.handleChange(child.props.value, event),
+              onClick: (event: Event) => this.handleChange(child.props.value, event),
             };
           }
 
@@ -54,22 +76,5 @@ class ButtonGroup extends PureComponent {
     );
   }
 }
-
-ButtonGroup.propTypes = {
-  /** The content to display inside the button group. */
-  children: PropTypes.node,
-  /** A class name for the wrapper to give custom styles. */
-  className: PropTypes.string,
-  /** If true, child components will be displayed segmented. */
-  segmented: PropTypes.bool,
-  /** The value of the currently active button. */
-  value: PropTypes.string,
-  /** Callback function that is fired when the active button changes. */
-  onChange: PropTypes.func,
-};
-
-ButtonGroup.defaultProps = {
-  segmented: false,
-};
 
 export default ButtonGroup;
