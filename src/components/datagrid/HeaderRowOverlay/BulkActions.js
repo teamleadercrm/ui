711f2f58dcbc34e4abe6271ca6a6a8bab3966402
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-11 09:36:47 +0200

    ♻️ Convert `BulkActions` to a functional component

--- src/components/datagrid/HeaderRowOverlay/BulkActions.js
+++ src/components/datagrid/HeaderRowOverlay/BulkActions.js
@@ -1,25 +1,16 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '../../box';
 
-class BulkActions extends PureComponent {
-  render() {
-    const { bulkActions } = this.props;
-    return (
-      <Box display="flex" flex="1" alignItems="center" data-teamleader-ui="datagrid-bulk-actions">
-        {bulkActions}
-      </Box>
-    );
-  }
-}
+const BulkActions = ({ bulkActions = [] }) => (
+  <Box display="flex" flex="1" alignItems="center" data-teamleader-ui="datagrid-bulk-actions">
+    {bulkActions}
+  </Box>
+);
 
 BulkActions.propTypes = {
   /** Button element(s), used to display the bulk actions */
   bulkActions: PropTypes.any,
-};
-
-BulkActions.defaultProps = {
-  bulkActions: [],
 };
 
 export default BulkActions;
