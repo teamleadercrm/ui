c765f133c70375d5cc86391f2088affbf04b49d8
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-11 09:50:12 +0200

    ♻️ Convert `FooterRow` to a functional component

--- src/components/datagrid/FooterRow.js
+++ src/components/datagrid/FooterRow.js
@@ -1,26 +1,22 @@
-import React, { forwardRef, PureComponent } from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import Cell from './Cell';
 import Row from './Row';
 import cx from 'classnames';
 import theme from './theme.css';
 
-class FooterRow extends PureComponent {
-  render() {
-    const { className, children, forwardedRef, sliceFrom, sliceTo, preserveSelectableSpace, ...others } = this.props;
+const FooterRow = ({ className, children, forwardedRef, sliceFrom, sliceTo, preserveSelectableSpace, ...others }) => {
+  const childrenArray = Array.isArray(children) ? children : [children];
+  const childrenSliced = childrenArray.slice(sliceFrom, sliceTo);
+  const classNames = cx(theme['footer-row'], className);
 
-    const childrenArray = Array.isArray(children) ? children : [children];
-    const childrenSliced = childrenArray.slice(sliceFrom, sliceTo);
-    const classNames = cx(theme['footer-row'], className);
-
-    return (
-      <Row className={classNames} data-teamleader-ui="datagrid-footer-row" ref={forwardedRef} {...others}>
-        {preserveSelectableSpace && <Cell flex="checkbox-width" preventOverflow={false} />}
-        {childrenSliced}
-      </Row>
-    );
-  }
-}
+  return (
+    <Row className={classNames} data-teamleader-ui="datagrid-footer-row" ref={forwardedRef} {...others}>
+      {preserveSelectableSpace && <Cell flex="checkbox-width" preventOverflow={false} />}
+      {childrenSliced}
+    </Row>
+  );
+};
 
 FooterRow.propTypes = {
   className: PropTypes.string,
