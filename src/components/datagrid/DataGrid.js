1e218844358ca22f5b68a067c48d6d671780b4cc
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-11 18:49:51 +0200

    ♻️ Convert `DataGrid` to a functional component

--- src/components/datagrid/DataGrid.js
+++ src/components/datagrid/DataGrid.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Box from '../box';
 import LoadingBar from '../loadingBar';
@@ -25,32 +25,48 @@ import theme from './theme.css';
  *    FooterRow: React.ComponentType<any>;
  * }}
  */
-class DataGrid extends PureComponent {
-  state = {
-    hoveredRow: null,
-    isOverflowing: false,
-    selectedRows: [],
-  };
+const DataGrid = ({
+  bordered = false,
+  children,
+  className,
+  processing = false,
+  comparableId,
+  selectable,
+  stickyFromLeft,
+  stickyFromRight,
+  onSelectionChange,
+  ...others
+}) => {
+  const [hoveredRow, setHoveredRow] = useState(null);
+  const [isOverflowing, setOverflowing] = useState(false);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const scrollableNode = useRef(null);
+  const [rowNodes, setRowNodes] = useState(new Map());
 
-  rowNodes = new Map();
+  const totalRowChildrenWidth = useMemo(
+    () =>
+      Array.from(rowNodes.values())
+        .filter((rowDOMNode) => rowDOMNode.children)
+        .map((rowDOMNode) =>
+          Array.from(rowDOMNode.children)
+            .map((child) => child.clientWidth)
+            .reduce((accumulatedChildWidth, currentChildWidth) => accumulatedChildWidth + currentChildWidth, 0),
+        )
+        .reduce((maxRowWidth, currentRowWidth) => (currentRowWidth > maxRowWidth ? currentRowWidth : maxRowWidth), 0),
+    [rowNodes, rowNodes.size],
+  );
 
-  scrollableNode = null;
+  useEffect(() => {
+    handleResize();
+  });
 
-  componentDidUpdate(prevProps) {
-    this.handleResize();
+  useEffect(() => {
+    handleSelectionChange([]);
+    setSelectedRows([]);
+  }, [comparableId]);
 
-    if (prevProps.comparableId !== this.props.comparableId) {
-      this.handleSelectionChange([]);
-
-      /* eslint-disable react/no-did-update-set-state */
-      this.setState({
-        selectedRows: [],
-      });
-    }
-  }
-
-  handleHeaderRowSelectionChange = (checked, event) => {
-    const allBodyRowIndexes = React.Children.map(this.props.children, (child) => {
+  const handleHeaderRowSelectionChange = (checked, event) => {
+    const allBodyRowIndexes = React.Children.map(children, (child) => {
       if (isComponentOfType(BodyRow, child)) {
         return child.key;
       }
@@ -58,185 +74,159 @@ class DataGrid extends PureComponent {
 
     const selectedBodyRowIndexes = checked ? allBodyRowIndexes : [];
 
-    this.setState({
-      selectedRows: selectedBodyRowIndexes,
-    });
-
-    this.handleSelectionChange(selectedBodyRowIndexes, event);
+    setSelectedRows(selectedBodyRowIndexes);
+    handleSelectionChange(selectedBodyRowIndexes, event);
   };
 
-  handleBodyRowMouseEnter = (row, event) => {
+  const handleBodyRowMouseEnter = (row, event) => {
     const { onClick, onMouseOver } = row.props;
 
-    onClick && this.setState({ hoveredRow: row.key });
+    onClick && setHoveredRow({ hoveredRow: row.key });
     onMouseOver && onMouseOver(event);
   };
 
-  handleBodyRowMouseLeave = (row, event) => {
+  const handleBodyRowMouseLeave = (row, event) => {
     const { onClick, onMouseOut } = row.props;
 
-    onClick && this.setState({ hoveredRow: null });
+    onClick && setHoveredRow(null);
     onMouseOut && onMouseOut(event);
   };
 
-  handleBodyRowSelectionChange = (rowIndex, event) => {
-    this.setState((prevState) => {
-      const selectedRows = prevState.selectedRows.includes(rowIndex)
-        ? prevState.selectedRows.filter((row) => row !== rowIndex)
-        : [...prevState.selectedRows, rowIndex];
+  const handleBodyRowSelectionChange = (rowIndex, event) => {
+    const rows = selectedRows.includes(rowIndex)
+      ? selectedRows.filter((row) => row !== rowIndex)
+      : [...selectedRows, rowIndex];
 
-      this.handleSelectionChange(selectedRows, event);
-
-      return {
-        ...prevState,
-        selectedRows,
-      };
-    });
+    setSelectedRows(rows);
+    handleSelectionChange(selectedRows, event);
   };
 
-  handleResize = () => {
-    if (isElementOverflowingX(this.scrollableNode) && this.rowNodes) {
-      this.setCalculatedRowWidth();
-      this.setState({ isOverflowing: true });
+  const handleResize = () => {
+    if (isElementOverflowingX(scrollableNode.current)) {
+      setOverflowing(true);
     } else {
-      this.setState({ isOverflowing: false });
+      setOverflowing(false);
     }
   };
 
-  handleSelectionChange(selection, event = null) {
-    if (this.props.onSelectionChange) {
-      this.props.onSelectionChange(selection, event);
+  const handleSelectionChange = (selection, event = null) => {
+    if (onSelectionChange) {
+      onSelectionChange(selection, event);
     }
-  }
-
-  setCalculatedRowWidth = () => {
-    const rowDOMNodes = [];
-    let maxRowWidth = 0;
-
-    [...this.rowNodes.values()]
-      .filter((rowDOMNode) => rowDOMNode != null)
-      .forEach((rowDOMNode) => {
-        if (rowDOMNode) {
-          const totalRowChildrenWidth = [...rowDOMNode.children]
-            .map((child) => child.offsetWidth)
-            .reduce((accumulatedChildWidth, currentChildWidth) => accumulatedChildWidth + currentChildWidth);
-
-          maxRowWidth = maxRowWidth < totalRowChildrenWidth ? totalRowChildrenWidth : maxRowWidth;
-          rowDOMNodes.push(rowDOMNode);
-        }
-      });
-
-    rowDOMNodes.forEach((rowDOMNode) => (rowDOMNode.style.minWidth = `${maxRowWidth}px`));
   };
 
-  render() {
-    const { bordered, children, className, processing, selectable, stickyFromLeft, stickyFromRight, ...others } =
-      this.props;
-    const { hoveredRow, isOverflowing, selectedRows } = this.state;
+  const classNames = cx(
+    theme['data-grid'],
+    {
+      [theme['is-bordered']]: bordered,
+      [theme['is-overflowing']]: isOverflowing,
+    },
+    className,
+  );
 
-    const classNames = cx(
-      theme['data-grid'],
-      {
-        [theme['is-bordered']]: bordered,
-        [theme['is-overflowing']]: isOverflowing,
-      },
-      className,
-    );
+  const rest = omit(others, ['comparableId', 'onSelectionChange']);
 
-    const rest = omit(others, ['comparableId', 'onSelectionChange']);
+  const sectionLeftClassNames = cx(theme['section'], theme['is-sticky-left'], {
+    [theme['has-blend-right']]: selectable || stickyFromLeft > 0,
+    [theme['has-border-right']]: selectable || stickyFromLeft > 0,
+  });
 
-    const sectionLeftClassNames = cx(theme['section'], theme['is-sticky-left'], {
-      [theme['has-blend-right']]: selectable || stickyFromLeft > 0,
-      [theme['has-border-right']]: selectable || stickyFromLeft > 0,
-    });
-
-    return (
-      <Box data-teamleader-ui="data-grid" className={classNames} {...rest}>
-        {processing && (
-          <div className={cx(theme['loading-bar'])}>
-            <LoadingBar />
-          </div>
-        )}
-        {selectedRows.length > 0 &&
-          React.Children.map(children, (child) => {
-            if (isComponentOfType(HeaderRowOverlay, child)) {
-              return React.cloneElement(child, {
-                numSelectedRows: selectedRows.length,
-              });
-            }
-          })}
-        <Box display="flex" className={cx(theme['section-wrapper'])}>
-          {(selectable || stickyFromLeft > 0) && (
-            <div className={sectionLeftClassNames}>
-              {React.Children.map(children, (child) => {
-                if (isComponentOfType(HeaderRow, child)) {
-                  return React.cloneElement(child, {
-                    onSelectionChange: this.handleHeaderRowSelectionChange,
-                    selected: selectedRows.length === children.find((child) => Array.isArray(child)).length,
-                    selectable,
-                    sliceTo: stickyFromLeft > 0 ? stickyFromLeft : 0,
-                  });
-                } else if (isComponentOfType(BodyRow, child)) {
-                  return React.cloneElement(child, {
-                    hovered: hoveredRow === child.key,
-                    onMouseEnter: (event) => this.handleBodyRowMouseEnter(child, event),
-                    onMouseLeave: (event) => this.handleBodyRowMouseLeave(child, event),
-                    onSelectionChange: (checked, event) => this.handleBodyRowSelectionChange(child.key, event),
-                    selected: selectedRows.indexOf(child.key) !== -1,
-                    selectable,
-                    sliceTo: stickyFromLeft > 0 ? stickyFromLeft : 0,
-                  });
-                } else if (isComponentOfType(FooterRow, child)) {
-                  return React.cloneElement(child, {
-                    preserveSelectableSpace: selectable,
-                    sliceTo: stickyFromLeft > 0 ? stickyFromLeft : 0,
-                  });
-                }
-              })}
-            </div>
-          )}
-          <div className={cx(theme['section'], theme['is-scrollable'])} ref={(node) => (this.scrollableNode = node)}>
-            {React.Children.map(children, (child, key) => {
-              if (isComponentOfType(HeaderRow, child) || isComponentOfType(FooterRow, child)) {
+  return (
+    <Box data-teamleader-ui="data-grid" className={classNames} {...rest}>
+      {processing && (
+        <div className={cx(theme['loading-bar'])}>
+          <LoadingBar />
+        </div>
+      )}
+      {selectedRows.length > 0 &&
+        React.Children.map(children, (child) => {
+          if (isComponentOfType(HeaderRowOverlay, child)) {
+            return React.cloneElement(child, {
+              numSelectedRows: selectedRows.length,
+            });
+          }
+        })}
+      <Box display="flex" className={cx(theme['section-wrapper'])}>
+        {(selectable || stickyFromLeft > 0) && (
+          <div className={sectionLeftClassNames}>
+            {React.Children.map(children, (child) => {
+              if (isComponentOfType(HeaderRow, child)) {
                 return React.cloneElement(child, {
-                  sliceFrom: stickyFromLeft > 0 ? stickyFromLeft : 0,
-                  sliceTo: stickyFromRight > 0 ? -stickyFromRight : undefined,
-                  ref: (rowNode) => this.rowNodes.set(key, rowNode),
+                  onSelectionChange: handleHeaderRowSelectionChange,
+                  selected: selectedRows.length === children.find((child) => Array.isArray(child)).length,
+                  selectable,
+                  sliceTo: stickyFromLeft > 0 ? stickyFromLeft : 0,
                 });
               } else if (isComponentOfType(BodyRow, child)) {
                 return React.cloneElement(child, {
                   hovered: hoveredRow === child.key,
-                  onMouseEnter: (event) => this.handleBodyRowMouseEnter(child, event),
-                  onMouseLeave: (event) => this.handleBodyRowMouseLeave(child, event),
-                  sliceFrom: stickyFromLeft > 0 ? stickyFromLeft : 0,
-                  sliceTo: stickyFromRight > 0 ? -stickyFromRight : undefined,
-                  ref: (rowNode) => this.rowNodes.set(key, rowNode),
+                  onMouseEnter: (event) => handleBodyRowMouseEnter(child, event),
+                  onMouseLeave: (event) => handleBodyRowMouseLeave(child, event),
+                  onSelectionChange: (checked, event) => handleBodyRowSelectionChange(child.key, event),
+                  selected: selectedRows.indexOf(child.key) !== -1,
+                  selectable,
+                  sliceTo: stickyFromLeft > 0 ? stickyFromLeft : 0,
+                });
+              } else if (isComponentOfType(FooterRow, child)) {
+                return React.cloneElement(child, {
+                  preserveSelectableSpace: selectable,
+                  sliceTo: stickyFromLeft > 0 ? stickyFromLeft : 0,
                 });
               }
             })}
           </div>
-          {stickyFromRight > 0 && (
-            <div className={cx(theme['section'], theme['has-blend-left'])}>
-              {React.Children.map(children, (child) => {
-                if (isComponentOfType(HeaderRow, child) || isComponentOfType(FooterRow, child)) {
-                  return React.cloneElement(child, { sliceFrom: -stickyFromRight });
-                } else if (isComponentOfType(BodyRow, child)) {
-                  return React.cloneElement(child, {
-                    hovered: hoveredRow === child.key,
-                    onMouseEnter: (event) => this.handleBodyRowMouseEnter(child, event),
-                    onMouseLeave: (event) => this.handleBodyRowMouseLeave(child, event),
-                    sliceFrom: -stickyFromRight,
-                  });
-                }
-              })}
-            </div>
-          )}
-        </Box>
-        <ReactResizeDetector handleWidth onResize={this.handleResize} refreshMode="throttle" refreshRate={16} />
+        )}
+        <div className={cx(theme['section'], theme['is-scrollable'])} ref={scrollableNode}>
+          {React.Children.map(children, (child, key) => {
+            if (isComponentOfType(HeaderRow, child) || isComponentOfType(FooterRow, child)) {
+              return React.cloneElement(child, {
+                sliceFrom: stickyFromLeft > 0 ? stickyFromLeft : 0,
+                sliceTo: stickyFromRight > 0 ? -stickyFromRight : undefined,
+                ref: (rowNode) => setRowNodes(rowNodes.set(key, rowNode)),
+                style: isOverflowing
+                  ? {
+                      minWidth: `${totalRowChildrenWidth - 10}px`,
+                    }
+                  : undefined,
+              });
+            } else if (isComponentOfType(BodyRow, child)) {
+              return React.cloneElement(child, {
+                hovered: hoveredRow === child.key,
+                onMouseEnter: (event) => handleBodyRowMouseEnter(child, event),
+                onMouseLeave: (event) => handleBodyRowMouseLeave(child, event),
+                sliceFrom: stickyFromLeft > 0 ? stickyFromLeft : 0,
+                sliceTo: stickyFromRight > 0 ? -stickyFromRight : undefined,
+                ref: (rowNode) => setRowNodes(rowNodes.set(key, rowNode)),
+                style: isOverflowing
+                  ? {
+                      minWidth: `${totalRowChildrenWidth - 10}px`,
+                    }
+                  : undefined,
+              });
+            }
+          })}
+        </div>
+        {stickyFromRight > 0 && (
+          <div className={cx(theme['section'], theme['has-blend-left'])}>
+            {React.Children.map(children, (child) => {
+              if (isComponentOfType(HeaderRow, child) || isComponentOfType(FooterRow, child)) {
+                return React.cloneElement(child, { sliceFrom: -stickyFromRight });
+              } else if (isComponentOfType(BodyRow, child)) {
+                return React.cloneElement(child, {
+                  hovered: hoveredRow === child.key,
+                  onMouseEnter: (event) => handleBodyRowMouseEnter(child, event),
+                  onMouseLeave: (event) => handleBodyRowMouseLeave(child, event),
+                  sliceFrom: -stickyFromRight,
+                });
+              }
+            })}
+          </div>
+        )}
       </Box>
-    );
-  }
-}
+      <ReactResizeDetector handleWidth onResize={handleResize} refreshMode="throttle" refreshRate={16} />
+    </Box>
+  );
+};
 
 DataGrid.propTypes = {
   /** If true, datagrid will have a border and rounded corners. */
@@ -257,11 +247,6 @@ DataGrid.propTypes = {
   onSelectionChange: PropTypes.func,
   /** If true, a loading bar will be rendered below the header row */
   processing: PropTypes.bool,
-};
-
-DataGrid.defaultProps = {
-  bordered: false,
-  processing: false,
 };
 
 DataGrid.HeaderRow = HeaderRow;
