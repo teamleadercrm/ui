323fc5f1b0906f968a1e4b8c35d7bef359f0ec5e
Lowie Benoot <lowie@teamleader.eu>
2022-05-12 09:42:28 +0200

    🔥 Remove the PropTypes

--- src/components/detailPage/DetailPage.tsx
+++ src/components/detailPage/DetailPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import DetailPageBody from './DetailPageBody';
 import DetailPageHeader from './DetailPageHeader';
 import Box from '../box';
@@ -7,10 +6,6 @@ import { BoxProps } from '../box/Box';
 
 const DetailPage = ({ children, ...others }: Omit<BoxProps, 'ref'>) => {
   return <Box {...others}>{children}</Box>;
-};
-
-DetailPage.propTypes = {
-  children: PropTypes.node.isRequired,
 };
 
 DetailPage.Body = DetailPageBody;
