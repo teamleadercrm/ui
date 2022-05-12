d29ec47da9123f043f78d8dc778a3152abbc32c1
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-12 10:50:43 +0200

    🏷️ Convert `HeaderRow` to typescript

--- src/components/datagrid/HeaderRow.tsx
+++ src/components/datagrid/HeaderRow.tsx
@@ -1,64 +1,48 @@
-import React, { forwardRef } from 'react';
-import PropTypes from 'prop-types';
-import Row from './Row';
+import React, { ChangeEvent, forwardRef } from 'react';
+import Row, { RowProps } from './Row';
 import HeaderCell from './HeaderCell';
 import Checkbox from '../checkbox';
 import cx from 'classnames';
 import theme from './theme.css';
 
-const HeaderRow = ({
-  className,
-  children,
-  forwardedRef,
-  sliceFrom,
-  sliceTo,
-  onSelectionChange,
-  selected,
-  selectable,
-  ...others
-}) => {
-  const childrenArray = Array.isArray(children) ? children : [children];
-  const childrenSliced = childrenArray.slice(sliceFrom, sliceTo);
-  const classNames = cx(theme['header-row'], className);
-
-  return (
-    <Row
-      backgroundColor="neutral"
-      className={classNames}
-      data-teamleader-ui="datagrid-header-row"
-      ref={forwardedRef}
-      {...others}
-    >
-      {selectable && (
-        <HeaderCell flex="checkbox-width" sortable={false}>
-          <Checkbox checked={selected} onChange={onSelectionChange} size="small" />
-        </HeaderCell>
-      )}
-      {childrenSliced}
-    </Row>
-  );
-};
-
-HeaderRow.propTypes = {
-  /** A class name for the row to give custom styles. */
-  className: PropTypes.string,
-  /** The cells to display inside the row. */
-  children: PropTypes.any,
+interface HeaderRowProps extends RowProps {
   /** Callback function that is fired when the checkbox on the left side has changed. */
-  onSelectionChange: PropTypes.func,
+  onSelectionChange?: (checked: boolean, event: ChangeEvent<HTMLInputElement>) => void;
   /** If true, checkboxes will be rendered on the left side of each row. */
-  selectable: PropTypes.bool,
+  selectable?: boolean;
   /** If true, the checkbox on the left side of the row will be checked. */
-  selected: PropTypes.bool,
+  selected?: boolean;
   /** Amount of cells that will be sliced off, starting from left. */
-  sliceFrom: PropTypes.number,
+  sliceFrom?: number;
   /** Amount of cells that will be sliced off, starting from right. */
-  sliceTo: PropTypes.number,
-};
+  sliceTo?: number;
+}
 
-/** @type {any} */
-const ForwardedHeaderRow = forwardRef((props, ref) => <HeaderRow {...props} forwardedRef={ref} />);
+const HeaderRow = forwardRef<HTMLElement, HeaderRowProps>(
+  ({ className, children, sliceFrom, sliceTo, onSelectionChange, selected, selectable, ...others }, ref) => {
+    const childrenArray = Array.isArray(children) ? children : [children];
+    const childrenSliced = childrenArray.slice(sliceFrom, sliceTo);
+    const classNames = cx(theme['header-row'], className);
 
-ForwardedHeaderRow.displayName = 'HeaderRow';
+    return (
+      <Row
+        backgroundColor="neutral"
+        className={classNames}
+        data-teamleader-ui="datagrid-header-row"
+        ref={ref}
+        {...others}
+      >
+        {selectable && (
+          <HeaderCell flex="checkbox-width" sortable={false}>
+            <Checkbox checked={selected} onChange={onSelectionChange} size="small" />
+          </HeaderCell>
+        )}
+        {childrenSliced}
+      </Row>
+    );
+  },
+);
 
-export default ForwardedHeaderRow;
+HeaderRow.displayName = 'HeaderRow';
+
+export default HeaderRow;
