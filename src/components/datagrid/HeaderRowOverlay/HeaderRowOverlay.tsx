d8de0fa728c55e9c8df09a2a76e0bbb6b82bf404
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-12 09:23:06 +0200

    🏷️ Convert `HeaderRowOverlay` to typescript

--- src/components/datagrid/HeaderRowOverlay/HeaderRowOverlay.tsx
+++ src/components/datagrid/HeaderRowOverlay/HeaderRowOverlay.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { ReactNode } from 'react';
 import cx from 'classnames';
 import Box from '../../box';
 import BulkActions from './BulkActions';
 import NumSelectedRows from './NumSelectedRows';
 import theme from './theme.css';
+
+export interface HeaderRowOverlayProps {
+  /** The size of the checkboxes in the grid, used to set the right spacing between this component and the checkbox in the HeaderCell */
+  headerCellCheckboxSize?: 'small' | 'medium' | 'large';
+  /** Passed down button element(s), are used to display the bulk actions */
+  children: ReactNode;
+  /** A class name for the container to give custom styles */
+  className?: string;
+  /** If this value is greater than 0 then this component renders, it is also passed down to display the number of selected rows */
+  numSelectedRows?: number;
+  /** Passed down function, is used to render the label accompanying the number of selected rows */
+  numSelectedRowsLabel?: (numSelectedRows?: number) => string;
+}
 
 const HeaderRowOverlay = ({
   children,
@@ -13,7 +25,7 @@ const HeaderRowOverlay = ({
   numSelectedRows = 0,
   numSelectedRowsLabel,
   ...others
-}) => {
+}: HeaderRowOverlayProps) => {
   const classNames = cx(
     theme['header-row-overlay'],
     theme[`data-grid-checkbox-size-${headerCellCheckboxSize}`],
@@ -32,19 +44,6 @@ const HeaderRowOverlay = ({
       <BulkActions bulkActions={children} />
     </Box>
   );
-};
-
-HeaderRowOverlay.propTypes = {
-  /** The size of the checkboxes in the grid, used to set the right spacing between this component and the checkbox in the HeaderCell */
-  headerCellCheckboxSize: PropTypes.oneOf(['small', 'medium', 'large']),
-  /** Passed down button element(s), are used to display the bulk actions */
-  children: PropTypes.any,
-  /** A class name for the container to give custom styles */
-  className: PropTypes.string,
-  /** If this value is greater than 0 then this component renders, it is also passed down to display the number of selected rows */
-  numSelectedRows: PropTypes.number,
-  /** Passed down function, is used to render the label accompanying the number of selected rows */
-  numSelectedRowsLabel: PropTypes.func,
 };
 
 export default HeaderRowOverlay;
