1b440fc43a8c074f27dbe892819e3a298171355a
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-12 10:33:28 +0200

    🏷️ Convert `BodyRow` to typescript

--- src/components/datagrid/BodyRow.tsx
+++ src/components/datagrid/BodyRow.tsx
@@ -1,84 +1,69 @@
 import React, { forwardRef, useCallback } from 'react';
-import PropTypes from 'prop-types';
 import Cell from './Cell';
 import Checkbox from '../checkbox';
-import Row from './Row';
+import Row, { RowProps } from './Row';
 import cx from 'classnames';
 import theme from './theme.css';
 
-const BodyRow = ({
-  className,
-  children,
-  forwardedRef,
-  hovered,
-  sliceFrom,
-  sliceTo,
-  onClick,
-  onSelectionChange,
-  selected,
-  selectable,
-  ...others
-}) => {
-  const handleClick = useCallback(
-    (event) => {
-      onClick && onClick(event);
-    },
-    [onClick],
-  );
-
-  const childrenArray = Array.isArray(children) ? children : [children];
-  const childrenSliced = childrenArray.slice(sliceFrom, sliceTo);
-  const classNames = cx(
-    theme['body-row'],
-    {
-      [theme['has-pointer-cursor']]: onClick,
-    },
-    className,
-  );
-
-  return (
-    <Row
-      backgroundColor={hovered ? 'neutral' : 'white'}
-      className={classNames}
-      data-teamleader-ui="datagrid-body-row"
-      onClick={handleClick}
-      ref={forwardedRef}
-      {...others}
-    >
-      {selectable && (
-        <Cell flex="checkbox-width" onClick={(event) => event.stopPropagation()} preventOverflow={false}>
-          <Checkbox checked={selected} onChange={onSelectionChange} size="small" />
-        </Cell>
-      )}
-      {childrenSliced}
-    </Row>
-  );
-};
-
-BodyRow.propTypes = {
-  /** A class name for the row to give custom styles. */
-  className: PropTypes.string,
-  /** The cells to display inside the row. */
-  children: PropTypes.any,
+interface BodyRowProps extends RowProps {
   /** If true, the row will show a hover state */
-  hovered: PropTypes.bool,
+  hovered?: boolean;
   /** Callback function that is fired when the row is clicked */
-  onClick: PropTypes.func,
+  onClick?: (event: Event) => void;
   /** Callback function that is fired when the checkbox on the left side has changed. */
-  onSelectionChange: PropTypes.func,
+  onSelectionChange?: (checked: boolean, event: Event) => void;
   /** If true, checkboxes will be rendered on the left side of each row. */
-  selectable: PropTypes.bool,
+  selectable?: boolean;
   /** If true, the checkbox on the left side of the row will be checked. */
-  selected: PropTypes.bool,
+  selected?: boolean;
   /** Amount of cells that will be sliced off, starting from left. */
-  sliceFrom: PropTypes.number,
+  sliceFrom?: number;
   /** Amount of cells that will be sliced off, starting from right. */
-  sliceTo: PropTypes.number,
-};
+  sliceTo?: number;
+}
 
-/** @type {any} */
-const ForwardedBodyRow = forwardRef((props, ref) => <BodyRow {...props} forwardedRef={ref} />);
+const BodyRow = forwardRef<any, BodyRowProps>(
+  (
+    { className, children, hovered, sliceFrom, sliceTo, onClick, onSelectionChange, selected, selectable, ...others },
+    ref,
+  ) => {
+    const handleClick = useCallback(
+      (event) => {
+        onClick && onClick(event);
+      },
+      [onClick],
+    );
 
-ForwardedBodyRow.displayName = 'BodyRow';
+    const childrenArray = Array.isArray(children) ? children : [children];
+    const childrenSliced = childrenArray.slice(sliceFrom, sliceTo);
+    const classNames = cx(
+      theme['body-row'],
+      {
+        [theme['has-pointer-cursor']]: onClick,
+      },
+      className,
+    );
 
-export default ForwardedBodyRow;
+    return (
+      <Row
+        backgroundColor={hovered ? 'neutral' : 'white'}
+        className={classNames}
+        data-teamleader-ui="datagrid-body-row"
+        onClick={handleClick}
+        ref={ref}
+        {...others}
+      >
+        {selectable && (
+          <Cell flex="checkbox-width" onClick={(event) => event.stopPropagation()} preventOverflow={false}>
+            <Checkbox checked={selected} onChange={onSelectionChange} size="small" />
+          </Cell>
+        )}
+        {childrenSliced}
+      </Row>
+    );
+  },
+);
+
+BodyRow.displayName = 'BodyRow';
+
+export default BodyRow;
