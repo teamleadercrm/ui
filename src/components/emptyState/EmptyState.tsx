a0635fd181c7e01cfe881366215535a9c17146dd
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2022-05-12 16:29:07 +0200

    convert EmptyState to functional component

--- src/components/emptyState/EmptyState.tsx
+++ src/components/emptyState/EmptyState.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '../box';
 import cx from 'classnames';
@@ -16,43 +16,38 @@ const illustrationMap = {
   large: <IllustrationEmptyState130X130Pointer />,
 };
 
-/** @type {React.ComponentType<any>} */
-class EmptyState extends PureComponent {
-  render() {
-    const { className, metaText, hidePointer, size, title, ...others } = this.props;
+const EmptyState = ({ className, metaText, hidePointer, size, title, ...others }) => {
+  const classNames = cx(
+    theme['wrapper'],
+    theme[`is-${size}`],
+    {
+      [theme['has-pointer']]: title,
+    },
+    className,
+  );
 
-    const classNames = cx(
-      theme['wrapper'],
-      theme[`is-${size}`],
-      {
-        [theme['has-pointer']]: title,
-      },
-      className,
-    );
-
-    return (
-      <Box
-        {...others}
-        alignItems="center"
-        className={classNames}
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        textAlign="center"
-      >
-        {title && !hidePointer && <div className={theme['pointer']}>{illustrationMap[size]}</div>}
-        <div className={theme['content']}>
-          {title && <Heading3 color="teal">{title}</Heading3>}
-          {metaText && (
-            <TextBody marginTop={title && 2} color="neutral">
-              {metaText}
-            </TextBody>
-          )}
-        </div>
-      </Box>
-    );
-  }
-}
+  return (
+    <Box
+      {...others}
+      alignItems="center"
+      className={classNames}
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      textAlign="center"
+    >
+      {title && !hidePointer && <div className={theme['pointer']}>{illustrationMap[size]}</div>}
+      <div className={theme['content']}>
+        {title && <Heading3 color="teal">{title}</Heading3>}
+        {metaText && (
+          <TextBody marginTop={title && 2} color="neutral">
+            {metaText}
+          </TextBody>
+        )}
+      </div>
+    </Box>
+  );
+};
 
 EmptyState.propTypes = {
   hidePointer: PropTypes.bool,
