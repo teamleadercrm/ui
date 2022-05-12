7df55be6599e771993efa42c53a5c52e82ef20d5
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-05-12 10:45:02 +0200

    🏷️ Convert `HeaderCell` to typescript

--- src/components/datagrid/HeaderCell.tsx
+++ src/components/datagrid/HeaderCell.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import theme from './theme.css';
-import Cell from './Cell';
+import Cell, { CellProps } from './Cell';
 import Icon from '../icon';
 import { UITextBody } from '../typography';
 import cx from 'classnames';
 import { IconArrowDownSmallOutline, IconArrowUpSmallOutline } from '@teamleader/ui-icons';
 
-const HeaderCell = ({ align = 'left', children, className, onClick, sortable, sorted, ...others }) => {
+export interface HeaderCellProps extends CellProps {
+  /** If true, sorting arrows will appear next to the text */
+  sortable?: boolean;
+  /** The order in which the grid rows will be sorted. */
+  sorted?: 'asc' | 'desc';
+}
+
+const HeaderCell = ({ align = 'left', children, className, onClick, sortable, sorted, ...others }: HeaderCellProps) => {
   const renderSortedIndicators = () => {
     if (sorted === 'asc' || (!sorted && sortable)) {
       return <IconArrowUpSmallOutline />;
@@ -38,21 +44,6 @@ const HeaderCell = ({ align = 'left', children, className, onClick, sortable, so
       {sortable && align === 'left' && <Icon marginLeft={1}>{renderSortedIndicators()}</Icon>}
     </Cell>
   );
-};
-
-HeaderCell.propTypes = {
-  /** The horizontal alignment of the text inside the cell. */
-  align: PropTypes.oneOf(['left', 'center', 'right']),
-  /** The content to display inside the cell. */
-  children: PropTypes.any,
-  /** A class name for the wrapper to give custom styles. */
-  className: PropTypes.string,
-  /** Callback function that is fired when clicking on the cell. */
-  onClick: PropTypes.func,
-  /** If true, sorting arrows will appear next to the text */
-  sortable: PropTypes.bool,
-  /** The order in which the grid rows will be sorted. */
-  sorted: PropTypes.oneOf(['asc', 'desc']),
 };
 
 export default HeaderCell;
