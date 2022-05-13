faf703276079b8face52a604a6ed7015311aab4a
qubis741 <jakub.kovacikk@gmail.com>
2022-05-13 15:04:30 +0200

    convert LoadingSpinner stories to TS

--- src/components/loadingSpinner/loadingSpinner.stories.tsx
+++ src/components/loadingSpinner/loadingSpinner.stories.tsx
@@ -1,10 +1,11 @@
+import { ComponentMeta } from '@storybook/react';
 import React from 'react';
 import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
-import LoadingSpinner from './LoadingSpinner';
+import LoadingSpinner, { LoadingSpinnerProps } from './LoadingSpinner';
 
 export default {
   component: LoadingSpinner,
   title: addStoryInGroup(LOW_LEVEL_BLOCKS, 'Loading spinner'),
-};
+} as ComponentMeta<typeof LoadingSpinner>;
 
-export const DefaultStory = (args) => <LoadingSpinner {...args} />;
+export const DefaultStory = (args: LoadingSpinnerProps) => <LoadingSpinner {...args} />;
