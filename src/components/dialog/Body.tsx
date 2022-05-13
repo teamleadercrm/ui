8a31c2370c9c3d3ee53b5612c19cfcc09b08303d
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2022-05-13 14:47:18 +0200

    extend Body with BoxProps + fix ref type

--- src/components/dialog/Body.tsx
+++ src/components/dialog/Body.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode, forwardRef } from 'react';
+import React, { ReactNode, forwardRef, RefObject } from 'react';
 
 import { Box } from '../../index';
+import { BoxProps } from '../box/Box';
 
-interface BodyProps {
+interface BodyProps extends Omit<BoxProps, 'ref'> {
   /** The content to display inside the dialog. */
   children?: ReactNode;
   /** If true, the content will be scrollable when it exceeds the available height. */
@@ -11,7 +12,7 @@ interface BodyProps {
 
 const Body = forwardRef(({ scrollable, children, ...rest }: BodyProps, ref) => {
   if (!scrollable) {
-    return <div ref={ref}>{children}</div>;
+    return <div ref={ref as RefObject<HTMLDivElement>}>{children}</div>;
   }
 
   return (
