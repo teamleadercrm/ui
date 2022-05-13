f9447af5ed74b75416878c847860917e0d941ce1
qubis741 <jakub.kovacikk@gmail.com>
2022-05-13 15:04:48 +0200

    converted LoadingBar to functional TS component

--- src/components/loadingBar/LoadingBar.tsx
+++ src/components/loadingBar/LoadingBar.tsx
@@ -1,46 +1,38 @@
-import React, { PureComponent } from 'react';
 import cx from 'classnames';
-import PropTypes from 'prop-types';
-
+import React from 'react';
 import Box from '../box';
 import theme from './theme.css';
 
-/** @type {React.ComponentType<any>} */
-class LoadingBar extends PureComponent {
-  render() {
-    const { className, color, size, tint, ...others } = this.props;
+type Size = 'small' | 'medium' | 'large';
 
-    const classNames = cx(
-      theme['loading-bar'],
-      theme[`is-${color}`],
-      theme[`is-${size}`],
-      theme[`is-${tint}`],
-      className,
-    );
+type Color = 'aqua' | 'gold' | 'mint' | 'neutral' | 'ruby' | 'teal' | 'violet';
 
-    return (
-      <Box data-teamleader-ui="loading-bar" className={classNames} {...others}>
-        <div className={theme['loading-bar-indicator']} />
-      </Box>
-    );
-  }
+type Tint = 'lightest' | 'light' | 'normal' | 'dark' | 'darkest';
+
+export interface LoadingBarProps {
+  /** A class name for the wrapper to add custom classes */
+  className: string;
+  /** The color of the components */
+  color: Color;
+  /** Size of the component */
+  size: Size;
+  /** The tint of the components color */
+  tint: Tint;
 }
 
-LoadingBar.propTypes = {
-  /** A class name for the wrapper to add custom classes */
-  className: PropTypes.string,
-  /** The color of the components */
-  color: PropTypes.oneOf(['aqua', 'gold', 'mint', 'neutral', 'ruby', 'teal', 'violet']),
-  /** Size of the component */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /** The tint of the components color */
-  tint: PropTypes.oneOf(['lightest', 'light', 'normal', 'dark', 'darkest']),
-};
-
-LoadingBar.defaultProps = {
-  color: 'mint',
-  size: 'small',
-  tint: 'normal',
+const LoadingBar = ({ className, color = 'mint', size = 'small', tint = 'normal', ...others }: LoadingBarProps) => {
+  const classNames = cx(
+    theme['loading-bar'],
+    theme[`is-${color}`],
+    theme[`is-${size}`],
+    theme[`is-${tint}`],
+    className,
+  );
+  return (
+    <Box data-teamleader-ui="loading-bar" className={classNames} {...others}>
+      <div className={theme['loading-bar-indicator']} />
+    </Box>
+  );
 };
 
 export default LoadingBar;
