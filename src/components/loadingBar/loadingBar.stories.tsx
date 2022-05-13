5212bd100c9151bd6e7ce7b6a6b8e4f6741cdfdf
qubis741 <jakub.kovacikk@gmail.com>
2022-05-13 15:05:06 +0200

    converted LoadingBar story to TS

--- src/components/loadingBar/loadingBar.stories.tsx
+++ src/components/loadingBar/loadingBar.stories.tsx
@@ -1,10 +1,11 @@
+import { ComponentMeta } from '@storybook/react';
 import React from 'react';
 import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
-import LoadingBar from './LoadingBar';
+import LoadingBar, { LoadingBarProps } from './LoadingBar';
 
 export default {
   component: LoadingBar,
   title: addStoryInGroup(LOW_LEVEL_BLOCKS, 'LoadingBar'),
-};
+} as ComponentMeta<typeof LoadingBar>;
 
-export const basic = (args) => <LoadingBar {...args} />;
+export const basic = (args: LoadingBarProps) => <LoadingBar {...args} />;
