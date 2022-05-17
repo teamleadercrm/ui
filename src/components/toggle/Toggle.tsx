960e82d5dd88de01a6a484d3ec6999f30c291e60
qubis741 <jakub.kovacikk@gmail.com>
2022-05-17 11:19:22 +0200

    converted Toggle component to functional TS component

--- src/components/toggle/Toggle.tsx
+++ src/components/toggle/Toggle.tsx
@@ -1,105 +1,93 @@
 import cx from 'classnames';
 import omit from 'lodash.omit';
-import PropTypes from 'prop-types';
-import React, { createRef, PureComponent } from 'react';
+import React, { ChangeEvent, ReactNode, useRef } from 'react';
 import Box, { omitBoxProps, pickBoxProps } from '../box';
+import { BoxProps } from '../box/Box';
 import { TextBodyCompact, TextDisplay, TextSmall } from '../typography';
 import theme from './theme.css';
 
-/** @type {React.ComponentType<any>} */
-class Toggle extends PureComponent {
-  inputNode = createRef();
+type Size = 'small' | 'medium' | 'large';
 
-  handleToggle = (event) => {
-    const { disabled, onChange } = this.props;
+export interface ToggleProps extends Omit<BoxProps, 'className' | 'size'> {
+  checked?: boolean;
+  children?: ReactNode;
+  disabled?: boolean;
+  /** The maximum number of lines the label can take */
+  maxLines?: number;
+  name?: string;
+  className?: string;
+  label?: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  size?: Size;
+}
 
-    if (event.pageX !== 0 && event.pageY !== 0) {
-      this.blur();
+const Toggle = ({
+  checked = false,
+  children,
+  disabled = false,
+  maxLines,
+  className,
+  label,
+  onChange,
+  size = 'medium',
+  ...others
+}: ToggleProps) => {
+  const ref = useRef<HTMLInputElement>(null);
+  const restProps = omit(others, ['onChange']);
+  const boxProps = pickBoxProps(restProps);
+  const inputProps = omitBoxProps(restProps);
+
+  const blur = () => {
+    if (ref.current) {
+      ref.current.blur();
     }
+  };
 
+  const handleToggle = (event: ChangeEvent<HTMLInputElement>) => {
+    blur();
     if (!disabled && onChange) {
       onChange(event);
     }
   };
+  const classNames = cx(
+    theme['toggle'],
+    theme[`is-${size}`],
+    {
+      [theme['is-checked']]: checked,
+      [theme['is-disabled']]: disabled,
+    },
+    className,
+  );
 
-  blur() {
-    if (this.inputNode.current) {
-      this.inputNode.current.blur();
-    }
-  }
+  const TextElement = size === 'small' ? TextSmall : size === 'medium' ? TextBodyCompact : TextDisplay;
 
-  focus() {
-    if (this.inputNode.current) {
-      this.inputNode.current.focus();
-    }
-  }
-
-  render() {
-    const { checked, disabled, className, maxLines, size, label, children, ...others } = this.props;
-
-    const restProps = omit(others, ['onChange']);
-    const boxProps = pickBoxProps(restProps);
-    const inputProps = omitBoxProps(restProps);
-
-    const classNames = cx(
-      theme['toggle'],
-      theme[`is-${size}`],
-      {
-        [theme['is-checked']]: checked,
-        [theme['is-disabled']]: disabled,
-      },
-      className,
-    );
-
-    const TextElement = size === 'small' ? TextSmall : size === 'medium' ? TextBodyCompact : TextDisplay;
-
-    return (
-      <Box element="label" data-teamleader-ui="toggle" className={classNames} {...boxProps}>
-        <input
-          className={theme['input']}
-          type="checkbox"
-          hidden
-          checked={checked}
-          disabled={disabled}
-          onChange={this.handleToggle}
-          ref={this.inputNode}
-          {...inputProps}
-        />
-        <span className={theme['track']}>
-          <span className={theme['thumb']} />
+  return (
+    <Box element="label" data-teamleader-ui="toggle" className={classNames} {...boxProps}>
+      <input
+        className={theme['input']}
+        type="checkbox"
+        hidden
+        checked={checked}
+        disabled={disabled}
+        onChange={handleToggle}
+        ref={ref}
+        {...inputProps}
+      />
+      <span className={theme['track']}>
+        <span className={theme['thumb']} />
+      </span>
+      {(label || children) && (
+        <span className={theme['label']}>
+          {label && (
+            <TextElement element="span" color={disabled ? 'neutral' : 'teal'} maxLines={maxLines}>
+              {label}
+            </TextElement>
+          )}
+          {children}
         </span>
-        {(label || children) && (
-          <span className={theme['label']}>
-            {label && (
-              <TextElement element="span" color={disabled ? 'neutral' : 'teal'} maxLines={maxLines}>
-                {label}
-              </TextElement>
-            )}
-            {children}
-          </span>
-        )}
-      </Box>
-    );
-  }
-}
-
-Toggle.propTypes = {
-  checked: PropTypes.bool,
-  children: PropTypes.node,
-  disabled: PropTypes.bool,
-  /** The maximum number of lines the label can take */
-  maxLines: PropTypes.number,
-  name: PropTypes.string,
-  className: PropTypes.string,
-  label: PropTypes.string,
-  onChange: PropTypes.func,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-};
-
-Toggle.defaultProps = {
-  checked: false,
-  disabled: false,
-  size: 'medium',
+      )}
+    </Box>
+  );
 };
 
 export default Toggle;
