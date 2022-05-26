d7280d308fb71364a3c660cf993ca1fad0bae0d8
Enis <eniskraasniqi@gmail.com>
2022-05-26 17:28:51 +0200

    convert icon.stories.spec.tsx to typescript

--- src/components/icon/icon.stories.spec.tsx
+++ src/components/icon/icon.stories.spec.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { IconGiftMediumOutline } from '@teamleader/ui-icons';
 
 import { Icon } from '../..';
+import { COLOURS, TINTS } from './Icon';
 
 export default {
   component: Icon,
   title: 'Icon',
 };
 
-const Wrapper = ({ children }) => <div style={{ minHeight: '50px', display: 'flex' }}>{children}</div>;
+const Wrapper = ({ children }: { children: React.ReactNode }) => (
+  <div style={{ minHeight: '50px', display: 'flex' }}>{children}</div>
+);
 
 export const Main = () => (
   <div>
-    {['neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua', 'teal'].map((color) => (
+    {(['neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua', 'teal'] as COLOURS[]).map((color) => (
       <Wrapper key={color}>
-        {['lightest', 'light', 'normal', 'dark', 'darkest'].map((tint) => (
+        {(['lightest', 'light', 'normal', 'dark', 'darkest'] as TINTS[]).map((tint) => (
           <Icon key={tint} color={color} tint={tint}>
             <IconGiftMediumOutline />
           </Icon>
