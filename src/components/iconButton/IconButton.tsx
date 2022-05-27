71bde5f4536f53e353e1edcb8236c4d562b9e6dc
Enis <eniskraasniqi@gmail.com>
2022-05-27 11:00:58 +0200

    convert iconButton.tsx to typescript

--- src/components/iconButton/IconButton.tsx
+++ src/components/iconButton/IconButton.tsx
@@ -1,54 +1,86 @@
-import React, { useRef, useImperativeHandle, forwardRef } from 'react';
-import PropTypes from 'prop-types';
+import React, { useRef, useImperativeHandle, forwardRef, ReactNode } from 'react';
 import Box from '../box';
 import Icon from '../icon';
 import cx from 'classnames';
 import buttonTheme from '../button/theme.css';
 import theme from './theme.css';
 import LoadingSpinner from '../loadingSpinner';
+import { BoxProps } from '../box/Box';
 
-/** @type {React.ComponentType<any>} */
+type SIZES = 'small' | 'medium' | 'large';
+type TINTS = 'lightest' | 'light' | 'normal' | 'dark' | 'darkest';
+type COLOURS = 'neutral' | 'white' | 'mint' | 'violet' | 'ruby' | 'gold' | 'aqua' | 'teal';
+
+interface IconButtonProps extends Omit<BoxProps, 'ref'> {
+  /** The content to display inside the button. */
+  children?: ReactNode;
+  /** A class name for the button to give custom styles. */
+  className?: string;
+  /** If true, component will be disabled. */
+  disabled?: boolean;
+  /** A custom element to be rendered */
+  element?: HTMLElement | string;
+  /** The icon displayed inside the button. */
+  icon?: HTMLElement;
+  /** Size of the button. */
+  size?: SIZES;
+  /** The color which the icon should have */
+  color?: COLOURS;
+  /** The tint which the icon should have */
+  tint?: TINTS;
+  /** If true, component will be shown in a selected state */
+  selected?: boolean;
+  /** Type of the button element. */
+  type?: string;
+  /** Callback function that is fired when the mouse button is released. */
+  onMouseUp?: (e: React.MouseEvent) => void;
+  /** Callback function that is fired when mouse leaves the component. */
+  onMouseLeave?: (e: React.MouseEvent) => void;
+  /** If true, component will show a loading spinner instead of icon or children. */
+  processing?: boolean;
+}
+
 const IconButton = forwardRef(
   (
     {
       children,
-      className,
+      className = '',
       disabled,
-      element,
+      element = 'button',
       icon,
-      size,
-      color,
-      tint,
+      size = 'medium',
+      color = 'neutral',
+      tint = 'darkest',
       selected,
-      type,
+      type = 'button',
       onMouseUp,
       onMouseLeave,
-      processing,
+      processing = false,
       ...others
-    },
+    }: IconButtonProps,
     ref,
   ) => {
-    const buttonRef = useRef();
+    const buttonRef = useRef<HTMLElement>(null);
     useImperativeHandle(ref, () => buttonRef.current);
 
-    const handleMouseUp = (event) => {
+    const blur = () => {
+      const currentButtonRef = buttonRef.current;
+      if (currentButtonRef?.blur) {
+        currentButtonRef.blur();
+      }
+    };
+
+    const handleMouseUp = (event: React.MouseEvent) => {
       blur();
       if (onMouseUp) {
         onMouseUp(event);
       }
     };
 
-    const handleMouseLeave = (event) => {
+    const handleMouseLeave = (event: React.MouseEvent) => {
       blur();
       if (onMouseLeave) {
         onMouseLeave(event);
-      }
-    };
-
-    const blur = () => {
-      const currentButtonRef = buttonRef.current;
-      if (currentButtonRef.blur) {
-        currentButtonRef.blur();
       }
     };
 
@@ -99,45 +131,6 @@ const IconButton = forwardRef(
     );
   },
 );
-
-IconButton.propTypes = {
-  /** The content to display inside the button. */
-  children: PropTypes.node,
-  /** A class name for the button to give custom styles. */
-  className: PropTypes.string,
-  /** If true, component will be disabled. */
-  disabled: PropTypes.bool,
-  /** A custom element to be rendered */
-  element: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
-  /** The icon displayed inside the button. */
-  icon: PropTypes.element,
-  /** Callback function that is fired when mouse leaves the component. */
-  onMouseLeave: PropTypes.func,
-  /** Callback function that is fired when the mouse button is released. */
-  onMouseUp: PropTypes.func,
-  /** If true, component will be shown in a selected state */
-  selected: PropTypes.bool,
-  /** If true, component will show a loading spinner instead of icon or children. */
-  processing: PropTypes.bool,
-  /** Size of the button. */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /** The color which the icon should have */
-  color: PropTypes.oneOf(['neutral', 'white', 'mint', 'violet', 'ruby', 'gold', 'aqua', 'teal']),
-  /** The tint which the icon should have */
-  tint: PropTypes.oneOf(['lightest', 'light', 'normal', 'dark', 'darkest']),
-  /** Type of the button element. */
-  type: PropTypes.string,
-};
-
-IconButton.defaultProps = {
-  className: '',
-  element: 'button',
-  size: 'medium',
-  color: 'neutral',
-  processing: false,
-  tint: 'darkest',
-  type: 'button',
-};
 
 IconButton.displayName = 'IconButton';
 
