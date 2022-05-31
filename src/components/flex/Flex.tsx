bc61c15a3d201d884808546e45e8dbcba9f0888e
Faraz <78449551+farazatarodi@users.noreply.github.com>
2022-05-31 22:52:57 +0200

    :label: expand Flex prop types

--- src/components/flex/Flex.tsx
+++ src/components/flex/Flex.tsx
@@ -1,17 +1,20 @@
-import React, { CSSProperties, ReactNode } from 'react';
+import React, { CSSProperties, HTMLProps, ReactNode } from 'react';
 import cx from 'classnames';
 import theme from './theme.css';
 
 const gaps = [0, 1, 2, 3, 4, 5, 6, 7, 8] as const;
 export type Gap = typeof gaps[number];
-interface FlexProps {
-  children: ReactNode;
-  direction?: 'row' | 'column';
-  gap?: Gap;
-  alignItems?: 'center' | 'flex-start' | 'flex-end' | 'baseline' | 'stretch';
-  justifyContent?: 'center' | 'flex-start' | 'flex-end' | 'space-around' | 'space-between' | 'space-evenly';
-  style?: CSSProperties;
-}
+
+type FlexProps = Partial<
+  {
+    children: ReactNode;
+    direction: 'row' | 'column';
+    gap: Gap;
+    alignItems: 'center' | 'flex-start' | 'flex-end' | 'baseline' | 'stretch';
+    justifyContent: 'center' | 'flex-start' | 'flex-end' | 'space-around' | 'space-between' | 'space-evenly';
+    style: CSSProperties;
+  } & HTMLProps<HTMLDivElement>
+>;
 
 const Flex = ({
   children,
