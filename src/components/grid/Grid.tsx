7aa16cfaa0a0b4b16c275d73948a42215e69dc88
Faraz <78449551+farazatarodi@users.noreply.github.com>
2022-06-01 13:51:51 +0200

    :sparkles: forward ref in Grid component

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,13 +1,20 @@
-import React, { ReactNode } from 'react';
+import React, { ForwardedRef, forwardRef, ReactNode } from 'react';
 import cx from 'classnames';
 
 import theme from './theme.css';
 
 type Gap = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
-export type GridProps = Partial<{ children: ReactNode; areas: string[]; rows: string[]; columns: string[]; gap: Gap }>;
+export type GridProps = Partial<{
+  children: ReactNode;
+  areas: string[];
+  rows: string[];
+  columns: string[];
+  gap: Gap;
+  ref: ForwardedRef<HTMLDivElement>;
+}>;
 
-const Grid = ({ children, areas, rows, columns, gap = 0 }: GridProps) => {
+const Grid = forwardRef(({ children, areas, rows, columns, gap = 0, ref }: GridProps) => {
   const classNames = cx(theme['grid'], {
     [theme[`gap-${gap}`]]: gap > 0,
   });
@@ -19,10 +26,12 @@ const Grid = ({ children, areas, rows, columns, gap = 0 }: GridProps) => {
   };
 
   return (
-    <div className={classNames} style={gridStyles}>
+    <div className={classNames} style={gridStyles} ref={ref}>
       {children}
     </div>
   );
-};
+});
+
+Grid.displayName = 'Grid';
 
 export default Grid;
