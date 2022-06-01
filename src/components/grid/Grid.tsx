d7052955a804f04f072199d323d5020c5c394113
Faraz <78449551+farazatarodi@users.noreply.github.com>
2022-06-01 12:54:46 +0200

    :label: add prop types to Grid component

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import cx from 'classnames';
 
 import theme from './theme.css';
 
-const Grid = () => {
+export type GridProps = Partial<{ children: ReactNode }>;
+
+const Grid = ({ children }: GridProps) => {
   const classNames = cx(theme['grid']);
 
-  return <div className={classNames}>Grid</div>;
+  return <div className={classNames}>{children}</div>;
 };
 
 export default Grid;
