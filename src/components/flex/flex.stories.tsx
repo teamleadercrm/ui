779f8935de5909cb59fba744a8e45ac7aef5c208
Faraz <78449551+farazatarodi@users.noreply.github.com>
2022-06-01 11:53:24 +0200

    :memo: add nesting story

--- src/components/flex/flex.stories.tsx
+++ src/components/flex/flex.stories.tsx
@@ -31,6 +31,7 @@ const MintBox = ({ size }: { size: number }) => (
     }}
   />
 );
+
 export const basic: ComponentStory<typeof Flex> = (args) => (
   <Box>
     <Heading3>Flex Box</Heading3>
@@ -48,3 +49,74 @@ basic.args = {
   direction: 'row',
   gap: 0,
 };
+
+export const nesting: ComponentStory<typeof Flex> = () => (
+  <Box borderColor="neutral" borderWidth={1} borderRadius="rounded" backgroundColor="neutral" backgroundTint="light">
+    <Flex gap={5} direction="column">
+      <Box
+        backgroundColor="mint"
+        backgroundTint="lightest"
+        borderColor="mint"
+        borderTint="light"
+        borderWidth={1}
+        borderRadius="rounded"
+        style={{
+          minHeight: '50px',
+          width: '100%',
+        }}
+      />
+      <Flex gap={5} alignItems="flex-start">
+        <Box
+          backgroundColor="teal"
+          backgroundTint="lightest"
+          borderColor="teal"
+          borderTint="light"
+          borderWidth={1}
+          borderRadius="rounded"
+          style={{
+            minHeight: '100px',
+            width: '20%',
+          }}
+        />
+        <Box
+          backgroundColor="teal"
+          backgroundTint="lightest"
+          borderColor="teal"
+          borderTint="light"
+          borderWidth={1}
+          borderRadius="rounded"
+          style={{
+            minHeight: '150px',
+            width: '80%',
+          }}
+        />
+      </Flex>
+      <Flex gap={5} justifyContent="center" alignItems="center">
+        <Box
+          backgroundColor="ruby"
+          backgroundTint="lightest"
+          borderColor="ruby"
+          borderTint="light"
+          borderWidth={1}
+          borderRadius="rounded"
+          style={{
+            minHeight: '100px',
+            width: '50%',
+          }}
+        />
+        <Box
+          backgroundColor="ruby"
+          backgroundTint="lightest"
+          borderColor="ruby"
+          borderTint="light"
+          borderWidth={1}
+          borderRadius="rounded"
+          style={{
+            minHeight: '150px',
+            width: '30%',
+          }}
+        />
+      </Flex>
+    </Flex>
+  </Box>
+);
