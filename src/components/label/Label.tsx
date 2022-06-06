5cf9f47d2968c38658104007f4123781dece9bea
Enis Krasniqi <eniskraasniqi@gmail.com>
2022-06-06 15:48:52 +0200

    convert label component to typescript

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -1,72 +1,60 @@
 import { IconInfoBadgedSmallFilled } from '@teamleader/ui-icons';
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
+import React, { ReactNode } from 'react';
 import Box from '../box';
 import Tooltip from '../tooltip';
 import Icon from '../icon';
 import { TextBodyCompact, TextDisplay, TextSmall } from '../typography';
+import { BoxProps } from '../box/Box';
 
 const TooltippedIcon = Tooltip(Icon);
 
-/** @type {React.ComponentType<any>} */
-export default class Label extends PureComponent {
-  render() {
-    const { children, inverse, required, size, tooltip, ...others } = this.props;
-
-    const childProps = {
-      inverse,
-      marginTop: 1,
-      size,
-    };
-
-    const Element = size === 'large' ? TextDisplay : TextBodyCompact;
-
-    return (
-      <Box display="block" element="label" marginBottom={3} {...others}>
-        {React.Children.map(children, (child) => {
-          if (typeof child !== 'string') {
-            return React.cloneElement(child, { ...childProps, ...child.props });
-          }
-
-          return (
-            <Box display="flex" alignItems="center">
-              <Element color={inverse ? 'neutral' : 'teal'} tint={inverse ? 'lightest' : 'darkest'} element="span">
-                {child}
-              </Element>
-              {required && (
-                <TextSmall color="ruby" element="span" marginLeft={1} tint="dark">
-                  *
-                </TextSmall>
-              )}
-              {tooltip && (
-                <TooltippedIcon
-                  tooltip={<TextSmall>{tooltip}</TextSmall>}
-                  tooltipSize="small"
-                  color={inverse ? 'neutral' : 'teal'}
-                  tint={inverse ? 'lightest' : 'darkest'}
-                  marginLeft={1}
-                >
-                  <IconInfoBadgedSmallFilled />
-                </TooltippedIcon>
-              )}
-            </Box>
-          );
-        })}
-      </Box>
-    );
-  }
+interface LabelProps extends Omit<BoxProps, 'children'> {
+  children: ReactNode;
+  inverse: boolean;
+  required: boolean;
+  size: 'small' | 'medium' | 'large';
+  tooltip?: string;
 }
 
-Label.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.element, PropTypes.string, PropTypes.array]),
-  inverse: PropTypes.bool,
-  required: PropTypes.bool,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  tooltip: PropTypes.string,
+const Label = ({ children, inverse = false, required = false, size = 'medium', tooltip, ...others }: LabelProps) => {
+  const childProps = {
+    inverse,
+    marginTop: 1,
+    size,
+  };
+  const Element = size === 'large' ? TextDisplay : TextBodyCompact;
+
+  return (
+    <Box display="block" element="label" marginBottom={3} {...others}>
+      {React.Children.map(children, (child) =>
+        typeof child !== 'string' && React.isValidElement(child) ? (
+          React.cloneElement(child, { ...childProps, ...child.props })
+        ) : (
+          <Box display="flex" alignItems="center">
+            <Element color={inverse ? 'neutral' : 'teal'} tint={inverse ? 'lightest' : 'darkest'} element="span">
+              {child}
+            </Element>
+            {required && (
+              <TextSmall color="ruby" element="span" marginLeft={1} tint="dark">
+                *
+              </TextSmall>
+            )}
+            {tooltip && (
+              <TooltippedIcon
+                tooltip={<TextSmall>{tooltip}</TextSmall>}
+                tooltipSize="small"
+                color={inverse ? 'neutral' : 'teal'}
+                tint={inverse ? 'lightest' : 'darkest'}
+                marginLeft={1}
+              >
+                <IconInfoBadgedSmallFilled />
+              </TooltippedIcon>
+            )}
+          </Box>
+        ),
+      )}
+    </Box>
+  );
 };
 
-Label.defaultProps = {
-  inverse: false,
-  required: false,
-  size: 'medium',
-};
+export default Label;
