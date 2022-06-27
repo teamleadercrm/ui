11c610c148d78422b2ca76bb0f16f71ea8403255
Kristof Colpaert <kristof.colpaert@teamleader.eu>
2022-06-27 15:55:02 +0200

    :art: Rewrite SingleLineInputBase.tsx in Typescript

--- src/components/input/SingleLineInputBase.tsx
+++ src/components/input/SingleLineInputBase.tsx
@@ -1,66 +1,73 @@
-import React, { PureComponent, forwardRef } from 'react';
-import PropTypes from 'prop-types';
+import React, { forwardRef, ReactElement, ReactNode, useState } from 'react';
 import cx from 'classnames';
 
-import InputBase from './InputBase';
-import Box, { omitBoxProps, pickBoxProps } from '../box';
-import ValidationText from '../validationText';
-
 import theme from './theme.css';
+import Box, { omitBoxProps, pickBoxProps } from '../box';
+import InputBase from './InputBase';
+import ValidationText, { HelpText } from '../validationText';
 
-class SingleLineInputBase extends PureComponent {
-  state = {
-    inputHasfocus: false,
-  };
+interface SingleLineInputBaseProps {
+  /** Sets a class name for the wrapper to give custom styles. */
+  className: string;
+  /** Element stuck to the left hand side of the component. */
+  connectedLeft: ReactElement;
+  /** Element stuck to the right hand side of the component. */
+  connectedRight: ReactElement;
+  /** Boolean indicating whether the input should render as disabled. */
+  disabled?: boolean;
+  /** The text string/element to use as error message below the input. */
+  error: boolean | ReactNode;
+  /** The text string to use as help text below the input. */
+  helpText: string;
+  /** Boolean indicating whether the input should render as inverse. */
+  inverse?: boolean;
+  /** The text string/element to use as a prefix inside the input field */
+  prefix: ReactElement[] | ReactElement;
+  /** Boolean indicating whether the input should render as read only. */
+  readOnly?: boolean;
+  /** The text string/element to use as success message below the input. */
+  success: boolean | ReactNode;
+  /** The text string/element to use as a suffix inside the input field */
+  suffix: ReactElement[] | ReactElement;
+  /** The text to use as warning message below the input. */
+  warning: boolean | ReactNode;
+  /** A custom width for the input field */
+  width: string;
+  /** Whether to disable styling that hints being able to type in the input field */
+  noInputStyling: boolean;
+}
 
-  handleBlur = (event) => {
-    this.setState({ inputHasfocus: false });
-    if (this.props.onBlur) {
-      this.props.onBlur(event);
-    }
-  };
-
-  handleFocus = (event) => {
-    this.setState({ inputHasfocus: true });
-    if (this.props.onFocus) {
-      this.props.onFocus(event);
-    }
-  };
-
-  renderOneOrMultipleElements(prop) {
-    if (Array.isArray(prop)) {
-      return prop.map((element, index) => React.cloneElement(element, { key: index }));
-    }
-
-    return prop;
-  }
-
-  render() {
-    const {
+const SingleLineInputBase = forwardRef(
+  (
+    {
       className,
+      disabled,
       connectedLeft,
       connectedRight,
-      disabled,
       error,
-      forwardedRef,
       helpText,
-      onFocus,
-      onBlur,
-      prefix,
       inverse,
+      prefix,
       readOnly,
       success,
       suffix,
-      width,
       warning,
+      width,
       noInputStyling,
       ...others
-    } = this.props;
+    }: SingleLineInputBaseProps,
+    ref,
+  ) => {
+    const [inputHasFocus, setInputHasFocus] = useState(false);
+
+    const handleBlur = () => {};
+
+    const handleFocus = () => {};
 
     const classNames = cx(
       theme['wrapper'],
       {
-        [theme['has-focus']]: this.state.inputHasfocus,
+        [theme['has-focus']]: inputHasFocus,
         [theme['has-error']]: error,
         [theme['has-success']]: success,
         [theme['has-warning']]: warning,
@@ -75,11 +82,11 @@ class SingleLineInputBase extends PureComponent {
 
     const boxProps = pickBoxProps(others);
     const inputProps = {
-      ref: forwardedRef,
+      ref,
       disabled,
       inverse,
-      onBlur: this.handleBlur,
-      onFocus: this.handleFocus,
+      onBlur: handleBlur,
+      onFocus: handleFocus,
       readOnly,
       ...omitBoxProps(others),
     };
@@ -89,44 +96,18 @@ class SingleLineInputBase extends PureComponent {
         <div className={cx(theme['input-wrapper'], noInputStyling && theme['is-input-disabled'])}>
           {connectedLeft}
           <div className={theme['input-inner-wrapper']} style={{ width, flex: width && '0 1 auto' }}>
-            {prefix && <div className={theme['prefix-wrapper']}>{this.renderOneOrMultipleElements(prefix)}</div>}
+            {prefix && <div className={theme['prefix-wrapper']}>{prefix}</div>}
             <InputBase {...inputProps} />
-            {suffix && <div className={theme['suffix-wrapper']}>{this.renderOneOrMultipleElements(suffix)}</div>}
+            {suffix && <div className={theme['suffix-wrapper']}>{suffix}</div>}
           </div>
           {connectedRight}
         </div>
         <ValidationText error={error} help={helpText} inverse={inverse} success={success} warning={warning} />
       </Box>
     );
-  }
-}
+  },
+);
 
-SingleLineInputBase.propTypes = {
-  /** Element stuck to the left hand side of the component. */
-  connectedLeft: PropTypes.element,
-  /** Element stuck to the right hand side of the component. */
-  connectedRight: PropTypes.element,
-  /** The text string/element to use as error message below the input. */
-  error: PropTypes.oneOfType([PropTypes.bool, PropTypes.node]),
-  /** The text string to use as help text below the input. */
-  helpText: PropTypes.string,
-  /** The text string/element to use as a prefix inside the input field */
-  prefix: PropTypes.oneOfType([PropTypes.array, PropTypes.element]),
-  /** The text string/element to use as success message below the input. */
-  success: PropTypes.oneOfType([PropTypes.bool, PropTypes.node]),
-  /** The text string/element to use as a suffix inside the input field */
-  suffix: PropTypes.oneOfType([PropTypes.array, PropTypes.element]),
-  /** The text to use as warning message below the input. */
-  warning: PropTypes.oneOfType([PropTypes.bool, PropTypes.node]),
-  /** A custom width for the input field */
-  width: PropTypes.string,
-  /** Whether to disable styling that hints being able to type in the input field */
-  noInputStyling: PropTypes.bool,
-};
+SingleLineInputBase.displayName = 'SingleLineInputBase';
 
-/** @type {any} */
-const ForwardedSingleInputBase = forwardRef((props, ref) => <SingleLineInputBase {...props} forwardedRef={ref} />);
-
-ForwardedSingleInputBase.displayName = 'SingleLineInputBase';
-
-export default ForwardedSingleInputBase;
+export default SingleLineInputBase;
