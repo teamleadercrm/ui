ab11fda4c23582bb29f150466f245fc884e63b66
Enis Krasniqi <eniskraasniqi@gmail.com>
2022-06-29 16:35:15 +0200

    convert OverviewPageHeader to typescript

---
+++ src/components/overviewPage/OverviewPageHeader.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+import Box from '../box';
+import Container from '../container';
+import { Heading1 } from '../typography';
+
+interface OverviewPageHeaderProps {
+  children: ReactNode;
+  title: string | ReactNode;
+}
+
+const OverviewPageHeader = ({ children, title, ...others }: OverviewPageHeaderProps) => {
+  return (
+    <Container {...others} display="flex" marginBottom={5} paddingTop={7} justifyContent="space-between">
+      <Heading1 color="teal">{title}</Heading1>
+      {children && <Box>{children}</Box>}
+    </Container>
+  );
+};
+
+export default OverviewPageHeader;
