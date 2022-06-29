0796284d12d9b140e3b0eb957f7450ccff5e8395
Kristof Colpaert <kristof.colpaert@teamleader.eu>
2022-06-29 16:39:21 +0200

    :art: Rewrite Input.tsx in Typescript

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,78 +1,22 @@
-import React, { forwardRef, PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { ChangeEvent, forwardRef, useState } from 'react';
+import SingleLineInputBase, { SingleLineInputBaseProps } from './SingleLineInputBase';
 
-import SingleLineInputBase from './SingleLineInputBase';
-
-class Input extends PureComponent {
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.value !== undefined) {
-      const newValue = nextProps.value || '';
-
-      if (newValue !== prevState.value) {
-        return {
-          value: newValue,
-        };
-      }
-    }
-
-    return null;
-  }
-
-  state = {
-    value: '',
-  };
-
-  render() {
-    const { onChange, forwardedRef, ...others } = this.props;
-
-    return (
-      <SingleLineInputBase
-        ref={forwardedRef}
-        value={this.state.value}
-        onChange={(event) => {
-          this.setState({ value: event.currentTarget.value });
-          onChange && onChange(event, event.currentTarget.value);
-        }}
-        {...others}
-      />
-    );
-  }
+interface InputProps extends Pick<SingleLineInputBaseProps, 'value'> {
+  /** Callback function that is fired when the component's value changes. */
+  onChange?: (event: ChangeEvent<HTMLElement>, value: string) => void;
 }
 
-Input.propTypes = {
-  /** The passed down type of the input element. It can be a valid HTML5 input type, except 'number' use a NumericInput in this case. */
-  type: PropTypes.oneOf([
-    'button',
-    'checkbox',
-    'color',
-    'date',
-    'datetime-local',
-    'email',
-    'file',
-    'hidden',
-    'image',
-    'month',
-    'password',
-    'radio',
-    'range',
-    'reset',
-    'search',
-    'submit',
-    'tel',
-    'text',
-    'time',
-    'url',
-    'week',
-  ]),
-};
+const Input = forwardRef(({ value, onChange, ...others }: InputProps, forwardedRef) => {
+  const [stateValue, setStateValue] = useState(value);
 
-Input.defaultProps = {
-  type: 'text',
-};
+  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setStateValue(event.currentTarget.value);
+    onChange && onChange(event, event.currentTarget.value);
+  };
 
-/** @type {React.ComponentType<any>} */
-const ForwardedInput = forwardRef((props, ref) => <Input {...props} forwardedRef={ref} />);
+  return <SingleLineInputBase ref={forwardedRef} value={stateValue} onChange={handleChange} {...others} />;
+});
 
-ForwardedInput.displayName = 'Input';
+Input.displayName = 'Input';
 
-export default ForwardedInput;
+export default Input;
