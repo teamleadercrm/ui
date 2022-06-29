eeae2ee477df9da47353d07525c5dd1af9014343
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2022-06-29 15:23:38 +0200

    convert stories to TS

--- src/components/marketingTypography/marketingTypography.stories.tsx
+++ src/components/marketingTypography/marketingTypography.stories.tsx
@@ -1,3 +1,4 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import { addStoryInGroup, MARKETING } from '../../../.storybook/utils';
 import MarketingHeading1 from './MarketingHeading1';
@@ -13,15 +14,15 @@ export default {
       url: 'https://www.figma.com/file/6nbw3mXc6VpIOYrbmUxn8C/Marketing-components?node-id=84%3A0',
     },
   },
-};
+} as ComponentMeta<typeof MarketingHeading1>;
 
-export const heading1 = (args) => <MarketingHeading1 {...args} />;
+export const heading1: ComponentStory<typeof MarketingHeading1> = (args) => <MarketingHeading1 {...args} />;
 
 heading1.args = {
   children: "I'm a heading 1 for marketing purposes only",
 };
 
-export const heading2 = (args) => <MarketingHeading2 {...args} />;
+export const heading2: ComponentStory<typeof MarketingHeading2> = (args) => <MarketingHeading2 {...args} />;
 
 heading2.args = {
   children: "I'm a heading 2 for marketing purposes only",
