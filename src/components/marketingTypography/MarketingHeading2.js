716d9beb0d7406c1d4c474192bbbf1885d0cbd87
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2022-06-29 15:15:35 +0200

    convert `MarketingHeading2` to functional component

--- src/components/marketingTypography/MarketingHeading2.js
+++ src/components/marketingTypography/MarketingHeading2.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '../box';
 import cx from 'classnames';
 import theme from './theme.css';
 
 /** @type {React.ComponentType<any>} */
-class MarketingHeading2 extends Component {
-  render() {
-    const { children, className, ...others } = this.props;
+const MarketingHeading2 = ({ children, className, ...others }) => {
+  const classNames = cx(theme['heading-2'], className);
 
-    const classNames = cx(theme['heading-2'], className);
-
-    return (
-      <Box {...others} className={classNames} element="h2">
-        {children}
-      </Box>
-    );
-  }
-}
+  return (
+    <Box {...others} className={classNames} element="h2">
+      {children}
+    </Box>
+  );
+};
 
 MarketingHeading2.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
