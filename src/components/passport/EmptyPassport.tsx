72aad0c9dff4b2c2cb3da2f905d152635cd8e159
Enis Krasniqi <eniskraasniqi@gmail.com>
2022-06-30 16:27:47 +0200

    convert EmptyPassport to typescript

--- src/components/passport/EmptyPassport.tsx
+++ src/components/passport/EmptyPassport.tsx
@@ -1,5 +1,4 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import cx from 'classnames';
 import theme from './theme.css';
 import Avatar from '../avatar';
@@ -7,46 +6,52 @@ import Box from '../box';
 import Link from '../link';
 import Popover from '../popover';
 import { Heading3, TextBody } from '../typography';
+import { GenericComponent } from '../../@types/types';
+import { AvatarProps } from '../avatar/Avatar';
+import { BoxProps } from '../box/Box';
+import { LinkProps } from '../link/Link';
 
-/** @type {React.ComponentType<any>} */
-class EmptyPassport extends PureComponent {
-  render() {
-    const { avatar, link, className, description, title, ...others } = this.props;
-
-    return (
-      <Popover {...others} backdrop="transparent" className={cx(theme['passport-empty'], className)}>
-        <Box paddingHorizontal={4} paddingVertical={5}>
-          <Box display="flex" flexDirection="column" alignItems="center">
-            {avatar && <Avatar {...avatar} size="small" marginBottom={4} />}
-            <Heading3 color="teal">{title}</Heading3>
-            {description && (
-              <TextBody color="neutral" marginTop={2}>
-                {description}
-              </TextBody>
-            )}
-            {link && (
-              <TextBody marginTop={4}>
-                <Link {...link} inherit={false} />
-              </TextBody>
-            )}
-          </Box>
-        </Box>
-      </Popover>
-    );
-  }
+interface EmptyPassportProps extends Omit<BoxProps, 'className'> {
+  /** Object containing the props of an avatar. */
+  avatar?: AvatarProps;
+  /** The class names for the wrapper to apply custom styling. */
+  className?: string;
+  /** The description of the empty Passport. */
+  description?: string;
+  /** Object containing the props of a Link. */
+  link?: LinkProps;
+  /** The title of the empty Passport. */
+  title: string;
 }
 
-EmptyPassport.propTypes = {
-  /** Object containing the props of an avatar. */
-  avatar: PropTypes.object,
-  /** The class names for the wrapper to apply custom styling. */
-  className: PropTypes.string,
-  /** The description of the empty Passport. */
-  description: PropTypes.string,
-  /** Object containing the props of a Link. */
-  link: PropTypes.object,
-  /** The title of the empty Passport. */
-  title: PropTypes.string.isRequired,
+const EmptyPassport: GenericComponent<EmptyPassportProps> = ({
+  avatar,
+  link,
+  className,
+  description,
+  title,
+  ...others
+}) => {
+  return (
+    <Popover {...others} backdrop="transparent" className={cx(theme['passport-empty'], className)}>
+      <Box paddingHorizontal={4} paddingVertical={5}>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          {avatar && <Avatar {...avatar} size="small" marginBottom={4} />}
+          <Heading3 color="teal">{title}</Heading3>
+          {description && (
+            <TextBody color="neutral" marginTop={2}>
+              {description}
+            </TextBody>
+          )}
+          {link && (
+            <TextBody marginTop={4}>
+              <Link {...link} inherit={false} />
+            </TextBody>
+          )}
+        </Box>
+      </Box>
+    </Popover>
+  );
 };
 
 export default EmptyPassport;
