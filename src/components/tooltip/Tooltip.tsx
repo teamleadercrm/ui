1a22704831a5aca5154e87c0b915a1816f57d7c2
qubis741 <jakub.kovacikk@gmail.com>
2022-06-30 16:43:52 +0200

    converted Tooltip to ts

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Box from '../box';
-import Transition from 'react-transition-group/Transition';
+import uiUtilities from '@teamleader/ui-utilities';
 import cx from 'classnames';
 import omit from 'lodash.omit';
+import React, { MouseEventHandler, ReactNode, useEffect, useMemo, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
-import { getViewport } from '../utils/utils';
+import Transition from 'react-transition-group/Transition';
+import { GenericComponent } from '../../@types/types';
+import Box from '../box';
 import DocumentObjectProvider, { Context as DocumentObjectContext } from '../hoc/DocumentObjectProvider';
+import { getViewport } from '../utils/utils';
 import theme from './theme.css';
-import uiUtilities from '@teamleader/ui-utilities';
 
-const POSITIONS = {
+type Color = 'white' | 'neutral' | 'mint' | 'violet' | 'ruby' | 'gold' | 'aqua' | 'inverse';
+type Position = 'bottom' | 'horizontal' | 'left' | 'right' | 'top' | 'vertical';
+
+const POSITIONS: Record<string, Position> = {
   BOTTOM: 'bottom',
   HORIZONTAL: 'horizontal',
   LEFT: 'left',
@@ -18,6 +21,12 @@ const POSITIONS = {
   TOP: 'top',
   VERTICAL: 'vertical',
 };
+
+interface PositionState {
+  position: Position;
+  top: number | string;
+  left: number | string;
+}
 
 const SIZES = {
   large: {
@@ -30,272 +39,254 @@ const SIZES = {
     paddingHorizontal: 3,
     paddingVertical: 2,
   },
-};
+} as const;
 
-/** @type {(component: React.ComponentType<any>) => React.ComponentType<any>} */
-const Tooltip = (ComposedComponent) => {
-  class TooltippedComponent extends Component {
-    tooltipRoot = this.props.documentObject.createElement('div');
-    ref = React.createRef();
+export interface TooltippedComponentProps {
+  children: ReactNode;
+  className?: string;
+  onClick?: MouseEventHandler;
+  onMouseEnter?: MouseEventHandler;
+  onMouseLeave?: MouseEventHandler;
+  onTooltipEntered?: () => void;
+  tooltip: ReactNode;
+  tooltipColor?: Color;
+  tooltipHideOnClick?: boolean;
+  tooltipIcon?: ReactNode;
+  tooltipPosition?: Position;
+  tooltipShowOnClick?: boolean;
+  tooltipSize?: keyof typeof SIZES;
+  documentObject: Document;
+  tooltipShowDelay?: number;
+  /** The z-index of the Tooltip */
+  zIndex?: number;
+  tooltipActive?: boolean;
+  ComposedComponent: React.ElementType;
+}
 
-    state = {
-      active: false,
-      position: this.props.tooltipPosition,
-    };
+interface TooltipProps extends Omit<TooltippedComponentProps, 'ComposedComponent'> {}
 
-    componentDidMount() {
-      const { tooltipActive } = this.props;
+const TooltippedComponent: GenericComponent<TooltippedComponentProps> = ({
+  children,
+  className,
+  documentObject,
+  tooltip,
+  tooltipColor = 'white',
+  onTooltipEntered,
+  tooltipHideOnClick = true,
+  tooltipIcon = null,
+  tooltipPosition = 'top',
+  tooltipShowOnClick = false,
+  tooltipSize = 'medium',
+  tooltipShowDelay = 100,
+  zIndex = 700,
+  onClick,
+  onMouseEnter,
+  onMouseLeave,
+  tooltipActive = null,
+  ComposedComponent,
+  ...other
+}) => {
+  const tooltipRoot = useMemo(() => documentObject.createElement('div'), []);
+  const ref = useRef(null);
+  const [active, setActive] = useState(false);
+  const [position, setPosition] = useState<PositionState>({ position: tooltipPosition, top: 'auto', left: 'auto' });
 
-      if (tooltipActive) {
-        this.activate(this.calculatePosition(this.ref.current));
-      }
+  const activate = (position: PositionState | undefined) => {
+    if (!position) {
+      return false;
+    }
+    documentObject.body.appendChild(tooltipRoot);
+    setActive(true);
+    setPosition({ position: position.position, top: position.top, left: position.left });
+  };
+
+  const getPosition = (element: Element) => {
+    if (tooltipPosition === POSITIONS.HORIZONTAL) {
+      const origin = element.getBoundingClientRect();
+      const { width: windowWidth } = getViewport();
+      const toRight = origin.left < windowWidth / 2 - origin.width / 2;
+
+      return toRight ? POSITIONS.RIGHT : POSITIONS.LEFT;
+    } else if (tooltipPosition === POSITIONS.VERTICAL) {
+      const origin = element.getBoundingClientRect();
+      const { height: windowHeight } = getViewport();
+      const toBottom = origin.top < windowHeight / 2 - origin.height / 2;
+
+      return toBottom ? POSITIONS.BOTTOM : POSITIONS.TOP;
     }
 
-    componentDidUpdate({ tooltipActive: prevTooltipActive }) {
-      const { tooltipActive } = this.props;
-      const { active } = this.state;
+    return tooltipPosition;
+  };
 
-      if (tooltipActive !== prevTooltipActive) {
-        if (tooltipActive && !active) {
-          this.activate(this.calculatePosition(this.ref.current));
-        }
-
-        if (!tooltipActive && active) {
-          this.deactivate();
-        }
-      }
+  const calculatePosition = (element: Element | null) => {
+    if (typeof element?.getBoundingClientRect !== 'function') {
+      return { top: 0, left: 0, position: tooltipPosition };
     }
 
-    getPosition(element) {
-      const { tooltipPosition } = this.props;
+    const { top, left, height, width } = element.getBoundingClientRect();
+    const position = getPosition(element);
+    const xOffset = window.scrollX || window.pageXOffset;
+    const yOffset = window.scrollY || window.pageYOffset;
 
-      if (tooltipPosition === POSITIONS.HORIZONTAL) {
-        const origin = element.getBoundingClientRect();
-        const { width: windowWidth } = getViewport();
-        const toRight = origin.left < windowWidth / 2 - origin.width / 2;
-
-        return toRight ? POSITIONS.RIGHT : POSITIONS.LEFT;
-      } else if (tooltipPosition === POSITIONS.VERTICAL) {
-        const origin = element.getBoundingClientRect();
-        const { height: windowHeight } = getViewport();
-        const toBottom = origin.top < windowHeight / 2 - origin.height / 2;
-
-        return toBottom ? POSITIONS.BOTTOM : POSITIONS.TOP;
-      }
-
-      return tooltipPosition;
-    }
-
-    activate({ top, left, position }) {
-      this.props.documentObject.body.appendChild(this.tooltipRoot);
-      this.setState({ active: true, top, left, position });
-    }
-
-    deactivate() {
-      this.setState({ active: false });
-    }
-
-    calculatePosition(element) {
-      const { tooltipPosition } = this.props;
-      if (typeof element.getBoundingClientRect !== 'function') {
-        return { top: 0, left: 0, position: tooltipPosition };
-      }
-
-      const { top, left, height, width } = element.getBoundingClientRect();
-      const position = this.getPosition(element);
-      const xOffset = window.scrollX || window.pageXOffset;
-      const yOffset = window.scrollY || window.pageYOffset;
-
-      if (position === POSITIONS.BOTTOM) {
-        return {
-          top: top + height + yOffset,
-          left: left + width / 2 + xOffset,
-          position,
-        };
-      } else if (position === POSITIONS.TOP) {
-        return {
-          top: top + yOffset,
-          left: left + width / 2 + xOffset,
-          position,
-        };
-      } else if (position === POSITIONS.LEFT) {
-        return {
-          top: top + height / 2 + yOffset,
-          left: left + xOffset,
-          position,
-        };
-      } else if (position === POSITIONS.RIGHT) {
-        return {
-          top: top + height / 2 + yOffset,
-          left: left + width + xOffset,
-          position,
-        };
-      }
-    }
-
-    handleMouseEnter = (event) => {
-      this.activate(this.calculatePosition(event.currentTarget));
-
-      if (this.props.onMouseEnter) {
-        this.props.onMouseEnter(event);
-      }
-    };
-
-    handleMouseLeave = (event) => {
-      this.deactivate();
-
-      if (this.props.onMouseLeave) {
-        this.props.onMouseLeave(event);
-      }
-    };
-
-    handleClick = (event) => {
-      if (this.props.tooltipHideOnClick && this.state.active) {
-        this.deactivate();
-      }
-
-      if (this.props.tooltipShowOnClick && !this.state.active) {
-        this.activate(this.calculatePosition(event.currentTarget));
-      }
-
-      if (this.props.onClick) {
-        this.props.onClick(event);
-      }
-    };
-
-    handleTransitionExited = () => {
-      this.props.documentObject.body.removeChild(this.tooltipRoot);
-    };
-
-    handleTransitionEntered = () => {
-      const { onTooltipEntered } = this.props;
-      onTooltipEntered && onTooltipEntered();
-    };
-
-    render() {
-      const { active, left, top, position } = this.state;
-      const {
-        children,
-        className,
-        tooltip,
-        tooltipColor,
-        tooltipIcon,
-        tooltipSize,
-        tooltipShowDelay,
-        zIndex,
-        onClick,
-        onMouseEnter,
-        onMouseLeave,
-        tooltipActive,
-        ...other
-      } = this.props;
-
-      const rest = omit(other, [
-        'onTooltipEntered',
-        'tooltipHideOnClick',
-        'tooltipPosition',
-        'tooltipShowOnClick',
-        'tooltipShowDelay',
-        'documentObject',
-      ]);
-
-      let childProps = {
-        ...rest,
-        className,
-        onClick,
-        onMouseEnter,
-        onMouseLeave,
-        ref: this.ref,
+    if (position === POSITIONS.BOTTOM) {
+      return {
+        top: top + height + yOffset,
+        left: left + width / 2 + xOffset,
+        position,
       };
+    } else if (position === POSITIONS.TOP) {
+      return {
+        top: top + yOffset,
+        left: left + width / 2 + xOffset,
+        position,
+      };
+    } else if (position === POSITIONS.LEFT) {
+      return {
+        top: top + height / 2 + yOffset,
+        left: left + xOffset,
+        position,
+      };
+    } else if (position === POSITIONS.RIGHT) {
+      return {
+        top: top + height / 2 + yOffset,
+        left: left + width + xOffset,
+        position,
+      };
+    }
+  };
 
-      if (tooltipActive === null) {
-        childProps = {
-          ...childProps,
-          onClick: this.handleClick,
-          onMouseEnter: this.handleMouseEnter,
-          onMouseLeave: this.handleMouseLeave,
-        };
+  const handleMouseEnter: MouseEventHandler = (event) => {
+    activate(calculatePosition(event.currentTarget));
+
+    if (onMouseEnter) {
+      onMouseEnter(event);
+    }
+  };
+
+  const handleMouseLeave: MouseEventHandler = (event) => {
+    setActive(false);
+
+    if (onMouseLeave) {
+      onMouseLeave(event);
+    }
+  };
+
+  const handleClick: MouseEventHandler = (event) => {
+    if (tooltipHideOnClick && active) {
+      setActive(false);
+    }
+
+    if (tooltipShowOnClick && !active) {
+      activate(calculatePosition(event.currentTarget));
+    }
+
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
+  const handleTransitionExited = () => {
+    documentObject.body.removeChild(tooltipRoot);
+  };
+
+  const handleTransitionEntered = () => {
+    onTooltipEntered && onTooltipEntered();
+  };
+
+  useEffect(() => {
+    if (tooltipActive) {
+      activate(calculatePosition(ref.current));
+    }
+
+    if (tooltipActive) {
+      if (tooltipActive && !active) {
+        activate(calculatePosition(ref.current));
       }
 
-      return React.createElement(
-        ComposedComponent,
-        childProps,
-        children,
-        createPortal(
-          <Transition
-            in={active}
-            onExited={this.handleTransitionExited}
-            onEntered={this.handleTransitionEntered}
-            timeout={{ enter: tooltipShowDelay, exit: 1000 }}
-          >
-            {(state) => {
-              const classNames = cx(
-                uiUtilities['box-shadow-200'],
-                theme['tooltip'],
-                theme[tooltipColor],
-                theme[tooltipSize],
-                {
-                  [theme['is-entering']]: state === 'entering',
-                  [theme['is-entered']]: state === 'entered',
-                  [theme['is-exiting']]: state === 'exiting',
-                  [theme[position]]: theme[position],
-                },
-              );
-
-              return (
-                <div className={classNames} data-teamleader-ui="tooltip" style={{ top, left, zIndex }}>
-                  <Box className={theme['inner']} {...SIZES[tooltipSize]}>
-                    {tooltipIcon && <div className={theme['icon']}>{tooltipIcon}</div>}
-                    <div className={theme['text']}>{tooltip}</div>
-                  </Box>
-                </div>
-              );
-            }}
-          </Transition>,
-          this.tooltipRoot,
-        ),
-      );
+      if (!tooltipActive && active) {
+        setActive(false);
+      }
     }
+  }, [tooltipActive, active]);
+
+  const rest = omit(other, [
+    'onTooltipEntered',
+    'tooltipHideOnClick',
+    'tooltipPosition',
+    'tooltipShowOnClick',
+    'tooltipShowDelay',
+    'documentObject',
+  ]);
+
+  let childProps = {
+    ...rest,
+    className,
+    onClick,
+    onMouseEnter,
+    onMouseLeave,
+    ref: ref,
+  };
+
+  if (tooltipActive === null) {
+    childProps = {
+      ...childProps,
+      onClick: handleClick,
+      onMouseEnter: handleMouseEnter,
+      onMouseLeave: handleMouseLeave,
+    };
   }
 
-  TooltippedComponent.propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    onClick: PropTypes.func,
-    onMouseEnter: PropTypes.func,
-    onMouseLeave: PropTypes.func,
-    onTooltipEntered: PropTypes.func,
-    tooltip: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-    tooltipColor: PropTypes.oneOf(['white', 'neutral', 'mint', 'violet', 'ruby', 'gold', 'aqua', 'inverse']),
-    tooltipHideOnClick: PropTypes.bool,
-    tooltipIcon: PropTypes.element,
-    tooltipPosition: PropTypes.oneOf(Object.keys(POSITIONS).map((key) => POSITIONS[key])),
-    tooltipShowOnClick: PropTypes.bool,
-    tooltipSize: PropTypes.oneOf(Object.keys(SIZES)),
-    documentObject: PropTypes.object.isRequired,
-    tooltipShowDelay: PropTypes.number,
-    /** The z-index of the Tooltip */
-    zIndex: PropTypes.number,
-    tooltipActive: PropTypes.bool,
-  };
+  return React.createElement(
+    ComposedComponent,
+    childProps,
+    children,
+    createPortal(
+      <Transition
+        in={active}
+        onExited={handleTransitionExited}
+        onEntered={handleTransitionEntered}
+        timeout={{ enter: tooltipShowDelay, exit: 1000 }}
+      >
+        {(state) => {
+          const classNames = cx(
+            uiUtilities['box-shadow-200'],
+            theme['tooltip'],
+            theme[tooltipColor],
+            theme[tooltipSize],
+            {
+              [theme['is-entering']]: state === 'entering',
+              [theme['is-entered']]: state === 'entered',
+              [theme['is-exiting']]: state === 'exiting',
+              [theme[position.position]]: theme[position.position],
+            },
+          );
 
-  TooltippedComponent.defaultProps = {
-    className: '',
-    tooltipColor: 'white',
-    tooltipHideOnClick: true,
-    tooltipIcon: null,
-    tooltipPosition: POSITIONS.TOP,
-    tooltipShowOnClick: false,
-    tooltipSize: 'medium',
-    tooltipShowDelay: 100,
-    zIndex: 700,
-    tooltipActive: null,
-  };
+          return (
+            <div
+              className={classNames}
+              data-teamleader-ui="tooltip"
+              style={{ top: position.top, left: position.left, zIndex }}
+            >
+              <Box className={theme['inner']} {...SIZES[tooltipSize]}>
+                {tooltipIcon && <div className={theme['icon']}>{tooltipIcon}</div>}
+                <div className={theme['text']}>{tooltip}</div>
+              </Box>
+            </div>
+          );
+        }}
+      </Transition>,
+      tooltipRoot,
+    ),
+  );
+};
 
-  return DocumentObjectProvider((props) => {
+const Tooltip = (ComposedComponent: TooltippedComponentProps['ComposedComponent']) => {
+  return DocumentObjectProvider((props: TooltipProps) => {
     return (
       <DocumentObjectContext.Consumer>
-        {(documentObject) => <TooltippedComponent {...props} documentObject={documentObject} />}
+        {(documentObject) => (
+          <TooltippedComponent {...props} documentObject={documentObject} ComposedComponent={ComposedComponent} />
+        )}
       </DocumentObjectContext.Consumer>
     );
   });
