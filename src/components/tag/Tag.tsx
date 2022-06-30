c9f21644c952fe8662cdcd11c8a64573d229abbd
qubis741 <jakub.kovacikk@gmail.com>
2022-06-30 08:10:17 +0200

    converted Tag to TS

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -1,55 +1,65 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import IconButton from '../iconButton';
-import Box from '../box';
-import { UITextBody, UITextSmall, UITextDisplay } from '../typography';
-import cx from 'classnames';
-import theme from './theme.css';
 import { IconCloseSmallOutline } from '@teamleader/ui-icons';
+import cx from 'classnames';
+import React, {
+  FunctionComponent,
+  PropsWithChildren,
+  ReactElement,
+  ReactNode,
+  ValidationMap,
+  WeakValidationMap,
+} from 'react';
+import { GenericComponent } from '../../@types/types';
+import Box from '../box';
+import { BoxProps } from '../box/Box';
+import IconButton from '../iconButton';
+import { UITextBody, UITextDisplay, UITextSmall } from '../typography';
+import theme from './theme.css';
 
-/** @type {React.ComponentType<any>} */
-class Tag extends PureComponent {
-  render() {
-    const { children, className, onRemoveClick, size, ...others } = this.props;
-
-    const classNames = cx(theme[`is-${size}`], theme['wrapper'], className);
-
-    const TextElement = size === 'small' ? UITextSmall : size === 'large' ? UITextDisplay : UITextBody;
-
-    return (
-      <Box {...others} className={classNames} data-teamleader-ui="tag" display="inline-flex">
-        <TextElement
-          color="teal"
-          marginLeft={size === 'small' ? 2 : 3}
-          marginRight={onRemoveClick ? 1 : size === 'small' ? 2 : 3}
-          marginVertical={size === 'small' ? 0 : 1}
-          maxLines={1}
-          tint="darkest"
-        >
-          {children}
-        </TextElement>
-        {onRemoveClick && (
-          <IconButton
-            className={theme['remove-button']}
-            flexShrink={0}
-            icon={<IconCloseSmallOutline />}
-            onClick={onRemoveClick}
-          />
-        )}
-      </Box>
-    );
-  }
+export interface FunctionComponent2<P = {}> {
+  (props: PropsWithChildren<P>, context?: any): ReactElement<any, any> | null;
+  propTypes?: WeakValidationMap<P> | undefined;
+  contextTypes?: ValidationMap<any> | undefined;
+  defaultProps?: Partial<P> | undefined;
+  displayName?: string | undefined;
+}
+export interface TagProps extends Omit<BoxProps, 'className' | 'size'> {
+  /** The tint of the components color */
+  children: ReactNode;
+  /** The tint of the components color */
+  className?: string;
+  /** The tint of the components color */
+  onRemoveClick: React.MouseEventHandler;
+  /** The tint of the components color */
+  size: 'small' | 'medium' | 'large';
 }
 
-Tag.propTypes = {
-  children: PropTypes.any.isRequired,
-  className: PropTypes.string,
-  onRemoveClick: PropTypes.func,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-};
+const Tag: GenericComponent<TagProps> = ({ children, className, onRemoveClick, size = 'medium', ...others }) => {
+  const classNames = cx(theme[`is-${size}`], theme['wrapper'], className);
 
-Tag.defaultProps = {
-  size: 'medium',
+  const TextElement = size === 'small' ? UITextSmall : size === 'large' ? UITextDisplay : UITextBody;
+
+  return (
+    <Box {...others} className={classNames} data-teamleader-ui="tag" display="inline-flex">
+      <TextElement
+        color="teal"
+        marginLeft={size === 'small' ? 2 : 3}
+        marginRight={onRemoveClick ? 1 : size === 'small' ? 2 : 3}
+        marginVertical={size === 'small' ? 0 : 1}
+        maxLines={1}
+        tint="darkest"
+      >
+        {children}
+      </TextElement>
+      {onRemoveClick && (
+        <IconButton
+          className={theme['remove-button']}
+          flexShrink={0}
+          icon={<IconCloseSmallOutline />}
+          onClick={onRemoveClick}
+        />
+      )}
+    </Box>
+  );
 };
 
 export default Tag;
--- src/components/tag/tag.stories.tsx
+++ src/components/tag/tag.stories.tsx
@@ -1,3 +1,4 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import { addStoryInGroup, LOW_LEVEL_BLOCKS } from '../../../.storybook/utils';
 import Tag from './Tag';
@@ -12,10 +13,11 @@ export default {
       url: 'https://www.figma.com/file/LHH25GN90ljQaBEUNMsdJn/Desktop-components?node-id=3943%3A1784',
     },
   },
-};
+} as ComponentMeta<typeof Tag>;
 
-export const Basic = (args) => <Tag {...args} onRemoveClick={() => console.log('Tag removed')} />;
+export const Basic: ComponentStory<typeof Tag> = (args) => <Tag {...args} />;
 
 Basic.args = {
   children: 'I am a tag',
+  onRemoveClick: () => console.log('Tag removed'),
 };
