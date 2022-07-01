a13d3d4dde5da1edb54168177990fb6935a99e9f
Kristof Colpaert <kristof.colpaert@teamleader.eu>
2022-07-01 10:31:33 +0200

    :art: Rewrite poweredByButton.stories.tsx in TS

--- src/components/poweredByButton/poweredByButton.stories.tsx
+++ src/components/poweredByButton/poweredByButton.stories.tsx
@@ -1,3 +1,4 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import { addStoryInGroup, MARKETING } from '../../../.storybook/utils';
 import Box from '../box';
@@ -13,12 +14,10 @@ export default {
       url: 'https://www.figma.com/file/LHH25GN90ljQaBEUNMsdJn/Desktop-components?node-id=14997%3A11428',
     },
   },
-};
+} as ComponentMeta<typeof PoweredByButton>;
 
-export const Basic = ({ ...args }) => (
+export const Basic: ComponentStory<typeof PoweredByButton> = (args) => (
   <Box backgroundColor="neutral" backgroundTint="dark" padding={3}>
     <PoweredByButton {...args} />
   </Box>
 );
-
-Basic.args = {};
