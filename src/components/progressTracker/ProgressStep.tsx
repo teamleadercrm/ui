9f458b1fcb5c96cdd364eb908141e9ea9db182f3
Stefaan <stefaan.devylder@gmail.com>
2022-07-01 10:39:11 +0200

    🎨 Change ProgressSetp to functional component

--- src/components/progressTracker/ProgressStep.tsx
+++ src/components/progressTracker/ProgressStep.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import theme from './theme.css';
@@ -6,27 +6,24 @@ import theme from './theme.css';
 import Box from '../box';
 import { TextSmall } from '../typography';
 
-class ProgressStep extends PureComponent {
-  render() {
-    const { label, meta, active, completed, onClick } = this.props;
-    const classNames = cx(theme['step'], {
-      [theme['is-active']]: active,
-      [theme['is-completed']]: completed,
-      [theme['is-clickable']]: !!onClick,
-      [theme['has-meta']]: !!meta,
-    });
-    return (
-      <Box className={classNames}>
-        <div className={theme['step-label-holder']}>
-          <TextSmall className={theme['step-label']}>{label}</TextSmall>
-          {meta && <TextSmall className={theme['step-meta']}>{meta}</TextSmall>}
-        </div>
-        <span className={theme['status-bullet-halo']} onClick={onClick} />
-        <span className={theme['status-bullet']} onClick={onClick} />
-      </Box>
-    );
-  }
-}
+const ProgressStep = ({ label, meta, active, completed, onClick }) => {
+  const classNames = cx(theme['step'], {
+    [theme['is-active']]: active,
+    [theme['is-completed']]: completed,
+    [theme['is-clickable']]: !!onClick,
+    [theme['has-meta']]: !!meta,
+  });
+  return (
+    <Box className={classNames}>
+      <div className={theme['step-label-holder']}>
+        <TextSmall className={theme['step-label']}>{label}</TextSmall>
+        {meta && <TextSmall className={theme['step-meta']}>{meta}</TextSmall>}
+      </div>
+      <span className={theme['status-bullet-halo']} onClick={onClick} />
+      <span className={theme['status-bullet']} onClick={onClick} />
+    </Box>
+  );
+};
 
 ProgressStep.propTypes = {
   /** The label for the progress step */
