4c5ab218f1b86ceebe8c4b9d97aabbd52e2eba2f
Kristof Colpaert <kristof.colpaert@teamleader.eu>
2022-07-01 14:13:56 +0200

    :art: Rename and rewrite (typescript) splitButton.stories.tsx

--- src/components/splitButton/splitButton.stories.tsx
+++ src/components/splitButton/splitButton.stories.tsx
@@ -3,6 +3,7 @@ import { addStoryInGroup, MID_LEVEL_BLOCKS } from '../../../.storybook/utils';
 import SplitButton from './SplitButton';
 import MenuDivider from '../menu/MenuDivider';
 import MenuItem from '../menu/MenuItem';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 const handleButtonClick = () => {
   console.log('clicked main button');
@@ -15,12 +16,12 @@ const handleMenuItemClick = () => {
 export default {
   component: SplitButton,
   title: addStoryInGroup(MID_LEVEL_BLOCKS, 'Split button'),
-};
+} as ComponentMeta<typeof SplitButton>;
 
-export const DefaultStory = (args) => (
+export const DefaultStory: ComponentStory<typeof SplitButton> = (args) => (
   <SplitButton {...args} onButtonClick={handleButtonClick}>
     <MenuItem onClick={handleMenuItemClick} label="Main action" />
-    <MenuItem onClick={handleMenuItemClick} label="Via file upload" caption="(.CVS, . XLS & .XLSX)" />
+    <MenuItem onClick={handleMenuItemClick} label="Via file upload" caption="(.CVS, . XLS &amp; .XLSX)" />
     <MenuDivider />
     <MenuItem onClick={handleMenuItemClick} label="Via Marketplace integrations" />
   </SplitButton>
