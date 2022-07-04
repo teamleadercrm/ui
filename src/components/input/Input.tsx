0ab898dd525977550f1b027ca7e1592515e1e0f0
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2022-07-04 12:54:06 +0200

    Input: fix value not being set on change

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -2,16 +2,14 @@ import React, { ChangeEvent, forwardRef, useState } from 'react';
 import { GenericComponent } from '../../@types/types';
 import SingleLineInputBase, { SingleLineInputBaseProps } from './SingleLineInputBase';
 
-interface InputProps extends Omit<SingleLineInputBaseProps, 'onChange' | 'value'> {
+interface InputProps extends Omit<SingleLineInputBaseProps, 'onChange'> {
   /** Callback function that is fired when the component's value changes. */
   onChange?: (event: ChangeEvent<HTMLElement>, value: string) => void;
-  /** Current value of the input element. */
-  value?: string | number;
 }
 
 const Input: GenericComponent<InputProps> = forwardRef<HTMLElement, InputProps>(
-  ({ value, onChange, ...others }: InputProps, forwardedRef) => {
-    const [stateValue, setStateValue] = useState(value);
+  ({ onChange, ...others }: InputProps, forwardedRef) => {
+    const [stateValue, setStateValue] = useState('');
 
     const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
       setStateValue(event.currentTarget.value);
