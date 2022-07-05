755a3fdfe838bdf41fe5dfb915004b2ec4c3ec86
Enis Krasniqi <eniskraasniqi@gmail.com>
2022-07-05 17:55:58 +0200

    move type Sizes from types.ts to a generalised SIZES constant

--- src/constants/sizes.ts
+++ src/constants/sizes.ts
@@ -1,11 +1,13 @@
 import without from 'lodash.without';
 
 export const TINY = 'tiny';
+export const SMALLEST = 'smallest';
 export const SMALL = 'small';
 export const MEDIUM = 'medium';
 export const LARGE = 'large';
+export const HERO = 'hero';
 export const FULLSCREEN = 'fullscreen';
 
-export const SIZE = { TINY, SMALL, MEDIUM, LARGE, FULLSCREEN };
+export const SIZE = { TINY, SMALL, SMALLEST, MEDIUM, LARGE, HERO, FULLSCREEN };
 export const SIZES = Object.keys(SIZE).map((key) => key.toLowerCase()) as unknown as Lowercase<keyof typeof SIZE>[];
 export const sizesWithout = (sizesToExclude: typeof SIZES[number][]) => without(SIZES, ...sizesToExclude);
