b968a1fa7a5ce859f214fa15cc92d034fee023e9
Enis Krasniqi <eniskraasniqi@gmail.com>
2022-07-05 17:12:15 +0200

    create sizes constant

--- src/constants/sizes.ts
+++ src/constants/sizes.ts
@@ -6,5 +6,6 @@ export const MEDIUM = 'medium';
 export const LARGE = 'large';
 export const FULLSCREEN = 'fullscreen';
 
-export const SIZES = [TINY, SMALL, MEDIUM, LARGE, FULLSCREEN];
-export const sizesWithout = (sizesToExclude) => without(SIZES, ...sizesToExclude);
+export const SIZE = { TINY, SMALL, MEDIUM, LARGE, FULLSCREEN };
+export const SIZES = Object.keys(SIZE).map((key) => key.toLowerCase()) as unknown as Lowercase<keyof typeof SIZE>[];
+export const sizesWithout = (sizesToExclude: typeof SIZES[number][]) => without(SIZES, ...sizesToExclude);
