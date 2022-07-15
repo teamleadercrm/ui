6ec9df5715b25de1dddf19b596dd404dabc39f9c
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-07-15 13:24:51 +0200

    Add required props to the story

--- src/components/marketingDialog/marketingDialog.stories.tsx
+++ src/components/marketingDialog/marketingDialog.stories.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { addStoryInGroup, MARKETING } from '../../../.storybook/utils';
 
 import MarketingDialog from './MarketingDialog';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Button from '../button';
 
 export default {
   component: MarketingDialog,
@@ -16,6 +17,43 @@ export default {
   },
 } as ComponentMeta<typeof MarketingDialog>;
 
-export const Basic: ComponentStory<typeof MarketingDialog> = (props) => <MarketingDialog {...props} />;
+export const Basic: ComponentStory<typeof MarketingDialog> = (props) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const toggleDialog = () => {
+    setDialogOpen((dialogOpen) => !dialogOpen);
+  };
 
-Basic.args = {};
+  return (
+    <>
+      <Button onClick={toggleDialog} label="Open a dialog" />
+      <MarketingDialog
+        {...props}
+        active={dialogOpen}
+        onCloseClick={toggleDialog}
+        onEscKeyDown={toggleDialog}
+        onOverlayClick={toggleDialog}
+      />
+    </>
+  );
+};
+
+Basic.args = {
+  active: true,
+  title: 'Discover our packages for free',
+  graphic: <iframe src="https://fast.wistia.net/embed/iframe/f7928s2ype?controlsVisibleOnLoad=false&playButton=true" />,
+  subtitle: 'Create multiple quotations',
+  body: (
+    <>
+      Switch to a package and improve your sales process by <strong>creating and sending multiple quotations</strong> in
+      one deal.
+    </>
+  ),
+  primaryAction: {
+    children: 'Try for free',
+    onClick: () => console.log('primaryAction.onClick'),
+  },
+  secondaryAction: {
+    children: 'Read more',
+    onClick: () => console.log('secondaryAction.onClick'),
+  },
+};
