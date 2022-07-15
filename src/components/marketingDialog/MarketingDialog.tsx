3978391596236e1a7895e0584946dee4e23384cf
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-07-15 09:25:19 +0200

    Add dummy marketing dialog

---
+++ src/components/marketingDialog/MarketingDialog.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface MarketingDialogProps {}
+
+const MarketingDialog = (props: MarketingDialogProps) => {
+  return <div />;
+};
+
+export default MarketingDialog;
