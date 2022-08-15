2724231146cb67061b77e2b0bcd519fe9be6c233
qubis741 <jakub.kovacikk@gmail.com>
2022-08-15 12:25:27 +0200

    convert `Marker` to functional component

--- src/components/typography/Marker.tsx
+++ src/components/typography/Marker.tsx
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Box from '../box';
 import cx from 'classnames';
+import React, { ReactNode } from 'react';
+import { GenericComponent } from '../../@types/types';
+import Box from '../box';
+import { BoxProps } from '../box/Box';
 import theme from './theme.css';
 
-/** @type {React.ComponentType<any>} */
-class Marker extends Component {
-  render() {
-    const { children, className, ...others } = this.props;
-
-    const classNames = cx(theme['marker'], className);
-
-    return (
-      <Box {...others} className={classNames} element="mark" paddingHorizontal={1} marginHorizontal={-1}>
-        {children}
-      </Box>
-    );
-  }
+export interface MarkerProps extends BoxProps {
+  children?: ReactNode;
+  className?: string;
 }
 
-Marker.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
+const Marker: GenericComponent<MarkerProps> = ({ children, className, ...others }) => {
+  const classNames = cx(theme['marker'], className);
+
+  return (
+    <Box {...others} className={classNames} element="mark" paddingHorizontal={1} marginHorizontal={-1}>
+      {children}
+    </Box>
+  );
 };
 
 export default Marker;
