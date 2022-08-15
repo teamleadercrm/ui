6bb852e4adce4d24c0b211acb05ffa52fe69067b
Enis Krasniqi <eniskraasniqi@gmail.com>
2022-08-15 19:36:56 +0200

    convert RadioButton to typescript

--- src/components/radio/RadioButton.tsx
+++ src/components/radio/RadioButton.tsx
@@ -1,20 +1,65 @@
 import cx from 'classnames';
 import omit from 'lodash.omit';
-import PropTypes from 'prop-types';
-import React, { createRef, PureComponent } from 'react';
+import React, { useRef, MouseEvent, ReactNode } from 'react';
+import { GenericComponent } from '../../@types/types';
 import Box, { omitBoxProps, pickBoxProps } from '../box';
 import { TextBodyCompact, TextDisplay, TextSmall } from '../typography';
+import { SIZES } from '../../constants';
+import { BoxProps } from '../box/Box';
 import theme from './theme.css';
 
-/** @type {React.ComponentType<any>} */
-class RadioButton extends PureComponent {
-  inputNode = createRef();
+export interface RadioButtonProps extends Omit<BoxProps, 'className' | 'children'> {
+  checked?: boolean;
+  children?: ReactNode;
+  className?: string;
+  disabled?: boolean;
+  name?: string;
+  onChange?: (checked: boolean, event: MouseEvent) => void;
+  onMouseEnter?: (event: MouseEvent) => void;
+  onMouseLeave?: (event: MouseEvent) => void;
+  label?: string;
+  size?: Exclude<typeof SIZES[number], 'fullscreen' | 'smallest' | 'tiny' | 'hero'>;
+}
 
-  handleToggle = (event) => {
-    const { disabled, checked, onChange } = this.props;
+const RadioButton: GenericComponent<RadioButtonProps> = ({
+  checked = false,
+  children,
+  className,
+  disabled = false,
+  name,
+  onChange,
+  onMouseEnter,
+  onMouseLeave,
+  label,
+  size = 'medium',
+  ...others
+}) => {
+  const inputNode = useRef<HTMLInputElement | null>(null);
+  const TextElement = size === 'small' ? TextSmall : size === 'medium' ? TextBodyCompact : TextDisplay;
 
+  const restProps = omit(others, ['onChange']);
+  const boxProps = pickBoxProps(restProps);
+  const inputProps = omitBoxProps(restProps);
+
+  const classNames = cx(
+    theme['radiobutton'],
+    theme[`is-${size}`],
+    {
+      [theme['is-checked']]: checked,
+      [theme['is-disabled']]: disabled,
+    },
+    className,
+  );
+
+  const blur = () => {
+    if (inputNode.current) {
+      inputNode.current.blur();
+    }
+  };
+
+  const handleToggle = (event: React.MouseEvent) => {
     if (event.pageX !== 0 && event.pageY !== 0) {
-      this.blur();
+      blur();
     }
 
     if (!disabled && onChange) {
@@ -22,88 +67,38 @@ class RadioButton extends PureComponent {
     }
   };
 
-  blur() {
-    if (this.inputNode.current) {
-      this.inputNode.current.blur();
-    }
-  }
-
-  focus() {
-    if (this.inputNode.current) {
-      this.inputNode.current.focus();
-    }
-  }
-
-  render() {
-    const { checked, disabled, className, size, label, children, onMouseEnter, onMouseLeave, ...others } = this.props;
-    const TextElement = size === 'small' ? TextSmall : size === 'medium' ? TextBodyCompact : TextDisplay;
-
-    const restProps = omit(others, ['onChange']);
-    const boxProps = pickBoxProps(restProps);
-    const inputProps = omitBoxProps(restProps);
-
-    const classNames = cx(
-      theme['radiobutton'],
-      theme[`is-${size}`],
-      {
-        [theme['is-checked']]: checked,
-        [theme['is-disabled']]: disabled,
-      },
-      className,
-    );
-
-    return (
-      <Box
-        element="label"
-        data-teamleader-ui="radiobutton"
-        className={classNames}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        {...boxProps}
-      >
-        <input
-          className={theme['input']}
-          type="radio"
-          checked={checked}
-          disabled={disabled}
-          onClick={this.handleToggle}
-          readOnly
-          ref={this.inputNode}
-          {...inputProps}
-        />
-        <span className={theme['shape']} />
-        {(label || children) && (
-          <span className={theme['label']}>
-            {label && (
-              <TextElement element="span" color={disabled ? 'neutral' : 'teal'}>
-                {label}
-              </TextElement>
-            )}
-            {children}
-          </span>
-        )}
-      </Box>
-    );
-  }
-}
-
-RadioButton.propTypes = {
-  checked: PropTypes.bool,
-  children: PropTypes.node,
-  disabled: PropTypes.bool,
-  name: PropTypes.string,
-  className: PropTypes.string,
-  label: PropTypes.string,
-  onChange: PropTypes.func,
-  onMouseEnter: PropTypes.func,
-  onMouseLeave: PropTypes.func,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-};
-
-RadioButton.defaultProps = {
-  checked: false,
-  disabled: false,
-  size: 'medium',
+  return (
+    <Box
+      element="label"
+      data-teamleader-ui="radiobutton"
+      className={classNames}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      {...boxProps}
+    >
+      <input
+        className={theme['input']}
+        type="radio"
+        checked={checked}
+        disabled={disabled}
+        onClick={handleToggle}
+        readOnly
+        ref={inputNode}
+        {...inputProps}
+      />
+      <span className={theme['shape']} />
+      {(label || children) && (
+        <span className={theme['label']}>
+          {label && (
+            <TextElement element="span" color={disabled ? 'neutral' : 'teal'}>
+              {label}
+            </TextElement>
+          )}
+          {children}
+        </span>
+      )}
+    </Box>
+  );
 };
 
 export default RadioButton;
