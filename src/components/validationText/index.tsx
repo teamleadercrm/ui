18ed826e8125f71be6cb34067a15f6c5b64669ca
qubis741 <jakub.kovacikk@gmail.com>
2022-08-15 14:09:42 +0200

    export interfaces and components of ValidationText

--- src/components/validationText/index.tsx
+++ src/components/validationText/index.tsx
@@ -1,8 +1,9 @@
-import ValidationText from './ValidationText';
-import ErrorText from './ErrorText';
-import HelpText from './HelpText';
-import SuccessText from './SuccessText';
-import WarningText from './WarningText';
+import ValidationText, { ValidationTextProps } from './ValidationText';
+import ErrorText, { ErrorTextProps } from './ErrorText';
+import HelpText, { HelpTextProps } from './HelpText';
+import SuccessText, { SuccessTextProps } from './SuccessText';
+import WarningText, { WarningTextProps } from './WarningText';
 
 export { ErrorText, HelpText, SuccessText, WarningText };
+export type { ErrorTextProps, HelpTextProps, SuccessTextProps, WarningTextProps, ValidationTextProps };
 export default ValidationText;
