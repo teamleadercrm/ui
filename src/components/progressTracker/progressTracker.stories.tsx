4d8bce124b54246ef5d5ac1c4cb496e9e3843fa0
Enis Krasniqi <eniskraasniqi@gmail.com>
2022-08-16 10:02:02 +0200

    fix controls of progressTracker stories

--- src/components/progressTracker/progressTracker.stories.tsx
+++ src/components/progressTracker/progressTracker.stories.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { boolean, number, select } from '@storybook/addon-knobs';
 import { addStoryInGroup, MID_LEVEL_BLOCKS } from '../../../.storybook/utils';
 import ProgressTracker from './ProgressTracker';
-import { COLORS } from '../../index';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 const steps = ['Draft', 'Book', 'Send invoices', 'Get paid'];
@@ -20,22 +18,11 @@ export default {
 } as ComponentMeta<typeof ProgressTracker>;
 
 export const DefaultStory: ComponentStory<typeof ProgressTracker> = (args) => (
-  <ProgressTracker
-    {...args}
-    done={boolean('Done', false)}
-    currentStep={number('Current step', 1, {
-      range: true,
-      min: 0,
-      max: steps.length - 1,
-      step: 1,
-    })}
-    color={select('Color', COLORS, 'mint')}
-    labelPosition={select('Label position', ['top', 'alternating', 'bottom'], 'top')}
-  >
+  <ProgressTracker {...args}>
     {steps.map((step, index) => (
       <ProgressTracker.ProgressStep
         label={step}
-        meta={boolean('Meta labels', true) ? `${10 + index * 3}/12/2020` : undefined}
+        meta={`${10 + index * 3}/12/2020`}
         key={index}
         onClick={() => console.log('step clicked')}
       />
