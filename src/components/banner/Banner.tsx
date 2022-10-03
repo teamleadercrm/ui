68fa3fb1149ff457d3c846416a307e350e9dd887
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-10-03 12:44:03 +0200

    ♻️ Extend `SectionProps` and `IslandProps`

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 
 import Box from '../box';
-import Island from '../island';
-import Section from '../section';
+import Island, { IslandProps } from '../island';
+import Section, { SectionProps } from '../section';
 import IconButton from '../iconButton';
 import theme from './theme.css';
 import { IconCloseMediumOutline } from '@teamleader/ui-icons';
-import { COLORS, SIZES } from '../../constants';
 
-export interface BannerProps {
+export interface BannerProps extends SectionProps, IslandProps {
   /** The content to display inside the banner. */
   children?: React.ReactNode;
   /** A class name for the wrapper to give custom styles. */
   className?: string;
-  /** The color of the banner. */
-  color?: typeof COLORS[number] | 'white';
-  /** The size of the banner. */
-  size?: Exclude<typeof SIZES[number], 'tiny' | 'fullscreen' | 'smallest' | 'hero'>;
   /** The icon displayed on the left inside the banner. */
   icon?: React.ReactElement;
   /** Callback function that is fired when the close button of the banner is clicked. */
