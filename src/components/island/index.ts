5baa2034e1c4187a743dfc06a808611234a4be31
Jelle De Clercq <jelle.declercq@teamleader.eu>
2022-10-03 11:40:36 +0200

    🧑‍💻 Re-export interfaces from index

--- src/components/island/index.ts
+++ src/components/island/index.ts
@@ -1,5 +1,6 @@
-import Island from './Island';
-import IslandGroup from './IslandGroup';
+import Island, { IslandProps } from './Island';
+import IslandGroup, { IslandGroupProps } from './IslandGroup';
 
 export default Island;
 export { Island, IslandGroup };
+export type { IslandProps, IslandGroupProps };
