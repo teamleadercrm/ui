992e2fbc07e715a3b6f93daf78e22a019fdd1ac0
qubis741 <jakub.kovacikk@gmail.com>
2022-10-05 16:25:54 +0200

    typed advanced collapsible chromatic story

--- src/components/advancedCollapsible/AdvancedCollapsible.tsx
+++ src/components/advancedCollapsible/AdvancedCollapsible.tsx
@@ -6,13 +6,18 @@ import { IconChevronDownSmallOutline, IconChevronRightSmallOutline } from '@team
 import theme from './theme.css';
 import { BoxProps } from '../box/Box';
 import { GenericComponent } from '../../@types/types';
-import { SIZES } from '../../constants';
+import { COLORS, SIZES } from '../../constants';
 
+export type AllowedAdvancedCollapsibleSize = Exclude<typeof SIZES[number], 'tiny' | 'fullscreen' | 'smallest' | 'hero'>;
+export type AllowedAdvancedCollapsibleColor = Exclude<
+  typeof COLORS[number],
+  'aqua' | 'gold' | 'mint' | 'ruby' | 'violet'
+>;
 interface AdvancedCollapsibleProps extends Omit<BoxProps, 'size'> {
-  color?: 'neutral' | 'teal';
+  color?: AllowedAdvancedCollapsibleColor;
   children: ReactNode;
   title: string;
-  size?: Exclude<typeof SIZES[number], 'tiny' | 'fullscreen' | 'smallest' | 'hero'>;
+  size?: AllowedAdvancedCollapsibleSize;
 }
 
 const AdvancedCollapsible: GenericComponent<AdvancedCollapsibleProps> = ({
--- src/components/advancedCollapsible/advancedCollapsable.stories.spec.tsx
+++ src/components/advancedCollapsible/advancedCollapsable.stories.spec.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
-import AdvancedCollapsible from './AdvancedCollapsible';
+import AdvancedCollapsible, {
+  AllowedAdvancedCollapsibleColor,
+  AllowedAdvancedCollapsibleSize,
+} from './AdvancedCollapsible';
 
 export default {
   component: AdvancedCollapsible,
   title: 'AdvancedCollapsible',
 };
 
+const sizeList: AllowedAdvancedCollapsibleSize[] = ['small', 'medium', 'large'];
+const colorList: AllowedAdvancedCollapsibleColor[] = ['neutral', 'teal'];
 export const Main = () => (
   <div>
-    {['small', 'medium', 'large'].map((size) =>
-      ['neutral', 'teal'].map((color) => (
+    {sizeList.map((size) =>
+      colorList.map((color) => (
         <div key={size + color} style={{ marginBottom: '12px' }}>
           <AdvancedCollapsible size={size} color={color} title={`Title - ${size} - ${color}`}>
             This is the content - {size} - ${color}
