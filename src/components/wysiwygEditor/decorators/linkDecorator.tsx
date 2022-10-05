28ab8b8e280be8e74b9ff5467f70e6bd949884e9
qubis741 <jakub.kovacikk@gmail.com>
2022-10-05 09:31:55 +0200

    converted linkDecorator to TS

--- src/components/wysiwygEditor/decorators/linkDecorator.tsx
+++ src/components/wysiwygEditor/decorators/linkDecorator.tsx
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
 import { IconExternalLinkSmallOutline } from '@teamleader/ui-icons';
+import React, { ReactNode, useState } from 'react';
+import { ContentBlock, ContentState } from 'react-draft-wysiwyg';
 
 import Box from '../../box';
 import Link from '../../link';
 
+import { GenericComponent } from '../../../@types/types';
 import theme from './theme.css';
 
-const findLinkEntities = (contentBlock, callback, contentState) => {
+const findLinkEntities = (contentBlock: ContentBlock, callback: () => void, contentState: ContentState) => {
   contentBlock.findEntityRanges((character) => {
     const entityKey = character.getEntity();
     return entityKey !== null && contentState.getEntity(entityKey).getType() === 'LINK';
   }, callback);
 };
 
-const LinkEntity = ({ entityKey, contentState, children }) => {
-  const [showOpenLinkIcon, setShowOpenLinkIcon] = useState();
+interface LinkEntityProps {
+  children: ReactNode;
+  entityKey: string;
+  contentState: ContentState;
+}
+
+const LinkEntity: GenericComponent<LinkEntityProps> = ({ entityKey, contentState, children }) => {
+  const [showOpenLinkIcon, setShowOpenLinkIcon] = useState(false);
   const { url } = contentState.getEntity(entityKey).getData();
 
   const openLink = () => {
@@ -31,7 +39,7 @@ const LinkEntity = ({ entityKey, contentState, children }) => {
 
   return (
     <Box display="inline-block" onMouseEnter={toggleShowOpenLinkIcon} onMouseLeave={toggleShowOpenLinkIcon}>
-      <Link className={theme['link']} inherit={false} onClick={(event) => event.preventDefault()}>
+      <Link className={theme['link']} inherit={false} onClick={(event: MouseEvent) => event.preventDefault()}>
         {children}
       </Link>
       {showOpenLinkIcon && <IconExternalLinkSmallOutline onClick={openLink} className={theme['icon']} />}
