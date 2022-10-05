8e1f24d7f8ef7dfb2b1511f2d2817ae5dd8d5d8e
qubis741 <jakub.kovacikk@gmail.com>
2022-10-05 12:41:28 +0200

    converted wysiwyg stories to TS

--- src/components/wysiwygEditor/wysiwygEditor.stories.tsx
+++ src/components/wysiwygEditor/wysiwygEditor.stories.tsx
@@ -1,3 +1,4 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import { addStoryInGroup, MID_LEVEL_BLOCKS } from '../../../.storybook/utils';
 import WysiwygEditor from './WysiwygEditor';
@@ -5,10 +6,15 @@ import WysiwygEditor from './WysiwygEditor';
 export default {
   component: WysiwygEditor,
   title: addStoryInGroup(MID_LEVEL_BLOCKS, 'WysiwygEditor'),
-};
+} as ComponentMeta<typeof WysiwygEditor>;
 
-export const Basic = (args) => <WysiwygEditor {...args} />;
+export const Basic: ComponentStory<typeof WysiwygEditor> = (args) => <WysiwygEditor {...args} />;
 
 Basic.args = {
   placeholder: 'Placeholder text',
+};
+Basic.argTypes = {
+  sucess: { control: 'text' },
+  error: { control: 'text' },
+  warning: { control: 'text' },
 };
