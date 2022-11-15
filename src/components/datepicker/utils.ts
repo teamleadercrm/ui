fbedc87eac0305826f7b5d9d1bdd7c079743641f
qubis741 <jakub.kovacikk@gmail.com>
2022-11-15 15:46:39 +0100

    added early return

--- src/components/datepicker/utils.ts
+++ src/components/datepicker/utils.ts
@@ -25,45 +25,47 @@ const getDateTimeAtStartOfDay = (date: Date) => {
   return date.getTime();
 };
 export const isAllowedDate = (date: Date, disabledDays?: AllowedDisabledDays) => {
-  if (disabledDays) {
-    const dateTime = getDateTimeAtStartOfDay(date);
-    if (disabledDays instanceof Date) {
-      return dateTime !== getDateTimeAtStartOfDay(disabledDays);
-    }
-    if (Array.isArray(disabledDays) && disabledDays.length > 0) {
-      return !disabledDays.some((disabledDay) => {
-        return dateTime === getDateTimeAtStartOfDay(disabledDay);
-      });
-    }
-    if ('from' in disabledDays && disabledDays.from && 'to' in disabledDays && disabledDays.to) {
-      return !(
-        getDateTimeAtStartOfDay(disabledDays.from) <= dateTime && dateTime <= getDateTimeAtStartOfDay(disabledDays.to)
-      );
-    }
-    if ('before' in disabledDays && disabledDays.before && !('after' in disabledDays)) {
-      return getDateTimeAtStartOfDay(disabledDays.before) <= dateTime;
-    }
-    if ('after' in disabledDays && disabledDays.after && !('before' in disabledDays)) {
-      return getDateTimeAtStartOfDay(disabledDays.after) >= dateTime;
-    }
-    const disabledDaysBeforeAfter = disabledDays as BeforeAfterModifier;
-    if (
-      'before' in disabledDaysBeforeAfter &&
-      'after' in disabledDaysBeforeAfter &&
-      disabledDaysBeforeAfter.before &&
-      disabledDaysBeforeAfter.after
-    ) {
-      return (
-        getDateTimeAtStartOfDay(disabledDaysBeforeAfter.before) <= dateTime &&
-        dateTime <= getDateTimeAtStartOfDay(disabledDaysBeforeAfter.after)
-      );
-    }
-    if ('daysOfWeek' in disabledDays && Array.isArray(disabledDays.daysOfWeek)) {
-      return !disabledDays.daysOfWeek.includes(date.getDay());
-    }
-    if (typeof disabledDays === 'function') {
-      return !disabledDays(date);
-    }
+  if (!disabledDays) {
+    return true;
   }
+  const dateTime = getDateTimeAtStartOfDay(date);
+  if (disabledDays instanceof Date) {
+    return dateTime !== getDateTimeAtStartOfDay(disabledDays);
+  }
+  if (Array.isArray(disabledDays) && disabledDays.length > 0) {
+    return !disabledDays.some((disabledDay) => {
+      return dateTime === getDateTimeAtStartOfDay(disabledDay);
+    });
+  }
+  if ('from' in disabledDays && disabledDays.from && 'to' in disabledDays && disabledDays.to) {
+    return !(
+      getDateTimeAtStartOfDay(disabledDays.from) <= dateTime && dateTime <= getDateTimeAtStartOfDay(disabledDays.to)
+    );
+  }
+  if ('before' in disabledDays && disabledDays.before && !('after' in disabledDays)) {
+    return getDateTimeAtStartOfDay(disabledDays.before) <= dateTime;
+  }
+  if ('after' in disabledDays && disabledDays.after && !('before' in disabledDays)) {
+    return getDateTimeAtStartOfDay(disabledDays.after) >= dateTime;
+  }
+  const disabledDaysBeforeAfter = disabledDays as BeforeAfterModifier;
+  if (
+    'before' in disabledDaysBeforeAfter &&
+    'after' in disabledDaysBeforeAfter &&
+    disabledDaysBeforeAfter.before &&
+    disabledDaysBeforeAfter.after
+  ) {
+    return (
+      getDateTimeAtStartOfDay(disabledDaysBeforeAfter.before) <= dateTime &&
+      dateTime <= getDateTimeAtStartOfDay(disabledDaysBeforeAfter.after)
+    );
+  }
+  if ('daysOfWeek' in disabledDays && Array.isArray(disabledDays.daysOfWeek)) {
+    return !disabledDays.daysOfWeek.includes(date.getDay());
+  }
+  if (typeof disabledDays === 'function') {
+    return !disabledDays(date);
+  }
+
   return true;
 };
