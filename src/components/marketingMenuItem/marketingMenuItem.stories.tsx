ce23b60051f5dcda061b61581be96dcfeb2ddbdd
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-11-18 13:00:52 +0100

    Add story for select with MarketingMenuItem

--- src/components/marketingMenuItem/marketingMenuItem.stories.tsx
+++ src/components/marketingMenuItem/marketingMenuItem.stories.tsx
@@ -1,10 +1,14 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { addStoryInGroup, MARKETING } from '../../../.storybook/utils';
 
 import MarketingMenuItem from './MarketingMenuItem';
 import { IconExternalLinkSmallOutline } from '@teamleader/ui-icons';
-import Menu, { MenuItem } from '../menu';
+import Menu, { MenuDivider, MenuItem } from '../menu';
+import Box, { BoxProps } from '../box';
+import { MenuListProps } from 'react-select';
+import Select from '../select';
+import { useCallback } from '@storybook/addons';
 
 export default {
   component: MarketingMenuItem,
@@ -22,6 +26,46 @@ export const MenuWithMenuUpsell: ComponentStory<typeof MarketingMenuItem> = (arg
 );
 
 MenuWithMenuUpsell.args = {
+  label: 'Unlock baz',
+  icon: <IconExternalLinkSmallOutline />,
+  element: 'a',
+  href: 'https://www.teamleader.be',
+  target: '_blank',
+};
+
+const options = [
+  { label: 'Foo', value: 'foo' },
+  { label: 'Bar', value: 'bar' },
+];
+
+export const DropdownWithUpsell: ComponentStory<typeof MarketingMenuItem> = (args) => {
+  const MenuList = useCallback(
+    (props: MenuListProps) => {
+      const { children, getStyles, innerRef, innerProps } = props;
+
+      return (
+        <Box
+          {...(innerProps as BoxProps)}
+          borderRadius="rounded"
+          display="flex"
+          flexDirection="column"
+          flex={1}
+          overflow="hidden"
+          style={getStyles('menuList', props) as CSSProperties}
+        >
+          <Box ref={innerRef}>{children}</Box>
+          <MenuDivider />
+          <MarketingMenuItem {...args} />
+        </Box>
+      );
+    },
+    [args],
+  );
+
+  return <Select options={options} components={{ MenuList }} />;
+};
+
+DropdownWithUpsell.args = {
   label: 'Unlock baz',
   icon: <IconExternalLinkSmallOutline />,
   element: 'a',
