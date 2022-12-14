1a84379df7308c2c7f5672b0d6e34475c94dbe14
Lowie Benoot <lowie@teamleader.eu>
2022-12-14 15:23:02 +0800

    ✅ Add snapshot test for Alert

---
+++ src/components/alert/__tests__/Alert.spec.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Alert from '../Alert';
+
+describe('Component - Alert', () => {
+  test('it renders', async () => {
+    const screen = render(
+      <Alert
+        active
+        primaryAction={{
+          label: 'Confirm',
+        }}
+        secondaryAction={{
+          label: 'Cancel',
+        }}
+        title="Alert title"
+        body="I am the alert body text"
+      />,
+    );
+
+    expect(screen.baseElement).toMatchSnapshot();
+  });
+});
