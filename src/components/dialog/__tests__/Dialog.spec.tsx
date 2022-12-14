bf5af3f63c1d995659e3806f84a2afdb0623ae2c
Lowie Benoot <lowie@teamleader.eu>
2022-12-14 15:18:32 +0800

    ✅ Add more specific tests for Dialog

--- src/components/dialog/__tests__/Dialog.spec.tsx
+++ src/components/dialog/__tests__/Dialog.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Dialog from '../Dialog';
 
 describe('Component - Dialog', () => {
@@ -25,5 +26,110 @@ describe('Component - Dialog', () => {
       </Dialog>,
     );
     expect(screen.baseElement).toMatchSnapshot();
+  });
+
+  test('it does not render anything when the dialog is not active', async () => {
+    const screen = render(
+      <Dialog
+        active={false}
+        title="Foobar"
+        primaryAction={{
+          label: 'Confirm',
+        }}
+      >
+        Foobar
+      </Dialog>,
+    );
+    expect(screen.baseElement).toMatchSnapshot();
+  });
+
+  test('it calls the onClose handler when clicking the close button', async () => {
+    const handleOnClose = jest.fn();
+    const user = userEvent.setup();
+
+    const screen = render(
+      <Dialog
+        active
+        title="Foobar"
+        primaryAction={{
+          label: 'Confirm',
+        }}
+        onCloseClick={handleOnClose}
+      >
+        Foobar
+      </Dialog>,
+    );
+
+    expect(handleOnClose).not.toBeCalled();
+    const closeButton = screen.baseElement.querySelector('section button[type="button"]');
+
+    await user.click(closeButton!);
+    expect(handleOnClose).toBeCalledTimes(1);
+  });
+
+  test('it calls the onClose handler when clicking the overlay', async () => {
+    const handleOnClose = jest.fn();
+    const user = userEvent.setup();
+
+    const screen = render(
+      <Dialog
+        active
+        title="Foobar"
+        primaryAction={{
+          label: 'Confirm',
+        }}
+        onOverlayClick={handleOnClose}
+      >
+        Foobar
+      </Dialog>,
+    );
+
+    expect(handleOnClose).not.toBeCalled();
+    const overlay = screen.baseElement.querySelector('[data-teamleader-ui="overlay"]');
+
+    await user.click(overlay!);
+    expect(handleOnClose).toBeCalledTimes(1);
+  });
+
+  test('renders a form', async () => {
+    const screen = render(
+      <Dialog
+        active
+        title="Foobar"
+        primaryAction={{
+          label: 'Confirm',
+        }}
+        form
+      >
+        Foobar
+      </Dialog>,
+    );
+
+    expect(screen.baseElement.querySelector('form')).toBeVisible();
+  });
+
+  test('calls the form onSubmit when pressing the submit button', async () => {
+    const handleSubmit = jest.fn((event) => event.preventDefault()); // need to add this preventDefault, otherwise it errors
+    const user = userEvent.setup();
+
+    const screen = render(
+      <Dialog
+        active
+        title="Foobar"
+        primaryAction={{
+          label: 'Confirm',
+          type: 'submit',
+        }}
+        form
+        onSubmit={handleSubmit}
+      >
+        Foobar
+      </Dialog>,
+    );
+
+    const submitButton = screen.getByRole('button', { name: 'Confirm' });
+    await user.click(submitButton);
+
+    expect(handleSubmit).toBeCalled();
   });
 });
