079d51839528607a183cb37c4af47c63a06e065f
Lowie Benoot <lowie@teamleader.eu>
2022-12-16 15:20:34 +0800

    ✅ Add snapshot test for the MarketingDialog component

---
+++ src/components/marketingDialog/__tests__/MarketingDialog.spec.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import MarketingDialog from '../';
+
+describe('Component - Dialog', () => {
+  it('renders', async () => {
+    const screen = render(
+      <MarketingDialog
+        active
+        title="Dundler Mifflin"
+        subtitle="Michael Scott"
+        graphic={<img src="https://foobar.com/michael_scott.png" />}
+        primaryAction={{
+          label: 'Confirm',
+        }}
+        secondaryAction={{
+          label: 'Cancel',
+        }}
+        onCloseClick={() => {}}
+        onOverlayClick={() => {}}
+        onEscKeyDown={() => {}}
+        body={<>foobar</>}
+      />,
+    );
+    expect(screen.baseElement).toMatchSnapshot();
+  });
+});
