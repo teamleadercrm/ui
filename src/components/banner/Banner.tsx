962e9e834b2410f069c8e2793ed6dd82ba84ee95
Faraz <78449551+farazatarodi@users.noreply.github.com>
2023-02-08 14:58:31 +0100

    ♻️ make Banner use the Box

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import cx from 'classnames';
 
-import Box from '../box';
-import Island, { IslandProps } from '../island';
-import Section, { SectionProps } from '../section';
+import Box, { BoxProps, Padding } from '../box';
 import IconButton from '../iconButton';
 import theme from './theme.css';
 import { IconCloseMediumOutline } from '@teamleader/ui-icons';
+import { COLORS, SIZES } from '../../constants';
 
-export interface BannerProps extends SectionProps, IslandProps {
+const PADDINGS: Record<'small' | 'medium' | 'large', Padding> = {
+  small: 3,
+  medium: 4,
+  large: 5,
+};
+
+export interface BannerProps extends Omit<BoxProps, 'size'> {
   /** The content to display inside the banner. */
   children?: React.ReactNode;
   /** A class name for the wrapper to give custom styles. */
@@ -18,13 +24,33 @@ export interface BannerProps extends SectionProps, IslandProps {
   onClose?: React.MouseEventHandler<HTMLButtonElement>;
   /** If true, component will take the full width of it's container. */
   fullWidth?: boolean;
+  /** The color for the Banner background and border */
+  color?: Exclude<typeof COLORS[number], 'teal'> | 'white';
+  /** Te size of the Banner component */
+  size?: Exclude<typeof SIZES[number], 'tiny' | 'smallest' | 'hero' | 'fullscreen'>;
 }
 
-const Banner = ({ children, className, color = 'white', icon, onClose, fullWidth, ...others }: BannerProps) => {
-  const Element = fullWidth ? Section : Island;
+const Banner = ({
+  children,
+  className,
+  color = 'white',
+  size = 'medium',
+  icon,
+  onClose,
+  fullWidth,
+  ...others
+}: BannerProps) => {
+  const classNames = cx(className, theme[color], theme['banner'], { [theme['banner_full-width']]: fullWidth });
 
   return (
-    <Element data-teamleader-ui="banner" className={className} color={color} {...others}>
+    <Box
+      data-teamleader-ui="banner"
+      className={classNames}
+      color={color}
+      padding={PADDINGS[size]}
+      borderRadius={fullWidth ? 'square' : 'rounded'}
+      {...others}
+    >
       <div className={theme['inner']}>
         {icon && <span className={theme['icon']}>{icon}</span>}
         <Box flex={1} element="span" paddingRight={onClose && 7}>
@@ -39,7 +65,7 @@ const Banner = ({ children, className, color = 'white', icon, onClose, fullWidth
           />
         )}
       </div>
-    </Element>
+    </Box>
   );
 };
 
