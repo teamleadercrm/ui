02cab0b1d6195af02169149b623dfd9d175dda45
farazatarodi <farazatarodi@gmail.com>
2023-04-05 15:49:33 +0200

    ♻️ move menu state management outside to parent component

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,10 +1,19 @@
 import cx from 'classnames';
-import React, { ReactElement, ReactNode, SyntheticEvent, useCallback, useEffect, useRef, useState } from 'react';
+import React, {
+  Dispatch,
+  ReactElement,
+  ReactNode,
+  SetStateAction,
+  SyntheticEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 import Box, { pickBoxProps } from '../box';
 import { BoxProps } from '../box/Box';
 import MarketingMenuItem from '../marketingMenuItem';
-import { events } from '../utils';
 import isComponentOfType from '../utils/is-component-of-type';
 import { getViewport } from '../utils/utils';
 import MenuItem from './MenuItem';
@@ -22,16 +31,13 @@ const POSITION: Record<string, 'auto' | 'static' | 'top-left' | 'top-right' | 'b
 export interface MenuProps<S = any> extends Omit<BoxProps, 'children' | 'className'> {
   /** If true, the menu will be active. */
   active?: boolean;
+  setActive?: Dispatch<SetStateAction<boolean>>;
   /** The content to display inside the menu. */
   children?: ReactNode;
   /** A class name for the wrapper to give custom styles. */
   className?: string;
-  /** Callback function that is fired when the menu hides. */
-  onHide?: () => void;
   /** Callback function that is fired when a menu item is clicked. */
   onSelect?: (selected: S) => void;
-  /** Callback function that is fired when the menu shows. */
-  onShow?: () => void;
   /** If true, a border is rendered around the menu. */
   outline?: boolean;
   /** The position in which the menu is rendered. */
@@ -46,11 +52,10 @@ type Position = Exclude<MenuProps['position'], undefined>;
 
 const Menu = <S,>({
   active = false,
+  setActive,
   children,
   className,
-  onHide,
   onSelect,
-  onShow,
   outline = true,
   position = 'static',
   selectable = true,
@@ -59,8 +64,8 @@ const Menu = <S,>({
 }: MenuProps<S>): ReactElement<any, any> | null => {
   const [positionState, setPositionState] = useState<Position>(position);
 
-  const menuNode = useRef<HTMLUListElement>(null);
-  const menuWrapper = useRef<HTMLElement>(null);
+  const menuRef = useRef<HTMLUListElement>(null);
+  const menuWrapperRef = useRef<HTMLElement>(null);
 
   const boxProps = pickBoxProps(others);
   const classNames = cx(
@@ -78,9 +83,10 @@ const Menu = <S,>({
   });
 
   const handleDocumentClick = (event: Event) => {
-    if (active && !events.targetIsDescendant(event, menuWrapper.current)) {
-      // eslint-disable-next-line @typescript-eslint/no-use-before-define
-      hide();
+    const clickedNode = event.target as HTMLElement;
+    const menuNode = menuRef.current;
+    if (clickedNode !== menuNode && clickedNode.contains(menuNode)) {
+      setActive && setActive(false);
     }
   };
 
@@ -96,30 +102,13 @@ const Menu = <S,>({
       onClick(event);
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    hide();
-  };
-
-  const addEvents = () => {
-    window.setTimeout(
-      () =>
-        events.addEventsToDocument({
-          click: handleDocumentClick,
-          touchstart: handleDocumentClick,
-        }),
-      0,
-    );
-  };
-
-  const removeEvents = () => {
-    events.removeEventsFromDocument({
-      click: handleDocumentClick,
-      touchstart: handleDocumentClick,
-    });
+    if (position !== POSITION.STATIC) {
+      setActive && setActive(false);
+    }
   };
 
   const calculatePosition = () => {
-    const parentNode = menuWrapper?.current?.parentNode as HTMLElement;
+    const parentNode = menuWrapperRef?.current?.parentNode as HTMLElement;
 
     if (!parentNode) {
       return 'static';
@@ -153,33 +142,29 @@ const Menu = <S,>({
     });
   }, [children]);
 
-  const show = () => {
-    onShow && onShow();
-    addEvents();
-  };
-
-  const hide = () => {
-    onHide && onHide();
-    removeEvents();
-  };
-
   useEffect(() => {
-    active ? show() : hide();
-
-    return () => {
-      active && removeEvents();
-    };
-  }, [active]);
+    if (position === POSITION.STATIC) {
+      setActive && setActive(true);
+    }
+  }, []);
 
   useEffect(() => {
     if (position === POSITION.AUTO) {
       setPositionState(calculatePosition());
     }
-  }, []);
 
-  return (
-    <Box data-teamleader-ui="menu" className={classNames} ref={menuWrapper} {...boxProps}>
-      <ul ref={menuNode} className={innerClassNames}>
+    if (position !== POSITION.STATIC && active) {
+      document.documentElement.addEventListener('click', handleDocumentClick);
+
+      return () => {
+        active && document.documentElement.removeEventListener('click', handleDocumentClick);
+      };
+    }
+  }, [active]);
+
+  return active ? (
+    <Box data-teamleader-ui="menu" className={classNames} ref={menuWrapperRef} {...boxProps}>
+      <ul ref={menuRef} className={innerClassNames}>
         {/* An invisible element so the first element doesn't look like its selected or focused */}
         <Box className={theme['invisible-menu-item']} data-teamleader-ui="menu-item" element="li">
           <Box element="button" />
@@ -187,7 +172,7 @@ const Menu = <S,>({
         {getItems()}
       </ul>
     </Box>
-  );
+  ) : null;
 };
 
 export default Menu;
