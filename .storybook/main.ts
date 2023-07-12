3fa54356a44797c636771fee02790174ae6a7ddc
Lowie Benoot <lowie@teamleader.eu>
2023-07-12 14:51:59 +0800

    ⬆️ run the storybook upgrade command

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,34 +1,31 @@
 const chromaticTestStories = ['../src/**/*.stories.spec.@(tsx|mdx)'];
 const storybookStories = ['./**/*.stories.@(tsx|mdx)', '../src/**/*.stories.@(tsx|mdx)'];
-
 module.exports = {
   stories: process.env.CHROMATIC ? chromaticTestStories : storybookStories,
-  addons: [
-    '@storybook/addon-backgrounds',
-    'storybook-addon-designs',
-    '@storybook/addon-docs',
-    '@storybook/addon-controls',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-        cssLoaderOptions: {
-          sourceMap: true,
-          modules: {
-            localIdentName: '[name]__[local]___[hash:base64:5]',
-          },
-          importLoaders: 1,
-        },
+  addons: ['@storybook/addon-backgrounds', '@storybook/addon-designs', '@storybook/addon-docs', '@storybook/addon-controls', {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss')
       },
-    },
-  ],
+      cssLoaderOptions: {
+        sourceMap: true,
+        modules: {
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        },
+        importLoaders: 1
+      }
+    }
+  }, '@storybook/addon-mdx-gfm'],
   features: {
     // Use babel config from project root .babelrc file
-    babelModeV7: true,
+    babelModeV7: true
   },
-  core: {
-    builder: 'webpack5',
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
