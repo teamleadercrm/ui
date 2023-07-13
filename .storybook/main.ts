73cced98c6abbd38c96806de2d368223c5a109f4
Lowie Benoot <lowie@teamleader.eu>
2023-07-13 15:36:49 +0800

    🚨 format the file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,30 +2,37 @@ const chromaticTestStories = ['../src/**/*.stories.spec.@(tsx|mdx)'];
 const storybookStories = ['./**/*.stories.@(tsx|mdx)', '../src/**/*.stories.@(tsx|mdx)'];
 module.exports = {
   stories: process.env.CHROMATIC ? chromaticTestStories : storybookStories,
-  addons: ['@storybook/addon-backgrounds', '@storybook/addon-designs', '@storybook/addon-docs', '@storybook/addon-controls', {
-    name: '@storybook/addon-postcss',
-    options: {
-      postcssLoaderOptions: {
-        implementation: require('postcss')
-      },
-      cssLoaderOptions: {
-        sourceMap: true,
-        modules: {
-          localIdentName: '[name]__[local]___[hash:base64:5]'
+  addons: [
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-designs',
+    '@storybook/addon-docs',
+    '@storybook/addon-controls',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
         },
-        importLoaders: 1
-      }
-    }
-  }, '@storybook/addon-mdx-gfm'],
+        cssLoaderOptions: {
+          sourceMap: true,
+          modules: {
+            localIdentName: '[name]__[local]___[hash:base64:5]',
+          },
+          importLoaders: 1,
+        },
+      },
+    },
+    '@storybook/addon-mdx-gfm',
+  ],
   features: {
     // Use babel config from project root .babelrc file
-    babelModeV7: true
+    babelModeV7: true,
   },
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
