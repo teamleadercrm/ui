f24e5849b8e58d7147e8138896ffb23cc2794591
Lowie Benoot <lowie@teamleader.eu>
2023-11-06 15:45:36 +0800

    ✨ `MarketingButtonGroup`: implement ref forwarding

--- src/components/marketingButtonGroup/MarketingButtonGroup.tsx
+++ src/components/marketingButtonGroup/MarketingButtonGroup.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, forwardRef } from 'react';
 import omit from 'lodash.omit';
 import Box, { omitBoxProps } from '../box';
 import Button, { ButtonProps } from './Button';
@@ -23,47 +23,50 @@ interface MarketingButtonGroupComponent extends GenericComponent<MarketingButton
   Button: GenericComponent<ButtonProps>;
 }
 
-const MarketingButtonGroup: MarketingButtonGroupComponent = ({ children, className, value, onChange, ...others }) => {
-  const handleChange = (value: string, event: React.ChangeEvent) => {
-    if (onChange) {
-      onChange(value, event);
-    }
-  };
+const MarketingButtonGroup: MarketingButtonGroupComponent = forwardRef<HTMLElement, MarketingButtonGroupProps>(
+  ({ children, className, value, onChange, ...others }, ref) => {
+    const handleChange = (value: string, event: React.ChangeEvent) => {
+      if (onChange) {
+        onChange(value, event);
+      }
+    };
 
-  const classNames = cx(theme['group'], theme['segmented'], className);
+    const classNames = cx(theme['group'], theme['segmented'], className);
 
-  return (
-    <Box data-teamleader-ui="button-group" className={classNames} {...others}>
-      {React.Children.map(children, (child) => {
-        if (!React.isValidElement(child)) {
-          return;
-        }
+    return (
+      <Box data-teamleader-ui="button-group" className={classNames} {...others} ref={ref}>
+        {React.Children.map(children, (child) => {
+          if (!React.isValidElement(child)) {
+            return;
+          }
 
-        if (!isComponentOfType(Button, child)) {
-          return child;
-        }
+          if (!isComponentOfType(Button, child)) {
+            return child;
+          }
 
-        let optionalChildProps = {};
-        if (value) {
-          optionalChildProps = {
-            active: child.props.value === value,
-            onClick: (event: React.ChangeEvent) => handleChange(child.props.value || '', event),
-          };
-        }
+          let optionalChildProps = {};
+          if (value) {
+            optionalChildProps = {
+              active: child.props.value === value,
+              onClick: (event: React.ChangeEvent) => handleChange(child.props.value || '', event),
+            };
+          }
 
-        const childProps = omit(child.props, ['value']);
-        const groupProps = omit(others, ['onChange']);
+          const childProps = omit(child.props, ['value']);
+          const groupProps = omit(others, ['onChange']);
 
-        return React.createElement(child.type, {
-          ...childProps,
-          ...optionalChildProps,
-          ...omitBoxProps(groupProps),
-        });
-      })}
-    </Box>
-  );
-};
+          return React.createElement(child.type, {
+            ...childProps,
+            ...optionalChildProps,
+            ...omitBoxProps(groupProps),
+          });
+        })}
+      </Box>
+    );
+  },
+);
 
+MarketingButtonGroup.displayName = 'MarketingButtonGroup';
 MarketingButtonGroup.Button = Button;
 
 export default MarketingButtonGroup;
