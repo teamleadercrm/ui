80660ead94ac2a2f0d5e9df0ee492de5d15470da
Lowie Benoot <lowie@teamleader.eu>
2023-11-06 18:02:55 +0800

    ✨ `Toast`: implement ref forwarding

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,7 +1,7 @@
 import { IconCloseMediumOutline } from '@teamleader/ui-icons';
 import uiUtilities from '@teamleader/ui-utilities';
 import cx from 'classnames';
-import React, { ReactElement, ReactNode, useEffect, useState } from 'react';
+import React, { ReactElement, ReactNode, forwardRef, useEffect, useState } from 'react';
 import { GenericComponent } from '../../@types/types';
 import IconButton from '../iconButton';
 import Link from '../link';
@@ -32,108 +32,102 @@ export interface ToastProps {
   timeout?: number;
 }
 
-const Toast: GenericComponent<ToastProps> = ({
-  children,
-  className,
-  label,
-  processing,
-  timeout,
-  onTimeout,
-  link,
-  action,
-  actionLabel,
-  onClose,
-}) => {
-  const [currentTimeout, setCurrentTimeout] = useState<number | undefined>(undefined);
-  const clearCurrentTimeout = () => {
-    clearTimeout(currentTimeout);
-    setCurrentTimeout(undefined);
-  };
-  const scheduleTimeout = () => {
-    if (currentTimeout) {
-      clearCurrentTimeout();
-    }
-
-    setCurrentTimeout(
-      window.setTimeout(() => {
-        if (onTimeout) {
-          onTimeout();
-        }
-
-        clearCurrentTimeout();
-      }, timeout),
-    );
-  };
-  const handleMouseEnter = () => {
-    if (timeout) {
-      clearCurrentTimeout();
-    }
-  };
-
-  const handleMouseLeave = () => {
-    if (timeout) {
-      scheduleTimeout();
-    }
-  };
-
-  const renderCustomAction = () => {
-    return (
-      action && (
-        <Link className={theme['action-link']} onClick={action}>
-          {actionLabel}
-        </Link>
-      )
-    );
-  };
-
-  const renderCustomLink = () => {
-    return (
-      link && (
-        <TextBody>{React.cloneElement(link, { className: cx(link.props.className, theme['action-link']) })}</TextBody>
-      )
-    );
-  };
-
-  const renderCloseButton = () => {
-    return (
-      onClose && (
-        <IconButton
-          className={theme['action-button']}
-          icon={<IconCloseMediumOutline />}
-          color="white"
-          onClick={onClose}
-        />
-      )
-    );
-  };
-
-  useEffect(() => {
-    if (timeout) {
-      scheduleTimeout();
-    }
-    return () => {
-      clearCurrentTimeout();
+const Toast: GenericComponent<ToastProps> = forwardRef<HTMLDivElement, ToastProps>(
+  ({ children, className, label, processing, timeout, onTimeout, link, action, actionLabel, onClose }, ref) => {
+    const [currentTimeout, setCurrentTimeout] = useState<number | undefined>(undefined);
+    const clearCurrentTimeout = () => {
+      clearTimeout(currentTimeout);
+      setCurrentTimeout(undefined);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [timeout]);
+    const scheduleTimeout = () => {
+      if (currentTimeout) {
+        clearCurrentTimeout();
+      }
 
-  const classNames = cx(uiUtilities['reset-box-sizing'], uiUtilities['box-shadow-400'], theme['toast'], className);
+      setCurrentTimeout(
+        window.setTimeout(() => {
+          if (onTimeout) {
+            onTimeout();
+          }
 
-  return (
-    <div
-      data-teamleader-ui="toast"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      className={classNames}
-    >
-      {processing && <LoadingSpinner className={theme['spinner']} color="neutral" tint="lightest" />}
-      <TextBody className={theme['label']} color="neutral" tint="lightest" element="div">
-        {label}
-        {children}
-      </TextBody>
-      {renderCustomAction() || renderCustomLink() || renderCloseButton()}
-    </div>
-  );
-};
+          clearCurrentTimeout();
+        }, timeout),
+      );
+    };
+    const handleMouseEnter = () => {
+      if (timeout) {
+        clearCurrentTimeout();
+      }
+    };
+
+    const handleMouseLeave = () => {
+      if (timeout) {
+        scheduleTimeout();
+      }
+    };
+
+    const renderCustomAction = () => {
+      return (
+        action && (
+          <Link className={theme['action-link']} onClick={action}>
+            {actionLabel}
+          </Link>
+        )
+      );
+    };
+
+    const renderCustomLink = () => {
+      return (
+        link && (
+          <TextBody>{React.cloneElement(link, { className: cx(link.props.className, theme['action-link']) })}</TextBody>
+        )
+      );
+    };
+
+    const renderCloseButton = () => {
+      return (
+        onClose && (
+          <IconButton
+            className={theme['action-button']}
+            icon={<IconCloseMediumOutline />}
+            color="white"
+            onClick={onClose}
+          />
+        )
+      );
+    };
+
+    useEffect(() => {
+      if (timeout) {
+        scheduleTimeout();
+      }
+      return () => {
+        clearCurrentTimeout();
+      };
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [timeout]);
+
+    const classNames = cx(uiUtilities['reset-box-sizing'], uiUtilities['box-shadow-400'], theme['toast'], className);
+
+    return (
+      <div
+        data-teamleader-ui="toast"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className={classNames}
+        ref={ref}
+      >
+        {processing && <LoadingSpinner className={theme['spinner']} color="neutral" tint="lightest" />}
+        <TextBody className={theme['label']} color="neutral" tint="lightest" element="div">
+          {label}
+          {children}
+        </TextBody>
+        {renderCustomAction() || renderCustomLink() || renderCloseButton()}
+      </div>
+    );
+  },
+);
+
+Toast.displayName = 'Toast';
 
 export default Toast;
