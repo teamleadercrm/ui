6bd7e1278e83077b95a7b2b97fdaf504bab21694
Lowie Benoot <lowie@teamleader.eu>
2023-11-06 15:34:45 +0800

    ✨ `Grid.GridItem`: implement ref forwarding

--- src/components/grid/GridItem.tsx
+++ src/components/grid/GridItem.tsx
@@ -1,12 +1,14 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, forwardRef } from 'react';
 import { GenericComponent } from '../../@types/types';
 
 export type GridItemProps = Partial<{ children: ReactNode; area: string }>;
 
-const GridItem: GenericComponent<GridItemProps> = ({ children, area }) => {
+const GridItem: GenericComponent<GridItemProps> = forwardRef<HTMLDivElement, GridItemProps>(({ children, area }) => {
   const gridItemStyles = { gridArea: area };
 
   return <div style={gridItemStyles}>{children}</div>;
-};
+});
+
+GridItem.displayName = 'Grid';
 
 export default GridItem;
